635a8429e4f18076a1177521fdf500d609328cf7
Hery Ramihajamalala <hramihaj@redhat.com>
2017-11-14 19:25:12 +0100

    rake rails:update
    Updating configuration to match with rails 5.0
    
    TODO preview

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,11 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development production preview test))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+
+# If you precompile assets before deploying to production, use this line
+Bundler.require *Rails.groups(:assets => %w(development production preview test))
+# If you want your assets lazily compiled in production, use this line
+# Bundler.require(:default, :assets, Rails.env)
 
 ActiveSupport::XmlMini.backend = 'Nokogiri'
 
