90fc0af06a466f6b7dd82922555689c424492f32
hallelujah <hramihaj@redhat.com>
2017-11-28 00:35:13 +0100

    [CVE] Rails SQLi also check for ActionController::Parameters
    We forbid all nested subquery like: Model.where({models: {column: true}}) in the code on purpose.
    To prepare for Rails 5.x we need to take into account ActionController::Parameters as well
    
    The fix is a bit reworked on for it to be more robust

---
+++ test/unit/rails_sqli_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class RailsSqliTest < ActiveSupport::TestCase
+  def test_action_controller_params_are_escaped
+    params = ActionController::Parameters.new('system_name' => {'0' => 'foo'})
+    # This should fix # https://groups.google.com/forum/#!topic/rubyonrails-security/8CVoclw-Xkk
+    # No error should be raised on that and the SQL
+
+    db_quote = System::Database.mysql? ? '`' : '"'
+    CMS::Section.find_by(params)
+    sanitized = ActiveRecord::Base.sanitize params['system_name'].to_s
+    assert_match sanitized, CMS::Section.where(params).to_sql
+  end
+
+  def test_quoting_acton_controller_parameters
+    params = ActionController::Parameters.new('system_name' => {'0' => 'foo'})
+    master_account.sections.find_by_system_name(params[:system_name])
+  end
+end
