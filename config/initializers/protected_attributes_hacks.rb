062141b9a0a7b322504cc95f3cb780fc39248fee
hallelujah <hramihaj@redhat.com>
2017-11-28 00:38:24 +0100

    Protected attributes gem is a bad citizen. Make it get along with BabySqueel
    They monkey patch ActiveRecord::QueryMethods instead of adding their own module

---
+++ config/initializers/protected_attributes_hacks.rb
@@ -0,0 +1,25 @@
+# OMG!!!! Such bad citizen!!!
+# https://github.com/westonganger/protected_attributes_continued/blob/master/lib/active_record/mass_assignment_security/relation.rb
+# They just reopened Rails module instead of using their proper module ...
+# Thus they totally short circuit all the other modules, not really good with integration with other gems like BabySqueel
+
+ActiveRecord::QueryMethods.module_eval do
+  remove_method :sanitize_forbidden_attributes
+end
+
+module ProtectedAttributesHacks
+  module QueryMethods
+    def sanitize_forbidden_attributes(attributes)
+      if model._uses_mass_assignment_security
+        # We just permit everything
+        super(attributes.respond_to?(:permit!) ? attributes.dup.permit! : attributes)
+      else
+        super
+      end
+    end
+  end
+end
+
+ActiveRecord::Relation.class_eval do
+  include ProtectedAttributesHacks::QueryMethods
+end
