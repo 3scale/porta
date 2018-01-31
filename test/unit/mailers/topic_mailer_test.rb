d6d52f01eaab53bf1da458dc61aac7abfd800a86
hallelujah <hramihaj@redhat.com>
2018-01-31 04:52:48 +0100

    Fixing some again

--- test/unit/mailers/topic_mailer_test.rb
+++ test/unit/mailers/topic_mailer_test.rb
@@ -11,8 +11,9 @@ class TopicMailerTest < ActionMailer::TestCase
     post =  FactoryBot.create(:post, user: sender)
     subscriber =  FactoryBot.build_stubbed(:simple_user, account: @buyer)
 
-    TopicMailer.new_post(subscriber, post).deliver_now
-    assert_equal 1, ActionMailer::Base.deliveries.count
+    assert_difference ActionMailer::Base.deliveries.method(:count), +1 do
+      TopicMailer.new_post(subscriber, post).deliver_now
+    end
   end
 
   test 'send from provider' do
