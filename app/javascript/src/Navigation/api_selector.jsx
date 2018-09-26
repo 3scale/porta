f070cdb1732de471f75d8522744dc51fd158d05c
dd di cesare <didi@posteo.net>
2018-09-26 16:50:56 +0200

    [navigation] Search box
    * Pretty Basic
    * Make it ugly and make it work first
    * Needs Flow types
    * Needs Test

---
+++ app/javascript/src/Navigation/api_selector.jsx
@@ -0,0 +1,74 @@
+// TODO: @flow
+// TODO: test
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+const apiPathRoot = apiId => `/apiconfig/services/${apiId}/metrics`
+
+const Suggestions = (props) => {
+  const { apis } = props
+  const options = apis.map((api, index) => (
+    <li key={index} className="PopNavigation-listItem">
+      <a className="PopNavigation-link" href={`${apiPathRoot(api.service.id)}`}>{api.service.name}</a>
+    </li>
+  ))
+  return <ul className="PopNavigation-results">{options}</ul>
+}
+
+const searchBoxTitle = (api, controllerName) => (api && controllerName !== 'dashboards') ? `API: ${api.service.name.toUpperCase()}` : 'Jump to an API'
+
+class ApiSearch extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      ...props,
+      displayedApis: props.apis
+    }
+  }
+
+  searchHandler = (event) => {
+    const searchQuery = event.target.value.toLowerCase()
+    const displayedApis = this.state.apis.filter((api) => api.service.name.toLowerCase().indexOf(searchQuery) !== -1)
+    this.setState({
+      displayedApis
+    })
+  }
+  render () {
+    return (
+      <div className="PopNavigation PopNavigation--context">
+        <a className="PopNavigation-trigger u-toggler" href="#context-menu" title="Context Switcher">
+          { searchBoxTitle(this.state.currentApi, this.state.controllerName) }
+        </a>
+        <ul id="context-menu" className="PopNavigation-list u-toggleable">
+          <li className="PopNavigation-listItem">
+            <div id="context-widget" className="nav-search-widget">
+              <form className="docs-search" data-role="search">
+                <a href="#" data-role="close" className="close-widget">x</a>
+                <input
+                  onChange={this.searchHandler}
+                  type="search"
+                  className="docs-search-input"
+                  placeholder="Search Service"
+                />
+              </form>
+            </div>
+          </li>
+          <li className="PopNavigation-listItem">
+            <Suggestions apis={this.state.displayedApis} />
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+const ApiSelector = (props, element) => {
+  render(
+    <ApiSearch {...props} />,
+    document.getElementById(element)
+  )
+}
+
+export {
+  ApiSelector
+}
