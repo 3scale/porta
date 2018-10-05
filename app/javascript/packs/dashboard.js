b51db1021bd975849c15f7a11a78b4fb5a30813f
dd di cesare <didi@posteo.net>
2018-10-05 16:19:46 +0200

    [dashboard] "Webpacking" Dashboard

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -1,5 +1,11 @@
+import { widget as dashboardWidget } from '../src/Dashboard/index'
+import { initialize as toggleWidget } from '../src/Dashboard/toggle'
+import { render as renderChartWidget } from '../src/Dashboard/chart'
 import { ApiFilterWrapper } from '../src/Dashboard/components/ApiFilter'
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.dashboardWidget = dashboardWidget
+  window.toggleWidget = toggleWidget
+  window.renderChartWidget = renderChartWidget
   window.ApiFilter = ApiFilterWrapper
 })
