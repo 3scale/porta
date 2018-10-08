3scale/porta

commit ab2c6faa583ec8a2ee3ba66ea7f18cc13f88a169

[rake] Simplify boot:database check
* No need to depend on environment
* Can check oracle and mysql reliably

lib/tasks/boot.rake
@@ -11,32 +11,7 @@ namespace :boot do
   task :database do
     begin
       require 'system/database'
-      Rake::Task[:environment].invoke
-
-      # Checks general availability of the server, not the database which is done below.
-      # Essentially needed for Oracle that needs to connect with SYSTEM user
-      # but doing for all databases does not harm.
-      System::Database.ready?
-
-      pool = ActiveRecord::Base.establish_connection
-      config = pool.spec.config
-      pool.with_connection do
-        puts "Connected to #{config.fetch(:adapter)}://#{config.fetch(:username)}@#{config.fetch(:host) { 'localhost' }}/#{config.fetch(:database)}"
-      end
-
-      pool.disconnect!
-    rescue ActiveRecord::NoDatabaseError => error
-      warn "Connected, but database does not exist: #{error}"
-      exit true
-    rescue StandardError => error
-      warn "Connection specification: #{System::Database.configuration_specification.config}"
-      warn "Failed to connect to database: #{error} (#{error.class})"
-
-      if (cause = error.cause)
-        warn "Caused by: #{cause} (#{cause.class})"
-      end
-
-      exit false
+      exit System::Database.ready?
     end
   end
 

lib/system/database.rb
@@ -4,6 +4,7 @@ require 'active_support/string_inquirer'
 
 module System
   module Database
+    class ConnectionError < ActiveRecord::NoDatabaseError; end
     module_function
 
     def configuration_specification
@@ -33,25 +34,28 @@ module System
       self.table_name = 'accounts'
 
       class << self
-        def connection_spec
+        def connection_config
           spec = configuration_specification.config.dup
           if oracle?
-            spec['password'] = ENV.fetch('ORACLE_SYSTEM_PASSWORD'){|key| raise KeyError, "Environment #{key} is mandatory"}
-            spec['username'] = 'SYSTEM'
+            spec[:password] = ENV.fetch('ORACLE_SYSTEM_PASSWORD') {|key| raise KeyError, "Environment #{key} is mandatory"}
+            spec[:username] = 'SYSTEM'
           end
           spec
         end
 
         def ready?
-          pool = establish_connection connection_spec
-          result = connection.select_value(sql_for_readiness).tap{ pool.disconnect! }
+          pool = establish_connection connection_config
+          result = nil
+          pool.with_connection do |connection|
+            result = connection.select_value(sql_for_readiness).tap { pool.disconnect! }
+          end
           result.to_s == '1'
         end
 
         def sql_for_readiness
           case
           when oracle?
-            'SELECT 1 FROM V$INSTANCE WHERE "STATUS" = \'OPEN\''
+            "SELECT 1 FROM v$pdbs WHERE name COLLATE BINARY_CI = '#{connection_config[:database]}' AND open_mode = 'READ WRITE'"
           when mysql?
             'SELECT 1'
           else
@@ -62,10 +66,25 @@ module System
     end
 
     def ready?
-      ConnectionProbe.ready?
-    rescue => e
-      Rails.logger.debug "Database is not ready, failed with error: #{e.message}"
-      false
+      config = ConnectionProbe.connection_config
+      if ConnectionProbe.ready?
+        puts "Connected to #{config.fetch(:adapter)}://#{config.fetch(:username)}@#{config.fetch(:host) { 'localhost' }}/#{config.fetch(:database)}"
+        return true
+      else
+        puts "Cannot connect to #{config.fetch(:adapter)}://#{config.fetch(:username)}@#{config.fetch(:host) { 'localhost' }}/#{config.fetch(:database)}"
+        return false
+      end
+
+    rescue ActiveRecord::NoDatabaseError => error # In case of mysql
+      puts "Connected, but database does not exist: #{error}"
+      true
+    rescue StandardError => error
+      puts "Connection specification: #{config}"
+      puts "Failed to connect to database: #{error} (#{error.class})"
+
+      if (cause = error.cause)
+        puts "Caused by: #{cause} (#{cause.class})"
+      end
     end
 
     module Scopes

commit fe0a2944429192b79ba20e49bbf27077603f424d

[fix] rake boot:database should only exit when db is not ready
Otherwise it will not run other following tasks
Bug introduced in https://github.com/3scale/porta/pull/83/commits/ab2c6faa583ec8a2ee3ba66ea7f18cc13f88a169

lib/tasks/boot.rake
@@ -11,7 +11,7 @@ namespace :boot do
   task :database do
     begin
       require 'system/database'
-      exit System::Database.ready?
+      exit false unless System::Database.ready?
     end
   end
 