2e4b0cc37d94f7c1bcf91190d9bcbefb3e379f94
Damian Peralta <dperalta@redhat.com>
2018-10-08 16:53:17 +0200

    Adding javascript file for service discovery feature

---
+++ app/assets/javascripts/provider/admin/apiconfig/services/service_discovery.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+
+  const form_source = document.getElementById('new_service_source');
+  const form_scratch = document.querySelectorAll('form#new_service, form#create_service')[0];
+  const form_discover = document.getElementById('service_discovery');
+
+  const clear_select_options = select => {
+    const options = select.getElementsByTagName('option');
+    for (let i = options.length -1; i > 0; i--) {
+      options[i].remove()
+    }
+  };
+
+  const clear_namespaces = () => {
+    return clear_select_options(form_discover.service_namespace);
+  };
+
+  const clear_service_names = () => {
+    return clear_select_options(form_discover.service_name);
+  };
+
+  const show_service_form = (source, discover_func, scratch_func) => {
+    if (source === 'discover') {
+      form_scratch.classList.add('is-hidden');
+      form_discover.classList.remove('is-hidden');
+      return typeof discover_func === "function" ? discover_func() : undefined;
+    } else {
+      form_scratch.classList.remove('is-hidden');
+      form_discover.classList.add('is-hidden');
+      return typeof scratch_func === "function" ? scratch_func() : undefined;
+    }
+  };
+
+  const change_service_source = () => {
+    clear_namespaces();
+    clear_service_names();
+    return show_service_form(form_source.source.value, fetch_namespaces);
+  };
+
+  const change_cluster_namespace = () => {
+    clear_service_names();
+    const selected_namespace = form_discover.service_namespace.value
+    return selected_namespace !== '' ? fetch_services(selected_namespace) : undefined;
+  };
+
+  const addOptionToSelect = (selectElem, val) => {
+    let opt = document.createElement('option');
+    opt.text = val;
+    opt.value = val;
+    return selectElem.appendChild(opt);
+  };
+
+  const fetch_namespaces = () => {
+    return $.getJSON('/p/admin/service_discovery/projects.json', (data) => {
+      const projects = data.projects;
+      projects.forEach((project, index, array) => addOptionToSelect(form_discover.service_namespace, project.metadata.name));
+      return projects.length > 0 ? (
+        form_discover.service_namespace.selectedIndex = 1,
+        change_cluster_namespace()
+      ) : undefined;
+    });
+  };
+
+  const fetch_services = namespace => {
+    return $.getJSON(`/p/admin/service_discovery/namespaces/${namespace}/services.json`, (data) => {
+      const services = data.services;
+      services.forEach((service, index, array) => addOptionToSelect(form_discover.service_name, service.metadata.name));
+      return services.length > 0 ? form_discover.service_name.selectedIndex = 1 : undefined;
+    });
+  };
+
+  if (form_source !== null) {
+    const radioGroup = form_source.source;
+    for(let item of radioGroup){
+      item.addEventListener('click', change_service_source);
+    }
+  }
+
+  if (form_discover !== null) {
+    form_discover.service_namespace.addEventListener('change', change_cluster_namespace);
+  }
+
+});
