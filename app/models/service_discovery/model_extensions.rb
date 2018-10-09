0687bf162d16e29f146ccda638a673ff58ad5751
Guilherme Cassolato <guicassolato@gmail.com>
2018-10-09 12:46:50 +0200

    Better logging for Service Discovery import methods

--- app/models/service_discovery/model_extensions.rb
+++ app/models/service_discovery/model_extensions.rb
@@ -11,27 +11,38 @@ module ServiceDiscovery
       end
 
       def import_cluster_service_endpoint(cluster_service)
-        proxy.save_and_deploy(api_backend: cluster_service.endpoint)
+        api_backend_url = cluster_service.endpoint
+
+        unless proxy.save_and_deploy(api_backend: api_backend_url)
+          log_cluster_service_import_event(cluster_service, message: "Could not save API backend URL #{api_backend_url}")
+        end
       end
 
       def import_cluster_active_docs(cluster_service)
-        cluster_service_id = cluster_service.self_link
-        logger = Rails.logger
-
         unless cluster_service.oas?
-          logger.info("API specification type for #{cluster_service_id} is not supported. Content-Type: #{cluster_service.specification_type}")
+          log_cluster_service_import_event(cluster_service, message: "API specification type not supported (Content-Type: #{cluster_service.specification_type})")
           return
         end
 
         if (spec_content = cluster_service.specification).blank?
-          logger.info("OAS specification for #{cluster_service_id} is empty and cannot be imported")
+          log_cluster_service_import_event(cluster_service, message: 'OAS specification is empty and cannot be imported')
           return
         end
 
-        provider.api_docs_services.create({ name: cluster_service.name,
-                                            body: spec_content,
-                                            published: true,
-                                            skip_swagger_validations: true })
+        api_docs_service = provider.api_docs_services.build(name: cluster_service.name,
+                                                            body: spec_content,
+                                                            published: true,
+                                                            skip_swagger_validations: true)
+
+        unless api_docs_service.save
+          log_cluster_service_import_event(cluster_service, message: "Could not create ActiveDocs - #{api_docs_service.errors.full_messages}")
+        end
+      end
+
+      protected
+
+      def log_cluster_service_import_event(cluster_service, message:)
+        Rails.logger.info("[ServiceDiscovery] #{cluster_service.self_link}: #{message}")
       end
     end
   end
