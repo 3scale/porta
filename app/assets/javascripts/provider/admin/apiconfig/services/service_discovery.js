ff69a7bd1f630953d02fce4a22da15e28ab463d1
Damian Peralta <dperalta@redhat.com>
2018-10-09 11:14:05 +0200

    Using camelCase names

--- app/assets/javascripts/provider/admin/apiconfig/services/service_discovery.js
+++ app/assets/javascripts/provider/admin/apiconfig/services/service_discovery.js
@@ -1,46 +1,46 @@
 document.addEventListener("DOMContentLoaded", (e) => {
 
-  const form_source = document.getElementById('new_service_source');
-  const form_scratch = document.querySelectorAll('form#new_service, form#create_service')[0];
-  const form_discover = document.getElementById('service_discovery');
+  const formSource = document.getElementById('new_service_source');
+  const formScratch = document.querySelectorAll('form#new_service, form#create_service')[0];
+  const formDiscover = document.getElementById('service_discovery');
 
-  const clear_select_options = select => {
+  const clearSelectOptions = select => {
     const options = select.getElementsByTagName('option');
     for (let i = options.length -1; i > 0; i--) {
       options[i].remove()
     }
   };
 
-  const clear_namespaces = () => {
-    return clear_select_options(form_discover.service_namespace);
+  const clearNamespaces = () => {
+    return clearSelectOptions(formDiscover.service_namespace);
   };
 
-  const clear_service_names = () => {
-    return clear_select_options(form_discover.service_name);
+  const clearServiceNames = () => {
+    return clearSelectOptions(formDiscover.service_name);
   };
 
-  const show_service_form = (source, discover_func, scratch_func) => {
+  const showServiceForm = (source, discoverFunc, scratchFunc) => {
     if (source === 'discover') {
-      form_scratch.classList.add('is-hidden');
-      form_discover.classList.remove('is-hidden');
-      return typeof discover_func === "function" ? discover_func() : undefined;
+      formScratch.classList.add('is-hidden');
+      formDiscover.classList.remove('is-hidden');
+      return typeof discoverFunc === "function" ? discoverFunc() : undefined;
     } else {
-      form_scratch.classList.remove('is-hidden');
-      form_discover.classList.add('is-hidden');
-      return typeof scratch_func === "function" ? scratch_func() : undefined;
+      formScratch.classList.remove('is-hidden');
+      formDiscover.classList.add('is-hidden');
+      return typeof scratchFunc === "function" ? scratchFunc() : undefined;
     }
   };
 
-  const change_service_source = () => {
-    clear_namespaces();
-    clear_service_names();
-    return show_service_form(form_source.source.value, fetch_namespaces);
+  const changeServiceSource = () => {
+    clearNamespaces();
+    clearServiceNames();
+    return showServiceForm(formSource.source.value, fetchNamespaces);
   };
 
-  const change_cluster_namespace = () => {
-    clear_service_names();
-    const selected_namespace = form_discover.service_namespace.value
-    return selected_namespace !== '' ? fetch_services(selected_namespace) : undefined;
+  const changeClusterNamespace = () => {
+    clearServiceNames();
+    const selectedNamespace = formDiscover.service_namespace.value
+    return selectedNamespace !== '' ? fetchServices(selectedNamespace) : undefined;
   };
 
   const addOptionToSelect = (selectElem, val) => {
@@ -56,14 +56,14 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
   const populateNamespaces = data => {
     const projects = data.projects;
-    projects.forEach((project, index, array) => addOptionToSelect(form_discover.service_namespace, project.metadata.name));
+    projects.forEach((project, index, array) => addOptionToSelect(formDiscover.service_namespace, project.metadata.name));
     return projects.length > 0 ? (
-      form_discover.service_namespace.selectedIndex = 1,
-      change_cluster_namespace()
+      formDiscover.service_namespace.selectedIndex = 1,
+      changeClusterNamespace()
     ) : undefined;
   }
 
-  const fetch_namespaces = () => {
+  const fetchNamespaces = () => {
     if (isFetchSupported()) {
       fetch('/p/admin/service_discovery/projects.json')
         .then(response => response.json())
@@ -77,11 +77,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
   const populateServices = data => {
     const services = data.services;
-    services.forEach((service, index, array) => addOptionToSelect(form_discover.service_name, service.metadata.name));
-    return services.length > 0 ? form_discover.service_name.selectedIndex = 1 : undefined;
+    services.forEach((service, index, array) => addOptionToSelect(formDiscover.service_name, service.metadata.name));
+    return services.length > 0 ? formDiscover.service_name.selectedIndex = 1 : undefined;
   }
 
-  const fetch_services = namespace => {
+  const fetchServices = namespace => {
     if (isFetchSupported()) {
       fetch(`/p/admin/service_discovery/namespaces/${namespace}/services.json`)
         .then(response => response.json())
@@ -93,15 +93,15 @@ document.addEventListener("DOMContentLoaded", (e) => {
     }
   };
 
-  if (form_source !== null) {
-    const radioGroup = form_source.source;
+  if (formSource !== null) {
+    const radioGroup = formSource.source;
     for(let item of radioGroup){
-      item.addEventListener('click', change_service_source);
+      item.addEventListener('click', changeServiceSource);
     }
   }
 
-  if (form_discover !== null) {
-    form_discover.service_namespace.addEventListener('change', change_cluster_namespace);
+  if (formDiscover !== null) {
+    formDiscover.service_namespace.addEventListener('change', changeClusterNamespace);
   }
 
 });
