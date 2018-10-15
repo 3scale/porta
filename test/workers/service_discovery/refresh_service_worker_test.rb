57fc7221328a26bbfa15af26cbd7b0b69b9ab76e
Guilherme Cassolato <guicassolato@gmail.com>
2018-10-15 10:33:47 +0200

    Fix service discovery workers tests

--- test/workers/service_discovery/refresh_service_worker_test.rb
+++ test/workers/service_discovery/refresh_service_worker_test.rb
@@ -4,6 +4,10 @@ require 'test_helper'
 
 module ServiceDiscovery
   class RefreshServiceWorkerTest < ActiveSupport::TestCase
+    setup do
+      ThreeScale.config.service_discovery.stubs(enabled: true)
+    end
+
     test 'perform' do
       service = FactoryGirl.create(:service)
       ImportClusterDefinitionsService.any_instance.expects(:refresh_service).with(service)
