4e9e689378df2c1bb3ee35f7df9ac59cecfb36c5
Guilherme Cassolato <guicassolato@gmail.com>
2018-10-15 10:33:47 +0200

    Fix service discovery workers tests

--- test/workers/service_discovery/create_service_worker_test.rb
+++ test/workers/service_discovery/create_service_worker_test.rb
@@ -4,6 +4,10 @@ require 'test_helper'
 
 module ServiceDiscovery
   class CreateServiceWorkerTest < ActiveSupport::TestCase
+    setup do
+      ThreeScale.config.service_discovery.stubs(enabled: true)
+    end
+
     test 'perform' do
       account = FactoryGirl.create(:simple_provider)
       ImportClusterDefinitionsService.any_instance.expects(:create_service).with(account, cluster_namespace: 'fake-project', cluster_service_name: 'fake-api')
