dd603a58bfd7c55d0ba8e580f111f214036c62ef
Jose Miguel Gallas <josemigallas@gmail.com>
2018-10-15 13:04:10 +0200

    sets title depending on active_menu

---
+++ app/javascript/src/Navigation/utils/getActiveMenuTitle.jsx
@@ -0,0 +1,32 @@
+export default function getActiveMenuTitle (activeMenu, currentApi) {
+  switch (activeMenu) {
+    case 'dashboard':
+      return 'Dashboard'
+
+    case 'personal':
+      return 'Personal Settings'
+
+    case 'account':
+      return 'Account'
+
+    case 'buyers':
+    case 'finance':
+    case 'cms':
+    case 'site':
+      return 'Audience'
+
+    case 'settings':
+      return 'Preferences'
+
+    case 'applications':
+    case 'active_docs':
+      return 'All APIs'
+
+    case 'serviceadmin':
+    case 'monitoring':
+      return `API: ${currentApi.service.name}`
+
+    default:
+      break
+  }
+}
