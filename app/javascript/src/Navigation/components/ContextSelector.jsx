27f25d5999debbedb457530190d9ad2e4b453001
Jose Miguel Gallas <josemigallas@gmail.com>
2018-10-15 09:20:59 +0200

    refactors components

---
+++ app/javascript/src/Navigation/components/ContextSelector.jsx
@@ -0,0 +1,37 @@
+// TODO: @flow
+// TODO: test
+import 'core-js/modules/es6.set'
+import 'core-js/modules/es6.map'
+import 'core-js/es6/array'
+import React from 'react'
+import { render } from 'react-dom'
+import { ApiSearch } from './ApiSearch'
+
+const AUDIENCE_PATH = '/buyers/accounts'
+
+const ContextSelector = ({ apis, currentApi, controllerName }) => {
+  const title = currentApi ? `API: ${currentApi.service.name}` : controllerName
+
+  return (
+    <div className="PopNavigation PopNavigation--context">
+      <a className="PopNavigation-trigger u-toggler" href="#context-menu" title="Context Selector">
+        <span> {title} <i className='fa fa-chevron-down' /></span>
+      </a>
+      <ul id="context-menu" className="PopNavigation-list u-toggleable">
+        <li className="PopNavigation-listItem">
+          <a className="PopNavigation-link" href={AUDIENCE_PATH}>Audience</a>
+        </li>
+        {apis.length > 1 ? <ApiSearch apis={apis} /> : null}
+      </ul>
+    </div>
+  )
+}
+
+const ContextSelectorWrapper = (props, element) => {
+  render(
+    <ContextSelector {...props} />,
+    document.getElementById(element)
+  )
+}
+
+export { ContextSelector, ContextSelectorWrapper }
