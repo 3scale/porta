82b06b65f63ff1327417de18e98a7b29fc87d777
Guilherme Cassolato <guicassolato@gmail.com>
2018-10-15 19:12:57 +0200

    Refactoring model extensions

--- app/services/service_discovery/import_cluster_definitions_service.rb
+++ app/services/service_discovery/import_cluster_definitions_service.rb
@@ -2,6 +2,8 @@
 
 module ServiceDiscovery
   class ImportClusterDefinitionsService
+    class ImportClusterDefinitionsError < StandardError; end
+
     def self.build_service(account, attributes = {})
       account.services.build(attributes)
     end
@@ -20,26 +22,28 @@ module ServiceDiscovery
       @cluster = ServiceDiscovery::ClusterClient.new
     end
 
-    attr_reader :cluster
+    attr_reader :cluster, :cluster_service
 
     def create_service(account, cluster_namespace:, cluster_service_name:)
-      cluster_service = find_cluster_service_by(namespace: cluster_namespace, name: cluster_service_name)
+      find_cluster_service_by(namespace: cluster_namespace, name: cluster_service_name)
+
       new_api_attributes = {
         name: cluster_service_name,
         system_name: [cluster_namespace, cluster_service_name].join('-'),
         kubernetes_service_link: cluster_service.self_link
       }
-      new_api = self.class.build_service(account, new_api_attributes)
+      service = self.class.build_service(account, new_api_attributes)
 
-      return unless new_api.save
+      return unless service.save
 
-      new_api.import_cluster_definitions(cluster_service)
+      import_cluster_definitions_to service
     end
 
     def refresh_service(service)
       service_self_link = service.kubernetes_service_link.presence || return
-      cluster_service = find_cluster_service_by(service_self_link: service_self_link)
-      service.import_cluster_definitions(cluster_service)
+      find_cluster_service_by(service_self_link: service_self_link)
+
+      import_cluster_definitions_to service
     end
 
     protected
@@ -52,7 +56,60 @@ module ServiceDiscovery
                                   criteria.values_at(:namespace, :name)
                                 end
 
-      cluster.find_discoverable_service_by(namespace: namespace, name: service_name)
+      @cluster_service = cluster.find_discoverable_service_by(namespace: namespace, name: service_name)
+    end
+
+    def import_cluster_definitions_to(service)
+      import_cluster_service_endpoint_to(service)
+      import_cluster_active_docs_to(service)
+    end
+
+    def import_cluster_service_endpoint_to(service)
+      proxy = service.proxy
+      api_backend_url = cluster_service.endpoint
+      return if proxy.api_backend == api_backend_url
+
+      unless proxy.save_and_deploy(api_backend: api_backend_url)
+        log_cluster_service_import_event(service, message: 'Could not save API backend URL', details: { api_backend_url:  api_backend_url })
+      end
+    end
+
+    def import_cluster_active_docs_to(service)
+      return unless valid_cluster_service_spec?(service)
+
+      api_docs_service = service.discovered_api_docs_service || build_api_doc_service(service)
+      api_docs_service.skip_swagger_validations = true
+      api_docs_service.body = cluster_service.specification_body
+
+      unless api_docs_service.save
+        log_cluster_service_import_event(service, message: 'Could not create ActiveDocs', details: { errors: api_docs_service.errors.full_messages })
+      end
+    end
+
+    def build_api_doc_service(service)
+      service.api_docs_services.build({ name: cluster_service.name, published: true, discovered: true }, without_protection: true)
+    end
+
+    def valid_cluster_service_spec?(service)
+      cluster_service_spec_oas?(service) && cluster_service_spec_present?(service)
+    end
+
+    def cluster_service_spec_oas?(service)
+      return true if cluster_service.specification_oas?
+      log_cluster_service_import_event(service, message: 'API specification type not supported', details: { api_spec_content_type: cluster_service.specification_type })
+      false
+    end
+
+    def cluster_service_spec_present?(service)
+      return true if cluster_service.specification_body.present?
+      log_cluster_service_import_event(service, message: 'OAS specification is empty and cannot be imported')
+      false
+    end
+
+    def log_cluster_service_import_event(service, message:, details: {})
+      exception = ImportClusterDefinitionsError.new(message)
+      exception_details = { service_id: service.id, cluster_service: { self_link: cluster_service.self_link } }.merge(details)
+      System::ErrorReporting.report_error(exception, exception_details)
     end
   end
 end
--- test/unit/services/service_discovery/import_cluster_definitions_service_test.rb
+++ test/unit/services/service_discovery/import_cluster_definitions_service_test.rb
@@ -6,11 +6,163 @@ module ServiceDiscovery
   class ImportClusterDefinitionsServiceTest < ActiveSupport::TestCase
     include TestHelpers::ServiceDiscovery
 
+    class AsyncMethodsTest < ActiveSupport::TestCase
+      setup do
+        @account = FactoryGirl.create(:simple_provider)
+        @service = FactoryGirl.create(:service, account: @account,
+                                                name: 'fake-api',
+                                                system_name: 'fake-project-fake-api',
+                                                kubernetes_service_link: '/api/v1/namespaces/fake-project/services/fake-api')
+      end
+
+      test 'create service async' do
+        CreateServiceWorker.expects(:perform_async).with(@account.id, 'fake-project', 'fake-api')
+        new_service = ImportClusterDefinitionsService.create_service(@account, cluster_namespace: 'fake-project',
+                                                                               cluster_service_name: 'fake-api')
+        refute new_service.persisted?
+        assert_equal 'fake-api', new_service.name
+      end
+
+      test 'refresh service async' do
+        RefreshServiceWorker.expects(:perform_async).with(@service.id)
+        ImportClusterDefinitionsService.refresh_service(@service)
+      end
+    end
+
     setup do
       stub_external_cluster_initialization!
       @account = FactoryGirl.create(:simple_provider)
+      @service = FactoryGirl.create(:service, account: @account,
+                                              name: 'fake-api',
+                                              system_name: 'fake-project-fake-api',
+                                              kubernetes_service_link: '/api/v1/namespaces/fake-project/services/fake-api')
+      FactoryGirl.create(:proxy, service: @service, api_backend: 'http://api.example.net:80')
 
-      cluster_service_metadata = {
+      @cluster_service = ServiceDiscovery::ClusterService.new raw_cluster_service_resource
+      @cluster_service.stubs(specification: mock_cluster_service_spec)
+
+      @import_service = ImportClusterDefinitionsService.new
+    end
+
+    test 'create service' do
+      cluster_service = ServiceDiscovery::ClusterService.new raw_cluster_service_resource(name: 'new-fake-api')
+      cluster_service.stubs(specification: mock_cluster_service_spec)
+      @import_service.cluster.stubs(:find_discoverable_service_by).returns(cluster_service)
+      assert_difference @account.services.method(:count) do
+        Proxy.any_instance.expects(:save_and_deploy).with(api_backend: cluster_service.endpoint).returns(true)
+        @import_service.create_service(@account, cluster_namespace: 'fake-project', cluster_service_name: 'new-fake-api')
+      end
+    end
+
+    test 'refresh service' do
+      @import_service.cluster.stubs(:find_discoverable_service_by).returns(@cluster_service)
+      @service.proxy.expects(:save_and_deploy).with(api_backend: @cluster_service.endpoint).returns(true)
+      @import_service.refresh_service(@service)
+    end
+
+    test 'import cluster definitions' do
+      @import_service.expects(:import_cluster_service_endpoint_to).with(@service)
+      @import_service.expects(:import_cluster_active_docs_to).with(@service)
+      @import_service.send(:import_cluster_definitions_to, @service)
+    end
+
+    test 'import cluster service endpoint' do
+      @import_service.stubs(cluster_service: @cluster_service)
+
+      assert_equal 'http://api.example.net:80', @service.proxy.api_backend
+      @import_service.send(:import_cluster_service_endpoint_to, @service)
+      assert @service.proxy.valid?
+      assert_equal 'https://fake-api.fake-project.svc.cluster.local:8443/api', @service.proxy.api_backend
+    end
+
+    test 'only import endpoint when it changes' do
+      @import_service.stubs(cluster_service: @cluster_service)
+
+      @service.proxy.expects(:save_and_deploy).never
+      @cluster_service.stubs(endpoint: 'http://api.example.net:80')
+      @import_service.send(:import_cluster_service_endpoint_to, @service)
+
+      @service.proxy.expects(:save_and_deploy).with(api_backend: 'http://my-new-endpoint').returns(true)
+      @cluster_service.stubs(endpoint: 'http://my-new-endpoint')
+      @import_service.send(:import_cluster_service_endpoint_to, @service)
+    end
+
+    test 'unsupported backend base path' do
+      @import_service.stubs(cluster_service: @cluster_service)
+
+      @account.stubs(:provider_can_use?).with(:apicast_v1).returns(true)
+      @account.stubs(:provider_can_use?).with(:apicast_v2).returns(true)
+      @account.stubs(:provider_can_use?).with(:proxy_private_base_path).returns(false)
+
+      System::ErrorReporting.expects(:report_error).with(responds_with(:message, 'Could not save API backend URL'), any_parameters)
+      @import_service.send(:import_cluster_service_endpoint_to, @service)
+      refute @service.proxy.valid?
+      assert_equal 'http://api.example.net:80', @service.proxy.reload.api_backend
+    end
+
+    test 'import api_doc' do
+      @import_service.stubs(cluster_service: @cluster_service)
+
+      assert_difference @service.api_docs_services.method(:count) do
+        @import_service.send(:import_cluster_active_docs_to, @service)
+
+        discovered_api_docs_service = @service.discovered_api_docs_service
+        assert discovered_api_docs_service.discovered
+        assert_equal '{ "swagger" : "fake-swagger" }', discovered_api_docs_service.body
+      end
+    end
+
+    test 'refreshes existing discovered api_docs_service' do
+      @import_service.stubs(cluster_service: @cluster_service)
+
+      discovered_api_docs_service = FactoryGirl.create(:api_docs_service, account: @account, service: @service, discovered: true)
+
+      assert_no_difference @service.api_docs_services.method(:count) do
+        @import_service.send(:import_cluster_active_docs_to, @service)
+
+        discovered_api_docs_service = @service.discovered_api_docs_service
+        assert_equal '{ "swagger" : "fake-swagger" }', discovered_api_docs_service.body
+      end
+    end
+
+    test 'non-oas spec' do
+      @import_service.stubs(cluster_service: @cluster_service)
+
+      @cluster_service.stubs(specification: mock_cluster_service_spec(type: 'application/json'))
+
+      assert_no_difference @account.api_docs_services.method(:count) do
+        System::ErrorReporting.expects(:report_error).with(responds_with(:message, 'API specification type not supported'), any_parameters)
+        @import_service.send(:import_cluster_active_docs_to, @service)
+      end
+    end
+
+    test 'blank oas spec' do
+      @import_service.stubs(cluster_service: @cluster_service)
+
+      @cluster_service.stubs(specification: mock_cluster_service_spec(body: ''))
+
+      assert_no_difference @account.api_docs_services.method(:count) do
+        System::ErrorReporting.expects(:report_error).with(responds_with(:message, 'OAS specification is empty and cannot be imported'), any_parameters)
+        @import_service.send(:import_cluster_active_docs_to, @service)
+      end
+    end
+
+    test 'invalid api_doc record' do
+      @import_service.stubs(cluster_service: @cluster_service)
+
+      assert_no_difference @account.api_docs_services.method(:count) do
+        api_doc = @import_service.send(:build_api_doc_service, @service)
+        api_doc.stubs(valid?: false)
+        @import_service.stubs(build_api_doc_service: api_doc)
+        System::ErrorReporting.expects(:report_error).with(responds_with(:message, 'Could not create ActiveDocs'), any_parameters)
+        @import_service.send(:import_cluster_active_docs_to, @service)
+      end
+    end
+
+    private
+
+    def cluster_service_metadata
+      {
         name: 'fake-api',
         namespace: 'fake-project',
         selfLink: '/api/v1/namespaces/fake-project/services/fake-api',
@@ -21,47 +173,16 @@ module ServiceDiscovery
           :'discovery.3scale.net/path' => 'api'
         }
       }
-      @fake_cluster_service = ServiceDiscovery::ClusterService.new cluster_service(metadata: cluster_service_metadata)
-      @import_service = ImportClusterDefinitionsService.new
     end
 
-    test 'create service async' do
-      CreateServiceWorker.expects(:perform_async).with(@account.id, 'fake-project', 'fake-api')
-      new_service = ImportClusterDefinitionsService.create_service(@account, cluster_namespace: 'fake-project',
-                                                                             cluster_service_name: 'fake-api')
-      refute new_service.persisted?
-      assert_equal 'fake-api', new_service.name
+    def raw_cluster_service_resource(metadata = {})
+      cluster_service(metadata: cluster_service_metadata.deep_merge(metadata))
     end
 
-    test 'create service' do
-      ServiceDiscovery::ClusterClient.any_instance.stubs(:find_discoverable_service_by).returns(@fake_cluster_service)
-
-      assert_difference @account.services.method(:count) do
-        Service.any_instance.expects(:import_cluster_definitions).with(@fake_cluster_service)
-        @import_service.create_service(@account, cluster_namespace: 'fake-project', cluster_service_name: 'fake-api')
-      end
-    end
-
-    test 'refresh service async' do
-      service = FactoryGirl.create(:service, account: @account,
-                                             name: 'fake-api',
-                                             system_name: 'fake-project-fake-api')
-
-      service.stubs(discovered?: true)
-      RefreshServiceWorker.expects(:perform_async).with(service.id)
-      ImportClusterDefinitionsService.refresh_service(service)
-    end
-
-    test 'refresh service' do
-      service = FactoryGirl.create(:service, account: @account,
-                                             name: 'fake-api',
-                                             system_name: 'fake-project-fake-api',
-                                             kubernetes_service_link: '/api/v1/namespaces/fake-project/services/fake-api')
-
-      ServiceDiscovery::ClusterClient.any_instance.stubs(:find_discoverable_service_by).returns(@fake_cluster_service)
-
-      Service.any_instance.expects(:import_cluster_definitions).with(@fake_cluster_service)
-      @import_service.refresh_service(service)
+    def mock_cluster_service_spec(options = {})
+      spec = ClusterServiceSpecification.new('http://my-api.example.com/doc')
+      spec.stubs({ fetch: true, body: '{ "swagger" : "fake-swagger" }', type: 'application/swagger+json' }.merge(options))
+      spec
     end
   end
 end
