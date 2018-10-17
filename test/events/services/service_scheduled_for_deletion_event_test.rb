3142b10ce20810c75906ff2646363add82b56f01
Michal Macejko <michal.macejko1@gmail.com>
2018-10-17 20:21:36 +0200

    [service deletion subscriber] use subscriber instead of after commit

---
+++ test/events/services/service_scheduled_for_deletion_event_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class Services::ServiceScheduledForDeletionEventTest < ActiveSupport::TestCase
+
+  def test_create
+    service = FactoryGirl.build_stubbed(:simple_service, id: 1, name: 'Alaska')
+    event   = Services::ServiceScheduledForDeletionEvent.create(service)
+
+    assert event
+    assert event.service_id, service.id
+    assert event.service_name, service.name
+    assert event.provider, service.provider
+  end
+end
