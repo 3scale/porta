bf610fd6a2892937902f8959827de014a1935fa2
Marta Noya <mnoyabon@redhat.com>
2018-10-17 11:29:48 +0200

    Remove unused Analytics Show route (/services/:service_id/stats) (admin_service_stats_root_path/url)

--- app/controllers/stats/dashboards_controller.rb
+++
@@ -1,7 +0,0 @@
-class Stats::DashboardsController < Stats::ServiceBaseController
-  sublayout :stats, :only => [:show]
-
-  def show
-    redirect_to admin_service_stats_usage_path(@service)
-  end
-end
