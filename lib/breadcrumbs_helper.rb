ccde4424637f99a6c366e068399c0d8c0e9272db
Michal Macejko <michal.macejko1@gmail.com>
2018-10-22 16:23:56 +0200

    [breadcrumbs] index link

--- lib/breadcrumbs_helper.rb
+++ lib/breadcrumbs_helper.rb
@@ -6,10 +6,22 @@ module BreadcrumbsHelper
   end
 
   def display_active_submenu
-    active_submenu.to_s.humanize
+    text = active_submenu.to_s.humanize
+    breadcrumb_unlinkable?(active_submenu) ? text : index_link(text)
   end
 
   def display_active_sidebar
-    active_sidebar.to_s.humanize
+    text = active_sidebar.to_s.humanize
+    breadcrumb_unlinkable?(active_sidebar) ? text : index_link(text)
+  end
+
+  private
+
+  def index_link(text)
+    link_to text, url_for(action: :index)
+  end
+
+  def breadcrumb_unlinkable?(key)
+    [:bla_bla].include?(key)
   end
 end
