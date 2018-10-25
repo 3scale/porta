3f6f436924b0f89f4b014f84eee6b347a7f03474
Hallelujah <hery@rails-royce.org>
2018-10-25 23:22:21 +0200

    [test] Fix service discovery with service_account

--- test/workers/service_discovery/create_service_worker_test.rb
+++ test/workers/service_discovery/create_service_worker_test.rb
@@ -6,12 +6,24 @@ module ServiceDiscovery
   class CreateServiceWorkerTest < ActiveSupport::TestCase
     setup do
       ThreeScale.config.service_discovery.stubs(enabled: true)
+      @user = nil
     end
 
     test 'perform' do
+      token_retriever = mock(service_usable?: true)
       account = FactoryGirl.create(:simple_provider)
-      ImportClusterDefinitionsService.any_instance.expects(:create_service).with(account, cluster_namespace: 'fake-project', cluster_service_name: 'fake-api')
-      CreateServiceWorker.new.perform(account.id, 'fake-project', 'fake-api')
+      ServiceDiscovery::TokenRetriever.expects(:new).with(@user).returns(token_retriever).at_least_once
+      import_definition = mock
+      import_definition.expects(:create_service).with(account, cluster_namespace: 'fake-project', cluster_service_name: 'fake-api')
+      ImportClusterDefinitionsService.expects(:new).with(@user).returns(import_definition)
+      CreateServiceWorker.new.perform(account.id, 'fake-project', 'fake-api', @user&.id)
+    end
+  end
+
+  class CreateServiceWorkerWithUserTest < CreateServiceWorkerTest
+    def setup
+      super
+      @user = FactoryGirl.create(:simple_user)
     end
   end
 end
