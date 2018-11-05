efafa90ede169df807a84090fd01d3fe02a2a729
Marta Noya <mnoyabon@redhat.com>
2018-11-05 10:49:43 +0100

    Fix random failure in testing master domains builder for when the master with the same org_name already exists in the DB

--- test/unit/signup/master_domains_builder_test.rb
+++ test/unit/signup/master_domains_builder_test.rb
@@ -4,23 +4,23 @@ require 'test_helper'
 
 class Signup::MasterDomainsBuilderTest < ActiveSupport::TestCase
   test 'org_name is base for the subdomain' do
-    new_domains = generate_domains(org_name: master_account.org_name)
-    assert_equal 'master-account', new_domains.subdomain
+    new_domains = generate_domains
+    assert_equal 'master-account-new', new_domains.subdomain
   end
 
   test 'current_subdomain is kept if present' do
-    new_domains = generate_domains(org_name: master_account.org_name, current_subdomain: 'master')
+    new_domains = generate_domains(current_subdomain: 'master')
     assert_equal 'master', new_domains.subdomain
   end
 
   test 'generates equal subdomain and self subdomain' do
-    new_domains = generate_domains(org_name: master_account.org_name)
+    new_domains = generate_domains
     assert_equal new_domains.subdomain, new_domains.self_subdomain
   end
 
   private
 
-  def generate_domains(org_name:, current_subdomain: nil)
+  def generate_domains(org_name: 'Master Account New', current_subdomain: nil)
     domains_builder_params = { org_name: org_name, current_subdomain: current_subdomain, invalid_subdomain_condition: master_account.method(:subdomain_exists?) }
     Signup::MasterDomainsBuilder.new(**domains_builder_params).generate
   end
