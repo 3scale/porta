424314d6528202e8709d0212d5849b77afae57f5
Marta Noya <mnoyabon@redhat.com>
2018-11-07 16:41:53 +0100

    Fix Oracle test Impersonation: Master Account exists always at that moment

--- test/unit/tasks/impersonation_admin_user_test.rb
+++ test/unit/tasks/impersonation_admin_user_test.rb
@@ -7,7 +7,7 @@ class Tasks::ImpersonationAdminUserTest < ActiveSupport::TestCase
     3.times do
       FactoryGirl.create(:active_admin,
                             username: '3scaleadmin',
-                            account: FactoryGirl.create(:simple_provider)
+                            account: FactoryGirl.create(:simple_provider, provider_account: master_account)
                           )
     end
     FactoryGirl.create_list(:active_admin, 2)
@@ -19,7 +19,7 @@ class Tasks::ImpersonationAdminUserTest < ActiveSupport::TestCase
     users = User.where(username: 'example-username')
     assert_equal 0, User.where(username: '3scaleadmin').count
     assert_equal 3, users.count
-    assert_equal 2, User.where('username <> \'3scaleadmin\'').where('username <> \'example-username\'').count
+    assert_equal 3, User.where('username <> \'3scaleadmin\'').where('username <> \'example-username\'').count
     users.each { |user| assert_match /\Aexample\-username\+#{user.account.self_domain}@domain\.example\.com\z/, user.email }
   end
 end
