fe8981e1435f316faadcdcf8bb4d640e1bc89a4d
Thomas Maas <thomas@webtypes.com>
2018-11-08 15:35:26 +0100

    various permission fixes for master on prem

--- config/abilities/switches.rb
+++ config/abilities/switches.rb
@@ -13,6 +13,10 @@ Ability.define do |user|
     # :see means buyer can use it (see buyer_any.rb)
     # :admin means provider can see the upgrade notices (see provider_admin.rb)
     # :manage means provider can show and hide it
+
+    # end_users account_plans service_plans finance require_cc_on_signup
+    # multiple_services multiple_applications multiple_users skip_email_engagement_footer
+    # groups branding web_hooks iam_tools
     settings.switches.each do |name, switch|
       if can?(:admin, name) && switch.allowed?
         if account.master_on_premises? && [:account_plans, :service_plans].include?(name)
