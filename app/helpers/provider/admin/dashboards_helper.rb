344bed7bb985ac8bc41342febd42d4f4a87a17d2
Jose Miguel Gallas <josemigallas@gmail.com>
2018-11-20 15:56:50 +0100

    implement some tests

--- app/helpers/provider/admin/dashboards_helper.rb
+++ app/helpers/provider/admin/dashboards_helper.rb
@@ -20,7 +20,7 @@ module Provider::Admin::DashboardsHelper
   end
 
   def dashboard_navigation_link(link_text, path, options = {})
-    class_name = "DashboardNavigation-link #{'u-notice' if options.fetch(:notice, false)}"
+    class_name = "DashboardNavigation-link#{' u-notice' if options.fetch(:notice, false)}"
     link_to path, class: class_name do
       icon_name = options.fetch(:icon_name, nil)
       link_text = link_text.prepend "#{icon(icon_name)} " if icon_name
--- test/unit/helpers/provider/admin/dashboards_helper_test.rb
+++ test/unit/helpers/provider/admin/dashboards_helper_test.rb
@@ -1,23 +1,50 @@
 require 'test_helper'
 
-class DashboardsHelperTest < ActionView::TestCase
+class Provider::Admin::DashboardsHelperTest < ActionView::TestCase
+  include DashboardsHelper
+
   def setup
-    # aqui lo que quieras que se ejecute antes de cada test
+    stubs(icon: '<i></i>')
   end
   
-  def test_dashboard_navigation_link
-    # aqui el test para el metodo correspondiente
+  test 'dashboard_navigation_link creates basic link' do
+    assert_equal '<a class="DashboardNavigation-link" href="/path">Link</a>',
+      dashboard_navigation_link("Link", "/path")
   end
   
-  def test_dashboard_secondary_link
-    # aqui el test para el metodo correspondiente
+  test 'dashboard_navigation_link can add an icon using options' do
+    assert_equal '<a class="DashboardNavigation-link" href="/path"><i></i> Link</a>',
+      dashboard_navigation_link("Link", "/path", { icon_name: 'foo' })
   end
-  
-  def test_dashboard_collection_link
-    # aqui el test para el metodo correspondiente
+
+  test 'dashboard_navigation_link can highlight link using options' do
+    assert_equal '<a class="DashboardNavigation-link u-notice" href="/path">Link</a>',
+      dashboard_navigation_link("Link", "/path", { notice: 'true' })
   end
-  
-  def test_dashboard_secondary_collection_link
-    # aqui el test para el metodo correspondiente
+
+  test 'dashboard_collection_link creates pluralized link' do
+    assert_equal '<a class="DashboardNavigation-link" href="/path">1 Number</a>',
+      dashboard_collection_link("Number", [1], "/path")
+
+    assert_equal '<a class="DashboardNavigation-link" href="/path">2 Numbers</a>',
+      dashboard_collection_link("Number", [1,2], "/path")
   end
+
+  test 'dashboard_collection_link can use custom plural' do
+    assert_equal '<a class="DashboardNavigation-link" href="/path">2 Plural</a>',
+      dashboard_collection_link("Singular", [1,2], "/path", { plural: 'Plural' })
+  end
+
+  test 'dashboard_network_link and dashboard_navigation_link are equivalent' do
+    navigation_link = dashboard_navigation_link('1 Number', '/path')
+    collection_link = dashboard_collection_link('Number', [1], '/path')
+
+    assert_equal navigation_link, collection_link
+
+    navigation_link = dashboard_navigation_link('2 Numbers', '/path')
+    collection_link = dashboard_collection_link('Number', [1,2], '/path')
+
+    assert_equal navigation_link, collection_link
+  end
+
 end
