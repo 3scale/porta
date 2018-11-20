eef5335cb3e11cbeae734999d63ef917f223c9e2
Jose Miguel Gallas <josemigallas@gmail.com>
2018-11-20 14:42:07 +0100

    adds test template

---
+++ test/unit/helpers/provider/admin/dashboards_helper_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class DashboardsHelperTest < ActionView::TestCase
+  def setup
+    # aqui lo que quieras que se ejecute antes de cada test
+  end
+  
+  def test_dashboard_navigation_link
+    # aqui el test para el metodo correspondiente
+  end
+  
+  def test_dashboard_secondary_link
+    # aqui el test para el metodo correspondiente
+  end
+  
+  def test_dashboard_collection_link
+    # aqui el test para el metodo correspondiente
+  end
+  
+  def test_dashboard_secondary_collection_link
+    # aqui el test para el metodo correspondiente
+  end
+end
