f0865df0d7e768535943a45556659d71c6d24199
Hallelujah <hery@rails-royce.org>
2018-11-22 00:14:20 +0100

    [thread-safety] Clean up stale object remaining in Thread.current
    The User.current == Thread.current[:cms_user] used in a previous scenario was not cleaned up
    Thus leading to erratic errors with the use of stale object in the code: object in memory but removed in database.
    
    For example, this was adding a wrong tenant_id to tags https://github.com/3scale/porta/blob/2da29fcf431decd77426ee126e42f6a21529a946/config/initializers/tags.rb#L34

---
+++ features/support/thread_safety.rb
@@ -0,0 +1,7 @@
+# Cleanup after each scenario the Thread.current[:cms_user]
+# It leads to stale object in memory though they were removed from the Database.
+# And it is really hard to debug
+
+After do
+  User.current = nil
+end
