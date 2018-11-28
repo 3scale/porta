d64a09ab3b7fa1beebf6e2107188bf8ad6019694
Marta Noya <mnoyabon@redhat.com>
2018-11-28 17:44:58 +0100

    [WIP] Test for Hotfix UI API/Overview fetch stats usage

---
+++ features/services/overview.feature
@@ -0,0 +1,9 @@
+Feature: TODO
+
+  Background:
+    Given a provider is logged in
+
+  @selenium @javascript
+  Scenario: TODO
+    When I go to the API dashboard page
+    Then testing the analytics chart
--- features/step_definitions/stats/provider_charts.rb
+++ features/step_definitions/stats/provider_charts.rb
@@ -32,6 +32,26 @@ Then(/^there should be a c3 chart with the following data:$/) do |table|
   end
 end
 
+Then(/^testing the analytics chart$/) do
+  page.document.synchronize(Capybara.default_max_wait_time,
+                            errors: [Cucumber::Ast::Table::Different, Selenium::WebDriver::Error::JavascriptError]) do
+    values = page.evaluate_script <<-JS
+      (function(){
+        var $chart = $('#mini-charts > .charts')
+        var series = $chart.data().chart.data()
+        var values = series.map(function(s){
+          return {
+            name: s.id,
+            total: s.values.map(val => val.value).reduce((previous, current) => previous + current, 0)
+          }
+        })
+        return values
+      }());
+    JS
+    pp values
+  end
+end
+
 When(/^I select today from the stats menu$/) do
   page.should_not have_css('.StatsChart-container.is-loading')
   page.should have_css('.StatsMenu')
