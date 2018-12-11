9ef11e8edde672ee74e471097d6e078f2ecfa0b1
dd di cesare <didi@posteo.net>
2018-12-11 16:54:41 +0700

    [navigation] Refactoring and fixing bug in navigation
    * No more js code in header partial
    * hideAllToggleable was failing sometimes when assigned before than
    defined in the view

--- app/javascript/packs/navigation.js
+++ app/javascript/packs/navigation.js
@@ -1,8 +1,25 @@
 import { toggleNavigation, hideAllToggleable } from '../src/Navigation/utils/toggle_navigation'
 import { ContextSelectorWrapper } from '../src/Navigation/components/ContextSelector'
 
+import $ from 'jquery'
+
 document.addEventListener('DOMContentLoaded', () => {
-  window.toggleNavigation = toggleNavigation
-  window.hideAllToggleable = hideAllToggleable
   window.ContextSelector = ContextSelectorWrapper
+  let store = window.localStorage
+  $(document)
+    .on('click', '.u-toggler', function (e) {
+      e.stopPropagation()
+      toggleNavigation(e.currentTarget)
+      e.preventDefault()
+    })
+    .on('click', 'body', function (e) {
+      if (e.target.type !== 'search') {
+        hideAllToggleable()
+      }
+    })
+    .on('click', '.vert-nav-toggle', function () {
+      const shouldVertNavCollapse = !JSON.parse(store.isVerticalNavCollapsed || false)
+      $('.vertical-nav').toggleClass('collapsed', shouldVertNavCollapse)
+      store.isVerticalNavCollapsed = shouldVertNavCollapse
+    })
 })
