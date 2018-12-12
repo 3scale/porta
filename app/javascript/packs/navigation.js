cf711cd8133e5ea07b485489d5c4dd0f0ac2fe09
dd di cesare <didi@posteo.net>
2018-12-12 11:16:25 +0700

    [navigation] Getting rid of jQuery

--- app/javascript/packs/navigation.js
+++ app/javascript/packs/navigation.js
@@ -1,25 +1,42 @@
 import { toggleNavigation, hideAllToggleable } from '../src/Navigation/utils/toggle_navigation'
 import { ContextSelectorWrapper } from '../src/Navigation/components/ContextSelector'
 
-import $ from 'jquery'
-
 document.addEventListener('DOMContentLoaded', () => {
   window.ContextSelector = ContextSelectorWrapper
   let store = window.localStorage
-  $(document)
-    .on('click', '.u-toggler', function (e) {
-      e.stopPropagation()
-      toggleNavigation(e.currentTarget)
-      e.preventDefault()
-    })
-    .on('click', 'body', function (e) {
-      if (e.target.type !== 'search') {
-        hideAllToggleable()
-      }
-    })
-    .on('click', '.vert-nav-toggle', function () {
-      const shouldVertNavCollapse = !JSON.parse(store.isVerticalNavCollapsed || false)
-      $('.vertical-nav').toggleClass('collapsed', shouldVertNavCollapse)
-      store.isVerticalNavCollapsed = shouldVertNavCollapse
-    })
+  const togglers = document.getElementsByClassName('u-toggler')
+  const vertNavTogglers = document.getElementsByClassName('vert-nav-toggle')
+  const eventOptions = {
+    capture: true,
+    passive: false,
+    useCapture: true
+  }
+
+  function addClickEventToCollection (collection, handler) {
+    for (let item of collection) {
+      item.addEventListener('click', handler, eventOptions)
+    }
+  }
+
+  document.body.addEventListener('click', function (e) {
+    if (e.target.type !== 'search') {
+      hideAllToggleable()
+    }
+  }, eventOptions)
+
+  addClickEventToCollection(togglers, function (e) {
+    e.stopPropagation()
+    toggleNavigation(e.currentTarget)
+    e.preventDefault()
+  })
+
+  addClickEventToCollection(vertNavTogglers, function () {
+    const shouldVertNavCollapse = !JSON.parse(store.isVerticalNavCollapsed || false)
+
+    document.querySelector('.vertical-nav')
+      .classList
+      .toggle('collapsed')
+
+    store.isVerticalNavCollapsed = shouldVertNavCollapse
+  })
 })
