7da887faabdeb5277e360ea8943c7da686b114e6
Jose Miguel Gallas <josemigallas@gmail.com>
2018-12-13 11:21:28 +0100

    [eslint] adds globals to eslintrc

--- app/javascript/src/Policies/webpack-public-path.js
+++ app/javascript/src/Policies/webpack-public-path.js
@@ -5,5 +5,5 @@
 // 2. https://github.com/coryhouse/react-slingshot/issues/181
 // 3. https://github.com/coryhouse/react-slingshot/pull/125
 // Documentation: https://webpack.js.org/configuration/output/#output-publicpath
-// eslint-disable-next-line no-undef
+// eslint-disable-next-line no-undef, camelcase
 __webpack_public_path__ = window.location.protocol + '//' + window.location.host + '/'
