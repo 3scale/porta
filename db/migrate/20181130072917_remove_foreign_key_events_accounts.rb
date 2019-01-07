c7126bc68e8884f593dfe6f6074b577d6a585f6e
Marta Noya <mnoyabon@redhat.com>
2019-01-07 15:27:46 +0100

    Fix migration for event_store_events when foreign key on provider_id does not exist

--- db/migrate/20181130072917_remove_foreign_key_events_accounts.rb
+++ db/migrate/20181130072917_remove_foreign_key_events_accounts.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 class RemoveForeignKeyEventsAccounts < ActiveRecord::Migration
-  def change
+  def up
+    return unless foreign_keys(:event_store_events).detect {|fk| fk.column == 'provider_id' }
+
     remove_foreign_key :event_store_events, column: :provider_id
+  end
+
+  def down
+    return if foreign_keys(:event_store_events).detect {|fk| fk.column == 'provider_id' }
+
+    add_foreign_key :event_store_events, :accounts, column: :provider_id, on_delete: :cascade
   end
 end
