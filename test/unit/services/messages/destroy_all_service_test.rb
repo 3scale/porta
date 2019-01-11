21264b3175e83eb005aae1da522ff9ebbc9322b4
Hallelujah <hery@rails-royce.org>
2019-01-11 12:04:57 +0100

    Fix some tests

--- test/unit/services/messages/destroy_all_service_test.rb
+++ test/unit/services/messages/destroy_all_service_test.rb
@@ -12,28 +12,33 @@ class Messages::DestroyAllServiceTest < ActiveSupport::TestCase
   end
 
   def test_run!
-    assert_nil @message.deleted_at
+    Sidekiq::Testing.fake! do
 
-    Messages::DestroyAllService.run!({
-      account: @account,
-      association_class: MessageRecipient,
-      scope: :hidden
-    })
+      assert_nil @message.deleted_at
 
-    assert_not_equal nil, @message.reload.deleted_at
-  end
-
-  def test_run_with_sidekiq_job
-    assert @message.present?
-
-    perform_enqueued_jobs do
       Messages::DestroyAllService.run!({
         account: @account,
         association_class: MessageRecipient,
         scope: :hidden
       })
-    end
+      @message.reload
 
-    assert_raise(ActiveRecord::RecordNotFound) { @message.reload }
+      assert_not_equal nil, @message.reload.deleted_at
+    end
+  end
+
+  def test_run_with_sidekiq_job
+    perform_enqueued_jobs do
+      Sidekiq::Testing.inline! do
+        assert @message.present?
+        Messages::DestroyAllService.run!({
+          account: @account,
+          association_class: MessageRecipient,
+          scope: :hidden
+        })
+      end
+
+      assert_raise(ActiveRecord::RecordNotFound) { @message.reload }
+    end
   end
 end
