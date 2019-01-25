3c291edae0112b6e8d88bc327cef6f6d0e4af705
Marta Noya <mnoyabon@redhat.com>
2019-01-25 16:16:05 +0100

    Do not automatically suspend or delete accounts for on-prem

--- app/workers/stale_account_worker.rb
+++ app/workers/stale_account_worker.rb
@@ -4,6 +4,7 @@ class StaleAccountWorker
   include Sidekiq::Worker
 
   def perform
+    return if ThreeScale.config.onpremises
     Account.tenants.suspended_since.find_each(&:schedule_for_deletion!)
   end
 end
