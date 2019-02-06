54b23aeeeb182d37a308b2f75d6e6076635441c8
Marta Noya <mnoyabon@redhat.com>
2019-02-06 15:46:03 +0100

    All TTL configs in its own file. Feature grouped as account_deletion

--- app/lib/account_suspension_config.rb
+++ app/lib/account_suspension_config.rb
@@ -12,7 +12,7 @@ module AccountSuspensionConfig
   end
 
   def load_config
-    config = ThreeScale.config.max_allowed_days.each_with_object({}) do |(key, value), collection|
+    config = ThreeScale.config.ttl.account_deletion.each_with_object({}) do |(key, value), collection|
       collection[key.to_sym] = value if value.is_a?(Integer)
     end
     ActiveSupport::OrderedOptions.new.merge(config)
@@ -20,7 +20,7 @@ module AccountSuspensionConfig
 
   def check_validity
     valid = %i[account_suspension account_inactivity contract_unpaid_time].all? { |key| config.key?(key) }
-    inform_invalid_configuration if !valid && ThreeScale.config.max_allowed_days.present?
+    inform_invalid_configuration if !valid && ThreeScale.config.ttl.account_deletion.present?
     valid
   end
 
