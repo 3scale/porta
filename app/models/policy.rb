af8332b1b40a71e4ffc1efa9f9bcf420957dc7b4
Marta Noya <mnoyabon@redhat.com>
2019-02-13 15:35:43 +0100

    Validate presence of name, version, account_id, and schema

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -4,4 +4,5 @@ class Policy < ApplicationRecord
   belongs_to :account
 
   validates :version, uniqueness: { scope: %i[account_id name] }
+  validates :name, :version, :account_id, :schema, presence: true
 end
