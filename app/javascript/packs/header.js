6876eea7f2b93506c4d1b9de8233aceb74c234b3
Damian Peralta <dperalta@redhat.com>
2019-02-21 16:58:41 +0100

    Adding Brand component

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -5,7 +5,8 @@ import Header from '../src/Navigation/pageHeader'
 document.addEventListener('DOMContentLoaded', () => {
   const headerElement = document.getElementById('headerPF4')
   const props = {
-    href: headerElement.dataset.href
+    href: headerElement.dataset.href,
+    classHeaderWithLogo: headerElement.dataset.classHeaderWithLogo
   }
   render(<Header {...props}/>, headerElement)
 })
