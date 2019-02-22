0aed35ec864e29737edc689ca0eea51ff2c9d8c6
Damian Peralta <dperalta@redhat.com>
2019-02-22 16:46:22 +0100

    Adding Avatar component

--- app/javascript/src/Navigation/components/Avatar.jsx
+++ app/javascript/src/Navigation/components/Avatar.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
-class SimpleAvatar extends React.Component {
-  // Values (class, link...) are hardcoded, check with app/views/shared/provider/_header.html.slim
-  render () {
-    return (
-      <div className="PopNavigation PopNavigation--session">
-        <a className="PopNavigation-trigger u-toggler" href="#session-menu" title="Session">
-          <i className="fa fa-user "></i>
-        </a>
-        <ul className="PopNavigation-list u-toggleable" id="session-menu">
-          <li className="PopNavigation-listItem">
-            <p className="PopNavigation-info">Signed in to Provider Name as admin.</p>
-            <a className="PopNavigation-link" title="Sign Out admin" href="/p/logout">
-              <i className="fa fa-times fa-fw"></i> Sign Out
-            </a>
-          </li>
-        </ul>
-      </div>
-    )
-  }
-}
+
+const SimpleAvatar = ({avatarLink, impersonated, accountName, displayName, logoutPath, username}) => <div className="PopNavigation PopNavigation--session">
+  <a className={`PopNavigation-trigger u-toggler ${impersonated}`} href="#session-menu" title="Session">
+    <i className="fa fa-user "></i>
+    {impersonated ? <i className="fa fa-bolt "></i> : null }
+  </a>
+  <ul className="PopNavigation-list u-toggleable" id="session-menu">
+    <li className="PopNavigation-listItem">
+      <p className="PopNavigation-info">
+        { impersonated ? <i className="fa fa-bolt "></i> : null }
+        { impersonated ? ' Impersonating a virtual admin user from ' : 'Signed in to '}
+        { `${accountName} as ${displayName}.` }
+      </p>
+      <a className="PopNavigation-link" title={`Sign Out ${username}`} href={logoutPath}>
+        <i className="fa fa-times fa-fw"></i> Sign Out
+      </a>
+    </li>
+  </ul>
+</div>
 
 export default SimpleAvatar
