01f0787b57184b1d1d455e629223d19fc505fade
Michal Macejko <michal.macejko1@gmail.com>
2019-03-05 09:29:08 +0100

    [THREESCALE-1980] pagination for application plan limits

--- app/controllers/admin/api/application_plan_limits_controller.rb
+++ app/controllers/admin/api/application_plan_limits_controller.rb
@@ -14,20 +14,26 @@ class Admin::Api::ApplicationPlanLimitsController < Admin::Api::BaseController
   ##~ op.description = "Returns the list of all limits associated to an application plan."
   ##~ op.group = "application_plan_limits"
   #
+  ##~ @parameter_per_page_50 = {:name => "per_page", :description => "Number of results per page. Default and max is 50.", :dataType => "int", :paramType => "query", :defaultValue => "50"}
+  #
   ##~ op.parameters.add @parameter_access_token
+  ##~ op.parameters.add @parameter_page
+  ##~ op.parameters.add @parameter_per_page_50
   ##~ op.parameters.add @parameter_application_plan_id_by_id_name
   #
-
   def index
-    respond_with(usage_limits)
+    respond_with(usage_limits.paginate(page: current_page, per_page: per_page))
   end
 
   protected
+
   def application_plan
     @application_plan ||= accessible_application_plans.find(params[:application_plan_id])
   end
 
   def usage_limits
-    @usage_limits ||= application_plan.usage_limits
+    # [Rails bug, will be fixed in Rails 5] https://github.com/rails/rails/issues/8005
+    # includes(:plan, metric: [:service])
+    @usage_limits ||= application_plan.usage_limits.includes(metric: [:service])
   end
 end
