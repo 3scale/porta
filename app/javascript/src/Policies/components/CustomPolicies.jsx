43ddd3159365b7a069e4ff5e1158b311ca294418
dd di cesare <didi@posteo.net>
2019-03-06 22:55:27 +0100

    [custom policies] Custom Policies Component

---
+++ app/javascript/src/Policies/components/CustomPolicies.jsx
@@ -0,0 +1,24 @@
+// @flow
+
+import * as React from 'react'
+import {PolicyList} from 'Policies/components/PolicyList'
+import 'Policies/styles/policies.scss'
+import type {RegistryPolicy} from 'Policies/types/Policies'
+
+const CustomPolicies = ({policies = []}: {policies: Array<RegistryPolicy>}): React.Node => {
+  return (
+    <section className="CustomPolicies">
+      <header className='CustomPolicies-header'>
+        <h2 className="CustomPolicies-title">Custom Policies</h2>
+        <a className="CustomPolicies-addPolicy" href="/p/admin/registry/policies/new" >
+          <i className="fa fa-plus-circle" /> Add new policy
+        </a>
+      </header>
+      <PolicyList policies={policies} />
+    </section>
+  )
+}
+
+export {
+  CustomPolicies
+}
