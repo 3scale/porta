7ed246e615dedce07b89809ff811100fea01f747
Guilherme Cassolato <guicassolato@gmail.com>
2019-03-07 11:59:38 +0100

    End-users feature globally denied in onprem

--- test/unit/abilities/end_users_test.rb
+++ test/unit/abilities/end_users_test.rb
@@ -7,6 +7,7 @@ class Abilities::EndUsersTest < ActiveSupport::TestCase
   def setup
     @provider = FactoryBot.create(:provider_account)
     assert @provider.settings.end_users.denied?
+    ThreeScale.config.stubs(onpremises: false)
   end
 
   test 'provider can manage end users' do
@@ -44,5 +45,49 @@ class Abilities::EndUsersTest < ActiveSupport::TestCase
 
     assert_can ability, :see, :end_users
     assert_cannot ability, :manage, :end_users
+  end
+
+  class Onpremises < ActiveSupport::TestCase
+    def setup
+      @provider = FactoryBot.create(:provider_account)
+      ThreeScale.config.stubs(onpremises: true)
+    end
+
+    test 'provider cannot manage end users' do
+      user = FactoryBot.create(:user, :account => @provider, :role => :member)
+      ability = Ability.new(user)
+
+      assert_cannot ability, :see, :end_users
+      assert_cannot ability, :admin, :end_users
+
+      @provider.settings.allow_end_users!
+      ability.reload!
+
+      assert_cannot ability, :see, :end_users
+      assert_cannot ability, :admin, :end_users
+
+      user.update_attribute :role, :admin
+      ability.reload!
+
+      assert_cannot ability, :manage, :end_users
+    end
+
+    test 'buyer cannot manage end users' do
+      buyer   = FactoryBot.create(:buyer_account, :provider_account => @provider)
+      user    = FactoryBot.create(:user, :account => buyer, :role => :member)
+      ability = Ability.new(user)
+
+      assert_cannot ability, :see, :end_users
+      assert_cannot ability, :admin, :end_users
+
+      @provider.settings.allow_end_users!
+      assert_cannot ability.reload!, :see, :end_users
+
+      @provider.settings.show_end_users!
+      ability.reload!
+
+      assert_cannot ability, :see, :end_users
+      assert_cannot ability, :manage, :end_users
+    end
   end
 end
