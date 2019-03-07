d11f7f951ae33c634c06ff152754dcc2eb09366b
Damian Peralta <dperalta@redhat.com>
2019-03-07 20:42:00 +0100

    Simplifying logic

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -8,24 +8,35 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   const headerContainer = document.getElementById('user_widget')
   const props = {
-    href: headerContainer.dataset.href,
-    classHeaderWithLogo: headerContainer.dataset.classHeaderWithLogo,
-    accountSettingsLink: headerContainer.dataset.accountSettingsLink,
-    accountSettingsClass: headerContainer.dataset.accountSettingsClass,
-    docsLink: headerContainer.dataset.docsLink,
-    isSaas: headerContainer.dataset.isSaas,
-    docsLinksClass: headerContainer.dataset.docsLinksClass,
-    customerPortalLink: headerContainer.dataset.customerPortalLink,
-    apiDocsLink: headerContainer.dataset.apiDocsLink,
-    liquidReferenceLink: headerContainer.dataset.liquidReferenceLink,
-    whatIsNewLink: headerContainer.dataset.whatIsNewLink,
-    avatarLinkClass: headerContainer.dataset.avatarLinkClass,
-    impersonated: headerContainer.dataset.impersonated,
-    accountName: headerContainer.dataset.accountName,
-    displayName: headerContainer.dataset.displayName,
-    logoutPath: headerContainer.dataset.logoutPath,
-    username: headerContainer.dataset.username
+    toolbarProps: {
+      accountSettingsLink: headerContainer.dataset.accountSettingsLink,
+      accountSettingsClass: headerContainer.dataset.accountSettingsClass
+    },
+    docsProps: {
+      docsLink: headerContainer.dataset.docsLink,
+      isSaas: headerContainer.dataset.isSaas,
+      docsLinksClass: headerContainer.dataset.docsLinksClass,
+      customerPortalLink: headerContainer.dataset.customerPortalLink,
+      apiDocsLink: headerContainer.dataset.apiDocsLink,
+      liquidReferenceLink: headerContainer.dataset.liquidReferenceLink,
+      whatIsNewLink: headerContainer.dataset.whatIsNewLink
+    },
+    avatarProps: {
+      avatarLinkClass: headerContainer.dataset.avatarLinkClass,
+      impersonated: headerContainer.dataset.impersonated,
+      accountName: headerContainer.dataset.accountName,
+      displayName: headerContainer.dataset.displayName,
+      logoutPath: headerContainer.dataset.logoutPath,
+      username: headerContainer.dataset.username
+    },
+    logoProps: {
+      href: headerContainer.dataset.href,
+      title: 'Dashboard',
+      target: '_self',
+      className: `Header-link ${headerContainer.dataset.classHeaderWithLogo}`
+    }
   }
+
   const elem = <div>
     <Header {...props}/>
     {/* HACK of the month. See features/support/current_user.rb if you are hungry for reasons */}
--- app/javascript/src/Navigation/components/header/index.jsx
+++ app/javascript/src/Navigation/components/header/index.jsx
@@ -4,41 +4,12 @@ import Avatar from 'Navigation/components/header/Avatar'
 import Toolbar from 'Navigation/components/header/Toolbar'
 import Brand from 'Navigation/components/header/Brand'
 
-const getChildProps = (props, child) => {
-  switch (child) {
-    case 'toolbar':
-      return {accountSettingsLink: props.accountSettingsLink, accountSettingsClass: props.accountSettingsClass}
-    case 'docs':
-      return {
-        docsLink: props.docsLink,
-        isSaas: props.isSaas,
-        docsLinksClass: props.docsLinksClass,
-        customerPortalLink: props.customerPortalLink,
-        apiDocsLink: props.apiDocsLink,
-        liquidReferenceLink: props.liquidReferenceLink,
-        whatIsNewLink: props.whatIsNewLink
-      }
-    case 'avatar':
-      return {
-        avatarLinkClass: props.avatarLinkClass,
-        avatarLink: props.avatarLink,
-        impersonated: props.impersonated,
-        accountName: props.accountName,
-        displayName: props.displayName,
-        logoutPath: props.logoutPath,
-        username: props.username
-      }
-    case 'logo':
-      return {href: props.href, target: '_self', title: 'Dashboard', className: `Header-link ${props.classHeaderWithLogo}`}
-  }
-}
-
-const Header = (props) => (
+const Header = ({logoProps, toolbarProps, docsProps, avatarProps}) => (
   <PageHeader
     logo={<Brand/>}
-    logoProps = {getChildProps(props, 'logo')}
-    toolbar = {<Toolbar toolbarProps={getChildProps(props, 'toolbar')} docsProps={getChildProps(props, 'docs')}/>}
-    avatar = {<Avatar {...getChildProps(props, 'avatar')}/>}/>
+    logoProps = {logoProps}
+    toolbar = {<Toolbar toolbarProps={toolbarProps} docsProps={docsProps}/>}
+    avatar = {<Avatar {...avatarProps}/>}/>
 )
 
 export default Header
