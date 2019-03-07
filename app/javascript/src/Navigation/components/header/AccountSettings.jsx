8e989a863538cfea1b55aded0e46f7c0b6479c8b
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2019-03-07 16:36:05 +0100

    Update app/javascript/src/Navigation/components/header/AccountSettings.jsx
    Co-Authored-By: damianpm <damianpm@users.noreply.github.com>

--- app/javascript/src/Navigation/components/header/AccountSettings.jsx
+++ app/javascript/src/Navigation/components/header/AccountSettings.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const AccountSettings = ({accountSettingsLink, accountSettingsClass}) =>
+const AccountSettings = ({accountSettingsLink, accountSettingsClass}) => (
   <div className="PopNavigation PopNavigation--account">
     <a className={`PopNavigation-trigger ${accountSettingsClass}`} href={accountSettingsLink} title="Account Settings">
       <i className="fa fa-cog"></i>
