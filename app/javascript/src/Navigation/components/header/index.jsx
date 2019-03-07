bd7f1cd95a400fb5fc4ba175ec94f7d20dcd3385
Damian Peralta <dperalta@redhat.com>
2019-03-07 19:30:06 +0100

    Fixing codeclimate errors

--- app/javascript/src/Navigation/components/header/index.jsx
+++ app/javascript/src/Navigation/components/header/index.jsx
@@ -4,52 +4,41 @@ import Avatar from 'Navigation/components/header/Avatar'
 import Toolbar from 'Navigation/components/header/Toolbar'
 import Brand from 'Navigation/components/header/Brand'
 
-const getToolbarProps = (props) => {
-  return {
-    accountSettingsLink: props.accountSettingsLink,
-    accountSettingsClass: props.accountSettingsClass
-  }
-}
-
-const getDocsProps = (props) => {
-  return {
-    docsLink: props.docsLink,
-    isSaas: props.isSaas,
-    docsLinksClass: props.docsLinksClass,
-    customerPortalLink: props.customerPortalLink,
-    apiDocsLink: props.apiDocsLink,
-    liquidReferenceLink: props.liquidReferenceLink,
-    whatIsNewLink: props.whatIsNewLink
-  }
-}
-
-const getAvatarProps = (props) => {
-  return {
-    avatarLinkClass: props.avatarLinkClass,
-    avatarLink: props.avatarLink,
-    impersonated: props.impersonated,
-    accountName: props.accountName,
-    displayName: props.displayName,
-    logoutPath: props.logoutPath,
-    username: props.username
-  }
-}
-
-const getLogoProps = (props) => {
-  return {
-    href: props.href,
-    target: '_self',
-    title: 'Dashboard',
-    className: `Header-link ${props.classHeaderWithLogo}`
+const getChildProps = (props, child) => {
+  switch (child) {
+    case 'toolbar':
+      return {accountSettingsLink: props.accountSettingsLink, accountSettingsClass: props.accountSettingsClass}
+    case 'docs':
+      return {
+        docsLink: props.docsLink,
+        isSaas: props.isSaas,
+        docsLinksClass: props.docsLinksClass,
+        customerPortalLink: props.customerPortalLink,
+        apiDocsLink: props.apiDocsLink,
+        liquidReferenceLink: props.liquidReferenceLink,
+        whatIsNewLink: props.whatIsNewLink
+      }
+    case 'avatar':
+      return {
+        avatarLinkClass: props.avatarLinkClass,
+        avatarLink: props.avatarLink,
+        impersonated: props.impersonated,
+        accountName: props.accountName,
+        displayName: props.displayName,
+        logoutPath: props.logoutPath,
+        username: props.username
+      }
+    case 'logo':
+      return {href: props.href, target: '_self', title: 'Dashboard', className: `Header-link ${props.classHeaderWithLogo}`}
   }
 }
 
 const Header = (props) => (
   <PageHeader
     logo={<Brand/>}
-    logoProps = {getLogoProps(props)}
-    toolbar = {<Toolbar toolbarProps={getToolbarProps(props)} docsProps={getDocsProps(props)}/>}
-    avatar = {<Avatar {...getAvatarProps(props)}/>}/>
+    logoProps = {getChildProps(props, 'logo')}
+    toolbar = {<Toolbar toolbarProps={getChildProps(props, 'toolbar')} docsProps={getChildProps(props, 'docs')}/>}
+    avatar = {<Avatar {...getChildProps(props, 'avatar')}/>}/>
 )
 
 export default Header
