a636bf92e3d79bf66b6dfa25e167874827097792
dd di cesare <didi@posteo.net>
2019-03-08 16:55:10 +0100

    [custom policies] Renamed json util fn

--- app/javascript/packs/custom_policies.js
+++ app/javascript/packs/custom_policies.js
@@ -1,10 +1,10 @@
 import {render} from 'react-dom'
 import React from 'react'
 import {CustomPolicies} from 'Policies/components/CustomPolicies'
-import {safeFromJson} from 'Policies/util'
+import {safeFromJsonString} from 'Policies/util'
 
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('custom-policies-container')
-  const policies = safeFromJson(container.dataset.policies)
+  const policies = safeFromJsonString(container.dataset.policies)
   render(<CustomPolicies policies={policies} />, container)
 })
--- app/javascript/packs/custom_policy.js
+++ app/javascript/packs/custom_policy.js
@@ -1,11 +1,10 @@
 import {render} from 'react-dom'
 import React from 'react'
 import {CustomPolicy} from 'Policies/components/CustomPolicy'
-import {safeFromJson} from 'Policies/util'
+import {safeFromJsonString} from 'Policies/util'
 
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('custom-policy-container')
-  const jsonPolicy = container.dataset.policy
-  const policy = safeFromJson(jsonPolicy)
+  const policy = safeFromJsonString(container.dataset.policy)
   render(<CustomPolicy policy={policy} />, container)
 })
