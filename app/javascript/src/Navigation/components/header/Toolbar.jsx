0507172499eab33676f556b5b222b0b80319026b
Damian Peralta <dperalta@redhat.com>
2019-03-08 17:20:22 +0100

    Removing useless methods

--- app/javascript/src/Navigation/components/header/Toolbar.jsx
+++ app/javascript/src/Navigation/components/header/Toolbar.jsx
@@ -2,32 +2,13 @@ import React from 'react'
 import { Toolbar as PFToolbar, ToolbarGroup, ToolbarItem } from '@patternfly/react-core'
 import { AccountSettingsMenu, DocumentationMenu } from 'Navigation/components/header/'
 
-const getAccountSettingsProps = toolbarProps => {
-  return {
-    accountSettingsLink: toolbarProps.accountSettingsLink,
-    accountSettingsClass: toolbarProps.accountSettingsClass
-  }
-}
-
-const getDocsProps = docsProps => {
-  return {
-    docsLink: docsProps.docsLink,
-    isSaas: docsProps.isSaas,
-    docsLinksClass: docsProps.docsLinksClass,
-    customerPortalLink: docsProps.customerPortalLink,
-    apiDocsLink: docsProps.apiDocsLink,
-    liquidReferenceLink: docsProps.liquidReferenceLink,
-    whatIsNewLink: docsProps.whatIsNewLink
-  }
-}
-
 const Toolbar = ({ toolbarProps, docsProps }) => <PFToolbar>
   <ToolbarGroup>
     <ToolbarItem>
-      <AccountSettingsMenu {...getAccountSettingsProps(toolbarProps)}/>
+      <AccountSettingsMenu {...toolbarProps}/>
     </ToolbarItem>
     <ToolbarItem>
-      <DocumentationMenu {...getDocsProps(docsProps)}/>
+      <DocumentationMenu {...docsProps}/>
     </ToolbarItem>
   </ToolbarGroup>
 </PFToolbar>
