13c95121723c3be9fc3fb1a1517a49b46c006b76
Hallelujah <hery@rails-royce.org>
2019-03-08 14:18:13 +0100

    Fix policies views using as_json instead of to_json

--- app/presenters/policy_schema_presenter.rb
+++ app/presenters/policy_schema_presenter.rb
@@ -1,17 +1,13 @@
 # frozen_string_literal: true
 
 class PolicySchemaPresenter < SimpleDelegator
-  def to_json(*)
-    @policy.to_json(root: false, only: [:id, :schema], methods: [:directory])
+  def as_json(*)
+    __getobj__.as_json(root: false, only: [:id, :schema], methods: [:directory])
   end
 
   class Collection < SimpleDelegator
-    def initialize(policies)
-      @policies = policies
-    end
-
-    def to_json(*)
-      @policies.to_json(root: false, only: [:id, :version], methods:[:summary, :humanName])
+    def as_json(*)
+      __getobj__.as_json(root: false, only: [:id, :version], methods:[:summary, :humanName])
     end
   end
 end
