9b4a8231ea27c0d46f4699a803da9c0054bf9f08
Damian Peralta <dperalta@redhat.com>
2019-03-08 17:27:24 +0100

    Simplyfying exports

--- app/javascript/src/Navigation/components/header/index.jsx
+++ app/javascript/src/Navigation/components/header/index.jsx
@@ -1,9 +1,7 @@
-import {AccountSettingsMenu} from 'Navigation/components/header/AccountSettingsMenu'
-import {Brand} from 'Navigation/components/header/Brand'
-import {DocumentationMenu} from 'Navigation/components/header/DocumentationMenu'
-import {DocumentationMenuItem} from 'Navigation/components/header/DocumentationMenuItem'
-import {Header} from 'Navigation/components/header/Header'
-import {SessionMenu} from 'Navigation/components/header/SessionMenu'
-import {Toolbar} from 'Navigation/components/header/Toolbar'
-
-export { AccountSettingsMenu, Brand, DocumentationMenu, DocumentationMenuItem, Header, SessionMenu, Toolbar }
+export {AccountSettingsMenu} from 'Navigation/components/header/AccountSettingsMenu'
+export {Brand} from 'Navigation/components/header/Brand'
+export {DocumentationMenu} from 'Navigation/components/header/DocumentationMenu'
+export {DocumentationMenuItem} from 'Navigation/components/header/DocumentationMenuItem'
+export {Header} from 'Navigation/components/header/Header'
+export {SessionMenu} from 'Navigation/components/header/SessionMenu'
+export {Toolbar} from 'Navigation/components/header/Toolbar'
