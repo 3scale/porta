c8268ffd6908abccee0bd2951eca9bdf6dafdd08
Marta Noya <mnoyabon@redhat.com>
2019-04-02 07:24:09 +0200

    Move the complex conditions to auto-suspend and auto-schedule_for_deletion to Account

--- app/workers/suspend_inactive_accounts_worker.rb
+++ app/workers/suspend_inactive_accounts_worker.rb
@@ -4,9 +4,6 @@ class SuspendInactiveAccountsWorker
   include Sidekiq::Worker
 
   def perform
-    return unless Features::AccountDeletionConfig.valid?
-    config = Features::AccountDeletionConfig.config
-    invalid_since_date, free_since_date = config.values_at(:account_inactivity, :contract_unpaid_time).map { |value| value.days.ago }
-    Account.tenants.free(free_since_date).without_application_plans_with_system_names(config[:disabled_for_app_plans]).without_suspended.without_deleted.inactive_since(invalid_since_date).find_each(&:suspend!)
+    Account.should_be_automatically_suspended.find_each(&:suspend!)
   end
 end
