46ad4b4073248e06e2606ed40d329fa012cc5750
Marta Noya <mnoyabon@redhat.com>
2019-04-10 12:13:58 +0200

    Update state_changed_at from deleted_at with a SQL query because deleted_at is removed from Rails programatically

--- db/migrate/20181105212016_duplicate_deleted_at_state_changed_at.rb
+++ db/migrate/20181105212016_duplicate_deleted_at_state_changed_at.rb
@@ -2,11 +2,12 @@
 
 class DuplicateDeletedAtStateChangedAt < ActiveRecord::Migration
   def up
-    Account.transaction do
-      Account.scheduled_for_deletion.where.has{ deleted_at != nil }.find_each do |account|
-        account.update_column(:state_changed_at, account.deleted_at) or raise ActiveRecord::Error
-      end
-    end
+    # Done in a query instead of Ruby because later on deleted_at is removed programatically and otherwise it says it doesn't exist
+    query = <<~SQL
+      UPDATE accounts
+      SET state_changed_at = deleted_at
+      WHERE state = 'scheduled_for_deletion' AND (deleted_at IS NOT NULL);
+    SQL
   end
 
   def down
