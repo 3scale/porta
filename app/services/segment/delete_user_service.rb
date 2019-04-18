f106225a2cb0ad471d8f4434a3ef4f554fc49567
Marta Noya <mnoyabon@redhat.com>
2019-04-18 15:51:06 +0200

    Segment Service to delete user

---
+++ app/services/segment/delete_user_service.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Segment
+  class DeleteUserService
+    def initialize(user_id, token)
+      @user_id = user_id
+      @token = token
+    end
+
+    attr_reader :user_id, :token
+
+    def call
+      return unless Features::SegmentDeletionConfig.enabled?
+      response = GBPRApiRequestService.call(request_body: request_body, custom_headers: {'Authorization' => "Bearer #{token}"})
+      response.body
+    end
+
+    private
+
+    def request_body
+      <<~GRAPHQL
+        {\"query\":
+          \"mutation { createWorkspaceRegulation(workspaceSlug: \\\"#{Features::SegmentDeletionConfig.config.workspace}\\\" type: SUPPRESS_AND_DELETE userId: \\\"#{user_id}\\\" ) { id } }\"
+        }
+      GRAPHQL
+    end
+  end
+end
