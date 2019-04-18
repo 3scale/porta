d13d7aaa3683811cbccc08c24a1c299109ec723b
Marta Noya <mnoyabon@redhat.com>
2019-04-18 15:39:04 +0200

    When a User is destroyed, save a simple UserDeletedEvent

---
+++ test/events/user_related_event_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require_relative 'categorizable_interface_test'
+
+class UserRelatedEventTest < ActiveSupport::TestCase
+  include CategorizableInterfaceTest
+end
