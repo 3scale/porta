b673d3f39f2d78f90326fd06cac4ef09f510745a
Michal Macejko <michal.macejko1@gmail.com>
2019-04-25 10:27:03 +0200

    [FindAndDeleteScheduledAccountsWorker] run on saas

--- app/workers/find_and_delete_scheduled_accounts_worker.rb
+++ app/workers/find_and_delete_scheduled_accounts_worker.rb
@@ -4,7 +4,6 @@ class FindAndDeleteScheduledAccountsWorker
   include Sidekiq::Worker
 
   def perform
-    return unless ThreeScale.config.onpremises
     Account.deleted_since.find_each(&DeleteAccountHierarchyWorker.method(:perform_later))
   end
 end
