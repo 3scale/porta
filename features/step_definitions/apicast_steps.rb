793dc91839ce3d895bcd16c8935792b7876c180c
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2019-04-26 16:22:54 +0200

    Prevents Runtime error in integration/edit when APIcast not available (#736)
    [THREESCALE-2327] rescue error when apicast not available

--- features/step_definitions/apicast_steps.rb
+++ features/step_definitions/apicast_steps.rb
@@ -6,3 +6,8 @@ Given(/^apicast registry is stubbed$/) do
     .to_return(status: 200, body: "{\"policies\":{\"apicast\":[{\"description\":\"Main functionality of APIcast.\",\"$schema\":\"http:\\/\\/apicast.io\\/policy-v1\\/schema#manifest#\",\"name\":\"APIcast policy\",\"configuration\":{\"properties\":{},\"type\":\"object\"},\"version\":\"builtin\"}]}}",
                headers: { 'Content-Type' => 'application/json' })
 end
+
+Given(/^apicast registry is undefined$/) do
+  ThreeScale.config.sandbox_proxy.expects(:apicast_registry_url).returns(nil)
+  JSONClient.expects(:get).with(nil).raises(SocketError)
+end
