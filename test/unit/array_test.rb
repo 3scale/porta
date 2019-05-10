984e800fbe3596281d13365311dad700dfe93752
Hallelujah <hery@rails-royce.org>
2019-05-10 11:15:48 +0300

    Array#to_xml test fix
    The change of Fixnum to Integer might break some XML integration. We need to warn product

--- test/unit/array_test.rb
+++ test/unit/array_test.rb
@@ -5,19 +5,18 @@ class ArrayTest < ActiveSupport::TestCase
     should 'not add the attr params as default' do
       xml = Nokogiri::XML::Document.parse([1,2].to_xml)
 
-      assert xml.xpath("//fixnums[@type]").empty?
+      assert xml.xpath("//integers[@type]").empty?
     end
 
     should 'not add the attr params if skip_types => true is passed' do
       xml = Nokogiri::XML::Document.parse([1,2].to_xml(:skip_types => true))
 
-      assert xml.xpath("//fixnums[@type]").empty?
+      assert xml.xpath("//integers[@type]").empty?
     end
 
     should 'add the attr params if skip_types => false is passed' do
       xml = Nokogiri::XML::Document.parse([1,2].to_xml(:skip_types => false))
-
-      assert !xml.xpath("//fixnums[@type]").empty?
+      assert !xml.xpath("//integers[@type]").empty?
     end
   end
 end
