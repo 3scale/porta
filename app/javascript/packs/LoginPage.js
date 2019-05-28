275575272df6e51ddd3f977d280589461dae6b42
Damian Peralta <dperalta@redhat.com>
2019-05-28 17:29:09 +0200

    Adding default 3scale form

--- app/javascript/packs/LoginPage.js
+++ app/javascript/packs/LoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import SimpleLoginPage from 'LoginPage/index'
 import '@patternfly/react-core/dist/styles/base.css'
+import SimpleLoginPage from 'LoginPage/index'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(<SimpleLoginPage />, document.getElementById('login-page-wrapper'))
