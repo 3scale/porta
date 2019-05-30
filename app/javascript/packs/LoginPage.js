dca4b9e29499ed8fa554b76aea6e9b4498ce5c97
Damian Peralta <dperalta@redhat.com>
2019-05-30 07:22:13 +0200

    Moving pf4 base styles to separate file

--- app/javascript/packs/LoginPage.js
+++ app/javascript/packs/LoginPage.js
@@ -1,11 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
-import '@patternfly/react-core/dist/styles/base.css'
 import {LoginPageWrapper} from 'LoginPage'
 import {safeFromJsonString} from 'utilities/json-utils'
 
 document.addEventListener('DOMContentLoaded', () => {
   const loginPageContainer = document.getElementById('login-page-wrapper')
   const loginPageProps = safeFromJsonString(loginPageContainer.dataset.loginProps)
-  LoginPageWrapper(LoginPageWrapper, 'login-page-wrapper')
+  LoginPageWrapper(loginPageProps, 'login-page-wrapper')
 })
