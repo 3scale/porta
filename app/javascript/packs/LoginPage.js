e7c4eeb40dba33da5c6265413ec9966004886bd6
Damian Peralta <dperalta@redhat.com>
2019-06-03 16:46:11 +0200

    Cleaning typos, deleting unused files

--- app/javascript/packs/LoginPage.js
+++ app/javascript/packs/LoginPage.js
@@ -2,7 +2,7 @@ import {LoginPageWrapper} from 'LoginPage'
 import {safeFromJsonString} from 'utilities/json-utils'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const loginPageContainer = document.getElementById('login-page-wrapper')
+  const loginPageContainer = document.getElementById('login-page-container')
   const loginPageProps = safeFromJsonString(loginPageContainer.dataset.loginProps)
-  LoginPageWrapper(loginPageProps, 'login-page-wrapper')
+  LoginPageWrapper(loginPageProps, 'login-page-container')
 })
