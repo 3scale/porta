f62a183c0afcd4b077529fb110f9fed296636b97
Damian Peralta <dperalta@redhat.com>
2019-06-03 16:14:08 +0200

    Updating pf4 to 3.21.0, using svg for bg

--- app/javascript/src/LoginPage/LoginPageWrapper.jsx
+++ app/javascript/src/LoginPage/LoginPageWrapper.jsx
@@ -3,8 +3,7 @@ import {createReactWrapper} from 'utilities/createReactWrapper'
 import 'url-polyfill'
 
 import {
-  LoginPage,
-  BackgroundImageSrc
+  LoginPage
 } from '@patternfly/react-core'
 
 import {
@@ -16,23 +15,7 @@ import {
 import 'LoginPage/assets/styles/loginPage.scss'
 
 import brandImg from 'LoginPage/assets/images/3scale_Logo_Reverse.png'
-
-import pfbg1200 from 'LoginPage/assets/images/pf4BG/pfbg_1200.png'
-import pfbg768 from 'LoginPage/assets/images/pf4BG/pfbg_768.png'
-import pfbg768x2 from 'LoginPage/assets/images/pf4BG/pfbg_768x2.png'
-import pfbg576 from 'LoginPage/assets/images/pf4BG/pfbg_576.png'
-import pfbg576x2 from 'LoginPage/assets/images/pf4BG/pfbg_576x2.png'
-
-const images = {
-  [BackgroundImageSrc.lg]: pfbg1200,
-  [BackgroundImageSrc.sm]: pfbg768,
-  [BackgroundImageSrc.sm2x]: pfbg768x2,
-  [BackgroundImageSrc.xs]: pfbg576,
-  [BackgroundImageSrc.xs2x]: pfbg576x2,
-  [BackgroundImageSrc.filter]: ''
-}
-
-// import PF4DownstreamBG from 'LoginPage/assets/images/PF4DownstreamBG.svg'
+import PF4DownstreamBG from 'LoginPage/assets/images/PF4DownstreamBG.svg'
 
 class SimpleLoginPage extends React.Component {
   constructor (props) {
@@ -65,7 +48,7 @@ class SimpleLoginPage extends React.Component {
         footerListVariants='inline'
         brandImgSrc={brandImg}
         brandImgAlt='Red Hat 3scale API Management'
-        backgroundImgSrc={images}
+        backgroundImgSrc={PF4DownstreamBG}
         backgroundImgAlt='Red Hat 3scale API Management'
         loginTitle={this.state.loginTitle}
         forgotCredentials={
