a69c9e361ea29f37320dd8c8d73a3655c4dc444e
Jose Miguel Gallas <josemigallas@gmail.com>
2019-06-05 18:41:41 +0200

    scaffold sections

--- app/javascript/packs/vertical_nav.js
+++ app/javascript/packs/vertical_nav.js
@@ -1,26 +1,15 @@
 import { VerticalNavWrapper as VerticalNav } from 'Navigation/components/VerticalNav'
+import { safeFromJsonString } from 'utilities/json-utils'
 
 import '@patternfly/react-core/dist/styles/base.css'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const itemGroups = {
-    analytics: [
-      { title: 'Usage', path: '' },
-      { title: 'Daily Averages', path: '' },
-      { title: 'Hourly Averages', path: '' },
-      { title: 'Top Applications', path: '' },
-      { title: 'Response Codes', path: '' },
-      { title: 'Alerts', path: '' }
-    ],
-    billing: [
-      { title: '3scale Invoices', path: '' },
-      { title: 'Payment Details', path: '' }
-    ]
-  }
+  const dataset = document.getElementById('vertical-nav-wrapper').dataset
+  const sections = safeFromJsonString(dataset.sections)
 
   // - if can?(:manage, :plans)
   //   { title: 'Integration Errors', path: admin_service_errors_path(@service) },
   VerticalNav({
-    itemGroups
+    sections
   }, 'vertical-nav-wrapper')
 })
