d356519a428c0bf1254c5fcdc96ab42ce0c48426
Damian Peralta <dperalta@redhat.com>
2019-06-05 14:46:58 +0200

    Adding js tests

---
+++ spec/javascripts/LoginPage/LoginPageWrapper.spec.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Enzyme, {mount} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import {SimpleLoginPage, Login3scaleForm, RequestPasswordForm} from 'LoginPage'
+
+Enzyme.configure({adapter: new Adapter()})
+
+const props = {
+  authenticationProviders: null,
+  providerAdminDashboardPath: 'provider-admin-path',
+  providerLoginPath: 'provider-login-path',
+  providerPasswordPath: 'password-path',
+  providerSessionsPath: 'sessions-path',
+  redirectUrl: 'redirect-url',
+  show3scaleLoginForm: true
+}
+
+it('should render itself with right props', () => {
+  const wrapper = mount(<SimpleLoginPage {...props}/>)
+  expect(wrapper.find('.pf-c-login').exists()).toEqual(true)
+  expect(wrapper.props().authenticationProviders).toEqual(null)
+  expect(wrapper.props().providerAdminDashboardPath).toEqual('provider-admin-path')
+  expect(wrapper.props().providerLoginPath).toEqual('provider-login-path')
+  expect(wrapper.props().providerPasswordPath).toEqual('password-path')
+  expect(wrapper.props().providerSessionsPath).toEqual('sessions-path')
+  expect(wrapper.props().redirectUrl).toEqual('redirect-url')
+  expect(wrapper.props().show3scaleLoginForm).toEqual(true)
+})
+
+it('should call getURL method on mount', () => {
+  const wrapper = mount(<SimpleLoginPage {...props}/>)
+  wrapper.instance().getURL = jest.fn()
+  wrapper.instance().componentDidMount()
+  expect(wrapper.instance().getURL).toHaveBeenCalled()
+})
+
+it('should render <Login3scaleForm/> Component on mount', () => {
+  const wrapper = mount(<SimpleLoginPage {...props}/>)
+  wrapper.instance().componentDidMount()
+  expect(wrapper.find(Login3scaleForm).exists()).toEqual(true)
+})
+
+it('should render <RequestPasswordForm/> component when formMode state is set to `password-reset`', () => {
+  const wrapper = mount(<SimpleLoginPage {...props}/>)
+  wrapper.setState({formMode: 'password-reset'})
+  expect(wrapper.find(RequestPasswordForm).exists()).toEqual(true)
+})
