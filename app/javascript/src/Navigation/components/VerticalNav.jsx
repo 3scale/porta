71784688b7dd2860a57f13979b8f8340d98de02a
Jose Miguel Gallas <josemigallas@gmail.com>
2019-06-06 15:58:22 +0200

    highlights active section and subsection

--- app/javascript/src/Navigation/components/VerticalNav.jsx
+++ app/javascript/src/Navigation/components/VerticalNav.jsx
@@ -1,73 +1,53 @@
 // @flow
 
-import React, { useState } from 'react'
+import React from 'react'
 import { Nav, NavExpandable, NavItem, NavList, NavGroup } from '@patternfly/react-core'
 import { createReactWrapper } from 'utilities/createReactWrapper'
 
 type Item = {
+  id: string,
   title: string,
   path: string
 }
 
 type Section = {
+  id: string,
   title: string,
+  path: string,
   items: Item[]
 }
 
 type Props = {
-  sections: Section[]
+  sections: Section[],
+  activeSection: ?string,
+  activeItem: ?string
 }
 
-const VerticalNav = ({ sections }: Props) => {
-  const [activeItem, setActiveItem] = useState()
-  const [activeGroup, setActiveGroup] = useState()
-
-  function onSelect (res) {
-    setActiveItem(res.itemId)
-    setActiveGroup(res.groupId)
-  }
-
-  return (
-    <div className="pf-c-page__sidebar">
-      <div className="pf-c-page__sidebar-body">
-        <Nav onSelect={onSelect}>
-          <NavList>
-            <NavItem
-              to="#simple-link1"
-              itemId='overview'
-              isActive={activeItem === 'overview'}
-            >
-              Overview
-            </NavItem>
-
-            {sections.map(section => (
-              <NavExpandable
-                title={section.title}
-                // srText={groupId}
-                groupId={section.title}
-                isActive={activeGroup === section.title}
-                // isExpanded
-              >
-                {section.items.map(item => (
-                  <NavGroup title="">
-                    <NavItem
-                      to={item.path}
-                      groupId={section.title}
-                      itemId={item.title}
-                      isActive={activeItem === item.title}
-                    >
-                      {item.title}
-                    </NavItem>
+const VerticalNav = ({ sections, activeSection, activeItem }: Props) => (
+  <div className="pf-c-page__sidebar">
+    <div className="pf-c-page__sidebar-body">
+      <Nav>
+        <NavList>
+          {sections.map(({ id, title, path, items }) => items
+            ? (
+              <NavExpandable title={title} isActive={activeSection === id} isExpanded={activeSection === id}>
+                {items.map(({id, title, path}) => (
+                  <NavGroup key={title} title="">
+                    <NavItem to={path} isActive={activeItem === id}>{title}</NavItem>
                   </NavGroup>
                 ))}
               </NavExpandable>
-            ))}
-          </NavList>
-        </Nav>
-      </div>
+            ) : (
+              <NavItem to={path} isActive={activeSection === id}>
+                {title}
+              </NavItem>
+            )
+          )}
+        </NavList>
+      </Nav>
     </div>
-  )
-}
+  </div>
+)
 
 const VerticalNavWrapper = (props: Props, containerId: string) => createReactWrapper(<VerticalNav {...props} />, containerId)
 
