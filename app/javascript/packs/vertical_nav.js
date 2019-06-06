f67d48762ff4a80d95b55efe8b11b54c82a62bbe
Jose Miguel Gallas <josemigallas@gmail.com>
2019-06-06 15:58:22 +0200

    highlights active section and subsection

--- app/javascript/packs/vertical_nav.js
+++ app/javascript/packs/vertical_nav.js
@@ -6,10 +6,11 @@ import '@patternfly/react-core/dist/styles/base.css'
 document.addEventListener('DOMContentLoaded', () => {
   const dataset = document.getElementById('vertical-nav-wrapper').dataset
   const sections = safeFromJsonString(dataset.sections)
+  const activeSection = dataset.active_section
+  const activeItem = dataset.active_item
 
+  // TODO: where does this go?
   // - if can?(:manage, :plans)
   //   { title: 'Integration Errors', path: admin_service_errors_path(@service) },
-  VerticalNav({
-    sections
-  }, 'vertical-nav-wrapper')
+  VerticalNav({ sections, activeSection, activeItem }, 'vertical-nav-wrapper')
 })
