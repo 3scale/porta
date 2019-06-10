9ecd2edbf0bbfc72dc9f02646622065efa54b202
Jose Miguel Gallas <josemigallas@gmail.com>
2019-06-10 14:06:55 +0200

    removes usage of PF react by using a proxy

--- app/javascript/src/Navigation/components/VerticalNav.jsx
+++ app/javascript/src/Navigation/components/VerticalNav.jsx
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import { Nav, NavExpandable, NavItem, NavList, NavGroup } from '@patternfly/react-core'
+import { Nav, NavExpandable, NavItem, NavList, NavGroup } from 'Navigation/components/PF4NavProxy'
 import { createReactWrapper } from 'utilities/createReactWrapper'
 
 type Item = {
