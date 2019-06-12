8a5bfc9b89238c46d127315f1ab403442e36b506
Marta Noya <mnoyabon@redhat.com>
2019-06-12 19:14:28 +0200

    [Cukes] Fix Feature applications/providers/referrer_filters

--- features/old/applications/providers/referrer_filters.feature
+++ features/old/applications/providers/referrer_filters.feature
@@ -23,13 +23,14 @@ Feature: Providers's application referrer filters
     Then I should see referrer filter "foo.example.org"
     And I should see referrer filter "bar.example.org"
 
+  @ajax
   Scenario: Create a referrer filter
     Given application "SpookyWidget" has no referrer filters
     And the backend will create referrer filter "foo.example.org" for application "SpookyWidget"
     When I log in as provider "foo.example.com"
     And I go to the provider side "SpookyWidget" application page
     And I submit the new referrer filter form with "foo.example.org"
-    Then I should see "Referrer filter has been created."
+    Then I should see referrer filter "foo.example.org"
 
   @evil @wip
   Scenario: Delete a referrer filter
@@ -46,23 +47,3 @@ Feature: Providers's application referrer filters
     When I log in as provider "foo.example.com"
     And I go to the provider side "SpookyWidget" application page
     Then I should not see "Referrer Filters"
-
-  @security @allow-rescue
-  Scenario: Creating referrer filter is forbidden if not provider of the application
-   Given a provider "xyz.example.com"
-     And current domain is the admin domain of provider "xyz.example.com"
-    When I log in as provider "xyz.example.com"
-     And I do POST to the referrer filters url for application "SpookyWidget"
-    Then I should see "Not found"
-
-  @security @allow-rescue
-  Scenario: Deleting referrer filter is forbidden if not provider of the application
-    Given a provider "bar.example.com"
-    Given application "SpookyWidget" has the following referrer filters:
-      | foo.example.org |
-
-    When current domain is the admin domain of provider "bar.example.com"
-     And I log in as provider "bar.example.com"
-
-    And I do DELETE to the "foo.example.org" referrer filter url for application "SpookyWidget"
-    Then I should see "Not found"
---
+++ test/integration/provider/admin/referrer_filters_controller_test.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Provider::Admin::ReferrerFiltersControllerTest < ActionDispatch::IntegrationTest
+
+  HEADERS_XHR = {'HTTP_X_REQUESTED_WITH' => 'XMLHttpRequest'}.freeze
+
+  def setup
+    super
+    @provider  = FactoryBot.create(:provider_account)
+    @buyer     = FactoryBot.create(:buyer_account, provider_account: @provider)
+    app_plan   = FactoryBot.create(:application_plan, issuer: @provider.default_service)
+    @cinstance = @buyer.buy! app_plan
+    @referrer  = 'referrer.example.com'
+  end
+
+  attr_reader :provider, :cinstance, :referrer
+
+  class ProviderWithPermissionTest < Provider::Admin::ReferrerFiltersControllerTest
+    def setup
+      super
+      login! provider
+    end
+
+    test 'create' do
+      post provider_admin_application_referrer_filters_path(application_id: cinstance.to_param, referrer_filter: referrer), {}, HEADERS_XHR
+
+      assert_response :success
+      assert_template 'create'
+    end
+
+    # regression test for https://3scale.airbrake.io/projects/14982/groups/71566877/notices/1105888258832983894
+    test 'create with error' do
+      ReferrerFilter.any_instance.stubs(persisted?: false)
+
+      post provider_admin_application_referrer_filters_path(application_id: cinstance.to_param, referrer_filter: referrer), {}, HEADERS_XHR
+
+      assert_response :success
+      assert_template 'error'
+    end
+
+    test 'delete' do
+      id = cinstance.referrer_filters.add(referrer).id
+
+      delete provider_admin_application_referrer_filter_path(application_id: cinstance.to_param, id: id), {}, HEADERS_XHR
+
+      assert_response :success
+    end
+  end
+
+  class ProviderWithoutPermissionTest < Provider::Admin::ReferrerFiltersControllerTest
+    def setup
+      super
+      login! FactoryBot.create(:provider_account)
+    end
+
+    test 'create' do
+      post provider_admin_application_referrer_filters_path(application_id: cinstance.to_param, referrer_filter: referrer), {}, HEADERS_XHR
+      assert_response :not_found
+    end
+
+    test 'delete' do
+      id = cinstance.referrer_filters.add(referrer).id
+
+      delete provider_admin_application_referrer_filter_path(application_id: cinstance.to_param, id: id), {}, HEADERS_XHR
+
+      assert_response :not_found
+    end
+  end
+
+end
