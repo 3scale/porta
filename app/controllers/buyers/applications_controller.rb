5ec69ac9ddc0dea78712acd51566a206512686a3
Jose Miguel Gallas <josemigallas@gmail.com>
2019-06-13 11:44:18 +0200

    highlights listing item

--- app/controllers/buyers/applications_controller.rb
+++ app/controllers/buyers/applications_controller.rb
@@ -21,7 +21,7 @@ class Buyers::ApplicationsController < FrontendController
   def index
     # TODO: Editing this action may require editing Api::ApplicationsController
 
-    activate_menu :audience, :applications
+    activate_menu :audience, :applications, :listing
 
     @states = Cinstance.allowed_states.collect(&:to_s).sort
     accessible_services = current_account.accessible_services
