f56dfa1cfbcd4493fac0866f3973eed34e951d67
Hallelujah <hery@rails-royce.org>
2019-06-27 12:45:10 +0200

    [db] Backfill proxy limit exceeded defaults

--- db/postgres_schema.rb
+++ db/postgres_schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190620092211) do
+ActiveRecord::Schema.define(version: 20190627103617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
