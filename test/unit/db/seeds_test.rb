26eb6d9410f2e3c0f7e6000000708f72c8126f84
Guilherme Cassolato <guicassolato@gmail.com>
2019-07-05 16:40:56 +0200

    Creation/deletion of the test_seed db without messing with the Rails env

--- test/unit/db/seeds_test.rb
+++ test/unit/db/seeds_test.rb
@@ -3,36 +3,33 @@
 require 'test_helper'
 
 class SeedsTest < ActiveSupport::TestCase
-  setup do
-    with_fake_env('test_seed') do
-      System::Application.load_tasks
-      Rake::Task['db:create'].invoke
-      Rake::Task['db:schema:load'].invoke
+  class SeedDatabaseConnection < ActiveRecord::Base
+    def self.abstract_class?
+      true
     end
-
   end
 
-  attr_reader :env
+  disable_transactional_fixtures!
 
-  teardown do
-    with_fake_env('test_seed') { Rake::Task['db:drop'].invoke }
+  setup do
+    @config = ActiveRecord::Base.configurations['test_seed']
+    adapter = ActiveRecord::Tasks::DatabaseTasks.send(:class_for_adapter, config['adapter']).new(config)
+    creation_options = adapter.send(:creation_options)
+    ActiveRecord::Base.connection.create_database config['database'], creation_options
+    ActiveRecord::Tasks::DatabaseTasks.load_schema_for(config)
+    SeedDatabaseConnection.establish_connection(:test_seed)
+  end
+
+  attr_reader :config
+
+  def after_teardown
+    super
+    SeedDatabaseConnection.connection.drop_database config['database']
   end
 
   test 'the seeds do not fail' do
-    ActiveRecord::Base.establish_connection(:test_seed)
-    assert Rails.application.load_seed
-    ActiveRecord::Base.establish_connection(:test)
-  end
-
-  private
-
-  def with_fake_env(new_env)
-    old_env = Rails.env
-    begin
-      Rails.env = ENV['RAILS_ENV'] = new_env
-      yield
-    ensure
-      Rails.env = ENV['RAILS_ENV'] = old_env
+    SeedDatabaseConnection.transaction(requires_new: true) do
+      assert Rails.application.load_seed
     end
   end
 end
