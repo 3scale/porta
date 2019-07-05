b8be3caf0d05de74dec4d4f2ff0ba73899d0250d
Guilherme Cassolato <guicassolato@gmail.com>
2019-07-05 17:43:05 +0200

    Clean db before seeds test

--- test/unit/db/seeds_test.rb
+++ test/unit/db/seeds_test.rb
@@ -3,6 +3,13 @@
 require 'test_helper'
 
 class SeedsTest < ActiveSupport::TestCase
+  setup do
+    # some annoying test is failing to clear the db in the teardown making this one to randomly fail, so adding this to ensure the test database is "virgin"
+    DatabaseCleaner.clean
+  end
+
+  disable_transactional_fixtures!
+
   test 'the seeds do not fail' do
     assert Rails.application.load_seed
   end
