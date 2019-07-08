b3d43caebb511d373cf2b0e6dca4e37b63134fa8
Guilherme Cassolato <guicassolato@gmail.com>
2019-07-08 11:10:29 +0200

    No need for DatabaseCleaner.clean again with transactional fixtures disabled

--- test/unit/db/seeds_test.rb
+++ test/unit/db/seeds_test.rb
@@ -3,11 +3,6 @@
 require 'test_helper'
 
 class SeedsTest < ActiveSupport::TestCase
-  setup do
-    # some annoying test is failing to clear the db in the teardown making this one to randomly fail, so adding this to ensure the test database is "virgin"
-    DatabaseCleaner.clean
-  end
-
   disable_transactional_fixtures!
 
   test 'the seeds do not fail' do
