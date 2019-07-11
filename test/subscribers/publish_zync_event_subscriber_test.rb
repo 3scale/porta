d300bfb6220fe1cbcbcd782d297dbbb6ce41f33d
Guilherme Cassolato <guicassolato@gmail.com>
2019-07-11 19:08:44 +0200

    DomainsChangedEvent not issued on Saas

--- test/subscribers/publish_zync_event_subscriber_test.rb
+++ test/subscribers/publish_zync_event_subscriber_test.rb
@@ -8,7 +8,48 @@ class PublishZyncEventSubscriberTest < ActiveSupport::TestCase
   def test_create
     application = FactoryBot.build_stubbed(:simple_cinstance, tenant_id: 1)
     event = Applications::ApplicationCreatedEvent.new(application: application)
-
     assert @subscriber.call(event)
+  end
+
+  class DomainEventsTest < ActiveSupport::TestCase
+    setup do
+      @subscriber = PublishZyncEventSubscriber.new
+      ThreeScale.config.stubs(onpremises: true)
+    end
+
+    attr_reader :subscriber
+
+    test 'proxy domains event' do
+      proxy = FactoryBot.build_stubbed(:proxy)
+      event = Domains::ProxyDomainsChangedEvent.create proxy
+      assert subscriber.call(event)
+    end
+
+    test 'provider domains event' do
+      provider = FactoryBot.build_stubbed(:simple_provider)
+      event = Domains::ProviderDomainsChangedEvent.create provider
+      assert subscriber.call(event)
+    end
+  end
+
+  class NotOnpremDomainEventsTest < ActiveSupport::TestCase
+    setup do
+      @subscriber = PublishZyncEventSubscriber.new
+      ThreeScale.config.stubs(onpremises: false)
+    end
+
+    attr_reader :subscriber
+
+    test 'proxy domains event' do
+      proxy = FactoryBot.build_stubbed(:proxy)
+      event = Domains::ProxyDomainsChangedEvent.create proxy
+      refute subscriber.call(event)
+    end
+
+    test 'provider domains event' do
+      provider = FactoryBot.build_stubbed(:simple_provider)
+      event = Domains::ProviderDomainsChangedEvent.create provider
+      refute subscriber.call(event)
+    end
   end
 end
