00df050cb7fcae600b024c701b47187b8e929615
Guilherme Cassolato <guicassolato@gmail.com>
2019-07-17 11:27:49 +0200

    Removed 'OAS' from the Backend API sidebar

--- app/helpers/vertical_nav_helper.rb
+++ app/helpers/vertical_nav_helper.rb
@@ -248,7 +248,6 @@ module VerticalNavHelper
     sections = []
     return sections unless @backend_api
     sections << {id: :overview,         title: 'Overview',           path: provider_admin_backend_api_path(@backend_api)}
-    sections << {id: :oas,              title: 'OAS',                path: "#"}
     sections << {id: :methods_metrics,  title: 'Methods & Metrics',  path: admin_service_metrics_path(@backend_api.service)}
     sections << {id: :mapping_rules,    title: 'Mapping Rules',      path: "#"}
   end
