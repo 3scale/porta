fd28f421bf2739bdd13872aad9fcc84334647cf7
Guilherme Cassolato <guicassolato@gmail.com>
2019-07-18 19:11:05 +0200

    Backend API mapping rules paginated

--- app/controllers/provider/admin/backend_apis/mapping_rules_controller.rb
+++ app/controllers/provider/admin/backend_apis/mapping_rules_controller.rb
@@ -3,6 +3,12 @@
 class Provider::Admin::BackendApis::MappingRulesController < Provider::Admin::BackendApis::BaseController
   activate_menu :backend_api, :mapping_rules
 
+  include ThreeScale::Search::Helpers
+
   def index
+    @mapping_rules = @backend_api.mapping_rules
+                                 .order_by(params[:sort], params[:direction])
+                                 .includes(:metric)
+                                 .paginate(pagination_params)
   end
 end
