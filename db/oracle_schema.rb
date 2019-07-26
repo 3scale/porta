0574c27415f4abd4331a7b214376cd0e38552870
Marta Noya <mnoyabon@redhat.com>
2019-07-26 19:09:09 +0200

    Path not null validation from Rails

--- db/oracle_schema.rb
+++ db/oracle_schema.rb
@@ -220,7 +220,7 @@ ActiveRecord::Schema.define(version: 20190722114341) do
   add_index "authentication_providers", ["account_id"], name: "index_authentication_providers_on_account_id"
 
   create_table "backend_api_configs", force: :cascade do |t|
-    t.string   "path",                          default: "", null: false
+    t.string   "path",                          default: ""
     t.integer  "service_id",     precision: 38
     t.integer  "backend_api_id", precision: 38
     t.datetime "created_at",                                 null: false
--- db/schema.rb
+++ db/schema.rb
@@ -220,7 +220,7 @@ ActiveRecord::Schema.define(version: 20190722114341) do
   add_index "authentication_providers", ["account_id"], name: "index_authentication_providers_on_account_id", using: :btree
 
   create_table "backend_api_configs", force: :cascade do |t|
-    t.string   "path",           limit: 255, default: "", null: false
+    t.string   "path",           limit: 255, default: ""
     t.integer  "service_id",     limit: 8
     t.integer  "backend_api_id", limit: 8
     t.datetime "created_at",                              null: false
