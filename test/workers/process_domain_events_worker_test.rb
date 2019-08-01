a93f4ecafd9d05f96e1d57c78628f81c4b73a271
Michal Macejko <michal.macejko1@gmail.com>
2019-08-01 15:43:01 +0200

    [ProcessDomainEventsWorker] use perform instead of enqueue

--- test/workers/process_domain_events_worker_test.rb
+++ test/workers/process_domain_events_worker_test.rb
@@ -1,16 +1,23 @@
 require 'test_helper'
 
 class ProcessDomainEventsWorkerTest < ActiveSupport::TestCase
+
+  def test_enqueue
+    proxy = FactoryBot.create(:simple_proxy)
+    event = Domains::ProxyDomainsChangedEvent.create(proxy)
+    ProcessDomainEventsWorker.enqueue(event)
+  end
+
   def test_proxy_domain_changed_event
     provider = FactoryBot.create(:simple_provider)
     proxy = FactoryBot.create(:simple_proxy)
     proxy.update_attributes!(endpoint: "http://#{provider.admin_domain}")
-    event = Domains::ProxyDomainsChangedEvent.create(proxy)
+    event = Domains::ProxyDomainsChangedEvent.create_and_publish!(proxy)
 
     provider_domains_changed = EventStore::Repository.adapter.where(event_type: 'Domains::ProviderDomainsChangedEvent')
 
     assert_difference provider_domains_changed.method(:count) do
-      ProcessDomainEventsWorker.new.perform(event)
+      ProcessDomainEventsWorker.new.perform(event.event_id)
     end
   end
 
@@ -18,12 +25,12 @@ class ProcessDomainEventsWorkerTest < ActiveSupport::TestCase
     provider = FactoryBot.create(:simple_provider)
     proxy = FactoryBot.create(:simple_proxy)
     proxy.update_attributes!(endpoint: "http://#{provider.admin_domain}")
-    event = Domains::ProviderDomainsChangedEvent.create(provider)
+    event = Domains::ProviderDomainsChangedEvent.create_and_publish!(provider)
 
     proxy_domains_changed = EventStore::Repository.adapter.where(event_type: 'Domains::ProxyDomainsChangedEvent')
 
     assert_difference proxy_domains_changed.method(:count) do
-      ProcessDomainEventsWorker.new.perform(event)
+      ProcessDomainEventsWorker.new.perform(event.event_id)
     end
   end
 end
