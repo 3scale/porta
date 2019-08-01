e2a76296725427b9b78694b8b9011c422ba93002
Marta Noya <mnoyabon@redhat.com>
2019-08-01 10:52:15 +0200

    Destroy by worker 'stale' objects (Events + DeletedObject)

--- app/models/deleted_object.rb
+++ app/models/deleted_object.rb
@@ -10,14 +10,12 @@ class DeletedObject < ApplicationRecord
 
   scope(:deleted_owner, lambda do
     join_sql = <<-SQL
-      INNER JOIN deleted_objects AS deleted_owner
+      INNER JOIN deleted_objects deleted_owner
         ON deleted_objects.owner_type = deleted_owner.object_type
         AND deleted_objects.owner_id = deleted_owner.object_id
     SQL
     joins(join_sql)
   end)
 
-  scope(:stale, lambda do
-    where.has { ((id.in DeletedObject.deleted_owner.select(:id)) | (object_type == Service.name)) & (created_at <= 1.week.ago) }
-  end)
+  scope :stale, -> { where.has { ((id.in DeletedObject.deleted_owner.select(:id)) | (object_type == Service.name)) & (created_at <= 1.week.ago) } }
 end
