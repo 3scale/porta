00ce6011f14d7b1625b58f917b8afc82be259f36
Kadu Ribeiro <mail@carlosribeiro.me>
2019-08-02 12:24:19 -0300

    Do not allow blank plan in service subscription (#1039)
    Currently when you try to subscribe in some service, the plan listing is
    blank and when you try to submit it, it explodes an exception and it
    renders a weird 404 page.
    
    This commit adds the `Please Select` option in the subscription page only if there is no Default plan and validates if there is some plan selected

--- test/integration/buyers/service_contracts_controller_test.rb
+++ test/integration/buyers/service_contracts_controller_test.rb
@@ -15,11 +15,10 @@ class Buyers::ServiceContractsControllerTest < ActionDispatch::IntegrationTest
     @buyer1.buy! @application_plan
     @buyer2.buy! @application_plan
 
-    host! @provider.admin_domain
-    provider_login_with @provider.admins.first.username, 'supersecret'
+    login! @provider
   end
 
-  def test_can_unsubscribe_from_service_contract
+  test 'can unsubscribe from service contract' do
     get admin_buyers_account_service_contracts_path(account_id: @buyer1.id)
 
     assert_response :success
@@ -30,7 +29,7 @@ class Buyers::ServiceContractsControllerTest < ActionDispatch::IntegrationTest
     assert page.xpath("//a[@data-method='delete']").text =~ /Unsubscribe/
   end
 
-  def test_unsubscribe_developers_from_service_with_suspended_application
+  test 'unsubscribe developers from service with suspended application' do
     apps = @buyer1.bought_cinstances.by_service_id(@service_contract.service_id)
     apps.update_all state: 'suspended'
 
@@ -47,7 +46,7 @@ class Buyers::ServiceContractsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 1, @buyer2.bought_cinstances.by_service_id(@service_contract.service_id).count
   end
 
-  def test_unsubscribe_developers_from_service_with_one_not_suspended_application
+  test 'unsubscribe developers from service with one not suspended application' do
     apps = @buyer1.bought_cinstances.by_service_id(@service_contract.service_id)
 
     assert_no_difference(-> {apps.count})  do
@@ -61,7 +60,7 @@ class Buyers::ServiceContractsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  def test_unsubscribe_developers_from_service_with_two_no_suspended_applications
+  test 'unsubscribe developers from service with two not suspended applications' do
     application_plan2 = FactoryBot.create(:application_plan, issuer: @service)
 
     @buyer1.buy! application_plan2
@@ -74,10 +73,26 @@ class Buyers::ServiceContractsControllerTest < ActionDispatch::IntegrationTest
     assert_match I18n.t('service_contracts.unsubscribe_failure'), flash[:error]
   end
 
-  def test_unauthorized_access_master_on_premises
+  test 'unauthorized access master_on_premises' do
     login! master_account
     ThreeScale.stubs(master_on_premises?: true)
     get admin_buyers_account_service_contracts_path(account_id: @provider.id)
     assert_response :forbidden
+  end
+
+  test 'renders a prompt if there is no default service plan' do
+    get new_admin_buyers_account_service_contract_path(account_id: @buyer1.id, service_id: @service.id)
+
+    page = Nokogiri::HTML::Document.parse(response.body)
+    assert page.xpath("//select[@id='service_contract_plan_id']/option").map(&:text).include?('Please select')
+  end
+
+  test 'does not render a prompt if there is a default service plan' do
+    @service.update_attributes(default_service_plan: @service_plan)
+      
+    get new_admin_buyers_account_service_contract_path(account_id: @buyer1.id, service_id: @service.id)
+
+    page = Nokogiri::HTML::Document.parse(response.body)
+    assert page.xpath("//select[@id='service_contract_plan_id']/option").map(&:text).exclude?('Please select')
   end
 end
