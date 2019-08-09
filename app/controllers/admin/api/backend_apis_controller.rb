60d139d4b46d35d10d03f04cae2bb18042a96261
Marta Noya <mnoyabon@redhat.com>
2019-08-09 10:53:04 +0200

    [API] Backend API CRUD [Part 1 - Base]

---
+++ app/controllers/admin/api/backend_apis_controller.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+class Admin::Api::BackendApisController < Admin::Api::BaseController
+  self.access_token_scopes = :account_management
+
+  # TODO: ApiDocs documentation
+
+  before_action :find_backend_api, only: %i[show update destroy]
+  before_action :authorize
+
+  clear_respond_to
+  respond_to :json
+
+  representer ::BackendApi
+
+  def index
+    # TODO: paginate?
+    respond_with(current_account.backend_apis)
+  end
+
+  def create
+    backend_api = current_account.backend_apis.create(backend_api_params)
+    respond_with(backend_api)
+  end
+
+  def show
+    respond_with(backend_api)
+  end
+
+  def update
+    backend_api.update(backend_api_params)
+    respond_with(backend_api)
+  end
+
+  def destroy
+    backend_api.destroy
+    respond_with(backend_api)
+  end
+
+  private
+
+  attr_reader :backend_api
+
+  def authorize
+    provider_can_use!(:api_as_product)
+    # TODO: I guess it should authorize also that it has permission for the specific action :)
+  end
+
+  def find_backend_api
+    @backend_api = current_account.backend_apis.find(params[:id])
+  end
+
+  def backend_api_params
+    # Not sure about :system_name but for Service it is readonly # TODO ?
+    params.require(:backend_api).permit(:name, :description, :private_endpoint)
+  end
+end
