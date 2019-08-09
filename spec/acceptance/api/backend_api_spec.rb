a0c3a00cb52cb3edb40e5bdf7da70d1a25678a64
Marta Noya <mnoyabon@redhat.com>
2019-08-09 10:32:27 +0200

    Representer Collection BackendAPI

--- spec/acceptance/api/backend_api_spec.rb
+++ spec/acceptance/api/backend_api_spec.rb
@@ -4,10 +4,24 @@ require 'rails_helper'
 
 resource 'BackendApi' do
   let(:resource) { FactoryBot.create(:backend_api) }
+  let(:expected_properties) { %w[id name system_name description private_endpoint account_id created_at updated_at] }
 
   json(:resource) do
     let(:root) { 'backend_api' }
 
-    it { subject.should have_properties(%w[id name system_name description private_endpoint account_id created_at updated_at]).from(resource) }
+    it { subject.should have_properties(expected_properties).from(resource) }
+  end
+
+  json(:collection) do
+    let(:root) { 'backend_apis' }
+    context do
+      let(:collection) { [resource, resource] }
+      it 'contains the backend apis data by its representer' do
+        subject.each do |subject_backend_api|
+          subject_backend_api.should include('backend_api')
+          subject_backend_api.fetch('backend_api').should have_properties(expected_properties).from(resource)
+        end
+      end
+    end
   end
 end
