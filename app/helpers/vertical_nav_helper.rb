ad46a58a31aba8889e419c48da369ae2cfed807e
Kadu Ribeiro <mail@carlosribeiro.me>
2019-08-09 11:15:30 -0300

    Create an indepentent page to manage Mapping Rules (#1054) (#1063)
    Our current page that manages the mapping rules don't have
    pagination, causing a timeout if you have a lot of rules.
    
    This commit creates a new independent page to manage Mapping rules
    with a paginated list of itens.

--- app/helpers/vertical_nav_helper.rb
+++ app/helpers/vertical_nav_helper.rb
@@ -237,6 +237,7 @@ module VerticalNavHelper
     items = []
     items << {id: :configuration,   title: 'Configuration',     path: path_to_service(@service)}
     items << {id: :methods_metrics, title: 'Methods & Metrics', path: admin_service_metrics_path(@service)}
+    items << {id: :mapping_rules,   title: 'Mapping Rules',     path: admin_service_proxy_rules_path(@service)} if provider_can_use?(:independent_mapping_rules)
     items << {id: :settings,        title: 'Settings',          path: settings_admin_service_path(@service)}
   end
 
