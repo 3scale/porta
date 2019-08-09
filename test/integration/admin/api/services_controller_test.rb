f794b4cd61c7a4a3240e5f6815732ce82ed96aa3
Marta Noya <mnoyabon@redhat.com>
2019-08-09 17:10:16 +0200

    Refactor Test API ServicesController

--- test/integration/admin/api/services_controller_test.rb
+++ test/integration/admin/api/services_controller_test.rb
@@ -3,69 +3,73 @@
 require 'test_helper'
 
 class Admin::Api::ServicesControllerTest < ActionDispatch::IntegrationTest
+  class MasterHostTest < Admin::Api::ServicesControllerTest
+    setup do
+      login! master_account
+    end
 
-  setup do
-    login! master_account
-  end
-
-  def test_create
-    Account.any_instance.stubs(can_create_service?: true)
-    %i[xml json].each do |format|
-      requested_name = "example name #{format.to_s}"
-      requested_description = "example description #{format.to_s}"
-      assert_difference(master_account.services.method(:count)) do
-        post admin_api_services_path(format: format), {name: requested_name, description: requested_description}
-        assert_response :created
+    test 'create' do
+      Account.any_instance.stubs(can_create_service?: true)
+      %i[xml json].each do |format|
+        requested_name = "example name #{format.to_s}"
+        requested_description = "example description #{format.to_s}"
+        assert_difference(master_account.services.method(:count)) do
+          post admin_api_services_path(format: format), {name: requested_name, description: requested_description}
+          assert_response :created
+        end
+        service = master_account.services.last
+        assert_equal requested_name, service.name
+        assert_equal requested_description, service.description
       end
-      service = master_account.services.last
-      assert_equal requested_name, service.name
-      assert_equal requested_description, service.description
     end
-  end
 
-  def test_update
-    service = master_account.default_service
-    %i[xml json].each do |format|
-      requested_name = "example name #{format.to_s}"
-      requested_description = "example description #{format.to_s}"
-      put admin_api_service_path(service, format: format), {name: requested_name, description: requested_description}
+    test 'update' do
+      service = master_account.default_service
+      %i[xml json].each do |format|
+        requested_name = "example name #{format.to_s}"
+        requested_description = "example description #{format.to_s}"
+        put admin_api_service_path(service, format: format), {name: requested_name, description: requested_description}
+        assert_response :ok
+        assert_equal requested_name, service.reload.name
+        assert_equal requested_description, service.description
+      end
+    end
+
+    test 'show' do
+      service = master_account.default_service
+      %i[xml json].each do |format|
+        get admin_api_service_path(service, format: format)
+        assert_response :ok
+        assert response.body.include?('deployment_option')
+        assert response.body.include?(service.deployment_option)
+      end
+    end
+
+    test 'index works for SaaS but it is unauthorized for Master On-prem' do
+      ThreeScale.stubs(master_on_premises?: false)
+      get admin_api_services_path
       assert_response :ok
-      assert_equal requested_name, service.reload.name
-      assert_equal requested_description, service.description
+
+      ThreeScale.stubs(master_on_premises?: true)
+      get admin_api_services_path
+      assert_response :forbidden
     end
   end
 
-  def test_show
-    service = master_account.default_service
-    %i[xml json].each do |format|
-      get admin_api_service_path(service, format: format)
-      assert_response :ok
-      assert response.body.include?('deployment_option')
-      assert response.body.include?(service.deployment_option)
-    end
-  end
-
-  test 'GET index works for Saas' do
-    get admin_api_services_path
-    assert_response :ok
-  end
-
-  test 'GET index is unauthorized for Master On-prem' do
-    ThreeScale.stubs(master_on_premises?: true)
-    get admin_api_services_path
-    assert_response :forbidden
-  end
-
-  class WithProviderKey < ActionDispatch::IntegrationTest
-
-    def test_delete
-      provider = FactoryBot.create(:provider_account)
+  class TenantHostTest < ActionDispatch::IntegrationTest
+    setup do
+      @provider = FactoryBot.create(:provider_account)
       host! provider.admin_domain
+    end
+
+    attr_reader :provider
+
+    test 'delete with api_key' do
       service = FactoryBot.create(:service, account: provider)
-      refute service.deleted?
-      delete admin_api_service_path(service, provider_key: provider.api_key)
-      assert_response :ok
-      assert service.reload.deleted?
+      assert_change(of: -> { service.reload.deleted? }, from: false, to: true) do
+        delete admin_api_service_path(service, provider_key: provider.api_key)
+        assert_response :ok
+      end
     end
   end
 end
