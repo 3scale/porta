83936f1a0fb0733702944e9e7fdfeee15e1eb903
Marta Noya <mnoyabon@redhat.com>
2019-08-14 12:33:46 +0200

    Fix ServiceDiscovery::ConfigAndTest: merge hash instead of stub

--- test/unit/service_discovery/config_test.rb
+++ test/unit/service_discovery/config_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class ServiceDiscovery::ConfigAndTest < ActiveSupport::TestCase
   def subject
-    ThreeScale.config.stubs(service_discovery: config)
+    ThreeScale.config.merge!(service_discovery: config)
     ServiceDiscovery::Config
   end
 
@@ -28,57 +28,57 @@ class ServiceDiscovery::ConfigAndTest < ActiveSupport::TestCase
   end
   test 'server_scheme' do
     assert_equal config.server_scheme, subject.server_scheme
-    config.stubs(server_scheme: nil)
+    config.merge!(server_scheme: nil)
     assert_equal 'https', subject.server_scheme
   end
 
   test 'server_host' do
     assert_equal config.server_host, subject.server_host
-    config.stubs(server_host: nil)
+    config.merge!(server_host: nil)
     assert_equal 'openshift.default.svc.cluster.local', subject.server_host
   end
 
   test 'server_port' do
     assert_equal config.server_port, subject.server_port
-    config.stubs(server_port: nil)
+    config.merge!(server_port: nil)
     assert_equal 443, subject.server_port
   end
 
   test 'verify_ssl' do
     assert_equal config.verify_ssl, subject.verify_ssl
-    config.stubs(verify_ssl: nil)
+    config.merge!(verify_ssl: nil)
     assert_equal OpenSSL::SSL::VERIFY_NONE, subject.verify_ssl
   end
 
   test 'timeout' do
     assert_equal config.timeout, subject.timeout
-    config.stubs(timeout: nil)
+    config.merge!(timeout: nil)
     assert_equal 1, subject.timeout
   end
 
   test 'open_timeout' do
     assert_equal config.open_timeout, subject.open_timeout
-    config.stubs(open_timeout: nil)
+    config.merge!(open_timeout: nil)
     assert_equal 1, subject.open_timeout
   end
 
   test 'max_retry' do
     assert_equal config.max_retry, subject.max_retry
-    config.stubs(max_retry: nil)
+    config.merge!(max_retry: nil)
     assert_equal 5, subject.max_retry
   end
 
   test 'authentication_method' do
     assert_instance_of ActiveSupport::StringInquirer, subject.authentication_method
     assert_equal config.authentication_method, subject.authentication_method
-    config.stubs(authentication_method: nil)
+    config.merge!(authentication_method: nil)
     assert_equal 'service_account', subject.authentication_method
   end
 
   test 'oauth_server_type' do
     assert_instance_of ActiveSupport::StringInquirer, subject.oauth_server_type
     assert_equal config.oauth_server_type, subject.oauth_server_type
-    config.stubs(oauth_server_type: nil)
+    config.merge!(oauth_server_type: nil)
     assert_equal 'builtin', subject.oauth_server_type
   end
 
@@ -92,7 +92,7 @@ class ServiceDiscovery::ConfigAndTest < ActiveSupport::TestCase
 
   test 'verify_ssl?' do
     assert_equal true, subject.verify_ssl?
-    config.stubs(verify_ssl: nil)
+    config.merge!(verify_ssl: nil)
     assert_equal false, subject.verify_ssl?
   end
 
