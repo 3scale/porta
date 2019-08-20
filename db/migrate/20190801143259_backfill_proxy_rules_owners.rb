e43256dee6f2d415433b28eb5f58b3abb52dec7a
Hallelujah <hery@rails-royce.org>
2019-08-20 16:22:07 +0200

    [lib] Add a counter to long migration
    So we have at least a bit of information on how our migration is going

--- db/migrate/20190801143259_backfill_proxy_rules_owners.rb
+++ db/migrate/20190801143259_backfill_proxy_rules_owners.rb
@@ -1,9 +1,13 @@
+require 'progress_counter'
+
 class BackfillProxyRulesOwners < ActiveRecord::Migration
   def up
     say_with_time 'Updating proxy rules owners...' do
       ProxyRule.reset_column_information
+      progress = ProgressCounter.new(ProxyRule.count)
       ProxyRule.find_each do |proxy_rule|
         proxy_rule.update_columns(owner_id: proxy_rule.proxy_id, owner_type: 'Proxy') unless proxy_rule.owner_type?
+        progress.call
       end
     end
   end
