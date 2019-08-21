49af90f4278449e601ee3ca0c164c4bdca62288f
Guilherme Cassolato <guicassolato@gmail.com>
2019-08-21 14:51:02 +0200

    Backend API metric UI pages
    - New/create metric/method
    - Edit/update metric/method
    - Destroy metric/method
    
    Closes https://issues.jboss.org/browse/THREESCALE-3089

--- app/controllers/provider/admin/backend_apis/metrics_controller.rb
+++ app/controllers/provider/admin/backend_apis/metrics_controller.rb
@@ -2,11 +2,8 @@
 
 class Provider::Admin::BackendApis::MetricsController < Provider::Admin::BackendApis::BaseController
   before_action :find_metric, except: %i[new create index]
-  before_action :build_metric, only: %i[new create]
 
-  # FIXME: This should be removed. It is still needed because either
-  #        1. the page is still pointing the form to the old metrics controller (new/edit) or
-  #        2. the page uses a helper shared with the old metrics page (app/helpers/services_helper.rb:26) (index)
+  # FIXME: This should be removed. It is still needed because some pages use a helper shared with the old metrics page (app/helpers/services_helper.rb:26) (index)
   before_action :find_service
 
   activate_menu :backend_api, :methods_metrics
@@ -17,19 +14,41 @@ class Provider::Admin::BackendApis::MetricsController < Provider::Admin::Backend
   end
 
   def new
-    render template: '/api/metrics/new'
+    @metric = collection.build
   end
 
   def create
-    # TODO: https://issues.jboss.org/browse/THREESCALE-3089
+    @metric = collection.build(metric_params)
+    if @metric.save
+      onboarding.bubble_update('metric')
+      flash[:notice] = "The #{metric_type} was created"
+      redirect_to provider_admin_backend_api_metrics_path(@backend_api)
+    else
+      flash[:error] = "#{metric_type.capitalize} could not be created"
+      render :new
+    end
   end
 
-  def edit
-    render template: '/api/metrics/edit'
-  end
+  def edit; end
 
   def update
-    # TODO: https://issues.jboss.org/browse/THREESCALE-3089
+    if @metric.update_attributes(metric_params)
+      flash[:notice] = "The #{metric_type} was updated"
+      redirect_to provider_admin_backend_api_metrics_path(@backend_api)
+    else
+      flash[:error] = "#{metric_type.capitalize} could not be updated"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @metric.destroy
+      flash[:notice] = "The #{metric_type} was deleted"
+      redirect_to provider_admin_backend_api_metrics_path(@backend_api)
+    else
+      flash[:error] = "The #{metric_type} could not be deleted"
+      render :edit
+    end
   end
 
   helper_method :bubbles
@@ -49,16 +68,20 @@ class Provider::Admin::BackendApis::MetricsController < Provider::Admin::Backend
     @metric = find_backend_api_metric_by(params[:id])
   end
 
-  def build_metric
-    metrics = if (metric_id = params[:metric_id])
-                find_backend_api_metric_by(metric_id).children
-              else
-                @backend_api.metrics
-              end
-    @metric = metrics.build(params[:metric] || {})
-  end
-
   def find_backend_api_metric_by(id)
     @backend_api.metrics.find(id)
+  end
+
+  def metric_params
+    params.require(:metric).permit(:friendly_name, :system_name, :unit, :description)
+  end
+
+  def metric_type
+    @metric.child? ? 'method' : 'metric'
+  end
+
+  def collection
+    metric_id = params[:metric_id]
+    metric_id ? find_backend_api_metric_by(metric_id).children : @backend_api.metrics
   end
 end
