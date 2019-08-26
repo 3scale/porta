00fc58d734221854ff88092489834171a6b994c0
Jose Miguel Gallas <josemigallas@gmail.com>
2019-08-26 11:22:06 +0200

    collapse backend api metrics

---
+++ app/javascript/packs/plans-metrics.js
@@ -0,0 +1,20 @@
+import { safeFromJsonString } from 'utilities/json-utils'
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('th.backend_api_metric_title')
+    .forEach(th => {
+      const { collapsible, metrics } = th.dataset
+      const ids = safeFromJsonString(metrics)
+
+      if (collapsible) {
+        const caret = document.createElement('i')
+        caret.classList.add('fa', 'fa-caret-down')
+
+        const span = th.querySelector('span')
+        span.insertBefore(caret, span.firstChild)
+        span.addEventListener('click', () => {
+          ids.forEach(id => document.getElementById(`metric_${id}`).classList.toggle('hidden'))
+        })
+      }
+    })
+})
