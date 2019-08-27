dc0312a6a2a4b7891d612ce02c594c23322dbe8e
Jose Miguel Gallas <josemigallas@gmail.com>
2019-08-27 12:42:50 +0200

    :ambulance: patching up IE11 compatibility

--- app/javascript/packs/plans-metrics.js
+++ app/javascript/packs/plans-metrics.js
@@ -1,21 +1,35 @@
 import { safeFromJsonString } from 'utilities/json-utils'
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('th.backend_api_metric_title')
-    .forEach(th => {
-      const { collapsible, metrics } = th.dataset
-      const ids = safeFromJsonString(metrics)
+  function toggleMetricVisibility (id) {
+    document.getElementById(`metric_${id}`)
+      .classList
+      .toggle('hidden')
+  }
 
-      if (collapsible) {
-        const caret = document.createElement('i')
-        caret.classList.add('fa', 'fa-caret-down')
+  const ths = document.querySelectorAll('th.backend_api_metric_title')
+  // NodeList.foreEach not supported in IE11
+  for (const th of ths) {
+    const { collapsible, metrics } = th.dataset
+    const ids = safeFromJsonString(metrics)
 
-        const span = th.querySelector('span')
-        span.insertBefore(caret, span.firstChild)
-        span.addEventListener('click', () => {
-          th.classList.toggle('collapsed')
-          ids.forEach(id => document.getElementById(`metric_${id}`).classList.toggle('hidden'))
-        })
+    if (collapsible) {
+      const toggleBackendAPI = () => {
+        th.classList.toggle('collapsed')
+        ids.forEach(toggleMetricVisibility)
       }
-    })
+
+      const caret = document.createElement('i')
+      // Multiple arguments for 'add' not supported in IE11
+      caret.classList.add('fa')
+      caret.classList.add('fa-caret-down')
+
+      const span = th.querySelector('span')
+      span.insertBefore(caret, span.firstChild)
+      span.addEventListener('click', toggleBackendAPI)
+
+      // First render with all metrics collapsed
+      toggleBackendAPI()
+    }
+  }
 })
