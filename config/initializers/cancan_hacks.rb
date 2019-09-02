0beb12d20fc6073e7587f89c39c8fa41db349137
Marta Noya <mnoyabon@redhat.com>
2019-09-02 13:07:33 +0200

    Fix NoMethodError: undefined method `actions' for nil:NilClass possible_relevant_rules does not return nil values

--- config/initializers/cancan_hacks.rb
+++ config/initializers/cancan_hacks.rb
@@ -1,50 +1,21 @@
-# This makes CanCan work with Inherited resources.
-#
-# Inspiration comes from hexors's comment over here:
-# http://github.com/ryanb/cancan/issues#issue/23
-#
-# TODO: Maybe this is no longer needed, as there is a new version of canca. Check it.
-module CanCanHacks
-  def self.included(base)
-    base.extend(ClassMethods)
+# frozen_string_literal: true
 
-     class << base
-      alias_method_chain :authorize_resource, :inherited_resources
-    end
-  end
+require 'cancan_hacks'
+require 'cancan/ability/rules'
 
-   private
-
-   def authorize_inherited_resource
-    authorize! params[:action].to_sym, resource_for_authorization
-  end
-
-   def resource_for_authorization
-    case params[:action].to_sym
-    when :index
-      resource_class
-    when :new, :create
-      build_resource
-    else
-      resource || resource_class
-    end
-  end
-
-   module ClassMethods
-    def authorize_resource_with_inherited_resources(options = {})
-      if inherits_resources?
-        before_action :authorize_inherited_resource
-      else
-        authorize_resource_without_inherited_resources(options)
+module CanCan
+  module Ability
+    Rules.module_eval do
+      # possible_relevant_rules does not return `nil` values anymore
+      def possible_relevant_rules(subject)
+        if subject.is_a?(Hash)
+          rules
+        else
+          positions = @rules_index.values_at(subject, *alternative_subjects(subject))
+          positions.flatten!.sort!
+          positions.map { |i| @rules[i] }.compact
+        end
       end
-    end
-
-     private
-
-     def inherits_resources?
-      included_modules.include?(InheritedResources::BaseHelpers)
     end
   end
 end
-
- ActionController::Base.send(:include, CanCanHacks)
