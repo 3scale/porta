34b6f4539c6fb152c6905f076465605a1d4d29be
Marta Noya <mnoyabon@redhat.com>
2019-09-03 17:28:38 +0200

    [Refactor] Small improvements in the code   - Acceptance test builds resource because it is going to be saved later anyway   - Acceptance test collection uses 2 different objects   - Ensure and Test the order of pagination with oldest_first   - Integration test creates backend_api only when it is necessary   - In the Controller, memoize backend_api instead of before action to find it   - Add TODOs of links in backend api representer

--- app/representers/backend_api_representer.rb
+++ app/representers/backend_api_representer.rb
@@ -13,4 +13,8 @@ module BackendApiRepresenter
   property :account_id
   property :created_at
   property :updated_at
+
+  # TODO: links of metrics as part of https://issues.jboss.org/browse/THREESCALE-3209
+  # TODO: links of proxy rules as part of https://issues.jboss.org/browse/THREESCALE-3208
+  # Right now none of them can be done yet because the routes do not exist yet
 end
--- spec/acceptance/api/backend_api_spec.rb
+++ spec/acceptance/api/backend_api_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 resource 'BackendApi' do
-  let(:resource) { FactoryBot.create(:backend_api) }
+  let(:resource) { FactoryBot.build(:backend_api) }
   let(:expected_properties) { %w[id name system_name description private_endpoint account_id created_at updated_at] }
 
   json(:resource) do
@@ -15,11 +15,13 @@ resource 'BackendApi' do
   json(:collection) do
     let(:root) { 'backend_apis' }
     context do
-      let(:collection) { [resource, resource] }
+      let(:collection) { [resource, FactoryBot.create(:backend_api)] }
       it 'contains the backend apis data by its representer' do
+        assert_equal collection.length, subject.length
         subject.each do |subject_backend_api|
           subject_backend_api.should include('backend_api')
-          subject_backend_api.fetch('backend_api').should have_properties(expected_properties).from(resource)
+          backend_api = collection.find { |backend_api| backend_api.id == subject_backend_api.fetch('backend_api').fetch('id') }
+          subject_backend_api.fetch('backend_api').should have_properties(expected_properties).from(backend_api)
         end
       end
     end
