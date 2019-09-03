981652d9cb1f9567f57b569e0855f2c7ab295e1e
Carlos Ribeiro <mail@carlosribeiro.me>
2019-09-03 15:13:24 -0300

    Implement Mark as Deleted for Backend API
    THREESCALE-3343
    
    This commit changed the behave of the destroy action from the
    Backend API controller to make it delete the object in background.

--- app/models/backend_api.rb
+++ app/models/backend_api.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class BackendApi < ApplicationRecord
-
   include SystemName
+
+  DELETED_STATE = :deleted
   ECHO_API_HOST = 'echo-api.3scale.net'
 
   has_many :proxy_rules, as: :owner, dependent: :destroy, inverse_of: :owner
@@ -16,6 +17,7 @@ class BackendApi < ApplicationRecord
   delegate :default_api_backend, to: :class
 
   validates :name,        length: { maximum: 511 }, presence: true
+  validates :state,       length: { maximum: 255 }
   validates :system_name, length: { maximum: 255 }, presence: true
 
   validates :private_endpoint, length: { maximum: 255 },
@@ -33,6 +35,18 @@ class BackendApi < ApplicationRecord
   scope :orphans, -> { joining { services.outer }.where { BabySqueel[:backend_api_configs].backend_api_id == nil } }
 
   scope :oldest_first, -> { order(created_at: :asc) }
+  scope :accessible, -> { where.not(state: DELETED_STATE) }
+
+  state_machine initial: :published do
+    state :published
+    state DELETED_STATE
+
+    event :mark_as_deleted do
+      transition [:published] => DELETED_STATE
+    end
+
+    after_transition to: [DELETED_STATE], do: :schedule_deletion
+  end
 
   def self.default_api_backend
     "https://#{ECHO_API_HOST}:443"
@@ -57,6 +71,10 @@ class BackendApi < ApplicationRecord
   end
 
   private
+
+  def schedule_deletion
+    DeleteObjectHierarchyWorker.perform_later(self)
+  end
 
   def set_private_endpoint
     return if account.provider_can_use?(:api_as_product)
