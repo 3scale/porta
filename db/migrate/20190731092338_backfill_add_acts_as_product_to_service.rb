79c60b24fdf43bb382eb0927b5706793cf251178
Hallelujah <hery@rails-royce.org>
2019-09-09 21:05:48 +0300

    [db] Do not backfill act_as_product column
    It will be removed in later migration

--- db/migrate/20190731092338_backfill_add_acts_as_product_to_service.rb
+++ db/migrate/20190731092338_backfill_add_acts_as_product_to_service.rb
@@ -6,6 +6,7 @@ class BackfillAddActsAsProductToService < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def change
+    return puts "Nothing to do, this migration should not be executed"
     progress = ProgressCounter.new(Service.count)
     Service.all.select(:id).find_in_batches(batch_size: 200) do |records|
       Service.where(id: records.map(&:id)).update_all act_as_product: false
