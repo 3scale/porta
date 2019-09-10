f47ab75869f5a15395a492902568e93e29ceda63
Guilherme Cassolato <guicassolato@gmail.com>
2019-09-10 13:09:36 +0200

    Fix cuke change plan with branding switch

--- features/step_definitions/switches_steps.rb
+++ features/step_definitions/switches_steps.rb
@@ -19,7 +19,7 @@ Given /^the provider has ("(?:.+?)"(?: switch)? visible)$/ do |sentence|
 end
 
 Then /^I should see the invitation to upgrade my plan$/ do
-  assert has_content?("Upgrade")
+  assert find(:css, 'a#change-plan.important-button')&.has_content?('Upgrade')
 end
 
 Then /^I should see upgrade notice for "(.+?)"$/ do |switch|
