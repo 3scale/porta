497785d9d33aadb832068e23b2605b8ae92cb162
Marta Noya <mnoyabon@redhat.com>
2019-09-13 10:44:43 +0200

    [API] BackendApi responds with link to its metrics

--- spec/acceptance/api/backend_api_spec.rb
+++ spec/acceptance/api/backend_api_spec.rb
@@ -10,6 +10,7 @@ resource 'BackendApi' do
     let(:root) { 'backend_api' }
 
     it { subject.should have_properties(expected_properties).from(resource) }
+    it { should have_links('metrics') }
   end
 
   json(:collection) do
