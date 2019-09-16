b92fe35b590d440b335c29080b496fa26ac34753
Jose Miguel Gallas <josemigallas@gmail.com>
2019-09-16 11:26:12 +0200

    :cucumber: fixes test

--- features/step_definitions/dashboard_steps.rb
+++ features/step_definitions/dashboard_steps.rb
@@ -7,14 +7,14 @@ def service_for_name (name)
   page.find("#service_#{service_id}")
 end
 
-def hits_for_name (name)
+def hits_for_name (name, opts = {})
   service_id = service_id_for_name(name)
-  page.find_by_id("dashboard-widget-service_id-#{service_id}service_hits")
+  page.find_by_id("dashboard-widget-service_id-#{service_id}service_hits", opts)
 end
 
-def top_traffic_for_name (name)
+def top_traffic_for_name (name, opts = {})
   service_id = service_id_for_name(name)
-  page.find_by_id("dashboard-widget-service_id-#{service_id}service_top_traffic")
+  page.find_by_id("dashboard-widget-service_id-#{service_id}service_top_traffic", opts)
 end
 
 When(/^service "([^"]*)" is (folded|unfolded)$/) do |service_name, state|
@@ -25,8 +25,8 @@ When(/^service "([^"]*)" is (folded|unfolded)$/) do |service_name, state|
 end
 
 Then(/^I should not see "([^"]*)" overview data$/) do |service_name|
-  hits = hits_for_name(service_name)
-  top_traffic = top_traffic_for_name(service_name)
+  hits = hits_for_name(service_name, visible: false)
+  top_traffic = top_traffic_for_name(service_name, visible: false)
 
   assert_not hits.visible?
   assert_not top_traffic.visible?
