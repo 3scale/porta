27455d6349713fdc3531839a83919849354ae182
Guilherme Cassolato <guicassolato@gmail.com>
2019-09-16 14:47:39 +0200

    Fix prepending of backend api config paths to the injected proxy_rules

--- app/decorators/proxy_rule_decorator.rb
+++ app/decorators/proxy_rule_decorator.rb
@@ -5,11 +5,16 @@ class ProxyRuleDecorator < ApplicationDecorator
   self.include_root_in_json = false
 
   def pattern
-    pattern_value = super
+    pattern_value = object.pattern
     backend_api_path ? File.join('/', backend_api_path, pattern_value) : pattern_value
   end
 
   private
+
+  def delegatable?(method)
+    return false if method.to_sym == :pattern
+    object.respond_to?(method)
+  end
 
   def backend_api_path
     context[:backend_api_path]
