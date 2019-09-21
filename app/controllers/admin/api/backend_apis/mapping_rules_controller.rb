e305ff957ff4e2551c52c4b17af16a19e7205951
Marta Noya <mnoyabon@redhat.com>
2019-09-21 10:56:34 +0200

    [Doc] ApiDocs for Mapping Rules under Backend API

--- app/controllers/admin/api/backend_apis/mapping_rules_controller.rb
+++ app/controllers/admin/api/backend_apis/mapping_rules_controller.rb
@@ -4,24 +4,106 @@ class Admin::Api::BackendApis::MappingRulesController < Admin::Api::BackendApis:
   represents :json, entity: ::ProxyRuleRepresenter::JSON, collection: ::ProxyRulesRepresenter::JSON
   wrap_parameters ProxyRule, name: :mapping_rule
 
+  ##~ sapi = source2swagger.namespace("Account Management API")
+  ##~ e = sapi.apis.add
+  ##~ e.path = "/admin/api/backend_apis/{backend_api_id}/mapping_rules.json"
+  ##~ e.responseClass = "List[mapping_rule]"
+  #
+  ##~ op            = e.operations.add
+  ##~ op.httpMethod = "GET"
+  ##~ op.summary    = "Backend API Mapping Rules List"
+  ##~ op.description = "Returns the Mapping Rules of a Backend API."
+  ##~ op.group = "mapping_rule"
+  #
+  ##~ op.parameters.add @parameter_access_token
+  ##~ op.parameters.add @parameter_backend_api_id_by_id_name
+  #
   def index
     respond_with(backend_api.mapping_rules)
   end
 
+  ##~ e = sapi.apis.add
+  ##~ e.path = "/admin/api/backend_apis/{backend_api_id}/mapping_rules/{id}.json"
+  ##~ e.responseClass = "mapping_rule"
+  #
+  ##~ op            = e.operations.add
+  ##~ op.httpMethod = "GET"
+  ##~ op.summary    = "Backend API Mapping Rules Read"
+  ##~ op.description = "Returns the Mapping Rule of a Backend API."
+  ##~ op.group = "mapping_rule"
+  #
+  ##~ op.parameters.add @parameter_access_token
+  ##~ op.parameters.add @parameter_backend_api_id_by_id_name
+  ##~ op.parameters.add @parameter_mapping_rule_id_by_id
+  #
   def show
     respond_with(mapping_rule)
   end
 
+  ##~ e = sapi.apis.add
+  ##~ e.path = "/admin/api/backend_apis/{backend_api_id}/mapping_rules.json"
+  ##~ e.responseClass = "mapping_rule"
+  #
+  ##~ op            = e.operations.add
+  ##~ op.httpMethod = "POST"
+  ##~ op.summary    = "Backend API Mapping Rule Create"
+  ##~ op.description = "Creates a Mapping Rule of a Backend API."
+  ##~ op.group = "mapping_rule"
+  #
+  ##~ op.parameters.add @parameter_access_token
+  ##~ op.parameters.add @parameter_backend_api_id_by_id_name
+  ##~ op.parameters.add name: "http_method", description: "HTTP method.", dataType: "string", paramType: "query", required: true
+  ##~ op.parameters.add name: "pattern", description: "Mapping Rule pattern.", dataType: "string", paramType: "query", required: true
+  ##~ op.parameters.add name: "delta", description: "Increase the metric by this delta.", dataType: "int", paramType: "query", required: true
+  ##~ op.parameters.add name: "metric_id", description: "Metric ID.", dataType: "int", paramType: "query", required: true, threescale_name: "metric_ids"
+  ##~ op.parameters.add name: "position", description: "Mapping Rule position", dataType: "int", paramType: "query"
+  ##~ op.parameters.add name: "last", description: "Last matched Mapping Rule to process", dataType: "bool", paramType: "query"
+  #
   def create
     mapping_rule = backend_api.mapping_rules.create(mapping_rule_params)
     respond_with(mapping_rule)
   end
 
+  ##~ e = sapi.apis.add
+  ##~ e.path = "/admin/api/backend_apis/{backend_api_id}/mapping_rules/{id}.json"
+  ##~ e.responseClass = "mapping_rule"
+  #
+  ##~ op            = e.operations.add
+  ##~ op.httpMethod = "PUT"
+  ##~ op.summary    = "Backend API Mapping Rule Update"
+  ##~ op.description = "Updates a Mapping Rule of a Backend API."
+  ##~ op.group = "mapping_rule"
+  #
+  ##~ op.parameters.add @parameter_access_token
+  ##~ op.parameters.add @parameter_backend_api_id_by_id_name
+  ##~ op.parameters.add @parameter_mapping_rule_id_by_id
+  ##~ op.parameters.add name: "http_method", description: "HTTP method.", dataType: "string", paramType: "query"
+  ##~ op.parameters.add name: "pattern", description: "Mapping Rule pattern.", dataType: "string", paramType: "query"
+  ##~ op.parameters.add name: "delta", description: "Increase the metric by this delta.", dataType: "int", paramType: "query"
+  ##~ op.parameters.add name: "metric_id", description: "Metric ID.", dataType: "int", paramType: "query", threescale_name: "metric_ids"
+  ##~ op.parameters.add name: "position", description: "Mapping Rule position", dataType: "int", paramType: "query"
+  ##~ op.parameters.add name: "last", description: "Last matched Mapping Rule to process", dataType: "bool", paramType: "query"
+  #
   def update
     mapping_rule.update(mapping_rule_params)
     respond_with(mapping_rule)
   end
 
+  ##~ e = sapi.apis.add
+  ##~ e.path = "/admin/api/backend_apis/{backend_api_id}/mapping_rules/{id}.json"
+  ##~ e.responseClass = "mapping_rule"
+  #
+  ##~ op            = e.operations.add
+  ##~ op.httpMethod = "DELETE"
+  ##~ op.summary    = "Backend API Mapping Rule Delete"
+  ##~ op.description = "Deletes a Mapping Rule of a Backend API."
+
+  ##~ op.group = "mapping_rule"
+  #
+  ##~ op.parameters.add @parameter_access_token
+  ##~ op.parameters.add @parameter_backend_api_id_by_id_name
+  ##~ op.parameters.add @parameter_mapping_rule_id_by_id
+  #
   def destroy
     mapping_rule.destroy
     respond_with(mapping_rule)
