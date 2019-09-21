52d2627eb34380eaeaab081fcbac8ddca2996429
Marta Noya <mnoyabon@redhat.com>
2019-09-21 17:19:19 +0200

    Migration with default

--- db/migrate/20190904144157_add_state_to_backend_apis.rb
+++ db/migrate/20190904144157_add_state_to_backend_apis.rb
@@ -2,7 +2,7 @@ class AddStateToBackendApis < ActiveRecord::Migration
   disable_ddl_transaction! if System::Database.postgres?
 
   def change
-    add_column :backend_apis, :state, :string
+    safety_assured { add_column :backend_apis, :state, :string, default: :published, null: false }
     add_index :backend_apis, :state, System::Database.postgres? ? { algorithm: :concurrently } : {}
   end
 end
