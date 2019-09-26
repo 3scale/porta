d42cd4eaf618498b504cd329d6db9b7fd408792b
Marta Noya <mnoyabon@redhat.com>
2019-09-26 09:18:12 +0200

    [Refactor + Bugfix] Use simple lambda and correct param

--- app/models/backend_api.rb
+++ app/models/backend_api.rb
@@ -34,11 +34,11 @@ class BackendApi < ApplicationRecord
 
   scope :orphans, -> { where.has { id.not_in(BackendApiConfig.selecting { :backend_api_id }) } }
 
-  scope :not_used_by, -> do |service_id|
+  scope :not_used_by, ->(service_id) {
     where.has do
       not_exists BackendApiConfig.by_service(service_id).by_backend_api(BabySqueel[:backend_apis].id).select(:id)
     end
-  end
+  }
 
   scope :oldest_first, -> { order(created_at: :asc) }
   scope :accessible, -> { where.not(state: DELETED_STATE) }
