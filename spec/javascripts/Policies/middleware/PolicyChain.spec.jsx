fbfdd6c9e554a900473fc2f13b3447869fea3807
Jose Miguel Gallas <josemigallas@gmail.com>
2019-09-27 16:24:32 +0200

    fixes tests

--- spec/javascripts/Policies/middleware/PolicyChain.spec.jsx
+++ spec/javascripts/Policies/middleware/PolicyChain.spec.jsx
@@ -31,6 +31,11 @@ const create = () => {
 
 describe('PolicyChain Middleware', () => {
   const { invoke, store, next } = create()
+
+  beforeEach(() => {
+    store.dispatch.mockClear()
+  })
+
   it('Passes through non of middleware actions', () => {
     const action = {type: 'TEST'}
     invoke(action)
@@ -38,14 +43,17 @@ describe('PolicyChain Middleware', () => {
     expect(next).toHaveBeenCalledWith(action)
   })
 
-  it('Dispatches LOAD_CHAIN_SUCCESS action', () => {
+  it('Dispatches SET_ORIGINAL_POLICY_CHAIN and LOAD_CHAIN_SUCCESS action', () => {
     invoke({
       type: 'LOAD_CHAIN',
       storedChain: [{name: 'echo', version: 'builtin', configuration: {config: 'bond'}, enabled: true}]
     })
 
-    expect(store.dispatch.mock.calls[0][0].type).toBe('LOAD_CHAIN_SUCCESS')
+    expect(store.dispatch.mock.calls[0][0].type).toBe('SET_ORIGINAL_POLICY_CHAIN')
     expect(store.dispatch.mock.calls[0][0].payload[0].data).toEqual({config: 'bond'})
+
+    expect(store.dispatch.mock.calls[1][0].type).toBe('LOAD_CHAIN_SUCCESS')
+    expect(store.dispatch.mock.calls[1][0].payload[0].data).toEqual({config: 'bond'})
   })
 
   it('Dispatches LOAD_CHAIN_ERROR action', () => {
@@ -108,5 +116,23 @@ describe('PolicyChain Middleware', () => {
         uuid: '007'
       }]
     ))
+  })
+
+  it('Dispatches ENABLE_SUBMIT_BUTTON action when updating policies', () => {
+    invoke({
+      type: 'UPDATE_POLICY_IN_CHAIN',
+      policyConfig: {}
+    })
+
+    expect(store.dispatch.mock.calls[0][0].type).toBe('ENABLE_SUBMIT_BUTTON')
+  })
+
+  it('Dispatches ENABLE_SUBMIT_BUTTON action when removing policies', () => {
+    invoke({
+      type: 'REMOVE_POLICY_FROM_CHAIN',
+      policy: {}
+    })
+
+    expect(store.dispatch.mock.calls[0][0].type).toBe('ENABLE_SUBMIT_BUTTON')
   })
 })
