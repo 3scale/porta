0a77578e53ec02d510e808849ea64dd00a6e6c60
dd di cesare <didi@posteo.net>
2019-09-27 10:50:31 +0200

    [settings] Auth settings behaviour updated
    * When Istio integration is seleected, common settings are disbled
    * Updated template also

--- app/javascript/src/Settings/authentication-widget.jsx
+++ app/javascript/src/Settings/authentication-widget.jsx
@@ -3,6 +3,10 @@ import 'Settings/styles/authentication.scss'
 const AUTH_WRAPPER_ID = 'auth-wrapper'
 const AUTH_METHOD_CLASS = 'auth-method'
 const AUTH_SETS_CLASS = 'auth-settings'
+const INTEGRATION_WRAPPER_ID = 'integration-wrapper'
+const INTEGRATION_METHODS_CLASS = 'integration-method'
+const SERVICE_MESH_ID = 'service_deployment_option_service_mesh_istio'
+const COMMON_SETTINGS_ID = 'common-settings'
 
 const toggleActive = (setting, active) => {
   setting.classList.toggle('hidden', active)
@@ -10,9 +14,12 @@ const toggleActive = (setting, active) => {
 }
 
 export function initialize () {
-  const wrapper = document.getElementById(AUTH_WRAPPER_ID)
-  const [...methods] = wrapper.getElementsByClassName(AUTH_METHOD_CLASS)
-  const [...settings] = wrapper.getElementsByClassName(AUTH_SETS_CLASS)
+  const authWrapper = document.getElementById(AUTH_WRAPPER_ID)
+  const [...methods] = authWrapper.getElementsByClassName(AUTH_METHOD_CLASS)
+  const [...settings] = authWrapper.getElementsByClassName(AUTH_SETS_CLASS)
+  const [...integrations] = document.getElementById(INTEGRATION_WRAPPER_ID).getElementsByClassName(INTEGRATION_METHODS_CLASS)
+  const commonSettings = document.getElementById(COMMON_SETTINGS_ID)
 
   methods.forEach(m => m.addEventListener('click', () => settings.forEach(s => toggleActive(s, s.id !== `${m.id}_settings`))))
+  integrations.forEach(i => i.addEventListener('click', () => toggleActive(commonSettings, i.id === SERVICE_MESH_ID)))
 }
