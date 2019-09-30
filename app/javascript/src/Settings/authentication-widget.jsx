bf6db510af7c1cb0fea6ee2218cb45bb94a0679f
dd di cesare <didi@posteo.net>
2019-09-30 11:50:52 +0700

    [settings] Adapted behaviour for OIDC and MESH integration
    * TODO: THIS NEEDS TO BE A REACT COMPONENT.

--- app/javascript/src/Settings/authentication-widget.jsx
+++ app/javascript/src/Settings/authentication-widget.jsx
@@ -1,14 +1,16 @@
-// TODO: Create React module for Integration Settings.
+// TODO: Create React module for Integration Settings. Please.
 
 import 'Settings/styles/authentication.scss'
 
 const AUTH_WRAPPER_ID = 'auth-wrapper'
 const AUTH_METHOD_CLASS = 'auth-method'
 const AUTH_SETS_CLASS = 'auth-settings'
+const AUTH_SETS_WRP_ID = 'auth-settings-wrapper'
 const INTEGRATION_WRAPPER_ID = 'integration-wrapper'
 const INTEGRATION_METHODS_CLASS = 'integration-method'
 const SERVICE_MESH_ID = 'service_deployment_option_service_mesh_istio'
 const APICAST_SETTINGS_CLASS = 'apicast-only-settings'
+const OIDC_ID = 'service_proxy_authentication_method_oidc'
 
 const toggleActive = (setting, active) => {
   setting.classList.toggle('hidden', active)
@@ -17,11 +19,20 @@ const toggleActive = (setting, active) => {
 
 export function initialize () {
   const authWrapper = document.getElementById(AUTH_WRAPPER_ID)
+  const authSettingsWrapper = document.getElementById(AUTH_SETS_WRP_ID)
   const [...methods] = authWrapper.getElementsByClassName(AUTH_METHOD_CLASS)
   const [...settings] = authWrapper.getElementsByClassName(AUTH_SETS_CLASS)
   const [...integrations] = document.getElementById(INTEGRATION_WRAPPER_ID).getElementsByClassName(INTEGRATION_METHODS_CLASS)
   const [...apicastSettings] = document.getElementsByClassName(APICAST_SETTINGS_CLASS)
+  const oidc = document.getElementById(OIDC_ID)
+  const serviceMesh = document.getElementById(SERVICE_MESH_ID)
 
-  methods.forEach(m => m.addEventListener('click', () => settings.forEach(s => toggleActive(s, s.id !== `${m.id}_settings`))))
-  integrations.forEach(i => i.addEventListener('click', () => apicastSettings.forEach(s => toggleActive(s, i.id === SERVICE_MESH_ID))))
+  methods.forEach(m => m.addEventListener('click', () => {
+    toggleActive(authSettingsWrapper, serviceMesh.checked && !oidc.checked)
+    settings.forEach(s => toggleActive(s, s.id !== `${m.id}_settings`))
+  }))
+  integrations.forEach(i => i.addEventListener('click', () => {
+    apicastSettings.forEach(s => toggleActive(s, i.id === SERVICE_MESH_ID))
+    toggleActive(authSettingsWrapper, i.id === SERVICE_MESH_ID && !oidc.checked)
+  }))
 }
