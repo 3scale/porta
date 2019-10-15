bc54b366de78d7d1d4f9546a63a1b9b6cfe685d6
Marta Noya <mnoyabon@redhat.com>
2019-10-15 17:44:51 +0200

    Remove API CRUD services/:service_id/backend_apis

--- app/representers/backend_api_configs_representer.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class BackendApiConfigsRepresenter < ThreeScale::CollectionRepresenter
-  include Roar::JSON::Collection
-  items extend: BackendApiConfigRepresenter
-end
