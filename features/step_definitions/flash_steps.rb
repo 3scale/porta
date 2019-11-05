e0d28212f10e1964e2b94d0c8e5e4ea0c9d7bf42
dd di cesare <didi@posteo.net>
2019-11-05 15:17:56 +0700

    [cuke_steps] Custom step for new flash messages

--- features/step_definitions/flash_steps.rb
+++ features/step_definitions/flash_steps.rb
@@ -1,3 +1,3 @@
 Then /^I should see the flash message "([^"]*)"$/ do |flash_text|
-  find(:xpath, "//div[@id='flash-messages']").text.should have_content(flash_text)
+  find(:xpath, "//div[@id='flash-messages'] | //div[@id='flashWrapper']").text.should have_content(flash_text)
 end
