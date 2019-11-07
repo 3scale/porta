8bb764576191c9fb72ef2078394e34978069dfc0
Marta Noya <mnoyabon@redhat.com>
2019-11-07 14:04:30 +0100

    Worker executed daily that calls to Segment for those newly deleted users

---
+++ app/workers/segment_delete_users_worker.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class SegmentDeleteUsersWorker < ApplicationJob
+
+  queue_as :low
+
+  # :reek:FeatureEnvy can be ignored here for the 'response'
+  def perform
+    return unless Features::SegmentDeletionConfig.enabled?
+    config = Features::SegmentDeletionConfig.config
+    DeletedObject.users.select(:id, :object_id).order(:id).find_in_batches(batch_size: config.request_size) do |records|
+      Segment::DeleteUsersService.call(records.map(&:object_id))
+      records.each(&DeleteObjectHierarchyWorker.method(:perform_later))
+      sleep(config.wait_time)
+    end
+  rescue Segment::ClientError, Segment::UnexpectedResponseError => error
+    response = error.response
+    System::ErrorReporting.report_error(error, parameters: {response: {status: response.status, body: response.body}})
+  end
+
+end
