02f0edb0398a2a485e3ffdf8e134b26be9f182d2
dd di cesare <didi@posteo.net>
2019-11-07 15:58:42 +0700

    [new service] Updated manual form component to handle values and errors

--- app/javascript/src/NewService/types/index.js
+++ app/javascript/src/NewService/types/index.js
@@ -2,6 +2,19 @@
 
 import * as React from 'react'
 
+export type ServiceFormTemplate = {
+  service: {
+    name: string,
+    system_name: string,
+    description: string
+  },
+  errors: {
+    name?: string[],
+    system_name?: string[],
+    description?: string[]
+  }
+}
+
 export type FormProps = {
   id: string,
   formActionPath: string,
