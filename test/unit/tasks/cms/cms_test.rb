a3a36ca21c39fa5539ecc001cdf2aea9cd9406f3
Carlos Ribeiro <mail@carlosribeiro.me>
2019-11-13 10:16:24 -0300

    Do not use inline module in Tasks unit test
    This commit adds all tests from Tasks unit tests inside a module instead
    of using inline modules.

--- test/unit/tasks/cms/cms_test.rb
+++ test/unit/tasks/cms/cms_test.rb
@@ -1,23 +1,25 @@
 require 'test_helper'
 
-class Tasks::Cms::CmsTest < ActiveSupport::TestCase
+module Tasks
+  class CmsTest < ActiveSupport::TestCase
 
-  def setup
-    @page = FactoryBot.build(:cms_partial, system_name: 'shared/pagination',
-      published: '<p>Title</p><a href="{{ part.link }}">Supertramp</a>')
+    def setup
+      @page = FactoryBot.build(:cms_partial, system_name: 'shared/pagination',
+        published: '<p>Title</p><a href="{{ part.link }}">Supertramp</a>')
 
-    @page.save(validate: false)
-  end
+      @page.save(validate: false)
+    end
 
-  def test_fix_pagination_href
-    assert_equal true,  @page.published.include?('{{ part.link }}')
-    assert_equal false, @page.published.include?('{{ part.url }}')
+    def test_fix_pagination_href
+      assert_equal true,  @page.published.include?('{{ part.link }}')
+      assert_equal false, @page.published.include?('{{ part.url }}')
 
-    execute_rake_task 'cms/cms.rake', 'cms:fix:pagination_href'
+      execute_rake_task 'cms/cms.rake', 'cms:fix:pagination_href'
 
-    @page.reload
+      @page.reload
 
-    assert_equal true,  @page.published.include?('{{ part.url }}')
-    assert_equal false, @page.published.include?('{{ part.link }}')
+      assert_equal true,  @page.published.include?('{{ part.url }}')
+      assert_equal false, @page.published.include?('{{ part.link }}')
+    end
   end
 end
--- test/unit/tasks/impersonation_admin_user_test.rb
+++ test/unit/tasks/impersonation_admin_user_test.rb
@@ -2,24 +2,26 @@
 
 require 'test_helper'
 
-class Tasks::ImpersonationAdminUserTest < ActiveSupport::TestCase
-  setup do
-    3.times do
-      FactoryBot.create(:active_admin,
-                            username: '3scaleadmin',
-                            account: FactoryBot.create(:simple_provider, provider_account: master_account)
-                          )
+module Tasks
+  class ImpersonationAdminUserTest < ActiveSupport::TestCase
+    setup do
+      3.times do
+        FactoryBot.create(:active_admin,
+                              username: '3scaleadmin',
+                              account: FactoryBot.create(:simple_provider, provider_account: master_account)
+                            )
+      end
+      FactoryBot.create_list(:active_admin, 2)
     end
-    FactoryBot.create_list(:active_admin, 2)
-  end
 
-  test 'update' do
-    execute_rake_task 'impersonation_admin_user.rake', 'impersonation_admin_user:update', 'example-username', 'domain.example.com'
+    test 'update' do
+      execute_rake_task 'impersonation_admin_user.rake', 'impersonation_admin_user:update', 'example-username', 'domain.example.com'
 
-    users = User.where(username: 'example-username')
-    assert_equal 0, User.where(username: '3scaleadmin').count
-    assert_equal 3, users.count
-    assert_equal 3, User.where('username <> \'3scaleadmin\'').where('username <> \'example-username\'').count
-    users.each { |user| assert_match /\Aexample\-username\+#{user.account.self_domain}@domain\.example\.com\z/, user.email }
+      users = User.where(username: 'example-username')
+      assert_equal 0, User.where(username: '3scaleadmin').count
+      assert_equal 3, users.count
+      assert_equal 3, User.where('username <> \'3scaleadmin\'').where('username <> \'example-username\'').count
+      users.each { |user| assert_match /\Aexample\-username\+#{user.account.self_domain}@domain\.example\.com\z/, user.email }
+    end
   end
 end
