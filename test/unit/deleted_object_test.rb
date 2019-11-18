7074fd68411b6bc4f43424ec27cd589fc00f226f
Marta Noya <mnoyabon@redhat.com>
2019-11-18 16:04:16 +0100

    [Refactor] Test DeletedObject#stale using Timecop

--- test/unit/deleted_object_test.rb
+++ test/unit/deleted_object_test.rb
@@ -35,27 +35,35 @@ class DeletedObjectTest < ActiveSupport::TestCase
   end
 
   test 'stale' do
-    service = FactoryBot.create(:simple_service)
-    metric = FactoryBot.create(:metric, service: service)
-    deleted_child_but_owner_persisted_old = DeletedObject.create(object: metric, owner: service, created_at: (1.week + 1.day).ago).id
+    deleted_child_but_owner_persisted_old = deleted_child_and_owner_service_deleted_old = deleted_object_service_but_owner_persisted_old = nil
+    Timecop.freeze((1.week + 1.day).ago) do
+      service = FactoryBot.create(:simple_service)
+      metric = FactoryBot.create(:metric, service: service)
+      deleted_child_but_owner_persisted_old = DeletedObject.create(object: metric, owner: service).id
 
-    service = FactoryBot.create(:simple_service)
-    account = service.account
-    metric = FactoryBot.create(:metric, service: service)
-    deleted_child_and_owner_service_deleted_old = DeletedObject.create(object: metric, owner: service, created_at: (1.week + 1.day).ago).id
-    deleted_object_service_but_owner_persisted_old = DeletedObject.create(object: service, owner: account, created_at: (1.week + 1.day).ago).id
+      service = FactoryBot.create(:simple_service)
+      account = service.account
+      metric = FactoryBot.create(:metric, service: service)
+      deleted_child_and_owner_service_deleted_old = DeletedObject.create(object: metric, owner: service).id
+      deleted_object_service_but_owner_persisted_old = DeletedObject.create(object: service, owner: account).id
+    end
 
-    service = FactoryBot.create(:simple_service)
-    metric = FactoryBot.create(:metric, service: service)
-    deleted_child_but_owner_persisted_recent = DeletedObject.create(object: metric, owner: service, created_at: (1.week - 1.day).ago).id
+    deleted_child_but_owner_persisted_recent = deleted_child_and_owner_service_deleted_recent = deleted_child_and_owner_account_deleted_recent = nil
+    deleted_object_service_but_owner_persisted_recent = deleted_object_account_but_owner_persisted_recent = nil
+    Timecop.freeze((1.week - 1.day).ago) do
+      service = FactoryBot.create(:simple_service)
+      metric = FactoryBot.create(:metric, service: service)
+      deleted_child_but_owner_persisted_recent = DeletedObject.create(object: metric, owner: service).id
 
-    service = FactoryBot.create(:simple_service)
-    account = service.account
-    metric = FactoryBot.create(:metric, service: service)
-    deleted_child_and_owner_service_deleted_recent = DeletedObject.create(object: metric, owner: service, created_at: (1.week - 1.day).ago).id
-    deleted_child_and_owner_account_deleted_recent = DeletedObject.create(object: account.admin_user, owner: account, created_at: (1.week - 1.day).ago).id
-    deleted_object_service_but_owner_persisted_recent = DeletedObject.create(object: service, owner: account, created_at: (1.week - 1.day).ago).id
-    deleted_object_account_but_owner_persisted_recent = DeletedObject.create(object: account, owner: master_account, created_at: (1.week - 1.day).ago).id
+      service = FactoryBot.create(:simple_service)
+      account = service.account
+      metric = FactoryBot.create(:metric, service: service)
+      deleted_child_and_owner_service_deleted_recent = DeletedObject.create(object: metric, owner: service).id
+      deleted_child_and_owner_account_deleted_recent = DeletedObject.create(object: account.admin_user, owner: account).id
+      deleted_object_service_but_owner_persisted_recent = DeletedObject.create(object: service, owner: account).id
+      deleted_object_account_but_owner_persisted_recent = DeletedObject.create(object: account, owner: master_account).id
+    end
+
 
     stale = DeletedObject.stale.pluck(:id)
     assert_includes stale, deleted_child_and_owner_service_deleted_old
