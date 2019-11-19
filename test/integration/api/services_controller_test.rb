adb669c3526ae0c75840abb61f0f4cbdb603811d
Marta Noya <mnoyabon@redhat.com>
2019-11-19 16:03:39 +0100

    Automatically fetch model error message when fails to create service

--- test/integration/api/services_controller_test.rb
+++ test/integration/api/services_controller_test.rb
@@ -174,7 +174,7 @@ class Api::ServicesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  class BackendApiCreation < Api::ServicesControllerTest
+  class BackendApiCreationTest < Api::ServicesControllerTest
     def setup
       super
       Logic::RollingUpdates.stubs(enabled?: true)
@@ -208,6 +208,17 @@ class Api::ServicesControllerTest < ActionDispatch::IntegrationTest
       end
 
       assert_equal 1, Service.last.backend_api_configs.count
+    end
+  end
+
+  class ServiceCreateTest < Api::ServicesControllerTest
+    test 'create error shows the right flash message' do
+      post admin_services_path, service: { name: '' }
+      assert_equal 'Couldn\'t create Product. Check your Plan limits', flash[:error]
+
+      @provider.settings.allow_multiple_services!
+      post admin_services_path, service: { name: '' }
+      assert_equal 'Name can\'t be blank', flash[:error]
     end
   end
 end
