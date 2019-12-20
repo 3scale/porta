d9276c8c858629ece9eb6dfdee0a3b6468afe60e
Carlos Ribeiro <mail@carlosribeiro.me>
2019-12-20 15:22:07 -0300

    Improve rubocop issues for system_name

--- app/lib/system_name.rb
+++ app/lib/system_name.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module SystemName
 
   extend ActiveSupport::Concern
@@ -10,11 +12,11 @@ module SystemName
   module ClassMethods
 
     def validates_system_name(opts = {})
-      validates :system_name, presence: true, format: { with: /\A\w[\w\-\/_]+\z/, allow_nil: true }
+      validates :system_name, presence: true, format: { with: %r{\A\w[\w\-/_]+\z}, allow_nil: true }
 
-      if opts[:uniqueness_scope]
-        validates :system_name, uniqueness: opts[:uniqueness_scope] == true ? true : { scope: opts[:uniqueness_scope] }
-      end
+      return unless opts[:uniqueness_scope]
+
+      validates :system_name, uniqueness: opts[:uniqueness_scope] == true ? true : { scope: opts[:uniqueness_scope] }
     end
 
     def has_system_name(opts = {})
@@ -26,37 +28,38 @@ module SystemName
 
   protected
 
-    def system_human_name
-      send(self._system_human_name)
-    end
+  def system_human_name
+    send(_system_human_name)
+  end
 
-    # Generates system_name from 'system_human_name'
-    def generate_system_name
-      generate_system_name! if self.system_name.blank?
-    end
+  # Generates system_name from 'system_human_name'
+  def generate_system_name
+    generate_system_name! if system_name.blank?
+  end
 
-    def generate_system_name!
-      if self.system_human_name.present?
-        slug = self.system_human_name.parameterize.underscore
-        self.system_name = slug.blank? ? internal_system_name : slug
-      else
-        # TODO: remove this branch when Feature#name validation is added
-        # and this ticket resolved https://github.com/3scale/system/issues/908
-        if self.is_a?(Feature)
-          msg = "Deprecated API with missing feature name probably called."
-          Rails.logger.warn msg
-        end
-
-        self.system_name = internal_system_name
+  def generate_system_name!
+    if system_human_name.present?
+      slug = system_human_name.parameterize.underscore
+      self.system_name = slug.presence || internal_system_name
+    else
+      # TODO: remove this branch when Feature#name validation is added
+      # and this ticket resolved https://github.com/3scale/system/issues/908
+      # See: https://issues.redhat.com/browse/THREESCALE-4179
+      if is_a?(Feature)
+        msg = "Deprecated API with missing feature name probably called."
+        Rails.logger.warn msg
       end
-    end
 
-    # This method is used when we need to generate a system_name
-    # but there is no suitable 'name' (human_name) from which
-    # we can guess a reasonable one.
-    #
-    def internal_system_name
-      model_name = ActiveModel::Name.new(self.class, self.class.parent)
-      "#{model_name.param_key}_#{SecureRandom.hex(6)}"
+      self.system_name = internal_system_name
     end
+  end
+
+  # This method is used when we need to generate a system_name
+  # but there is no suitable 'name' (human_name) from which
+  # we can guess a reasonable one.
+  #
+  def internal_system_name
+    model_name = ActiveModel::Name.new(self.class, self.class.parent)
+    "#{model_name.param_key}_#{SecureRandom.hex(6)}"
+  end
 end
