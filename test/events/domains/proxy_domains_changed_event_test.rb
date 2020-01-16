cb231de6787015aa7933450cf3cc57c0ceb7bae8
Guilherme Cassolato <guicassolato@gmail.com>
2020-01-16 16:54:14 +0100

    Check if proxy domains changed event is still valid on hierarchical deletion

--- test/events/domains/proxy_domains_changed_event_test.rb
+++ test/events/domains/proxy_domains_changed_event_test.rb
@@ -3,12 +3,23 @@
 require 'test_helper'
 
 class Domains::ProxyDomainsChangedEventTest < ActiveSupport::TestCase
+  setup do
+    @proxy = FactoryBot.create(:simple_proxy)
+  end
+
+  attr_reader :proxy
+
   test 'deserialises correctly when the Proxy is deleted' do
-    proxy = FactoryBot.create(:simple_proxy)
     event = Domains::ProxyDomainsChangedEvent.create_and_publish!(proxy)
 
     proxy.delete
 
     assert EventStore::Repository.find_event(event.event_id)
+  end
+
+  test 'invalid is missing provider' do
+    assert Domains::ProxyDomainsChangedEvent.create_and_publish!(proxy)
+    proxy.provider.delete
+    refute Domains::ProxyDomainsChangedEvent.create_and_publish!(proxy)
   end
 end
