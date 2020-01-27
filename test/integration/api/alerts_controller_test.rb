93846da6c919f2b8f5ecedec944d6dc8437675d2
Marta Noya <mnoyabon@redhat.com>
2020-01-27 20:30:06 +0100

    [Bugfix] TypeError: superclass mismatch for class AlertsControllerTest

--- test/integration/api/alerts_controller_test.rb
+++ test/integration/api/alerts_controller_test.rb
@@ -19,4 +19,40 @@ class Api::AlertsControllerTest < ActionDispatch::IntegrationTest
       assert_xpath("//a[contains(@href, '#{admin_service_application_path(cinstance.service, cinstance)}')]", cinstance.name)
     end
   end
+
+  test 'get index should filter by account_id, cinstance_id and fulltext' do
+    buyer1, buyer2 = FactoryBot.create_list(:simple_buyer, 2, provider_account: provider)
+    plan = FactoryBot.create(:simple_application_plan, issuer: provider.default_service)
+    cinstance1, cinstance2 = [buyer1, buyer2].map { |buyer| FactoryBot.create(:simple_cinstance, plan: plan, user_account: buyer) }
+    [cinstance1, cinstance2].each { |cinstance|FactoryBot.create(:limit_alert, account: provider, cinstance: cinstance) }
+
+
+
+    get admin_alerts_path
+    assert_equal 2, assigns(:alerts).count
+
+
+    get admin_alerts_path, account_id: buyer1.id
+
+    alerts = assigns(:alerts)
+    assert_equal 1, alerts.count
+    assert_equal buyer1, alerts.first.cinstance.buyer_account
+
+
+    get admin_alerts_path, cinstance_id: cinstance2.id
+
+    alerts = assigns(:alerts)
+    assert_equal 1, alerts.count
+    assert_equal cinstance2, alerts.first.cinstance
+
+
+
+    Account.expects(:search_ids).with(buyer1.name).returns([buyer1.id])
+
+    get admin_alerts_path, search: {account: { query: buyer1.name} }
+
+    alerts = assigns(:alerts)
+    assert_equal 1, alerts.count
+    assert_equal buyer1, alerts.first.cinstance.buyer_account
+  end
 end
