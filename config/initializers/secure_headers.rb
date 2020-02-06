15271dbc976ddf068ec018b80c69efe358d4d8f0
Carlos Ribeiro <mail@carlosribeiro.me>
2020-02-06 18:25:01 -0300

    Upgrade secure_headers to version 3
    Followed some instructions in
    https://github.com/twitter/secure_headers/blob/v3.9.0/upgrading-to-3-0.md
    
    Version 2 does not have any candidate with the CVE fix.
    Version 3 was a completely rewrite of secure_headers. Now they are a
    middleware instead of a check that we do in the controller.

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,12 +1,15 @@
 ::SecureHeaders::Configuration.default do |config|
-  config.hsts = false
+  config.hsts = "max-age=#{20.years.to_i}; includeSubdomains"
   config.x_frame_options = 'DENY'
   config.x_content_type_options = 'nosniff'
   config.x_xss_protection = '1; mode=block'
 
-  config.csp = false
+  config.csp = {
+    report_only:  true,
+    default_src: %w(https: 'self')
+  }
 
   # not sure what they do, so rather disabling them
-  config.x_download_options = false
-  config.x_permitted_cross_domain_policies = false
+  config.x_download_options = nil
+  config.x_permitted_cross_domain_policies = 'none'
 end
