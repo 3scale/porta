023e53ef891bbeb64e4b3eac1ba248768b4944ca
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2020-02-17 23:29:32 +0100

    refactor
    Co-Authored-By: Marta <Martouta@users.noreply.github.com>

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,7 +4,7 @@ require 'backend_client'
 
 class Service < ApplicationRecord
   # todo: remove after migration 20200211154433_remove_end_user_plans.rb
-  self.ignored_columns = ['end_user_registration_required', 'default_end_user_plan_id']
+  self.ignored_columns = %w[end_user_registration_required default_end_user_plan_id]
 
   include Backend::ModelExtensions::Service
   include Logic::Contracting::Service
