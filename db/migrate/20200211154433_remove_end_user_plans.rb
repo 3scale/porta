6b27953de650be78255b7983d756c6de3cda1bcf
Jose Miguel Gallas <josemigallas@gmail.com>
2020-02-17 12:26:46 +0100

    adds remove end-user migration

---
+++ db/migrate/20200211154433_remove_end_user_plans.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class RemoveEndUserPlans < ActiveRecord::Migration
+  def change
+    safety_assured { remove_column :settings, :end_users_switch }
+    safety_assured { remove_column :settings, :end_user_plans_ui_visible }
+
+    safety_assured { remove_column :cinstances, :end_user_required }
+
+    safety_assured { remove_column :plans, :end_user_required }
+
+    safety_assured { remove_column :services, :end_user_registration_required }
+    safety_assured { remove_column :services, :default_end_user_plan_id }
+
+    drop_table :end_user_plans
+  end
+end
