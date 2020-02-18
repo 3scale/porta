a8fe04f02ab2d328c7645efbcd2732f696b5868e
Hallelujah <hery@rails-royce.org>
2020-02-18 10:35:49 +0300

    Revert "Fix connection error on detection of Redis sentinel"
    This reverts commit bbd673330527d0bfd89a43fed62f5266eedc47c3.
    Because the fix is already in the updated redis gem

--- config/initializers/redis_hacks.rb
+++ config/initializers/redis_hacks.rb
@@ -4,34 +4,36 @@
 # See https://github.com/redis/redis-rb/pull/856
 # We can remove this once we can upgrade the gem to >= v4.1.2
 
-module RedisHacks
-  def sentinel_detect
-    @sentinels.each do |sentinel|
-      client = Redis::Client.new(@options.merge({
-        :host => sentinel[:host],
-        :port => sentinel[:port],
-        :password => sentinel[:password], # https://github.com/redis/redis-rb/pull/856
-        :reconnect_attempts => 0,
-      }))
+class Redis
+  class Client
+    class Connector
+      class Sentinel < Connector
+        def sentinel_detect
+          @sentinels.each do |sentinel|
+            client = Client.new(@options.merge({
+              :host => sentinel[:host],
+              :port => sentinel[:port],
+              :password => sentinel[:password], # https://github.com/redis/redis-rb/pull/856
+              :reconnect_attempts => 0,
+            }))
 
-      begin
-        if result = yield(client)
-          # This sentinel responded. Make sure we ask it first next time.
-          @sentinels.delete(sentinel)
-          @sentinels.unshift(sentinel)
+            begin
+              if result = yield(client)
+                # This sentinel responded. Make sure we ask it first next time.
+                @sentinels.delete(sentinel)
+                @sentinels.unshift(sentinel)
 
-          return result
+                return result
+              end
+            rescue BaseConnectionError
+            ensure
+              client.disconnect
+            end
+          end
+
+          raise CannotConnectError, "No sentinels available."
         end
-      rescue Redis::BaseConnectionError
-      rescue RuntimeError => exception
-        raise unless exception.message =~ /Name or service not known/
-      ensure
-        client.disconnect
       end
     end
-
-    raise CannotConnectError, "No sentinels available."
   end
 end
-
-Redis::Client::Connector::Sentinel.prepend(RedisHacks)
