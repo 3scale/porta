ccda362b1e5ab78c7092fdaf6ca3b00591b995be
Carlos Ribeiro <mail@carlosribeiro.me>
2020-02-18 14:47:54 -0300

    Move proxy_rule_uri test to integrations_helper_test

--- test/unit/helpers/api/integrations_helper_test.rb
+++ test/unit/helpers/api/integrations_helper_test.rb
@@ -230,4 +230,16 @@ class Api::IntegrationsHelperTest < ActionView::TestCase
       refute(is_https?(nil))
     end
   end
+
+  class ProxyRuleUri < self
+    test 'print the proper preview' do
+      pattern = '/foo'
+      backend_api = FactoryBot.create(:backend_api)
+      FactoryBot.create(:proxy_rule, owner: backend_api, proxy: nil, pattern: pattern)
+      backend_api_config = FactoryBot.create(:backend_api_config)
+      backend_api_config.stubs(:path).returns('/')
+
+      assert_equal pattern, proxy_rule_uri(backend_api_config.path, backend_api.proxy_rules.last)
+    end
+  end
 end
