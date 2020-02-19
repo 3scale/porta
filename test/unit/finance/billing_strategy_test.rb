cc9da694192b4686ba6d5809de7e3a5b6429b2dd
Guilherme Cassolato <guicassolato@gmail.com>
2020-02-19 13:57:54 +0100

    Fix random failures of billing strategy tests

--- test/unit/finance/billing_strategy_test.rb
+++ test/unit/finance/billing_strategy_test.rb
@@ -90,36 +90,42 @@ class Finance::BillingStrategyTest < ActiveSupport::TestCase
   end
 
   test 'self.daily with providers excluded' do
-    2.times { FactoryBot.create(:prepaid_billing, :account => FactoryBot.create(:simple_provider)) }
-    2.times { FactoryBot.create(:postpaid_billing, :account => FactoryBot.create(:simple_provider)) }
+    other_providers = FactoryBot.create_list(:simple_provider, 4)
+
+    (0..1).each { |i| FactoryBot.create(:prepaid_billing, account: other_providers[i]) }
+    (2..3).each { |i| FactoryBot.create(:postpaid_billing, account: other_providers[i]) }
 
     results = Finance::BillingStrategy.daily(exclude: [ @provider.id ])
 
-    # 5 == master + 4
-    assert_equal 5, results.providers_count
+    other_providers.each { |provider| assert results[provider.id] }
+    refute results[@provider.id]
+
     assert results.successful?
   end
 
   test 'self.daily only with selected providers' do
-    3.times { FactoryBot.create(:prepaid_billing, :account => FactoryBot.create(:simple_provider)) }
+    other_providers = FactoryBot.create_list(:simple_provider, 3)
+    3.times { |i| FactoryBot.create(:prepaid_billing, account: other_providers[i]) }
 
     results = Finance::BillingStrategy.daily(only: [ @provider.id ])
 
-    assert_equal 1, results.providers_count
+    other_providers.each { |provider| refute results[provider.id] }
+    assert results[@provider.id]
+
     assert results.successful?
   end
 
   test 'self.daily skips suspended accounts' do
     Finance::BillingStrategy.delete_all # don't really want the ones from setup
 
-    1.times { FactoryBot.create(:prepaid_billing, :account => FactoryBot.create(:simple_provider, state: 'suspended')) }
-    1.times { FactoryBot.create(:prepaid_billing, :account => FactoryBot.create(:simple_provider, state: 'approved')) }
+    suspended_provider = FactoryBot.create(:simple_provider, state: 'suspended')
+    FactoryBot.create(:prepaid_billing, account: suspended_provider)
 
     results = Finance::BillingStrategy.daily
 
-    assert_equal 2, results.providers_count
+    assert results.skipped[suspended_provider.id]
+
     assert results.successful?
-    assert_equal 1, results.skipped.size
   end
 
   test 'currency cache returns string' do
