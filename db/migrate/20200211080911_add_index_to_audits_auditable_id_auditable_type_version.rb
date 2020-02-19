f778a8513f3eaf83b0a8bbf6757e745f8f3360c4
Guilherme Cassolato <guicassolato@gmail.com>
2020-02-19 12:33:18 +0100

    Index options for postgres

--- db/migrate/20200211080911_add_index_to_audits_auditable_id_auditable_type_version.rb
+++ db/migrate/20200211080911_add_index_to_audits_auditable_id_auditable_type_version.rb
@@ -1,5 +1,8 @@
 class AddIndexToAuditsAuditableIdAuditableTypeVersion < ActiveRecord::Migration
+  disable_ddl_transaction! if System::Database.postgres?
+
   def change
-    add_index :audits, [:auditable_id, :auditable_type, :version]
+    index_options = System::Database.postgres? ? { algorithm: :concurrently } : {}
+    add_index :audits, [:auditable_id, :auditable_type, :version], index_options
   end
 end
