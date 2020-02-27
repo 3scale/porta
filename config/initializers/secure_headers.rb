519e8d18b96d5a823a030ab3e0847a9f1593d576
Carlos Ribeiro <mail@carlosribeiro.me>
2020-02-27 10:56:45 -0300

    Disable CSP

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -4,11 +4,7 @@
   config.x_content_type_options = 'nosniff'
   config.x_xss_protection = '1; mode=block'
 
-  config.csp = {
-    report_only:  true,
-    default_src: %w(https: 'self')
-  }
-
+  config.csp = SecureHeaders::OPT_OUT
   # not sure what they do, so rather disabling them
   config.x_download_options = SecureHeaders::OPT_OUT
   config.x_permitted_cross_domain_policies = 'none'
