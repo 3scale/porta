da02b4ad545bc2d94437be47db4c2c1b80d8ced6
Marta Noya <mnoyabon@redhat.com>
2020-03-03 18:30:39 +0100

    Do not issue ProxyConfigs::AffectingObjectChangedEvent when account is scheduled_for_deletion

--- test/events/proxy_configs/affecting_object_changed_event_test.rb
+++ test/events/proxy_configs/affecting_object_changed_event_test.rb
@@ -12,4 +12,38 @@ class ProxyConfigs::AffectingObjectChangedEventTest < ActiveSupport::TestCase
     assert_equal proxy_rule.id, event.object_id
     assert_equal 'ProxyRule', event.object_type
   end
+
+  test 'create_and_publish! persists when the proxy and the account are persisted and not scheduled_for_deletion' do
+    proxy = FactoryBot.create(:proxy)
+    proxy_rule = FactoryBot.build_stubbed(:proxy_rule, proxy: proxy)
+
+    event = ProxyConfigs::AffectingObjectChangedEvent.create_and_publish!(proxy, proxy_rule)
+
+    assert EventStore::Repository.find_event(event.event_id)
+  end
+
+  test 'create_and_publish! does not persist when the proxy is not present' do
+    proxy_rule = FactoryBot.build_stubbed(:proxy_rule)
+
+    refute ProxyConfigs::AffectingObjectChangedEvent.create_and_publish!(nil, proxy_rule)
+  end
+
+  test 'create_and_publish! does not persist when the account is not persisted' do
+    proxy = FactoryBot.create(:proxy)
+    proxy_rule = FactoryBot.build_stubbed(:proxy_rule, proxy: proxy)
+
+    proxy.account.delete
+
+    refute ProxyConfigs::AffectingObjectChangedEvent.create_and_publish!(proxy.reload, proxy_rule)
+  end
+  test 'create_and_publish! does not persist when the account is scheduled_for_deletion' do
+    proxy = FactoryBot.create(:proxy)
+    proxy_rule = FactoryBot.build_stubbed(:proxy_rule, proxy: proxy)
+
+    proxy.account.schedule_for_deletion!
+
+    refute ProxyConfigs::AffectingObjectChangedEvent.create_and_publish!(proxy, proxy_rule)
+  end
+
+
 end
