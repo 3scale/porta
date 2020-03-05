5e725207ff8d0ade4f497d1cad744dc0db905cf8
Damian Peralta <damianperaltam@gmail.com>
2020-03-05 11:07:25 +0100

    Remove CRA styles

--- portafly/src/index.tsx
+++ portafly/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@patternfly/react-core/dist/styles/base.css'
-import './index.css'
 import { App } from 'root'
 
 ReactDOM.render(<App />, document.getElementById('root'))
