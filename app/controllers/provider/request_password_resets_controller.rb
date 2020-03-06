b293b8d3809e29ead54b01979dcfbd46a9b54c2c
Carlos Ribeiro <mail@carlosribeiro.me>
2020-03-06 16:20:59 -0300

    Create a custom page for Request Password Reset
    Our current request password reset page is the same as login page, only
    adding a custom query parameter and handled the diplay by the frontend.
    This was making hard for us to block access from Mater provider in this
    page.
    
    This commit creates a new page along with a new pack that renders only
    the RequestPasswordForm component. This also removes all code handling
    request password reset from LoginPageWrapper component.

---
+++ app/controllers/provider/request_password_resets_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+class Provider::RequestPasswordResetsController < FrontendController
+  layout 'provider/login'
+
+  skip_before_action :login_required
+
+  before_action :ensure_provider_domain
+  before_action :find_provider
+
+  def new
+    redirect_to provider_admin_dashboard_url if logged_in?
+  end
+
+  def create
+  end
+
+  private
+
+  def find_provider
+    @provider ||= site_account_request.find_provider
+  end
+
+end
