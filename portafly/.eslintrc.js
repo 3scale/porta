98b3e6a4753b6d85d362691038fec042a7149605
Jose Miguel Gallas <josemigallas@gmail.com>
2020-03-10 13:45:29 +0100

    [portafy] adds standard as linter

---
+++ portafly/.eslintrc.js
@@ -0,0 +1,10 @@
+/**
+ * This file should NOT exist. There si however an issue with standard and Typescript that
+ * needs to be worked around: https://standardjs.com/index.html#typescript
+ */
+module.exports = {
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
+  }
+}
