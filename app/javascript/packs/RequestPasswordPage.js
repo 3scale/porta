daa54e6dd5c360f831885b5aae85e4e503f97bc4
Damian Peralta <damianperaltam@gmail.com>
2020-03-10 12:44:32 +0100

    Removing IE11 code

--- app/javascript/packs/RequestPasswordPage.js
+++ app/javascript/packs/RequestPasswordPage.js
@@ -1,21 +1,11 @@
 import '@babel/polyfill'
 import 'core-js/es7/object'
-import { isBrowserIE11 } from 'utilities/ie11Utils'
 
-const isIE11 = isBrowserIE11(window)
-if (isIE11) {
-  // eslint-disable-next-line no-unused-expressions
-  import('LoginPage/assets/styles/ie11-pf4BaseStyles.css')
-}
-
-import { RequestPasswordWrapper } from 'LoginPage'
+import { RequestPasswordWrapper as RequestPassword } from 'LoginPage'
 import { safeFromJsonString } from 'utilities/json-utils'
 
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('pf-request-page-container')
-  if (isIE11) {
-    container.classList.add('isIe11', 'pf-c-page')
-  }
   const requestPageProps = safeFromJsonString(container.dataset.requestProps)
-  RequestPasswordWrapper(requestPageProps, 'pf-request-page-container')
+  RequestPassword(requestPageProps, 'pf-request-page-container')
 })
