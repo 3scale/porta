a3a01d263429a0b6a62874c4f591744a783d8770
Cadu Ribeiro <mail@carlosribeiro.me>
2020-03-12 15:42:29 -0300

    use ruby 1.9 syntax for hashes
    Co-Authored-By: Marta <Martouta@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -164,7 +164,7 @@ without fake Core server your after commit callbacks will crash and you might ge
   get '/auth/:system_name/bounce' => 'provider/sessions#bounce', constraints: ProviderDomainConstraint, as: :authorization_provider_bounce
 
   namespace :provider, path: 'p', constraints: ProviderDomainConstraint do
-    resource :password, :only => %i(new show update destroy) do
+    resource :password, only: %i[new show update destroy] do
       get 'reset'
     end
   end
