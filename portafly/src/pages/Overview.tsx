f43a2a8f5404a16e7de499a67bcb7745561e9144
Damian Peralta <damianperaltam@gmail.com>
2020-03-17 12:40:01 +0100

    Adding date-fns and currency examples

--- portafly/src/pages/Overview.tsx
+++ portafly/src/pages/Overview.tsx
@@ -11,7 +11,12 @@ import {
   Button
 } from '@patternfly/react-core'
 
-import moment from 'moment'
+import { format, formatDistance, formatRelative, subDays } from 'date-fns'
+import {
+  enUS as DATEFNS_LOCALE_EN,
+  ja as DATEFNS_LOCALE_JP
+} from 'date-fns/locale'
+import currency from 'currency.js'
 import { I18nContext } from 'i18n/I18nProvider'
 import { 
   OVERVIEW as STRINGS_ENG,
@@ -26,13 +31,18 @@ const Overview: React.FunctionComponent = ({children}) => {
   const [strings, setStrings] = useState(STRINGS_ENG)
   const [commonStrings, setCommonStrings] = useState(COMMONS_ENG)
   const [locale, setLocale] = useState('en')
+  const [dateFnsLocale, setDateFnsLocale] = useState(DATEFNS_LOCALE_EN)
+  const [currencySymbol, setCurrencySymbol] = useState('$')
   const polyglot = useContext(I18nContext)
   polyglot.locale(locale)  
   polyglot.extend({ ...strings, ...commonStrings })
 
   const onClick = (event: React.SyntheticEvent<HTMLButtonElement>) => {
-    setStrings(event.currentTarget.value === 'en' ? STRINGS_ENG : STRINGS_JAP)
-    setCommonStrings(event.currentTarget.value === 'en' ? COMMONS_ENG : COMMONS_JAP)
+    const isEng = event.currentTarget.value === 'en'
+    setStrings(isEng ? STRINGS_ENG : STRINGS_JAP)
+    setCommonStrings(isEng ? COMMONS_ENG : COMMONS_JAP)
+    setDateFnsLocale(isEng ? DATEFNS_LOCALE_EN : DATEFNS_LOCALE_JP)
+    setCurrencySymbol(isEng ? '$' : '¥')
     setLocale(event.currentTarget.value)
   }
 
@@ -48,7 +58,7 @@ const Overview: React.FunctionComponent = ({children}) => {
           </p>
           <Title size={'3xl'}>{polyglot.t('body_title')}</Title>
           <Text>
-          <b>{polyglot.t('subtitle')}</b>     
+          <b>{ polyglot.t('subtitle') }</b>
           </Text>
         </TextContent>
       </PageSection>
@@ -56,19 +66,29 @@ const Overview: React.FunctionComponent = ({children}) => {
         <Card>
           <CardBody>
             <TextContent>
-              <p>{polyglot.t('greetings')}</p>
+              <p>{ polyglot.t('greetings') }</p>
               <p>
-                {polyglot.t('greetings_with_place', { 
-                  name: 'Juan Doe',
-                  place: 'portafly'
-                })}
+                { polyglot.t('greetings_with_place', { name: 'Juan Doe', place: 'portafly'}) }
               </p>
-              <p>{polyglot.t('dates')}:</p>
-              <p>Date.now(): { polyglot.t('date', { date: Date.now() }) }</p>
+              <p>{ polyglot.t('dates') }:</p>
+              <p><b>JS Date():</b></p>
+              <p>{ polyglot.t('date', { date: Date.now() }) }</p>
               <p>new Date('2015-03-25'): { polyglot.t('date', { date: new Date('2015-03-25') }) }</p>
-              <p>moment(): { polyglot.t('date', { date: moment() }) }</p>
-              <p>moment('1995-12-25'):  { polyglot.t('date', { date: moment('1995-12-25') }) }</p>
-              <p>{polyglot.t('plurals')}:</p>
+              <p><b>date-fns:</b></p>
+              <p> 
+                { polyglot.t(format(new Date(), "eeee, dd MMM yyyy", {locale: dateFnsLocale }) )}
+              </p>
+              <p> 
+                { polyglot.t(formatRelative(subDays(new Date(), 3), new Date(), { locale: dateFnsLocale })) }
+              </p>
+              <p> 
+                { polyglot.t(formatDistance(new Date(2016, 7, 1), new Date(2015, 0, 1), { locale: dateFnsLocale })) }
+              </p>
+              <p><b>Currencies:</b></p>
+              <p>
+                { polyglot.t(currency(1500, { symbol: currencySymbol, precision: 2 }).format(true)) }
+              </p>
+              <p><b>{ polyglot.t('plurals') }:</b></p>
               <ul>
                 <li>{ polyglot.t('count_apple', { smart_count: 0 }) }</li>
                 <li>{ polyglot.t('count_apple', { smart_count: 1 }) }</li>
