91cac4d1101b43cfae3b85239af82f8e6e9126ae
Guilherme Cassolato <guicassolato@gmail.com>
2020-03-18 18:46:53 +0100

    Cannot update metric system_name
    It was enforced already via API, but not via UI. Now the `system_name` form fields in the UI, both for product metrics and backend api metrics, will be disabled whenever the metric is persisted.
    + refactoring of create/update params in the metrics controller aiming some DRY
    + moving a couple functional tests to test/integrations.

---
+++ app/concerns/metric_params.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module MetricParams
+  extend ActiveSupport::Concern
+
+  DEFAULT_PARAMS = %i[friendly_name unit description].freeze
+  private_constant :DEFAULT_PARAMS
+
+  included do
+    private
+
+    def create_params
+      params.fetch(:metric).permit(DEFAULT_PARAMS | %i[system_name])
+    end
+
+    def update_params
+      params.fetch(:metric).permit(DEFAULT_PARAMS)
+    end
+  end
+end
