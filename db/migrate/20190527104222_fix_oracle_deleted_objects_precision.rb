b95fab610ffae4c3649e27f5c432b6acbeff9950
Marta Noya <mnoyabon@redhat.com>
2020-03-27 13:35:51 +0100

    [Bugfix] [Oracle] Patch Upgrade from 2.5 to 2.6. FixOracleDeletedObjectsPrecision with data

--- db/migrate/20190527104222_fix_oracle_deleted_objects_precision.rb
+++ db/migrate/20190527104222_fix_oracle_deleted_objects_precision.rb
@@ -1,8 +1,21 @@
 class FixOracleDeletedObjectsPrecision < ActiveRecord::Migration
-  def change
+  def up
     return unless System::Database.oracle?
+
     # Safety assured because this will run only for on-prem using Oracle at booting time
-    safety_assured { change_column :deleted_objects, :owner_id, :bigint, limit: 8 }
+
+    add_column :deleted_objects, :owner_id_tmp,  :bigint, limit: 8
+    add_column :deleted_objects, :object_id_tmp, :bigint, limit: 8
+
+    DeletedObject.update_all('owner_id_tmp = owner_id, object_id_tmp = object_id')
+    DeletedObject.update_all(owner_id: nil, object_id: nil)
+
+    safety_assured { change_column :deleted_objects, :owner_id,  :bigint, limit: 8 }
     safety_assured { change_column :deleted_objects, :object_id, :bigint, limit: 8 }
+
+    DeletedObject.update_all('owner_id = owner_id_tmp, object_id = object_id_tmp')
+
+    safety_assured { remove_column :deleted_objects, :owner_id_tmp }
+    safety_assured { remove_column :deleted_objects, :object_id_tmp }
   end
 end
