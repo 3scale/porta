78a1ca25fcc75a076393e4392e0460b80020b994
Jose Miguel Gallas <josemigallas@gmail.com>
2020-03-31 12:12:00 +0200

    🎨fixes format

--- portafly/src/i18n/locales/index.ts
+++ portafly/src/i18n/locales/index.ts
@@ -2,7 +2,7 @@ import { ISupportedLanguages } from 'i18n'
 import * as en from 'i18n/locales/en'
 
 export type ITranslationsPages = keyof typeof en
-export type ITranslations = { [P in ITranslationsPages]: any}
+export type ITranslations = { [P in ITranslationsPages]: any }
 
 const Translations: Record<ISupportedLanguages, ITranslations> = {
   en
