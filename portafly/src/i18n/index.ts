ec151d9200f8e32f2504210c055815983bb8d7a9
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2020-04-01 11:04:04 +0200

    Add types and encapsulates translations (#1800)
    adds some types and encapsulates translations files

---
+++ portafly/src/i18n/index.ts
@@ -0,0 +1,4 @@
+export * from './i18n'
+export * from './locales'
+export * from './supported-languages'
+export * from './useTranslation'
--- portafly/src/pages/Overview.tsx
+++ portafly/src/pages/Overview.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'i18n/useTranslation'
 import { useA11yRouteChange, useDocumentTitle } from 'components'
 import {
   PageSection,
