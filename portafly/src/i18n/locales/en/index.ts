6eb302ec2809560c0eca337945071f6cdb986ee2
dd di cesare <didi@posteo.net>
2020-04-02 11:12:45 +0700

    [i18n] Converting JSON files to YAML
    * added TODO to make tsconfig recognize those files

--- portafly/src/i18n/locales/en/index.ts
+++ portafly/src/i18n/locales/en/index.ts
@@ -1,8 +1,9 @@
-import shared from 'i18n/locales/en/shared.json'
-import overview from 'i18n/locales/en/overview.json'
-import analytics from 'i18n/locales/en/analytics.json'
-import applications from 'i18n/locales/en/applications.json'
-import integration from 'i18n/locales/en/integration.json'
+// @ts-nocheck // TODO: make tslint to recognize yml files
+import shared from 'i18n/locales/en/shared.yml'
+import overview from 'i18n/locales/en/overview.yml'
+import analytics from 'i18n/locales/en/analytics.yml'
+import applications from 'i18n/locales/en/applications.yml'
+import integration from 'i18n/locales/en/integration.yml'
 
 export {
   shared,
