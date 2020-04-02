f129f9b7f4d04eb9293fc051d9b198f3a04fc7bd
dd di cesare <didi@posteo.net>
2020-04-02 11:15:07 +0700

    [webpack] Use yaml-loader for yml files

--- portafly/config/webpack.config.js
+++ portafly/config/webpack.config.js
@@ -485,6 +485,13 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+            // ** CUSTOM-PORTAFLY START **
+            {
+              test: /\.ya?ml$/,
+              type: 'json',
+              use: 'yaml-loader'
+            },
+            // ** CUSTOM-PORTAFLY END **
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
@@ -496,7 +503,7 @@ module.exports = function(webpackEnv) {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.ya?ml$/],
               options: {
                 name: 'static/media/[name].[hash:8].[ext]',
               },
