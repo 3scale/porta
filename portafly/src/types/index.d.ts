9296e86771782a3ed291c431a3ef720e2b7312df
dd di cesare <didi@posteo.net>
2020-04-03 15:29:56 +0700

    [types] Interface for DeveloperAccount

--- portafly/src/types/index.d.ts
+++ portafly/src/types/index.d.ts
@@ -1,2 +1,12 @@
 export type ArgumentsType<T extends (...args: any[]) => any> =
   T extends (...args: infer A) => any ? A : never;
+
+export interface IDeveloperAccount {
+  admin_name: string,
+  apps_count: number
+  created_at: string, // TODO: Find a specific date as string type
+  id: number,
+  org_name: string,
+  state: string,
+  updated_at: string,
+}
