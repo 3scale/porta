f073fc9f1098d54ebf1105ecafbaccb2bade2549
Marta Noya <mnoyabon@redhat.com>
2020-04-27 13:30:26 +0200

    Call to Pisoni directly from Worker

--- app/workers/backend_delete_application_worker.rb
+++ app/workers/backend_delete_application_worker.rb
@@ -6,33 +6,16 @@ class BackendDeleteApplicationWorker < ApplicationJob
   def perform(event_id)
     event = EventStore::Repository.find_event!(event_id)
 
-    @application = build_application(event)
+    DeletedObject.application_keys.where(owner_type: Contract, owner_id: event.application.id).order(:id).find_each do |deleted_object|
+      ThreeScale::Core::ApplicationKey.delete(event.service_id.to_s, event.application_id, deleted_object.metadata[:value])
+    end
 
-    [ApplicationKey, ReferrerFilter].each(&method(:delete_backend_associations))
+    DeletedObject.referrer_filters.where(owner_type: Contract, owner_id: event.application.id).order(:id).find_each do |deleted_object|
+      ThreeScale::Core::ApplicationReferrerFilter.delete(event.service_id.to_s, event.application_id, deleted_object.metadata[:value])
+    end
 
-    application.delete_backend_application
+    ThreeScale::Core::Application.delete(event.service_id.to_s, event.application_id)
   rescue ActiveRecord::RecordNotFound => exception
     System::ErrorReporting.report_error(exception, parameters: {event_id: event_id})
-  end
-
-  private
-
-  attr_reader :application
-
-  def build_application(event)
-    app_attrs = {id: event.application.id, application_id: event.application_id}
-    Cinstance.new(app_attrs, without_protection: true).tap do |app|
-      app.service = Service.new({id: event.service_id}, without_protection: true)
-    end
-  end
-
-  def build_association_object(deleted_object)
-    deleted_object.object_instance { |object| object.application = application }
-  end
-
-  def delete_backend_associations(klass)
-    DeletedObject.public_send(klass.to_s.underscore.pluralize).where(owner: application).order(:id).find_each do |deleted_object|
-      build_association_object(deleted_object).destroy_backend_value
-    end
   end
 end
