f0fb7a93677c5cdcd5d8c887da9a426f6d0e3ce4
HemaHG <hhg@redhat.com>
2020-04-28 17:36:16 +0530

    Upadting test
    Signed-off-by: HemaHG <hhg@redhat.com>

--- app/workers/backend_delete_stats_worker.rb
+++ app/workers/backend_delete_stats_worker.rb
@@ -4,7 +4,7 @@ class BackendDeleteStatsWorker
   include Sidekiq::Worker
 
   def perform(event_id)
-    @event = EventStore::Repository.find_event!(event_id)
+    event = EventStore::Repository.find_event!(event_id)
 
     ThreeScale::Core::Service.delete_stats(event.service_id, {})
   end
