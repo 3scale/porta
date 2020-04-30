402b0d05915b6e38f2d9a04dfafd4f5acc50232e
Marta Noya <mnoyabon@redhat.com>
2020-04-30 11:34:49 +0200

    Test only backend apis created in this test to avoid leaks from other tests

--- test/unit/tasks/backend_api_test.rb
+++ test/unit/tasks/backend_api_test.rb
@@ -29,11 +29,11 @@ module Tasks
     test 'create_default_metrics' do
       backend_apis_without_metrics = FactoryBot.create_list(:backend_api, 2)
       backend_apis_without_metrics.each { |backend_api| backend_api.metrics.delete_all }
-      _backend_apis_with_metrics = FactoryBot.create_list(:backend_api, 2)
+      backend_apis_with_metrics = FactoryBot.create_list(:backend_api, 2)
 
       execute_rake_task 'backend_api.rake', 'backend_api:create_default_metrics'
 
-      BackendApi.all.each do |backend_api|
+      (backend_apis_without_metrics + backend_apis_with_metrics).each do |backend_api|
         assert_equal 1, backend_api.metrics.count
         assert_instance_of Metric, backend_api.metrics.top_level.hits
       end
