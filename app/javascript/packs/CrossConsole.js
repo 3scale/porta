3897395c6ae3c9e30836881d7659aa2173e9c051
Christie Molloy <cmolloy@redhat.com>
2020-05-06 16:22:57 -0400

    add the cross console navigation

---
+++ app/javascript/packs/CrossConsole.js
@@ -0,0 +1,6 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     function toggleMenu () {
+//         var contextSelectorMenu = document.getElementsByClassName('pf-c-context-selector__menu');
+//         contextSelectorMenu.classList.toggle('pf-m-expanded');
+//     }
+// })
