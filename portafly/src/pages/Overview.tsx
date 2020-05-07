bebd9cb24b47bc59e03425bc95e15501046c449c
Jose Miguel Gallas <josemigallas@gmail.com>
2020-05-07 13:27:31 +0200

    [portafly] adds a provider for showing toast alerts

--- portafly/src/pages/Overview.tsx
+++ portafly/src/pages/Overview.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { useTranslation } from 'i18n/useTranslation'
-import { useA11yRouteChange, useDocumentTitle } from 'components'
+import { useA11yRouteChange, useDocumentTitle, useAlertsContext } from 'components'
 import {
   PageSection,
   TextContent,
   Title,
   Text,
   Card,
-  CardBody
+  CardBody,
+  Button
 } from '@patternfly/react-core'
+import { BellIcon } from '@patternfly/react-icons'
 
 const Overview: React.FunctionComponent = () => {
   const { t } = useTranslation('overview')
   useA11yRouteChange()
   useDocumentTitle(t('page_title'))
+  const { addAlert } = useAlertsContext()
   return (
     <>
       <PageSection variant="light">
@@ -29,6 +32,16 @@ const Overview: React.FunctionComponent = () => {
           <CardBody>
             <TextContent>
               <p>{t('shared:format.uppercase', { text: 'Ohai' })}</p>
+              {/* This is just for testing, will be removed */}
+              <Button
+                icon={<BellIcon />}
+                onClick={() => {
+                  const id = Date.now().toString()
+                  addAlert({ id, variant: 'info', title: `Test Alert ${id}` })
+                }}
+              >
+                Add an Alert
+              </Button>
             </TextContent>
           </CardBody>
         </Card>
