2879ede370484b2bc965629aaf29240d2f5b6a6e
Marta Noya <mnoyabon@redhat.com>
2020-05-08 11:40:29 +0200

    Backend Service Objects for deleting associations of Application + Use service.backend_id

--- app/workers/backend_delete_application_worker.rb
+++ app/workers/backend_delete_application_worker.rb
@@ -4,18 +4,22 @@ class BackendDeleteApplicationWorker < ApplicationJob
   queue_as :low
 
   def perform(event_id)
-    event = EventStore::Repository.find_event!(event_id)
+    @event = EventStore::Repository.find_event!(event_id)
 
-    DeletedObject.application_keys.where(owner_type: Contract, owner_id: event.application.id).order(:id).find_each do |deleted_object|
-      ThreeScale::Core::ApplicationKey.delete(event.service_id.to_s, event.application_id, deleted_object.metadata[:value])
-    end
+    delete_associations
 
-    DeletedObject.referrer_filters.where(owner_type: Contract, owner_id: event.application.id).order(:id).find_each do |deleted_object|
-      ThreeScale::Core::ApplicationReferrerFilter.delete(event.service_id.to_s, event.application_id, deleted_object.metadata[:value])
-    end
-
-    ThreeScale::Core::Application.delete(event.service_id.to_s, event.application_id)
+    ThreeScale::Core::Application.delete(event.service_backend_id, event.application_id)
   rescue ActiveRecord::RecordNotFound => exception
     System::ErrorReporting.report_error(exception, parameters: {event_id: event_id})
   end
+
+  private
+
+  attr_reader :event
+
+  def delete_associations
+    delete_params = { application_id: event.application.id, service_backend_id: event.service_backend_id, application_backend_id: event.application_id }
+    [ApplicationKeyBackendService, ReferrerFilterBackendService].each { |klass| klass.delete_all(delete_params) }
+  end
+
 end
--- test/events/applications/application_deleted_event_test.rb
+++ test/events/applications/application_deleted_event_test.rb
@@ -4,22 +4,23 @@ require 'test_helper'
 
 class Applications::ApplicationDeletedEventTest < ActiveSupport::TestCase
   def setup
-    @application = FactoryBot.create(:cinstance)
+    @application = FactoryBot.create(:simple_cinstance)
+    @service = application.service
   end
 
-  attr_reader :application
+  attr_reader :application, :service
 
   def test_create_and_publish_when_the_application_associations_do_not_exist_anymore
     assert provider_id = application.provider_account.id
-    application.service.proxy.destroy!
-    application.service.delete
+    service.proxy.destroy!
+    service.delete
     application.provider_account.delete
 
     event = Applications::ApplicationDeletedEvent.create_and_publish!(application.reload)
 
     event_stored = EventStore::Repository.find_event!(event.event_id)
     assert_equal application.id, event_stored.application.id
-    assert_equal application.service_id, event_stored.service_id
+    assert_equal service.backend_id, event_stored.service_backend_id
     assert_equal application.application_id, event_stored.application_id
     assert_equal provider_id, event_stored.metadata[:provider_id]
     assert_equal application.service_id, event_stored.metadata.dig(:zync, :service_id)
