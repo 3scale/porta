7a042a8d4e2537c0a67fb0dd690ccc593f1eb21c
Guilherme Cassolato <guicassolato@gmail.com>
2020-05-12 20:02:36 +0200

    Removes ignored columns of Service from JSON serialization with Apicast::ProviderSource
    `ActiveModel::Serializer::JSON` includes attributes whose corresponding columns are ignored in the model. See, for example, in the Rails console,
    
    ```ruby
    Service.find(2).attributes.keys - Service.column_names
    => ["tech_support_email", "admin_support_email", "act_as_product", "default_end_user_plan_id", "end_user_registration_required"]
    ```
    
    Closes https://issues.redhat.com/browse/THREESCALE-5199

--- test/unit/apicast/proxy_source_test.rb
+++ test/unit/apicast/proxy_source_test.rb
@@ -1,17 +1,26 @@
 require 'test_helper'
 
 class Apicast::ProxySourceTest < ActiveSupport::TestCase
+  setup do
+    @proxy = FactoryBot.create(:proxy)
+    @source = Apicast::ProxySource.new(@proxy)
+  end
 
-  def test_proxy_rules
-    proxy = FactoryBot.create(:proxy)
+  attr_reader :proxy, :source
+
+  test 'proxy rules' do
     rule_1 = FactoryBot.create(:proxy_rule, proxy: proxy, last: true)
     rule_2 = FactoryBot.create(:proxy_rule, proxy: proxy)
 
-    source = Apicast::ProxySource.new(proxy).to_hash
-    proxy_rules_source = source['proxy']['proxy_rules']
+    proxy_rules_source = source.to_hash.dig('proxy', 'proxy_rules')
     assert proxy_rules_source
     assert find_by_id_in_hash(rule_1.id, proxy_rules_source)['last']
     refute find_by_id_in_hash(rule_2.id, proxy_rules_source)['last']
+  end
+
+  test 'does not include ignored columns of Service' do
+    source_hash = source.to_hash.with_indifferent_access
+    Service.ignored_columns.each { |column| source_hash[column] }
   end
 
   private
