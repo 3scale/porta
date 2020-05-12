1eeaf8be6f6c2556770216a204ddfe844c76b643
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2020-05-12 10:37:52 +0200

    [potafly] improves Login page a bit  (#1871)
    * a bit better readability
    * improve flow by checking authToken at the beginning
    * add test for redirect when there's a token already

--- portafly/src/tests/pages/Login.test.tsx
+++ portafly/src/tests/pages/Login.test.tsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 import { render } from 'tests/custom-render'
 import { Login } from 'pages'
+import { useAuth } from 'auth'
+
+jest.mock('auth')
 
 it('should render the Login page', () => {
+  (useAuth as jest.Mock).mockReturnValue({ authToken: null, setAuthToken: jest.fn() })
   const wrapper = render(<Login />)
+  wrapper.debug()
+
+  expect(wrapper.container.firstChild).toMatchSnapshot()
+})
+
+it('should not render when there is an auth token already', () => {
+  (useAuth as jest.Mock).mockReturnValue({ authToken: 'le token', setAuthToken: jest.fn() })
+  const wrapper = render(<Login />)
+  wrapper.debug()
+
   expect(wrapper.container.firstChild).toMatchSnapshot()
 })
