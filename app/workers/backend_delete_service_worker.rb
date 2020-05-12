1b0ecbc8451d5c8c1f06931a97ad63327445f66b
HemaHG <hhg@redhat.com>
2020-05-12 17:51:22 +0530

    Deletion of stats in Pisoni 1.29.0 doesn't need the data of the 'delete_job' param anymore
    Signed-off-by: HemaHG <hhg@redhat.com>

--- app/workers/backend_delete_service_worker.rb
+++ app/workers/backend_delete_service_worker.rb
@@ -10,18 +10,10 @@ class BackendDeleteServiceWorker
   def perform(event_id)
     event = EventStore::Repository.find_event!(event_id)
     service_id = event.service_id
-    batch = Sidekiq::Batch.new
-    batch.description = "Deleting in Backend the Service ##{service_id}"
-    batch.on(:success, self.class, {'service_id' => service_id})
-    batch.jobs do
-      BackendDeleteStatsWorker.perform_async(event_id)
-    end
+    ThreeScale::Core::Service.delete_stats(service_id, {})
+    service = Service.new({id: service_id}, without_protection: true)
+    service.delete_backend_service
   rescue ActiveRecord::RecordNotFound => exception
     System::ErrorReporting.report_error(exception, parameters: {event_id: event_id})
-  end
-
-  def on_success(_bid, options)
-    service = Service.new({id: options['service_id']}, without_protection: true)
-    service.delete_backend_service
   end
 end
