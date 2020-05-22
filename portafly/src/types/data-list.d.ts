2cf09b36c29e5c785eb9ad5a867eb51649c997c3
Jose Miguel Gallas <josemigallas@gmail.com>
2020-05-22 12:19:52 +0200

    [data-list] fixes some types

--- portafly/src/types/data-list.d.ts
+++ portafly/src/types/data-list.d.ts
@@ -1,4 +1,4 @@
 import { IRow, ICell } from '@patternfly/react-table'
 
-export type DataListRow = IRow & { id: number }
+export type DataListRow = IRow & { cells: (React.ReactNode | IRowCell)[], id: number }
 export type DataListCol = ICell
