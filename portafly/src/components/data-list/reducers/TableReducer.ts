00a74c4c90606a7e8e797d0ed21ca43dfc570e29
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2020-05-26 13:11:25 +0200

    Adds Sorting to Data List (#1902)
    * [data-list] implements sorting
    
    * extract index correction
    rename action type
    clone sortedRows

--- portafly/src/components/data-list/reducers/TableReducer.ts
+++ portafly/src/components/data-list/reducers/TableReducer.ts
@@ -5,18 +5,41 @@ import {
   createReducer
 } from 'utils'
 import { DataListRow, DataListCol } from 'types'
+import { SortByDirection } from '@patternfly/react-table'
 
 export type TableState = {
   rows: DataListRow[]
   columns: DataListCol[]
+  sortBy?: {
+    index: number
+    direction: SortByDirection
+  }
 }
 
 // Action Handlers
+const SET_SORT_BY = 'SET_SORT_BY'
 const SELECT_ONE = 'SELECT_ONE'
 const SELECT_PAGE = 'SELECT_PAGE'
 const SELECT_ALL = 'SELECT_ALL'
 
 const tableActionHandlers: ActionHandlers<TableState, any> = {
+  [SET_SORT_BY]: (state, action: Action<{ index: number, direction: SortByDirection }>) => {
+    const { index, direction } = action.payload
+    const sortedRows = [...state.rows]
+    sortedRows.sort((a, b) => {
+      const prev = a.cells[index]
+      const next = b.cells[index]
+      if (prev < next) return -1
+      if (prev > next) return 1
+      return 0
+    })
+
+    if (direction === SortByDirection.desc) {
+      sortedRows.reverse()
+    }
+
+    return { ...state, rows: sortedRows, sortBy: action.payload }
+  },
   [SELECT_ONE]: (state, action: Action<{ id: number, selected: boolean }>) => {
     const { id, selected } = action.payload
     const newRows = state.rows.map((r) => (r.id === id ? { ...r, selected } : r))
@@ -60,6 +83,10 @@ interface IUseTable {
 const useTable = ({ state, dispatch }: IUseTable) => ({
   columns: state.table.columns,
   rows: state.table.rows,
+  sortBy: state.table.sortBy,
+  setSortBy: (index: number, direction: SortByDirection) => (
+    dispatch({ type: SET_SORT_BY, payload: { index, direction } })
+  ),
   selectedRows: state.table.rows.filter((r) => Boolean(r.selected)),
   selectOne: (id: number, selected: boolean) => (
     dispatch({ type: SELECT_ONE, payload: { id, selected } })
