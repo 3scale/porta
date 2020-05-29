1480a466de17148f16dba94dd8fd8e413717abdf
Jose Miguel Gallas <josemigallas@gmail.com>
2020-05-29 14:04:54 +0200

    ✅ removes deprecation warning by updating waitForElement -> waitFor

--- portafly/src/tests/components/LazyRoute.test.tsx
+++ portafly/src/tests/components/LazyRoute.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { render } from 'tests/custom-render'
 import { LazyRoute } from 'components'
-import { waitForElement } from '@testing-library/react'
+import { waitFor } from '@testing-library/react'
 
 const getComponent = () => Promise.resolve({
   default: () => <div data-testid="async-component">Hello!</div>
@@ -17,5 +17,5 @@ test('should render the async component', async () => {
   const { queryByTestId, getByTestId } = render(<LazyRoute getComponent={getComponent} />)
 
   expect(queryByTestId('async-component')).not.toBeInTheDocument()
-  waitForElement(() => expect(getByTestId('async-component')).toBeInTheDocument())
+  waitFor(() => expect(getByTestId('async-component')).toBeInTheDocument())
 })
