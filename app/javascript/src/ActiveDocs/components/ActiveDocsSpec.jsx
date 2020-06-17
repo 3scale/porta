63d1fce7e1fc8745c4e33c153f5de98bbfb76df0
Damián Peralta Mariñelarena <damianpm@users.noreply.github.com>
2020-06-17 17:39:35 +0200

    Implement Autocomplete of OAS3 with 3scale account data (#1935)
    * Autocomplete query params with responseInterceptor
    
    * Updating tests and moving fetch inside component
    
    * Hack to workaround cukes failing
    
    * Deleting hack, using promises
    
    * Extracting autocomplete to own file and adding dev portal support
    
    * Adding autocomplete description params
    
    * Passing responseInterceptor as prop

--- app/javascript/src/ActiveDocs/components/ActiveDocsSpec.jsx
+++ app/javascript/src/ActiveDocs/components/ActiveDocsSpec.jsx
@@ -1,18 +1,25 @@
 // @flow
 
 import React from 'react'
-
 import { createReactWrapper } from 'utilities/createReactWrapper'
 import SwaggerUI from 'swagger-ui-react'
-
 import 'swagger-ui-react/swagger-ui.css'
+import type { SwaggerResponse } from 'Types/SwaggerTypes'
 
 type ActiveDocsSpecProps = {
-  url: string
+ url: string,
+ responseInterceptor: (response: SwaggerResponse) => (response: SwaggerResponse, accountDataUrl: string) => SwaggerResponse
 }
 
-const ActiveDocsSpec = ({ url }: ActiveDocsSpecProps) => <SwaggerUI url={url} />
+const ActiveDocsSpec = ({ url, responseInterceptor }: ActiveDocsSpecProps) => (
+  <SwaggerUI
+    url={url}
+    responseInterceptor={responseInterceptor}
+  />
+)
 
-const ActiveDocsSpecWrapper = (props: ActiveDocsSpecProps, id: string) => createReactWrapper(<ActiveDocsSpec {...props} />, id)
+const ActiveDocsSpecWrapper = (props: ActiveDocsSpecProps, id: string) => (
+  createReactWrapper(<ActiveDocsSpec {...props} />, id)
+)
 
 export { ActiveDocsSpec, ActiveDocsSpecWrapper }
