843148866fcdb4b3f8f860101a7ba7255b55930c
Jose Miguel Gallas <josemigallas@gmail.com>
2020-06-19 10:43:17 +0200

    fixes test for rake task

--- test/unit/tasks/accounts_test.rb
+++ test/unit/tasks/accounts_test.rb
@@ -7,12 +7,20 @@ module Tasks
     test 'accounts:reset_bought_cinstances_count' do
       account = FactoryBot.create(:simple_buyer)
       FactoryBot.create(:cinstance, user_account: account)
-      account.update(bought_cinstances_count: nil)
+
+      sql_update_nil = <<-SQL
+        UPDATE  accounts
+        SET     bought_cinstances_count = NULL
+        WHERE   accounts.id = #{account.id}
+      SQL
+      ActiveRecord::Base.connection.execute(sql_update_nil)
+
+      assert_nil account.reload.bought_cinstances_count
 
       execute_rake_task 'accounts.rake', 'accounts:reset_bought_cinstances_count'
-      account.reload
 
-      assert_equal account.bought_cinstances_count, account.bought_cinstances.size
+      account.reload
+      assert_equal account.bought_cinstances_count, account.bought_cinstances.count
     end
   end
 end
