27d341b387821eacc015dd867a01c49fb9eb3a19
Srijita <srmukher@redhat.com>
2020-06-23 17:19:18 +0530

    Allowing all parameters in the Service Discovery request (#1951)
    * Allowing all parameters in the Service Discovery request
    Also added the integration test for service_discovery/auth_controller.
    
    * adding commit suggestion
    
    * fixup
    
    * Passed array content as separate arguments
    
    Signed-off-by: Srijita Mukherjee <srijita92mukherjee@gmail.com>
    Co-authored-by: Guilherme Cassolato <guicassolato@gmail.com>

---
+++ test/integration/master/service_discovery/auth_controller_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Master::ServiceDiscovery::AuthControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @provider = FactoryBot.create(:simple_provider)
+    ThreeScale.config.service_discovery.stubs(enabled: true, authentication_method: 'oauth')
+    Rails.application.reload_routes!
+    host! master_account.internal_admin_domain
+  end
+
+  attr_reader :provider
+
+  test 'master callback redirects to provider callback' do
+    provider_self_domain = provider.internal_self_domain
+    params = { code: '123', referrer: '/apiconfig/services/new', state: '' }
+    get auth_service_discovery_callback_path(self_domain: provider_self_domain, **params)
+    assert_redirected_to url_for(host: provider_self_domain, controller: 'provider/admin/service_discovery/auth', action: :show, **params)
+  end
+end
