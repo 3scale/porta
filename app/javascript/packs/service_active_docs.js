7812c6ea41f89ed48d771b7ed995d9c57ebd3d3c
Damián Peralta Mariñelarena <damianpm@users.noreply.github.com>
2020-06-24 16:21:33 +0200

    Revert "Implement Autocomplete of OAS3 with 3scale account data (#1935)"
    This reverts commit d1735f109cd6c7d5adef633538537d42ae918fca.

--- app/javascript/packs/service_active_docs.js
+++ app/javascript/packs/service_active_docs.js
@@ -1,20 +1,8 @@
 import { ActiveDocsSpecWrapper as ActiveDocsSpec } from 'ActiveDocs/components/ActiveDocsSpec'
-import { autocompleteOAS3 } from './OAS3Autocomplete'
 
 document.addEventListener('DOMContentLoaded', () => {
   const containerId = 'swagger-ui-container'
-  const AUTOCOMPLETE_CONFIG = {
-    dataUrl: {
-      provider: '/p/admin/api_docs/account_data.json',
-      buyer: '/api_docs/account_data.json'
-    }
-  }
-  const { url, baseUrl, accountType } = document.getElementById(containerId).dataset
-  const dataUrl = accountType ? AUTOCOMPLETE_CONFIG.dataUrl[accountType] : AUTOCOMPLETE_CONFIG.dataUrl['buyer']
+  const { url } = document.getElementById(containerId).dataset
 
-  const accountDataUrl = `${baseUrl}${dataUrl}`
-
-  const responseInterceptor = (response) => autocompleteOAS3(response, accountDataUrl)
-
-  ActiveDocsSpec({ url, responseInterceptor }, containerId)
+  ActiveDocsSpec({ url }, containerId)
 })
