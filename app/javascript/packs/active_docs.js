0bf4ecefdd607e3e0595ad0b4dd7a1d0d233a1de
Damián Peralta Mariñelarena <damianpm@users.noreply.github.com>
2020-06-24 17:32:09 +0200

    Revert "Implement Autocomplete of OAS3 with 3scale account data (#1935)" (#1989)
    This reverts commit 63d1fce7e1fc8745c4e33c153f5de98bbfb76df0.

--- app/javascript/packs/active_docs.js
+++ app/javascript/packs/active_docs.js
@@ -1,8 +1,7 @@
 // We can define the 3scale plugins here and export the modified bundle
 import SwaggerUI from 'swagger-ui'
-import { autocompleteOAS3 } from './OAS3Autocomplete'
 import 'swagger-ui/dist/swagger-ui.css'
+
 import 'ActiveDocs/swagger-ui-3-patch.scss'
 
-window.autocompleteOAS3 = autocompleteOAS3
 window.SwaggerUI = SwaggerUI
--- app/javascript/src/ActiveDocs/components/ActiveDocsSpec.jsx
+++ app/javascript/src/ActiveDocs/components/ActiveDocsSpec.jsx
@@ -1,25 +1,18 @@
 // @flow
 
 import React from 'react'
+
 import { createReactWrapper } from 'utilities/createReactWrapper'
 import SwaggerUI from 'swagger-ui-react'
+
 import 'swagger-ui-react/swagger-ui.css'
-import type { SwaggerResponse } from 'Types/SwaggerTypes'
 
 type ActiveDocsSpecProps = {
- url: string,
- responseInterceptor: (response: SwaggerResponse) => (response: SwaggerResponse, accountDataUrl: string) => SwaggerResponse
+  url: string
 }
 
-const ActiveDocsSpec = ({ url, responseInterceptor }: ActiveDocsSpecProps) => (
-  <SwaggerUI
-    url={url}
-    responseInterceptor={responseInterceptor}
-  />
-)
+const ActiveDocsSpec = ({ url }: ActiveDocsSpecProps) => <SwaggerUI url={url} />
 
-const ActiveDocsSpecWrapper = (props: ActiveDocsSpecProps, id: string) => (
-  createReactWrapper(<ActiveDocsSpec {...props} />, id)
-)
+const ActiveDocsSpecWrapper = (props: ActiveDocsSpecProps, id: string) => createReactWrapper(<ActiveDocsSpec {...props} />, id)
 
 export { ActiveDocsSpec, ActiveDocsSpecWrapper }
