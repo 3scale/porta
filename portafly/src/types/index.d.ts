e296a7e1220c13b27b37c227373faba88608f365
Jose Miguel Gallas <josemigallas@gmail.com>
2020-06-25 11:45:01 +0200

    [portafly] implements account link for table rows

--- portafly/src/types/index.d.ts
+++ portafly/src/types/index.d.ts
@@ -3,11 +3,14 @@ export * from './data-list'
 export type ArgumentsType<T extends (...args: any[]) => any> =
   T extends (...args: infer A) => any ? A : never;
 
-export interface IDeveloperAccount {
-  admin_name: string,
-  created_at: string, // TODO: Find a specific date as string type
-  id: number,
-  org_name: string,
-  state: string,
-  updated_at: string,
+export interface IDeveloperAccount extends IAccount {
+  admin_name: string
+  created_at: string // TODO: Find a specific date as string type
+  state: string
+  updated_at: string
+}
+
+export interface IAccount {
+  id: number
+  org_name: string
 }
