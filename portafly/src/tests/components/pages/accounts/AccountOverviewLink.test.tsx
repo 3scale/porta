284271d0f68615e57522a3ac5ceb5c5568f24db8
Jose Miguel Gallas <josemigallas@gmail.com>
2020-06-25 11:45:01 +0200

    [portafly] implements account link for table rows

---
+++ portafly/src/tests/components/pages/accounts/AccountOverviewLink.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render } from 'tests/custom-render'
+import { factories } from 'tests/factories'
+import { AccountOverviewLink } from 'components'
+
+const account = factories.DeveloperAccount.build()
+
+const setup = () => {
+  const wrapper = render(<AccountOverviewLink account={account} />)
+  const row = wrapper.getByText(account.org_name).closest('a') as HTMLElement
+  return { ...wrapper, row }
+}
+
+it('should render properly', () => {
+  const { row } = setup()
+  expect(row).toBeInTheDocument()
+})
+
+it('should have a link', () => {
+  const { row } = setup()
+  expect(row.getAttribute('aria-label')).toBe('accounts_table.account_overview_link_aria_label')
+})
+
+it('should point to the correct url', () => {
+  const { row } = setup()
+  expect(row.getAttribute('href')).toMatch(`/accounts/${account.id}`)
+})
