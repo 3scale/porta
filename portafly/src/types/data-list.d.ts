e6d8b19fa1c94b44ec2ba789e51a758e2203cfdd
Jose Miguel Gallas <josemigallas@gmail.com>
2020-06-26 16:40:03 +0200

    ♻️[portafly] stringValue in DataListRowCell

--- portafly/src/types/data-list.d.ts
+++ portafly/src/types/data-list.d.ts
@@ -2,7 +2,7 @@ import { IRow, ICell, IRowCell } from '@patternfly/react-table'
 import { TFunction } from 'i18next'
 
 export type DataListRow = Omit<IRow, 'cells'> & { id: number, cells: (DataListRowCell | string)[] }
-export type DataListRowCell = IRowCell & { filterableString: string }
+export type DataListRowCell = IRowCell & { stringValue: string }
 export type DataListCol = ICell & { categoryName: string }
 export type DataListRowGenerator = (collection: Array) => DataListRow[]
 export type DataListColumnGenerator = (t: TFunction) => DataListCol[]
