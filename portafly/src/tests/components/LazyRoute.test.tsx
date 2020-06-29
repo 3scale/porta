48e695407bc26a4afc5e03e813274f254d6d539b
Jose Miguel Gallas <josemigallas@gmail.com>
2020-06-29 17:07:18 +0200

    [portafly] refactor LazyRoute to support passing props better

--- portafly/src/tests/components/LazyRoute.test.tsx
+++ portafly/src/tests/components/LazyRoute.test.tsx
@@ -6,15 +6,16 @@ import { waitFor } from '@testing-library/react'
 const getComponent = () => Promise.resolve({
   default: () => <div data-testid="async-component">Hello!</div>
 })
+const TestComponent = React.lazy(getComponent)
 
-test('should render a spinner while waiting for a component module to be loaded', () => {
-  const { queryByTestId, getByText } = render(<LazyRoute getComponent={getComponent} />)
-  expect(getByText('loading.title')).toBeInTheDocument()
+test('should render a spinner while waiting for a component module to be loaded', async () => {
+  const { queryByTestId, getByText } = render(<LazyRoute component={TestComponent} />)
+  waitFor(() => expect(getByText('loading.title')).toBeInTheDocument())
   expect(queryByTestId('async-component')).not.toBeInTheDocument()
 })
 
 test('should render the async component', async () => {
-  const { queryByTestId, getByTestId } = render(<LazyRoute getComponent={getComponent} />)
+  const { queryByTestId, getByTestId } = render(<LazyRoute component={TestComponent} />)
 
   expect(queryByTestId('async-component')).not.toBeInTheDocument()
   waitFor(() => expect(getByTestId('async-component')).toBeInTheDocument())
