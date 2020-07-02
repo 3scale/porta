9aa5c35b308341df4a6118770bc654756a9d6da3
Jose Miguel Gallas <josemigallas@gmail.com>
2020-07-02 16:03:12 +0200

    🍝[portafly] removes unnecessary async

--- portafly/src/tests/components/shared/data-list/StateLabel.test.tsx
+++ portafly/src/tests/components/shared/data-list/StateLabel.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { render } from 'tests/custom-render'
 import { StateLabel } from 'components'
 
-it('should set the color of the label no matter the case', async () => {
+it('should set the color of the label no matter the case', () => {
   const { container, rerender } = render(<StateLabel state="Approved" />)
   expect(container.querySelector('.pf-m-green')).toBeInTheDocument()
 
