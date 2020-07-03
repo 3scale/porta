a279d95a8cbd68bc53254b457542f63f2002ffbc
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2020-07-03 17:07:08 +0200

    [portafly] adds shared component for State labels
    * [portafly] adds shared component for State labels
    
    * 🍝[portafly] removes unnecessary async

---
+++ portafly/src/tests/components/shared/data-list/StateLabel.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { render } from 'tests/custom-render'
+import { StateLabel } from 'components'
+
+it('should set the color of the label no matter the case', () => {
+  const { container, rerender } = render(<StateLabel state="Approved" />)
+  expect(container.querySelector('.pf-m-green')).toBeInTheDocument()
+
+  rerender(<StateLabel state="approved" />)
+  expect(container.querySelector('.pf-m-green')).toBeInTheDocument()
+})
