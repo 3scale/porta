80b5bceedd1ece7b392fc7eeb3b6e92c8966abbe
dd di cesare <didi@posteo.net>
2020-07-06 09:42:13 +0200

    [portafly] Rearranging types
    * Index file should not include types, but export from their locations

--- portafly/src/types/index.d.ts
+++ portafly/src/types/index.d.ts
@@ -1,16 +1,3 @@
 export * from './data-list'
-
-export type ArgumentsType<T extends (...args: any[]) => any> =
-  T extends (...args: infer A) => any ? A : never;
-
-export interface IDeveloperAccount extends IAccount {
-  admin_name: string
-  created_at: string // TODO: Find a specific date as string type
-  state: string
-  updated_at: string
-}
-
-export interface IAccount {
-  id: number
-  org_name: string
-}
+export * from './account'
+export * from './utils'
