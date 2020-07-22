6d107c327d7d62190a52f266eb909383991cabe2
Jose Miguel Gallas <josemigallas@gmail.com>
2020-07-22 13:57:26 +0200

    🐛 [portafly] fixes http utils (again)

--- portafly/src/utils/http.ts
+++ portafly/src/utils/http.ts
@@ -18,14 +18,19 @@ const fetchData = async <T>(request: Request): Promise<T> => {
   return response.parsedBody as T
 }
 
+const getStringUrl = (path: string, params: URLSearchParams) => {
+  const host = process.env.REACT_APP_API_HOST
+  // REACT_APP_API_HOST will be undefined in Openshift and URL is not compatible with relative paths
+  const url = host ? new URL(path, host) : path
+  return `${url.toString()}?${params.toString()}`
+}
+
 const craftRequest = (path: string, params = new URLSearchParams()) => {
   const authToken = getToken()
   params.append('access_token', authToken as string)
 
-  const url = `${process.env.REACT_APP_API_HOST || '/'}${path}?${params.toString()}`.replace('//', '/')
-
   return new Request(
-    url,
+    getStringUrl(path, params),
     {
       headers: { Accept: 'application/json' }
     }
