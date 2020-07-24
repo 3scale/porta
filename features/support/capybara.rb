70712eedf5b63a91162e219b96bb5062af5aacf6
Hallelujah <hery@rails-royce.org>
2020-07-24 13:39:56 +0300

    Raise the viewport to 2048 height
    Something changed in the browsers
    Adding more heights so there is no scroll bar in templates

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -64,7 +64,7 @@ Capybara.register_driver :headless_firefox do |app|
   options = Selenium::WebDriver::Firefox::Options.new
 
   options.add_argument('-headless')
-  options.add_argument('--window-size=1280,1024')
+  options.add_argument('--window-size=1280,2048')
 
   driver = Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
 
@@ -74,7 +74,7 @@ end
 Capybara.register_driver :firefox do |app|
   options = Selenium::WebDriver::Firefox::Options.new
 
-  options.add_argument('--window-size=1280,1024')
+  options.add_argument('--window-size=1280,2048')
 
   driver = Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
 
@@ -84,6 +84,7 @@ end
 Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--auto-open-devtools-for-tabs')
+  options.add_argument('--window-size=1280,2048')
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
@@ -93,7 +94,7 @@ Capybara.register_driver :headless_chrome do |app|
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-popup-blocking')
-  options.add_argument('--window-size=1280,1024')
+  options.add_argument('--window-size=1280,2048')
 
   options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
   options.add_option(:w3c, false)
