ab77ec8af96f2a12cb93b4ebc8032ac179147e5a
Jose Miguel Gallas <josemigallas@gmail.com>
2020-07-28 13:27:16 +0200

    [portafly] updates http utils to better support POST requests and validation errors

--- portafly/src/utils/http.ts
+++ portafly/src/utils/http.ts
@@ -1,21 +1,43 @@
+/* eslint-disable @typescript-eslint/no-throw-literal */
 import { getToken } from 'utils'
 
-interface HttpResponse<T> extends Response {
-  parsedBody?: T
+export type ValidationException = {
+  validationErrors: ValidationErrors
 }
 
+export type ValidationErrors = Record<string, string[]>
+
+// Anything else than a 200 should be considered an error.
 const fetchData = async <T>(request: Request): Promise<T> => {
-  const response: HttpResponse<T> = await fetch(request)
-  try {
-    response.parsedBody = await response.json()
-  } catch (ex) {
-    throw new Error(`The request ${request} didn't return a valid body`)
-  }
+  const response = await fetch(request)
 
   if (!response.ok) {
     throw new Error(response.statusText)
   }
-  return response.parsedBody as T
+
+  return await response.json() as T
+}
+
+// Watch for validation errors (422) and throw then in an error. Other than 201 is unexpected and
+// should be considered an error as well
+const postData = async <T>(request: Request, formData: FormData): Promise<T | any> => {
+  const body = Array.from(formData)
+    .reduce((acc, [key, value]) => acc.concat(`&${key}=${value}`), '')
+
+  const response = await fetch(request, { method: 'POST', body })
+
+  if (response.status === 422) {
+    const responseBody = await response.json()
+    throw { validationErrors: responseBody.errors } as ValidationException
+  }
+
+  if (response.status === 201) {
+    const created = await response.json() as T
+
+    return created
+  }
+
+  throw new Error(response.statusText)
 }
 
 const getStringUrl = (path: string, params: URLSearchParams) => {
@@ -32,9 +54,12 @@ const craftRequest = (path: string, params = new URLSearchParams()) => {
   return new Request(
     getStringUrl(path, params),
     {
-      headers: { Accept: 'application/json' }
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
     }
   )
 }
 
-export { fetchData, craftRequest }
+export { fetchData, postData, craftRequest }
