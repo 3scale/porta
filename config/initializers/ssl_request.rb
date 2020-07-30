848c726fb71ca708c3799a0fae51e87cbf8c6cdb
Hallelujah <hery@rails-royce.org>
2020-07-30 14:24:57 +0300

    Remove DevDomain middleware and utility

--- config/initializers/ssl_request.rb
+++ config/initializers/ssl_request.rb
@@ -1,6 +1,3 @@
-# NOTE: The middleware is called before Threescale::Middleware::DevDomain
-# So we only have the original host in request that is good
-
 if Rails.application.config.force_ssl
   apicast_regexp = Regexp.compile(Rails.configuration.three_scale.apicast_internal_host_regexp.presence || '\A(?!.*)\Z'.freeze)
   internal_request = ->(request) { apicast_regexp.match(request.host) }
