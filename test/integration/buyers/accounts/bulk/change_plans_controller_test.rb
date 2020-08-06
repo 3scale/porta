97719c914485fdb074bcdf5a2739389c980b5031
Marta Noya <mnoyabon@redhat.com>
2020-08-06 18:08:16 +0200

    Move '.decorate' from View to Controller for buyers/accounts/bulk/_selected_accounts

---
+++ test/integration/buyers/accounts/bulk/change_plans_controller_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Buyers::Accounts::Bulk::ChangePlansControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    tenant = FactoryBot.create(:provider_account)
+    tenant.settings.account_plans.allow
+    tenant.settings.update_column(:account_plans_ui_visible, true)
+    login! tenant
+    @buyer = FactoryBot.create(:buyer_account, provider_account: tenant)
+  end
+
+  attr_reader :buyer
+
+  test '#new displays the buyer\'s admin_user_display_name' do
+    get new_admin_buyers_accounts_bulk_change_plan_path, selected: [buyer.id]
+
+    assert_xpath('//main//span', buyer.decorate.admin_user_display_name)
+  end
+end
