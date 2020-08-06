8e44090211afd11d2d230207fbf6331f7113e8d3
Jose Miguel Gallas <josemigallas@gmail.com>
2020-08-06 12:40:33 +0200

    tests

--- test/workers/send_user_invitation_worker_test.rb
+++ test/workers/send_user_invitation_worker_test.rb
@@ -4,64 +4,53 @@ require 'test_helper'
 
 class SendUserInvitationWorkerTest < ActiveJob::TestCase
   def test_provider_mailer_perform
-    ProviderInvitationMailer.any_instance.expects(:invitation).once
+    Timecop.freeze(Time.zone.parse('2017-11-23 03:25:08 UTC +00:00')) do
+      ProviderInvitationMailer.any_instance.expects(:invitation).once
 
-    provider_account = FactoryBot.create(:provider_account)
-    invitation = FactoryBot.create(:invitation, account: provider_account)
+      provider_account = FactoryBot.create(:provider_account)
+      invitation = FactoryBot.create(:invitation, account: provider_account)
 
-    assert_nil invitation.sent_at
-    SendUserInvitationWorker.new.perform(invitation.id)
-    assert invitation.reload.sent_at
+      assert_not invitation.sent_at
+      SendUserInvitationWorker.new.perform(invitation.id)
+      assert_in_delta Time.zone.now, invitation.reload.sent_at, 1.second
+    end
   end
 
   def test_buyer_mailer_perform
-    InvitationMailer.any_instance.expects(:invitation).once
+    Timecop.freeze(Time.zone.parse('2017-11-23 03:25:08 UTC +00:00')) do
+      InvitationMailer.any_instance.expects(:invitation).once
 
-    buyer_account = FactoryBot.create(:buyer_account)
-    invitation = FactoryBot.create(:invitation, account: buyer_account)
+      buyer_account = FactoryBot.create(:buyer_account)
+      invitation = FactoryBot.create(:invitation, account: buyer_account)
 
-    assert_nil invitation.sent_at
-    SendUserInvitationWorker.new.perform(invitation.id)
-    assert invitation.reload.sent_at
+      assert_not invitation.sent_at
+      SendUserInvitationWorker.new.perform(invitation.id)
+      assert_in_delta Time.zone.now, invitation.reload.sent_at, 1.second
+    end
   end
 
-  def test_handles_ssl_error
-    ProviderInvitationMailer.any_instance.expects(:invitation).raises(OpenSSL::SSL::SSLError)
+  def test_handles_errors
+    errors = SendUserInvitationWorker::RETRY_ERRORS + SendUserInvitationWorker::DISCARD_ERRORS
+    errors.each do |error_class|
+      ProviderInvitationMailer.any_instance.expects(:invitation).raises(error_class)
 
-    invitation = FactoryBot.create(:invitation)
+      invitation = FactoryBot.create(:invitation)
 
-    assert_nil invitation.sent_at
-    SendUserInvitationWorker.new.perform(invitation.id)
-    assert_nil invitation.reload.sent_at
+      assert_not invitation.sent_at
+      SendUserInvitationWorker.new.perform(invitation.id)
+      assert_not invitation.reload.sent_at, error_class.to_s
+    end
   end
 
-  def test_handles_smpt_error
-    ProviderInvitationMailer.any_instance.expects(:invitation).raises(Net::SMTPAuthenticationError)
-
+  # regression test for: https://github.com/3scale/system/pull/3316
+  def test_send_invitation_with_helper_tag
+    provider = FactoryBot.create(:simple_provider)
+    buyer = FactoryBot.create(:simple_buyer, provider_account: provider)
+    FactoryBot.create(:cms_email_template, system_name: 'invitation', provider: provider, published: '{% debug:help %}', rails_view_path: 'emails/invitation')
     invitation = FactoryBot.create(:invitation)
 
-    assert_nil invitation.sent_at
-    SendUserInvitationWorker.new.perform(invitation.id)
-    assert_nil invitation.reload.sent_at
-  end
-
-  def test_handles_socket_error
-    ProviderInvitationMailer.any_instance.expects(:invitation).raises(SocketError)
-
-    invitation = FactoryBot.create(:invitation)
-
-    assert_nil invitation.sent_at
-    SendUserInvitationWorker.new.perform(invitation.id)
-    assert_nil invitation.reload.sent_at
-  end
-
-  def test_handles_not_found_error
-    ProviderInvitationMailer.any_instance.expects(:invitation).raises(ActiveRecord::RecordNotFound)
-
-    invitation = FactoryBot.create(:invitation)
-
-    assert_nil invitation.sent_at
-    SendUserInvitationWorker.new.perform(invitation.id)
-    assert_nil invitation.reload.sent_at
+    assert_difference('ActionMailer::Base.deliveries.count') do
+      SendUserInvitationWorker.new.perform(invitation.id)
+    end
   end
 end
