dc519f7f14f8794de1a17e3a06655064cd571f7d
Jose Miguel Gallas <josemigallas@gmail.com>
2020-08-10 12:13:27 +0200

    worker

--- app/workers/send_user_invitation_worker.rb
+++ app/workers/send_user_invitation_worker.rb
@@ -2,7 +2,21 @@
 
 class SendUserInvitationWorker < ApplicationJob
   queue_as :default
-  include Net
+
+  RETRY_ERRORS = [
+    OpenSSL::SSL::SSLError,
+    Net::SMTPAuthenticationError,
+    Net::SMTPFatalError,
+    Net::SMTPServerBusy,
+    Net::SMTPSyntaxError,
+    Net::SMTPUnknownError,
+    Net::SMTPUnsupportedCommand,
+    SocketError,
+  ].freeze
+
+  DISCARD_ERRORS = [
+    ActiveRecord::RecordNotFound
+  ].freeze
 
   def perform(invitation_id)
     invitation = Invitation.find(invitation_id)
@@ -11,7 +25,10 @@ class SendUserInvitationWorker < ApplicationJob
     mailer.invitation(invitation).deliver_now!
 
     invitation.update!(sent_at: Time.zone.now)
-  rescue OpenSSL::SSL::SSLError, Net::SMTPError, SocketError, ActiveRecord::RecordNotFound => err
-    logger.error(err.message)
+  rescue *RETRY_ERRORS => error
+    logger.error(error.message)
+  rescue *DISCARD_ERRORS => error
+    logger.error(error.message)
+    false
   end
 end
