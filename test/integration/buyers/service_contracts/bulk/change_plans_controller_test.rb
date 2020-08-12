a82de01fedc808d3c1be821366068cb26a1db1d8
Marta Noya <mnoyabon@redhat.com>
2020-08-12 21:42:04 +0200

    Buyers::ServiceContracts::Bulk::ChangePlansController#new renders with the display_name in the title of the contract

--- test/integration/buyers/service_contracts/bulk/change_plans_controller_test.rb
+++ test/integration/buyers/service_contracts/bulk/change_plans_controller_test.rb
@@ -4,13 +4,13 @@ require 'test_helper'
 
 class Buyers::ServiceContracts::Bulk::ChangePlansControllerTest < ActionDispatch::IntegrationTest
   def setup
-    tenant = FactoryBot.create(:provider_account)
+    @tenant = FactoryBot.create(:provider_account)
     @service_contract = FactoryBot.create(:service_contract, plan: tenant.service_plans.first!)
     @change_service_plan = FactoryBot.create(:service_plan, issuer: tenant.default_service)
     login! tenant
   end
 
-  attr_reader :service_contract, :change_service_plan
+  attr_reader :service_contract, :change_service_plan, :tenant
 
   test '#create renders errors correctly' do
     Contract.any_instance.stubs :save do
@@ -20,5 +20,20 @@ class Buyers::ServiceContracts::Bulk::ChangePlansControllerTest < ActionDispatch
     post admin_buyers_service_contracts_bulk_change_plan_path, selected: [service_contract.id], change_plans: {plan_id: change_service_plan.id}, action: 'create'
     assert_response :unprocessable_entity
     assert_template 'buyers/applications/bulk/shared/errors.html'
+  end
+
+  test '#new renders with the display_name in the title of the contract' do
+    another_service_contract = FactoryBot.create(:service_contract, plan: FactoryBot.create(:simple_service_plan, issuer: tenant.default_service))
+    contracts = [service_contract, another_service_contract]
+
+    tenant.settings.service_plans_ui_visible = true
+
+    get new_admin_buyers_service_contracts_bulk_change_plan_path, selected: contracts.map(&:id)
+
+    page = Nokogiri::HTML::Document.parse(response.body)
+
+    expected_display_names = contracts.map { |contract| contract.decorate.account_admin_user_display_name }
+    li_title_display_names = page.xpath("//ul[@class='bulk_operation_items']/li").map { |li| li.attribute('title').value.split(' - ')[1] }
+    assert_same_elements expected_display_names, li_title_display_names
   end
 end
