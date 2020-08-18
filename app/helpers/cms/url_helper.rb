df759a439a886d4debf71ff745526038387f63fa
Guilherme Cassolato <guicassolato@gmail.com>
2020-08-18 11:32:29 +0200

    Merge pull request #2126 from 3scale/fix/dev_gtld
    [hotfix] Remove dev_gtld refs left behind

--- app/helpers/cms/url_helper.rb
+++ app/helpers/cms/url_helper.rb
@@ -26,12 +26,7 @@ module CMS
       uri.query = { return_to: page.path ? page.path : HARD_WIRED_PATHS[page.system_name],
                     access_code: current_account.site_access_code,
                     cms_token: page.provider.settings.cms_token! }.to_query
-
-      if Rails.env.development?
-        uri.port = request.port
-        uri.host += ".#{ThreeScale.config.dev_gtld}"
-      end
-
+      uri.port = request.port if Rails.env.development?
       uri
     end
 
