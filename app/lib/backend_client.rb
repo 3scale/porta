c4995504c8553d09d6a143dd1542268b0cb5667c
Hallelujah <hery@rails-royce.org>
2020-09-10 15:49:09 +0200

    [backend_client] Allow setting port from :host configuration
    So we can use another backend on a different port
    
    Fixes:
    
      Failed to open TCP connection to http://backend-listener-internal:80:80 (getaddrinfo: Name or service not known

--- app/lib/backend_client.rb
+++ app/lib/backend_client.rb
@@ -1,9 +1,30 @@
+# frozen_string_literal: true
+
 module BackendClient
   class BackendError < RuntimeError; end
   class TooManyItems < BackendError; end
   class InvalidItem  < BackendError; end
 
-  def self.config
-    System::Application.config.backend_client
+  class << self
+    def threescale_client_config
+      config.merge(
+        port: uri.port,
+        host: uri.host
+      )
+    end
+
+    def config
+      System::Application.config.backend_client
+    end
+
+    protected
+
+    def uri
+      URI(config[:url].presence || "#{scheme}://#{config[:host]}")
+    end
+
+    def scheme
+      config[:secure] ? 'https' : 'http'
+    end
   end
 end
