0c56a4507cdf3882d4c5fe772c5ce078bccd509b
Marta Noya <mnoyabon@redhat.com>
2020-09-14 19:44:25 +0200

    Decorate contracts

--- app/decorators/contract_decorator.rb
+++ app/decorators/contract_decorator.rb
@@ -3,8 +3,6 @@
 class ContractDecorator < ApplicationDecorator
   delegate :admin_user_display_name, to: :account, prefix: :account
 
-  private
-
   def account
     @account ||= super.decorate
   end
