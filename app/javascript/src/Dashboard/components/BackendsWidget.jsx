60ab5400891a9d8a365c4ccf93d3b47665ef6d96
Christie Molloy <cmolloy@redhat.com>
2020-10-07 17:04:59 -0400

    lots more updates

--- app/javascript/src/Dashboard/components/BackendsWidget.jsx
+++ app/javascript/src/Dashboard/components/BackendsWidget.jsx
@@ -1,9 +1,28 @@
 // @flow
 
-import React from 'react';
-import { Card } from '@patternfly/react-core';
-import 'Dashboard/styles/dashboard.scss';
-import 'patternflyStyles/dashboard';
+import React, {useState} from 'react'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Title,
+  Dropdown,
+  DropdownItem,
+  DropdownPosition,
+  KebabToggle,
+  DataList,
+  DataListItem,
+  DataListCell,
+  DataListItemRow,
+  DataListItemCells,
+  DataListAction
+} from '@patternfly/react-core'
+import CubesIcon from '@patternfly/react-icons/dist/js/icons/cubes-icon'
+import 'Dashboard/styles/dashboard.scss'
+import 'patternflyStyles/dashboard'
 
 import { createReactWrapper } from 'utilities/createReactWrapper'
 
@@ -24,11 +43,113 @@ type Props = {
 }
 
 const BackendsWidget = (props: Props) => {
-  console.log(props)
+  console.log('what are the props' + JSON.stringify(props))
+  console.log('what are the props 2' + JSON.stringify(props.backends[0].name))
+
+  const [ isOpenArray, setIsOpenArray ] = useState([])
+
+  function handleChange (e) {
+    const item = e.target.id
+    var indexOfItem = isOpenArray.indexOf(item)
+
+    if (indexOfItem !== -1) {
+      var array = [...isOpenArray]
+      if (indexOfItem === 0) {
+        array.shift()
+        setIsOpenArray(array)
+      } else {
+        var newArray = array.splice(indexOfItem, 1)
+        setIsOpenArray(newArray)
+      }
+    } else {
+      setIsOpenArray([item, ...isOpenArray])
+    }
+  }
+
+  const APIDataListItem = props.backends.map((api, index) => {
+    let dateUpdatedAt = new Date(api.updated_at)
+
+    return (
+      <DataListItem aria-labelledby="single-action-item1">
+        <DataListItemRow>
+          <DataListItemCells
+            dataListCells={[
+              <DataListCell key="primary content">
+                <a href={api.link} id="single-action-item1">
+                  {api.name}
+                </a>
+              </DataListCell>,
+              <DataListCell key="secondary content" className="dashboard-list-secondary">
+                {dateUpdatedAt.toUTCString()}
+              </DataListCell>
+            ]}
+          />
+          <DataListAction
+            aria-labelledby="multi-actions-item1 multi-actions-action1"
+            id="actions-menu"
+            aria-label="Actions"
+            isPlainButtonAction
+          >
+            <Dropdown
+              isPlain
+              id="actions-menu"
+              position={DropdownPosition.right}
+              isOpen={isOpenArray.indexOf(`toggle-${index}`) !== -1}
+              className="dashboard-list-item-action"
+              onClick={handleChange}
+              toggle={<KebabToggle id={`toggle-${index}`} />}
+              dropdownItems={[
+                <DropdownItem key={`link-${index}`} href={api.links[0].path}>
+                  Edit
+                </DropdownItem>,
+                <DropdownItem key={`link-${index}`} href={api.links[1].path}>
+                  Overview
+                </DropdownItem>,
+                <DropdownItem key={`link-${index}`} href={api.links[2].path}>
+                  Analytics
+                </DropdownItem>,
+                <DropdownItem key={`link-${index}`} href={api.links[3].path}>
+                  Methods and Metrics
+                </DropdownItem>,
+                <DropdownItem key={`link-${index}`} href={api.links[4].path}>
+                  Mapping Rules
+                </DropdownItem>
+              ]}
+            />
+          </DataListAction>
+        </DataListItemRow>
+      </DataListItem>
+    )
+  })
 
   return (
-    <Card>
-      Backends go here
+    <Card className="pf-c-card">
+      <CardHeader>
+        <div className="dashboard-list-icon-title-layout">
+          <CubesIcon/>
+          <Title headingLevel="h1" size="xl">
+            Backends
+          </Title>
+          <CardActions>
+            <Button component="a" variant="primary" href={props.newBackendPath}>
+              New Backend
+            </Button>
+          </CardActions>
+        </div>
+        <div className="dashboard-list-subtitle">
+          Most recently updated
+        </div>
+      </CardHeader>
+      <CardBody>
+        <DataList>
+          {APIDataListItem}
+        </DataList>
+      </CardBody>
+      <CardFooter>
+        <Button variant="link" component="a" isInline href="/p/admin/backend_apis">
+          Go to Backends
+        </Button>
+      </CardFooter>
     </Card>
   )
 }
--- app/javascript/src/Products/components/IndexPage.js
+++ app/javascript/src/Products/components/IndexPage.js
@@ -1,6 +1,31 @@
 // @flow
 
-import React from 'react'
+import React, {useState} from 'react'
+import {
+  Level,
+  LevelItem,
+  InputGroup,
+  TextInput,
+  Button,
+  ButtonVariant,
+  PageSection,
+  Pagination,
+  // PaginationVariant,
+  PageSectionVariants,
+  Title,
+  Divider,
+  Toolbar,
+  ToolbarItem,
+  ToolbarContent
+} from '@patternfly/react-core'
+import {
+  Table,
+  TableHeader,
+  TableBody
+} from '@patternfly/react-table'
+import SearchIcon from '@patternfly/react-icons/dist/js/icons/search-icon'
+import 'Products/components/styles/products.scss'
+import 'patternflyStyles/dashboard'
 
 import { createReactWrapper } from 'utilities/createReactWrapper'
 
@@ -22,10 +47,120 @@ type Props = {
 }
 
 const ProductsIndexPage = (props: Props) => {
-  console.log(props)
+  console.log('THIS IS THE PROPS' + JSON.stringify(props))
+
+  const [perPage, setPerPage] = useState(20)
+  const [page, setPage] = useState(1)
+  const tableColumns = [
+    'Name',
+    'System name',
+    'Recently updated',
+    'Hits (last 30 days)',
+    'Applications',
+    'Backends used',
+    'Unread alerts'
+  ]
+
+  const tableRows = props.products.map((tableRow) => {
+    return {
+      cells: [
+        { title: <a href="/">{tableRow.name}</a> },
+        '',
+        <span className="api-table-timestamp">{tableRow.updated_at}</span>,
+        '',
+        tableRow.apps_count,
+        tableRow.backends_count,
+        tableRow.unread_alerts_count
+      ]
+    }
+  })
+
+  const tableActions = [
+    {
+      title: <a href="/">Overview</a>
+    },
+    {
+      title: <a href="/">Analytics</a>
+    },
+    {
+      title: <a href="/">Applications</a>
+    },
+    {
+      title: <a href="/">ActiveDocs</a>
+    },
+    {
+      title: <a href="/">Integration</a>
+    }
+  ]
+
+  const onSetPage = (_event, pageNumber) => {
+    setPage(pageNumber)
+  }
+
+  const onPerPageSelect = (_event, perPage) => {
+    setPerPage(perPage)
+  }
 
   return (
-    <div>Products Index</div>
+    <React.Fragment>
+      <PageSection variant={PageSectionVariants.light}>
+        <Level>
+          <LevelItem>
+            <Title headingLevel="h1" size="2xl">API Products</Title>
+          </LevelItem>
+          <LevelItem>
+            <Button variant="primary">
+              New Product
+            </Button>
+          </LevelItem>
+        </Level>
+        <p className="api-table-description">Here is some content about Products. We could also include a link to documentation.</p>
+      </PageSection>
+      <Divider/>
+      <PageSection variant={PageSectionVariants.light}>
+        <Toolbar id="top-toolbar">
+          <ToolbarContent>
+            <ToolbarItem>
+              <InputGroup className="api-table-search">
+                <TextInput placeholder="Find a product" name="findProduct" id="findProduct" type="search" aria-label="Find a product" />
+                <Button variant={ButtonVariant.control} aria-label="search button for search input">
+                  <SearchIcon />
+                </Button>
+              </InputGroup>
+            </ToolbarItem>
+            <ToolbarItem variant="pagination" align={{ default: 'alignRight' }}>
+              <Pagination
+                itemCount={37}
+                perPage={perPage}
+                page={page}
+                onSetPage={onSetPage}
+                widgetId="pagination-options-menu-top"
+                onPerPageSelect={onPerPageSelect}
+              />
+            </ToolbarItem>
+          </ToolbarContent>
+        </Toolbar>
+        <Table aria-label="Actions Table" actions={tableActions} cells={tableColumns} rows={tableRows}>
+          <TableHeader />
+          <TableBody />
+        </Table>
+        {/* <Toolbar id="bottom-toolbar">
+          <ToolbarContent>
+            <ToolbarItem variant="pagination" align={{ default: 'alignRight' }}>
+              <Pagination
+                itemCount={37}
+                perPage={perPage}
+                page={page}
+                variant={PaginationVariant.bottom}
+                onSetPage={onSetPage}
+                widgetId="pagination-options-menu-top"
+                onPerPageSelect={onPerPageSelect}
+              />
+            </ToolbarItem>
+          </ToolbarContent>
+        </Toolbar> */}
+      </PageSection>
+    </React.Fragment>
   )
 }
 
