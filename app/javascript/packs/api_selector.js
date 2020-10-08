aac94bced9c0c18d1a914aa9076d961194b15b00
josemigallas <josemigallas@gmail.com>
2020-10-08 16:36:34 +0200

    removes services from the ApiSelector (context switcher)

---
+++ app/javascript/packs/api_selector.js
@@ -0,0 +1,15 @@
+import { ContaoIcon } from '@patternfly/react-icons'
+import { ContextSelectorWrapper } from 'Navigation/components/ContextSelector'
+
+const containerId = 'api_selector'
+
+document.addEventListener('DOMContentLoaded', function () {
+  const apiSelector = document.getElementById(containerId)
+
+  const { currentApi } = apiSelector.dataset
+
+  ContextSelectorWrapper({
+    ...apiSelector.dataset,
+    currentApi: JSON.parse(currentApi)
+  }, containerId)
+})
