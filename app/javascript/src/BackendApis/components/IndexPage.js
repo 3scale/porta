7ad35a4d7a27151ef02d7699e9b28c9d1f474444
Christie Molloy <cmolloy@redhat.com>
2020-10-08 14:45:33 -0400

    add backend table

--- app/javascript/src/BackendApis/components/IndexPage.js
+++ app/javascript/src/BackendApis/components/IndexPage.js
@@ -1,6 +1,30 @@
 // @flow
 
-import React from 'react'
+import React, {useState} from 'react'
+import {
+  Level,
+  LevelItem,
+  InputGroup,
+  TextInput,
+  Button,
+  ButtonVariant,
+  PageSection,
+  Pagination,
+  PaginationVariant,
+  PageSectionVariants,
+  Title,
+  Divider,
+  Toolbar,
+  ToolbarItem
+} from '@patternfly/react-core'
+import {
+  Table,
+  TableHeader,
+  TableBody
+} from '@patternfly/react-table'
+import SearchIcon from '@patternfly/react-icons/dist/js/icons/search-icon'
+import 'BackendApis/styles/backends.scss'
+import 'patternflyStyles/dashboard'
 
 import { createReactWrapper } from 'utilities/createReactWrapper'
 
@@ -20,10 +44,131 @@ type Props = {
 }
 
 const BackendsIndexPage = (props: Props) => {
-  console.log(props)
+  console.log('THIS IS THE PROPS' + JSON.stringify(props))
+
+  const [perPage, setPerPage] = useState(20)
+  const [page, setPage] = useState(1)
+  const [passedInProps, setPassedInProps] = useState(props)
+
+  const tableColumns = [
+    'Name',
+    'System name',
+    'Recently updated',
+    'Private base URL',
+    'Products using'
+  ]
+
+  console.log('what is the state' + setPassedInProps)
+
+  const tableRows = props.backends.map((tableRow) => {
+    return {
+      cells: [
+        { title: <a href="/">{tableRow.name}</a> },
+        tableRow.system_name,
+        <span className="api-table-timestamp">{tableRow.updated_at}</span>,
+        tableRow.apps_count,
+        tableRow.products_count,
+        tableRow.unread_alerts_count
+      ]
+    }
+  })
+
+  const linkToPage = (event, rowId, rowData, extra, actionNumber) => {
+    const path = passedInProps && passedInProps.backends[rowId].links[actionNumber].path
+    window.location.href = path
+  }
+
+  const tableActions = (products) => [
+    {
+      title: 'Edit',
+      onClick: (event, rowId, rowData, extra) => linkToPage(event, rowId, rowData, extra, 0)
+    },
+    {
+      title: 'Overview',
+      onClick: (event, rowId, rowData, extra) => linkToPage(event, rowId, rowData, extra, 1)
+    },
+    {
+      title: 'Analytics',
+      onClick: (event, rowId, rowData, extra) => linkToPage(event, rowId, rowData, extra, 2)
+    },
+    {
+      title: 'Methods & Metrics',
+      onClick: (event, rowId, rowData, extra) => linkToPage(event, rowId, rowData, extra, 3)
+    },
+    {
+      title: 'Mapping Rules',
+      onClick: (event, rowId, rowData, extra) => linkToPage(event, rowId, rowData, extra, 4)
+    }
+  ]
+
+  console.log('what is tableActions' + JSON.stringify(tableActions()) + typeof (tableActions()))
+
+  const onSetPage = (_event, pageNumber) => {
+    setPage(pageNumber)
+  }
+
+  const onPerPageSelect = (_event, perPage) => {
+    setPerPage(perPage)
+  }
 
   return (
-    <div>Backends Index</div>
+    <React.Fragment>
+      <PageSection className="api-table-page-section" variant={PageSectionVariants.light}>
+        <Level>
+          <LevelItem>
+            <Title headingLevel="h1" size="2xl">API Products</Title>
+          </LevelItem>
+          <LevelItem>
+            <Button variant="primary">
+              New Product
+            </Button>
+          </LevelItem>
+        </Level>
+        <p className="api-table-description">Here is some content about Products. We could also include a link to documentation.</p>
+        <Divider/>
+        <Toolbar id="top-toolbar" className="pf-c-toolbar">
+          <div className="pf-c-toolbar__content">
+            <ToolbarItem>
+              <InputGroup className="api-table-search">
+                <TextInput placeholder="Find a product" name="findProduct" id="findProduct" type="search" aria-label="Find a product" />
+                <Button variant={ButtonVariant.control} aria-label="search button for search input">
+                  <SearchIcon />
+                </Button>
+              </InputGroup>
+            </ToolbarItem>
+            <ToolbarItem className="api-toolbar-pagination" align={{ default: 'alignRight' }}>
+              <Pagination
+                itemCount={37}
+                perPage={perPage}
+                page={page}
+                onSetPage={onSetPage}
+                widgetId="pagination-options-menu-top"
+                onPerPageSelect={onPerPageSelect}
+              />
+            </ToolbarItem>
+          </div>
+        </Toolbar>
+        <Table aria-label="Actions Table" actions={tableActions(props.backends)} cells={tableColumns} rows={tableRows}>
+          <TableHeader />
+          <TableBody />
+        </Table>
+        <Toolbar id="bottom-toolbar" className="pf-c-toolbar">
+          <div className="pf-c-toolbar__content">
+            <ToolbarItem className="api-toolbar-pagination" align={{ default: 'alignRight' }}>
+              <Pagination
+                itemCount={37}
+                perPage={perPage}
+                page={page}
+                variant={PaginationVariant.bottom}
+                onSetPage={onSetPage}
+                widgetId="pagination-options-menu-top"
+                onPerPageSelect={onPerPageSelect}
+              />
+            </ToolbarItem>
+          </div>
+        </Toolbar>
+      </PageSection>
+    </React.Fragment>
   )
 }
 
