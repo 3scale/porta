41de8052a134fa53f22022ec357d07bf96731ca3
Damian Peralta <damianperaltam@gmail.com>
2020-10-08 12:41:23 +0200

    Updating labels for member permissions

--- app/javascript/src/Users/utils/index.jsx
+++ app/javascript/src/Users/utils/index.jsx
@@ -3,13 +3,26 @@
 import type { Feature, AdminSection } from 'Users/types'
 
 const FEATURE_NAMES: { [feature: Feature]: string } = {
-  portal: 'Developer Portal',
-  finance: 'Billing',
-  settings: 'Settings',
-  partners: 'Accounts -- Applications',
-  monitoring: 'Analytics',
-  plans: 'Integration & Application Plans',
-  policy_registry: 'Policy Registry'
+  portal: 'Create, read, update, and delete content & code of the <strong>developer portal</strong>',
+  finance: 'Setup and manage <strong>customer billing</strong>',
+  settings: 'Update <strong>feature settings</strong>',
+  partners: 'Create, read, update and delete:',
+  monitoring: 'Access & query <strong>analytics</strong> of:',
+  plans: 'Create, read, update and delete:',
+  policy_registry: 'Create, read, update and delete:'
+}
+
+const FEATURE_NAMES_DESCRIPTION_ITEMS: { [string]: Array<string> } = {
+  partners: [
+    'developer <strong>accounts</strong></span>',
+    '<strong>applications</strong> of selected API Products'
+  ],
+  monitoring: ['all API BAckends', 'selected API Products'],
+  plans: [
+    '<strong>attribues, metrics, methods, and mapping rules of all existing API Backends</strong><br/>',
+    '<strong>attribues, application plans, active docs, and integration</strong> of selected existing API Products'
+  ],
+  policy_registry: ['the APIcast <strong>policy chain and its policies</strong>']
 }
 
 const FEATURES_GRANTING_SERVICE_ACCESS = ['partners', 'monitoring', 'plans']
@@ -20,6 +33,10 @@ export function getFeatureName (feature: Feature): string {
   }
 
   throw new Error(`${feature} is not a known feature`)
+}
+
+export function getFeatureNameDescription (feature: Feature) {
+  return FEATURE_NAMES_DESCRIPTION_ITEMS[feature]
 }
 
 export function canFeatureSetServicePermissions (features: AdminSection | Array<AdminSection>): boolean {
