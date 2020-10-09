39c4929b396e0991bde315802dea0123c9a08f00
josemigallas <josemigallas@gmail.com>
2020-10-09 11:49:43 +0200

    extracts data-list componet

---
+++ app/javascript/src/Dashboard/components/APIDataListItem.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useRef } from 'react'
+
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownPosition,
+  KebabToggle,
+  DataListItem,
+  DataListCell,
+  DataListItemRow,
+  DataListItemCells,
+  DataListAction
+} from '@patternfly/react-core'
+
+type Props = {
+  api: {
+    id: number,
+    link: string,
+    links: Array<{
+      name: string,
+      path: string
+    }>,
+    name: string,
+    type: string,
+    updated_at: string
+  }
+}
+
+const APIDataListItem = ({ api }: Props) => {
+  const { id, name, updated_at: updatedAt, link, links } = api
+  const [isOpen, setIsOpen] = useState(false)
+  const ref = useRef(null)
+  // TODO: useClickOutside(ref, () => setIsOpen(false))
+
+  return (
+    <DataListItem key={id} aria-labelledby="single-action-item1">
+      <DataListItemRow>
+        <DataListItemCells
+          dataListCells={[
+            <DataListCell key="primary content">
+              <a href={link} id="single-action-item1">
+                {name}
+              </a>
+            </DataListCell>,
+            <DataListCell key="secondary content" className="dashboard-list-secondary">
+              {updatedAt}
+            </DataListCell>
+          ]}
+        />
+        <DataListAction
+          aria-labelledby="multi-actions-item1 multi-actions-action1"
+          id="actions-menu"
+          aria-label="Actions"
+          isPlainButtonAction
+        >
+          <Dropdown
+            isPlain
+            id="actions-menu"
+            ref={ref}
+            position={DropdownPosition.right}
+            isOpen={isOpen}
+            className="dashboard-list-item-action"
+            onClick={() => setIsOpen(!isOpen)}
+            toggle={<KebabToggle id={id.toString()} />}
+            dropdownItems={[
+              <DropdownItem key={`link-${links[0].path}`} href={links[0].path}>
+                Edit
+              </DropdownItem>,
+              <DropdownItem key={`link-${links[1].path}`} href={links[1].path}>
+                Overview
+              </DropdownItem>,
+              <DropdownItem key={`link-${links[2].path}`} href={links[2].path}>
+                Analytics
+              </DropdownItem>,
+              <DropdownItem key={`link-${links[3].path}`} href={links[3].path}>
+                Methods and Metrics
+              </DropdownItem>,
+              <DropdownItem key={`link-${links[4].path}`} href={links[4].path}>
+                Mapping Rules
+              </DropdownItem>
+            ]}
+          />
+        </DataListAction>
+      </DataListItemRow>
+    </DataListItem>
+  )
+}
+
+export { APIDataListItem }
