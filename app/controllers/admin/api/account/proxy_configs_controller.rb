872b049f92f447b7f25d36a5b8438e171727e109
Marta Noya <mnoyabon@redhat.com>
2020-10-14 15:38:52 +0200

    ActiveDocs for Proxy Configs List (Provider)

--- app/controllers/admin/api/account/proxy_configs_controller.rb
+++ app/controllers/admin/api/account/proxy_configs_controller.rb
@@ -13,6 +13,19 @@ class Admin::Api::Account::ProxyConfigsController < Admin::Api::BaseController
     render_error 'invalid environment', status: :unprocessable_entity
   end
 
+  ##~ e = sapi.apis.add
+  ##~ e.path = "/admin/api/account/proxy_configs/{environment}.json"
+  ##~ e.responseClass = "proxy_config"
+  #
+  ##~ op             = e.operations.add
+  ##~ op.httpMethod  = "GET"
+  ##~ op.summary     = "Proxy Configs List (Provider)"
+  ##~ op.description = "Returns the Proxy Configs of the provider"
+  ##~ op.group       = "proxy_config"
+  #
+  ##~ op.parameters.add @parameter_access_token
+  ##~ op.parameters.add @parameter_environment
+  #
   def index
     respond_with proxy_configs.order(:id).paginate(pagination_params)
   end
