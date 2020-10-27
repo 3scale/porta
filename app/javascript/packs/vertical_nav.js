ffc66b01adbb706480c7172bad03dfd49c51c4a2
Christie Molloy <cmolloy@redhat.com>
2020-10-27 16:04:02 -0400

    updates

--- app/javascript/packs/vertical_nav.js
+++ app/javascript/packs/vertical_nav.js
@@ -1,19 +1,15 @@
 import { VerticalNavWrapper as VerticalNav } from 'Navigation/components/VerticalNav'
 import { safeFromJsonString } from 'utilities/json-utils'
 
-const apiSelectorId = 'api_selector'
-
 document.addEventListener('DOMContentLoaded', () => {
   const dataset = document.getElementById('vertical-nav-wrapper').dataset
   const sections = safeFromJsonString(dataset.sections)
   const activeSection = dataset.active_section
   const activeItem = dataset.active_item
-
-  const apiSelector = document.getElementById(apiSelectorId)
-  const { currentApi } = apiSelector.dataset
+  const currentApi = dataset.current_api
 
   // TODO: where does this go?
   // - if can?(:manage, :plans)
   //   { title: 'Integration Errors', path: admin_service_errors_path(@service) },
-  VerticalNav({ sections, activeSection, activeItem, currentApi: safeFromJsonString(currentApi) }, 'vertical-nav-wrapper')
+  VerticalNav({ sections, activeSection, activeItem, currentApi: JSON.parse(currentApi) }, 'vertical-nav-wrapper')
 })
