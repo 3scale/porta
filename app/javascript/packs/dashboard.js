7cc8eba26ee951202ff1f86b2d9d6fd722756450
josemigallas <josemigallas@gmail.com>
2020-10-29 13:55:08 +0100

    cleans up dashboard tabs and old widgets

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -1,9 +1,7 @@
 import * as dashboardWidget from 'Dashboard/ajax-widget'
-import { initialize as toggleWidget } from 'Dashboard/toggle'
 import { render as renderChartWidget } from 'Dashboard/chart'
 
 document.addEventListener('DOMContentLoaded', () => {
   window.dashboardWidget = dashboardWidget
-  window.toggleWidget = toggleWidget
   window.renderChartWidget = renderChartWidget
 })
