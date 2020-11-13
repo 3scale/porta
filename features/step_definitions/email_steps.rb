c36051ef64f02c89166643000e9e69b47abd385b
josemigallas <josemigallas@gmail.com>
2020-11-13 10:03:46 +0100

    adds missing return

--- features/step_definitions/email_steps.rb
+++ features/step_definitions/email_steps.rb
@@ -32,7 +32,7 @@ module EmailHelpers
     # last_email_address will return the last email address used by email spec to find an email.
     # Note that last_email_address will be reset after each Scenario.
     # last_email_address || "example@3scale.localhost"
-    last_email_address if @current_user_for_email.nil?
+    return last_email_address if @current_user_for_email.nil?
 
     @current_user_for_email.email
   end
