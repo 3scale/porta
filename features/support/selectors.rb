13c10a48d7f2bfb035495c0b0c1a299631e7f261
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2020-11-17 10:21:38 +0100

    Remove Products/Backends from Master Dashboard and Context Selector (#2257)
    Removes services from Dashboard and Context Selector

--- features/support/selectors.rb
+++ features/support/selectors.rb
@@ -32,6 +32,8 @@ module HtmlSelectorsHelper
       'table.notification-settings tbody'
     when 'service widget'
       '.service-widget'
+    when 'the Context Selector'
+      '#api_selector'
 
     #
     # Invoicing helpers
