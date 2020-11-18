4089550c05b8b8f55c37f81d902af0a375353514
Guilherme Cassolato <guicassolato@gmail.com>
2020-11-18 15:14:31 +0100

    Prevent ThinkingSphinx from searching class names on no-inheritance indices

--- config/initializers/sphinx.rb
+++ config/initializers/sphinx.rb
@@ -1,3 +1,20 @@
 SPHINX_DELTA_INTERVAL = 90.minutes
 
+# Patches ThinkingSphinx so `sphinx_internal_class_name` field is avoided
+# for no inheritance indices despite other indices with inheritance defined
+ThinkingSphinx::Configuration::MinimumFields.prepend(Module.new do
+  private
+
+  def indices
+    super.reject do |index|
+      model = index.model
+      model.table_exists? && model.column_names.include?(model.inheritance_column)
+    end
+  end
+
+  def no_inheritance_columns?
+    true
+  end
+end)
+
 ThinkingSphinx::Callbacks.suspend!
