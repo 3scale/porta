c7dd403ef2caa5f4edf3fdde2b582a5f9e46b3d1
Marta Noya <tim4martanoya@gmail.com>
2020-12-18 00:25:46 +0100

    [Dev Portal] Register customer + cc + setup_intent/payment_method

--- app/lib/payment_gateways/stripe_crypt.rb
+++ app/lib/payment_gateways/stripe_crypt.rb
@@ -15,34 +15,39 @@ module PaymentGateways
     end
 
     def update!(params)
-      stripe_params = params.require(:stripe)
-      customer = create_stripe_customer(stripe_params)
-      update_account(customer, stripe_params)
-    rescue Stripe::CardError => error
-      report_error(error.message)
-    end
+      payment_method_id = params.require(:stripe).require(:payment_method_id)
+      payment_method = Stripe::PaymentMethod.retrieve(payment_method_id, api_key)
+      card = payment_method.card
 
-    def create_stripe_customer(stripe_params)
-      token   = stripe_params.require(:token)
-      api_key = payment_gateway_options.fetch(:login)
-      Stripe::Customer.create({ card: token,
-                                description: account.org_name,
-                                email: user.email,
-                                metadata: {
-                                  '3scale_account_reference' => buyer_reference
-                                }
-                              }, api_key)
-    end
-
-    def update_account(customer, stripe_params)
-      account.credit_card_expires_on_month = stripe_params.require(:expires_on_month)
-      account.credit_card_expires_on_year = stripe_params.require(:expires_on_year)
-      account.credit_card_partial_number = stripe_params.require(:partial_number)[-4..-1]
-      account.credit_card_auth_code = customer.id
+      account.credit_card_expires_on_month = card.exp_month
+      account.credit_card_expires_on_year = card.exp_year
+      account.credit_card_partial_number = card.last4
+      account.credit_card_auth_code = payment_method.customer
+      account.payment_method_id = payment_method_id
       account.save
     end
 
+    def create_stripe_customer
+      Stripe::Customer.create({
+        description: account.org_name,
+        email: user.email,
+        metadata: {  '3scale_account_reference' => buyer_reference }
+      }, api_key)
+    end
+
+    def create_stripe_setup_intent(customer = create_stripe_customer)
+      Stripe::SetupIntent.create({
+        payment_method_types: ['card'],
+        usage: 'off_session',
+        customer: customer.id
+      }, api_key)
+    end
+
     private
+
+    def api_key
+      payment_gateway_options.fetch(:login)
+    end
 
     def report_error(message)
       @errors.add(:base, message)
--- lib/developer_portal/app/controllers/developer_portal/admin/account/stripe_controller.rb
+++ lib/developer_portal/app/controllers/developer_portal/admin/account/stripe_controller.rb
@@ -3,11 +3,11 @@ module DeveloperPortal::Admin::Account
 
     def show
       @stripe_publishable_key = site_account.payment_gateway_options[:publishable_key]
-      render template: "accounts/payment_gateways/show"
+      @intent = stripe_crypt.create_stripe_setup_intent
+      render template: 'accounts/payment_gateways/show'
     end
 
     def hosted_success
-      stripe_crypt    = ::PaymentGateways::StripeCrypt.new(current_user)
       @payment_result = stripe_crypt.update!(params)
 
       if @payment_result
@@ -16,6 +16,12 @@ module DeveloperPortal::Admin::Account
         flash[:error] = "Couldn't save the credit card details: #{stripe_crypt.errors.full_messages.to_sentence}"
       end
       redirect_to after_hosted_success_path
+    end
+
+    private
+
+    def stripe_crypt
+      @stripe_crypt ||= ::PaymentGateways::StripeCrypt.new(current_user)
     end
   end
 end
