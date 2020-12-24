c1eda183a3570de3a1ac1bdbc3bf8e9eed06f43e
Marta Noya <tim4martanoya@gmail.com>
2020-12-24 12:45:33 +0100

    Remove RU from Liquid::Tags::StripeForm

--- test/unit/liquid/tags/stripe_form_test.rb
+++ test/unit/liquid/tags/stripe_form_test.rb
@@ -8,18 +8,7 @@ class StripeFormTest < ActiveSupport::TestCase
   end
 
   def test_renders_with_stripe_elements
-    account = FactoryBot.build_stubbed(:provider_account)
-    account.expects(:provider_can_use?).with(:stripe_elements).returns(true)
-    @context.registers[:site_account] = account
     @stripe.expects(:render_erb).with(@context, 'payment_gateways/stripe_elements', text: 'Edit Credit Card Details')
-    @stripe.render(@context)
-  end
-
-  def test_renders_with_stripe_v2
-    account = FactoryBot.build_stubbed(:provider_account)
-    account.expects(:provider_can_use?).with(:stripe_elements).returns(false)
-    @context.registers[:site_account] = account
-    @stripe.expects(:render_erb).with(@context, 'payment_gateways/stripe', text: 'Edit Credit Card Details')
     @stripe.render(@context)
   end
 end
