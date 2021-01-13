8e256ff5aa988e979c448a55960eac6d066733fd
josemigallas <josemigallas@gmail.com>
2021-01-13 11:42:42 +0100

    fixes vat_steps

--- features/step_definitions/finance/buyers/vat_steps.rb
+++ features/step_definitions/finance/buyers/vat_steps.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-Given "VAT rate of buyer/provider {account} is {int}%" do |account, rate|
+Given "VAT rate of {provider_or_buyer} is {int}%" do |account, rate|
   account.update!(vat_rate: rate.to_f)
 end
 
-Given "VAT code of buyer/provider {account} is {int}" do |account, rate|
+Given "VAT code of {provider_or_buyer} is {int}" do |account, rate|
   account.update!(vat_code: rate.to_f)
 end
