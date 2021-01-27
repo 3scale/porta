725922c9ebf584af43e07f8e74a1799a735618b3
josemigallas <josemigallas@gmail.com>
2021-01-27 12:14:37 +0100

    adds some logic

--- app/javascript/src/Applications/components/DefaultPlanSelector.jsx
+++ app/javascript/src/Applications/components/DefaultPlanSelector.jsx
@@ -1,7 +1,6 @@
 // @flow
 
-import React
-  from 'react'
+import React, { useState, useEffect } from 'react'
 
 import {
   Form,
@@ -12,59 +11,69 @@ import {
   SelectOption,
   SelectVariant
 } from '@patternfly/react-core'
+import { PlanSelectOptionObject } from 'Applications'
 
-import 'Applications/styles/applications.scss'
+type Props = {
+  currentPlanId: number,
+  plans: Array<{
+    id: number,
+    name: string
+  }>
+}
 
-type Props = {}
+const DefaultPlanSelector = ({ currentPlanId, plans }: Props) => {
+  const [selection, setSelection] = useState(() => {
+    if (currentPlanId) {
+      const plan = plans.find(p => p.id === currentPlanId)
+      return plan ? new PlanSelectOptionObject(plan) : undefined
+    }
+  })
+  const [isExpanded, setIsExpanded] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-// TODO: options will need to be sorted alphabetically
-const options = [
-  { value: '(Select none)', disabled: false, isPlaceholder: true },
-  { value: 'Application plan A', disabled: false },
-  { value: 'Application plan B', disabled: false },
-  { value: 'Some other application plan A', disabled: false },
-  { value: 'Yet another application plan', disabled: false }
-]
-const isExpanded = false
-const isDisabled = false
+  const onSelect = (_e, option: PlanSelectOptionObject) => {
+    setSelection(option)
+    setIsExpanded(false)
+  }
 
-// TODO: onToggle, onSelect, and onClear will need to be implemented
+  const onClear = () => {
+    setSelection(undefined)
+    // TODO: focus input
+  }
 
-const DefaultPlanSelector = (props: Props) => {
+  useEffect(() => {
+    console.log(selection)
+    // TODO: When plan changes, send a post request
+    console.log(setIsLoading)
+  }, [selection])
+
+  const options = [{ id: -1, name: '(Select none)' }, ...plans].map(p => new PlanSelectOptionObject(p))
+
   return (
-    <>
-      <PageSection variant={PageSectionVariants.light} className="pf-u-mb-md">
-        <Form>
-          <FormGroup
-            label="Default plan"
-            // isRequired
-            validated="default"
-            fieldId="cinstance_plan_id"
-            helperText="Default application plan (if any) is selected automatically upon service subscription."
+    <PageSection variant={PageSectionVariants.light} className="pf-u-mb-md">
+      <Form>
+        <FormGroup
+          label="Default plan"
+          validated="default"
+          fieldId="cinstance_plan_id"
+          helperText="Default application plan (if any) is selected automatically upon service subscription."
+        >
+          <Select
+            variant={SelectVariant.typeahead}
+            aria-label="Select application plan"
+            placeholderText="Select application plan"
+            onToggle={() => setIsExpanded(!isExpanded)}
+            onSelect={onSelect}
+            onClear={onClear}
+            isExpanded={isExpanded}
+            isDisabled={isLoading}
+            selections={selection}
           >
-            <Select
-              variant={SelectVariant.typeahead}
-              aria-label = "Select application plan"
-              placeholderText="Select application plan"
-              onToggle = {() => {}}
-              onSelect = {() => {}}
-              onClear = {() => {}}
-              isExpanded = {isExpanded}
-              isDisabled = {isDisabled}
-            >
-              {options.map((option, index) => (
-                <SelectOption
-                  isDisabled={option.disabled}
-                  key={index}
-                  value={option.value}
-                  isPlaceholder={option.isPlaceholder}
-                />
-              ))}
-            </Select>
-          </FormGroup>
-        </Form>
-      </PageSection>
-    </>
+            {options.map(p => <SelectOption key={p.id} value={p} />)}
+          </Select>
+        </FormGroup>
+      </Form>
+    </PageSection>
   )
 }
 
