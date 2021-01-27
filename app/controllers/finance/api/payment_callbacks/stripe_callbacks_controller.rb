d238f660e59f38748c77939cbf86b4bafc5700a0
Guilherme Cassolato <guicassolato@gmail.com>
2021-01-27 13:05:41 +0100

    [refactoring] Finance::Api::PaymentCallbacks::StripeCallbacksController#create

--- app/controllers/finance/api/payment_callbacks/stripe_callbacks_controller.rb
+++ app/controllers/finance/api/payment_callbacks/stripe_callbacks_controller.rb
@@ -20,38 +20,45 @@ class Finance::Api::PaymentCallbacks::StripeCallbacksController < Finance::Api::
 
   # Undocumented endpoint used for update callbacks of async-authorized payment transactions (mostly due to SCA regulations)
   def create
-    sig_header = request.headers['Stripe-Signature']
-    endpoint_secret = payment_gateway_options[:endpoint_secret].presence
+    return head(:no_content) if payment_intent.update_from_stripe_event(stripe_event)
 
-    raise MissingStripeEndpointSecret unless endpoint_secret
-
-    stripe_event = Stripe::Webhook.construct_event(request.raw_post, sig_header, endpoint_secret)
-    payment_intent_data = case stripe_event.type # Also checked by PaymentIntent#update_from_stripe_event, but here it can save us some processing and ensure an immediate response at the level of the controller in case of unsupported event types
-                          when 'payment_intent.succeeded'
-                            stripe_event.data.object
-                          else
-                            raise InvalidStripeEvent
-                          end
-
-    payment_intent = PaymentIntent.by_invoice(current_account.buyer_invoices).find_by!(payment_intent_id: payment_intent_data['id'])
-
-    unless payment_intent.update_from_stripe_event(stripe_event)
-      exception = StripeCallbackError.new('Cannot update Stripe payment intent')
-      report_error(exception, event: stripe_event, payment_intent: payment_intent)
-    end
-
-    head :ok
+    exception = StripeCallbackError.new('Cannot update Stripe payment intent')
+    report_error(exception, event: stripe_event, payment_intent: payment_intent)
   end
 
   protected
 
   def ensure_stripe_payment_gateway
     return if payment_gateway_type == :stripe
+
     render_error(:not_found, status: :not_found)
-    false
   end
 
   delegate :payment_gateway_type, :payment_gateway_options, to: :current_account
 
   delegate :report_error, to: System::ErrorReporting
+
+  def stripe_event
+    @stripe_event ||= begin
+      endpoint_secret = payment_gateway_options[:endpoint_secret].presence
+      raise MissingStripeEndpointSecret unless endpoint_secret
+
+      Stripe::Webhook.construct_event(request.raw_post, request.headers['Stripe-Signature'], endpoint_secret)
+    end
+  end
+
+  PAYMENT_INTENT_SUCCEEDED_STATUS = [Stripe::PaymentIntent::OBJECT_NAME, 'succeeded'].join('.').freeze
+
+  def extract_payment_intent_data
+    case stripe_event.type # Also checked by PaymentIntent#update_from_stripe_event, but here it can save us some processing and ensure an immediate response at the level of the controller in case of unsupported event types
+    when PAYMENT_INTENT_SUCCEEDED_STATUS
+      stripe_event.data.object
+    else
+      raise InvalidStripeEvent
+    end
+  end
+
+  def payment_intent
+    @payment_intent ||= PaymentIntent.by_invoice(current_account.buyer_invoices).find_by!(payment_intent_id: extract_payment_intent_data['id'])
+  end
 end
