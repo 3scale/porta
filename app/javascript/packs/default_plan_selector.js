12cdc1c579a3e80ffa1c8b818dc634ee7fb69027
josemigallas <josemigallas@gmail.com>
2021-01-27 12:14:07 +0100

    adds props

--- app/javascript/packs/default_plan_selector.js
+++ app/javascript/packs/default_plan_selector.js
@@ -1,6 +1,13 @@
 import { DefaultPlanSelectorWrapper } from 'Applications'
+import { safeFromJsonString } from 'utilities/json-utils'
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('in default_plan_selector')
-  DefaultPlanSelectorWrapper({}, 'default_plan')
+  const { dataset } = document.getElementById('default_plan')
+  const plans = safeFromJsonString(dataset.applicationPlans)
+  const currentPlanId = Number(dataset.currentPlanId)
+
+  DefaultPlanSelectorWrapper({
+    plans,
+    currentPlanId
+  }, 'default_plan')
 })
