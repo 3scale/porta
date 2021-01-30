6e4675d2a9f9f2f694fc35ef4beae3b135b34ae3
josemigallas <josemigallas@gmail.com>
2021-01-30 10:42:20 +0100

    * renames component * adds some additional PF styling * cleanup a bit * updates view helper and cleanup props * makes path a variable, instead of being hardcoded

---
+++ app/javascript/src/Applications/components/DefaultPlanSelectSection.jsx
@@ -0,0 +1,79 @@
+// @flow
+
+import React, { useState } from 'react'
+import { post } from 'utilities/ajax'
+
+import {
+  Form,
+  FormGroup,
+  Card,
+  CardBody
+} from '@patternfly/react-core'
+import { DefaultPlanSelect } from 'Applications'
+
+import type { Product, ApplicationPlan } from 'Applications/types'
+
+import './DefaultPlanSelectSection.scss'
+
+export type Props = {
+  product: Product,
+  initialDefaultPlan: ApplicationPlan | null,
+  path: string
+}
+
+const NO_DEFAULT_PLAN: ApplicationPlan = { id: -1, name: '(No default plan)' }
+
+const DefaultPlanSelectSection = ({ product, initialDefaultPlan, path }: Props) => {
+  const [defaultPlan, setDefaultPlan] = useState<ApplicationPlan>(initialDefaultPlan ?? NO_DEFAULT_PLAN)
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onSelectPlan = (plan: ApplicationPlan) => {
+    const body = plan.id >= 0 ? new URLSearchParams({ id: plan.id.toString() }) : undefined
+    const url = path.replace(':id', String(product.id))
+
+    post(url, body)
+      .then(data => {
+        console.log(data)
+        if (data.ok) {
+          // $FlowFixMe
+          $.flash('Default plan was updated')
+          setDefaultPlan(plan)
+        } else {
+          // $FlowFixMe
+          $.flash.error('Plan could not be updated')
+        }
+      })
+      .catch(err => {
+        console.error(err)
+        // $FlowFixMe
+        $.flash.error('An error ocurred. Please try again later.')
+      })
+      .finally(() => setIsLoading(false))
+
+    setIsLoading(true)
+  }
+
+  return (
+    <Card className="pf-c-card" id="default-plan-select-card">
+      <CardBody>
+        <Form onSubmit={e => e.preventDefault()}>
+          <FormGroup
+            label="Default plan"
+            fieldId="application_plan_id"
+            helperText="Default application plan (if any) is selected automatically upon service subscription."
+          >
+            <DefaultPlanSelect
+              plan={defaultPlan}
+              plans={[NO_DEFAULT_PLAN, ...product.appPlans].filter(p => p.id !== defaultPlan.id)} // Don't show the current default plan
+              onSelectPlan={onSelectPlan}
+              isDisabled={isLoading}
+            />
+          </FormGroup>
+        </Form>
+      </CardBody>
+    </Card>
+  )
+}
+
+export { DefaultPlanSelectSection }
