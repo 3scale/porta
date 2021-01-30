3d28a122718baee778f42b6fa405a986987b307f
josemigallas <josemigallas@gmail.com>
2021-01-30 10:42:20 +0100

    * renames component * adds some additional PF styling * cleanup a bit * updates view helper and cleanup props * makes path a variable, instead of being hardcoded

--- app/javascript/src/utilities/ajax.js
+++ app/javascript/src/utilities/ajax.js
@@ -1,12 +1,17 @@
 // @flow
 
-const post = (url: string, body?: URLSearchParams) => fetch(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-  },
-  body
-})
+const post = (url: string, body?: URLSearchParams) => {
+  const meta = document.querySelector('meta[name="csrf-token"]')
+  const token = (meta && meta.getAttribute('content')) || ''
+
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'X-CSRF-Token': token
+    },
+    body
+  })
+}
 
 export { post }
