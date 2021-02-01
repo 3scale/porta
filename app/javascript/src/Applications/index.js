45dd6ac85ef5b1e4d0b58bc6643a1814a1377cfb
josemigallas <josemigallas@gmail.com>
2021-02-01 17:16:25 +0100

    * moves Spinner into new module Common * reorganizes styles

--- app/javascript/src/Applications/index.js
+++ app/javascript/src/Applications/index.js
@@ -3,4 +3,3 @@
 export * from 'Applications/components/DefaultPlanSelect'
 export * from 'Applications/components/DefaultPlanSelectCard'
 export * from 'Applications/components/DefaultPlanSelectWrapper'
-export * from 'Applications/components/MySpinner'
