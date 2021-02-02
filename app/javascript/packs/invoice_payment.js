077d90d1dec73dbed597d6cb30d0568c2c089cb5
Damian Peralta <damianperaltam@gmail.com>
2021-02-02 11:58:55 +0100

    Refactor: typos, ordering, cleaning, etc.

--- app/javascript/packs/invoice_payment.js
+++ app/javascript/packs/invoice_payment.js
@@ -1,92 +1,84 @@
 import { loadStripe } from '@stripe/stripe-js'
 import 'PaymentGateways/stripe/styles/stripe.scss'
 
+const style = {
+  base: {
+    color: '#32325d',
+    fontFamily: 'Arial, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#32325d'
+    }
+  },
+  invalid: {
+    fontFamily: 'Arial, sans-serif',
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
   const stripePublishableKey = document.querySelector('.stripe-form').dataset.publishableKey
   const clientSecret = document.querySelector('.stripe-form').dataset.clientSecret
-  if (!stripePublishableKey || !clientSecret) {
-    return
-  }
-
+  const form = document.querySelector('#payment-form')
   const payButton = document.querySelector('#submit-payment')
+  const cardError = document.querySelector('#card-error')
+  const resultMessage = document.querySelector('.result-message')
+  const resultMessageLink = document.querySelector('.result-message a')
+  const errorMsg = document.querySelector('#card-error')
+  const spinner = document.querySelector('#spinner')
+  const buttonText = document.querySelector('#button-text')
 
-  const style = {
-    base: {
-      color: '#32325d',
-      fontFamily: 'Arial, sans-serif',
-      fontSmoothing: 'antialiased',
-      fontSize: '16px',
-      '::placeholder': {
-        color: '#32325d'
+  const stripe = await loadStripe(stripePublishableKey)
+  const elements = stripe.elements()
+  const card = elements.create('card', { style })
+
+  const payWithCard = (stripe, card, clientSecret) => {
+    setLoading(true)
+    stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card
       }
-    },
-    invalid: {
-      fontFamily: 'Arial, sans-serif',
-      color: '#fa755a',
-      iconColor: '#fa755a'
-    }
+    }).then(function (result) {
+      if (result.error) {
+        showError(result.error.message)
+      } else {
+        orderComplete(result.paymentIntent.id)
+      }
+    })
   }
 
-  const payWithCard = function (stripe, card, clientSecret) {
-    loading(true)
-    stripe
-      .confirmCardPayment(clientSecret, {
-        payment_method: {
-          card: card
-        }
-      })
-      .then(function (result) {
-        if (result.error) {
-          showError(result.error.message)
-        } else {
-          orderComplete(result.paymentIntent.id)
-        }
-      })
-  }
-
-  var orderComplete = function (paymentIntentId) {
-    loading(false)
-    document
-      .querySelector('.result-message a')
-      .setAttribute(
-        'href',
-        'https://dashboard.stripe.com/test/payments/' + paymentIntentId
-      )
-    document.querySelector('.result-message').classList.remove('hidden')
+  const orderComplete = (paymentIntentId) => {
+    setLoading(false)
+    resultMessageLink.setAttribute(
+      'href',
+      'https://dashboard.stripe.com/test/payments/' + paymentIntentId
+    )
+    resultMessage.classList.remove('hidden')
     payButton.disabled = true
   }
 
-  const showError = function (errorMsgText) {
-    loading(false)
-    var errorMsg = document.querySelector('#card-error')
+  const showError = (errorMsgText) => {
+    setLoading(false)
     errorMsg.textContent = errorMsgText
-    setTimeout(function () {
+    setTimeout(() => {
       errorMsg.textContent = ''
     }, 4000)
   }
 
-  const loading = function (isLoading) {
-    if (isLoading) {
-      payButton.disabled = true
-      document.querySelector('#spinner').classList.remove('hidden')
-      document.querySelector('#button-text').classList.add('hidden')
-    } else {
-      payButton.disabled = false
-      document.querySelector('#spinner').classList.add('hidden')
-      document.querySelector('#button-text').classList.remove('hidden')
-    }
+  const setLoading = (isLoading) => {
+    payButton.disabled = isLoading
+    spinner.classList.toggle('hidden', !isLoading)
+    buttonText.classList.toggle('hidden', isLoading)
   }
 
   payButton.disabled = true
-  const stripe = await loadStripe(stripePublishableKey)
-  const elements = stripe.elements()
-  const card = elements.create('card', { style: style })
   card.mount('#card-element')
-  card.on('change', function (event) {
-    payButton.disabled = event.empty
-    document.querySelector('#card-error').textContent = event.error ? event.error.message : ''
+  card.on('change', event => {
+    payButton.disabled = !event.complete
+    cardError.textContent = event.error ? event.error.message : ''
   })
-  const form = document.getElementById('payment-form')
   form.addEventListener('submit', function (event) {
     event.preventDefault()
     payWithCard(stripe, card, clientSecret)
