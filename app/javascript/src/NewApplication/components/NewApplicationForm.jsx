b5a05de2ecc71883b282538de05f2224f70b347c
Sarah Rambacher <srambach@redhat.com>
2021-02-02 17:18:59 -0500

    initial implementation of product selection modal

--- app/javascript/src/NewApplication/components/NewApplicationForm.jsx
+++ app/javascript/src/NewApplication/components/NewApplicationForm.jsx
@@ -6,8 +6,16 @@ import {
   Form,
   ActionGroup,
   Button,
+  Modal,
   PageSection,
-  PageSectionVariants
+  PageSectionVariants,
+  // Table,
+  InputGroup,
+  TextInput,
+  // SearchIcon,
+  Pagination,
+  Toolbar,
+  ToolbarItem
 } from '@patternfly/react-core'
 import {
   BuyerSelect,
@@ -79,62 +87,124 @@ const NewApplicationForm = ({
   const contract = buyer && product && buyer.contractedProducts.find(p => p.id === product.id)
   const contractedServicePlan = (contract && contract.withPlan) || (product && product.defaultServicePlan)
 
+  // const modalColumns = [
+  //   {title: 'Name'},
+  //   {title: 'System Name'},
+  //   {title: 'Last updated'}
+  // ]
+  // const modalRows = [
+  //   ['Product-01', 'product-01-sys-name', '04 Oct 2020, 08:05am']
+  // ]
+
   return (
-    <PageSection variant={PageSectionVariants.light}>
-      <Form
-        acceptCharset="UTF-8"
-        method="post"
-        action={url}
-        onSubmit={e => setLoading(true)}
-      >
-        <CSRFToken />
-        <input name="utf8" type="hidden" value="✓"/>
+    <>
+      <PageSection variant={PageSectionVariants.light}>
+        <Form
+          acceptCharset='UTF-8'
+          method='post'
+          action={url}
+          onSubmit={e => setLoading(true)}
+        >
+          <CSRFToken />
+          <input name='utf8' type='hidden' value='✓' />
 
-        {!buyer && (
-          <BuyerSelect />
-        )}
+          {!buyer && <BuyerSelect />}
 
-        <ProductSelect
-          product={product}
-          products={products}
-          onSelect={setProduct}
-          onShowAll={() => setModalOpen(true)}
-          isDisabled={!buyer}
-        />
-
-        {servicePlansAllowed && (
-          <ServicePlanSelect
-            servicePlan={servicePlan}
-            servicePlans={product ? product.servicePlans : []}
-            onSelect={setServicePlan}
-            isRequired={contractedServicePlan === null}
-            isDisabled={product === null || contractedServicePlan !== null}
+          <ProductSelect
+            product={product}
+            products={products}
+            onSelect={setProduct}
+            onShowAll={() => setModalOpen(true)}
+            isDisabled={!buyer}
           />
-        )}
 
-        <ApplicationPlanSelect
-          appPlan={appPlan}
-          appPlans={product ? product.appPlans : []}
-          onSelect={setAppPlan}
-          createApplicationPlanPath={createApplicationPlanPath.replace(':id', product ? product.id.toString() : '')}
-          isDisabled={product === null}
-        />
+          {servicePlansAllowed && (
+            <ServicePlanSelect
+              servicePlan={servicePlan}
+              servicePlans={product ? product.servicePlans : []}
+              onSelect={setServicePlan}
+              isRequired={contractedServicePlan === null}
+              isDisabled={product === null || contractedServicePlan !== null}
+            />
+          )}
 
-        <NameInput name={name} setName={setName} />
+          <ApplicationPlanSelect
+            appPlan={appPlan}
+            appPlans={product ? product.appPlans : []}
+            onSelect={setAppPlan}
+            createApplicationPlanPath={createApplicationPlanPath.replace(
+              ':id',
+              product ? product.id.toString() : ''
+            )}
+            isDisabled={product === null}
+          />
 
-        <DescriptionInput description={description} setDescription={setDescription} />
+          <NameInput name={name} setName={setName} />
 
-        <ActionGroup>
-          <Button
-            variant="primary"
-            type="submit"
-            isDisabled={!isFormComplete || loading}
-          >
-            Create Application
+          <DescriptionInput
+            description={description}
+            setDescription={setDescription}
+          />
+
+          <ActionGroup>
+            <Button
+              variant='primary'
+              type='submit'
+              isDisabled={!isFormComplete || loading}
+            >
+              Create Application
+            </Button>
+          </ActionGroup>
+        </Form>
+      </PageSection>
+
+      <Modal
+        title='Select a Product'
+        isLarge={true}
+        isOpen={modalOpen}
+        isFooterLeftAligned={true}
+        actions={[
+          <Button key='add' variant='primary'>
+            Add
+          </Button>,
+          <Button key='cancel' variant='secondary'>
+            Cancel
           </Button>
-        </ActionGroup>
-      </Form>
-    </PageSection>
+        ]}
+      >
+
+        {/* Toolbar is a component in the css, but a layout in react, so the class names are mismatched (pf-c-toolbar vs pf-l-toolbar) Styling doesn't work, but if you change it to pf-c in the inspector, it works */}
+        <Toolbar className="pf-c-toolbar pf-u-justify-content-space-between">
+          <ToolbarItem>
+            <InputGroup>
+              <TextInput name="searchInput" id="searchInput" type="search" aria-label="search for a product" />
+              <Button variant="control" aria-label="search button for search input">
+                search icon{/* <SearchIcon /> */}
+              </Button>
+            </InputGroup>
+          </ToolbarItem>
+          <ToolbarItem>
+            <Pagination
+              itemCount={8}
+              isCompact={true}
+              // perPage={this.state.perPage}
+              // page={this.state.page}
+              // onSetPage={this.onSetPage}
+              // widgetId="pagination-options-menu-top"
+              // onPerPageSelect={this.onPerPageSelect}
+            />
+          </ToolbarItem>
+        </Toolbar>
+        {/* <Table
+          caption='Products'
+          // sortBy={sortBy}
+          // onSort={this.onSort}
+          cells={modalColumns}
+          rows={modalRows}
+        >
+        </Table> */}
+      </Modal>
+    </>
   )
 }
 
