2bbb8030bb41f573685f6f41217477a96e319c2c
josemigallas <josemigallas@gmail.com>
2021-02-03 16:40:35 +0100

    fixes some js import errors

--- app/javascript/src/Plans/components/DefaultPlanSelectWrapper.jsx
+++ app/javascript/src/Plans/components/DefaultPlanSelectWrapper.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { DefaultPlanSelectCard } from 'Plans'
 import { createReactWrapper } from 'utilities/createReactWrapper'
 
-import type { Props } from 'Plans/DefaultPlanSelectCard'
+import type { Props } from 'Plans/components/DefaultPlanSelectCard'
 
 const DefaultPlanSelectWrapper = (props: Props, containerId: string) => createReactWrapper(<DefaultPlanSelectCard {...props} />, containerId)
 
