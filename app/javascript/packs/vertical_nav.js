1a1dd1810813d0e0f1a57b4c1d9dc34a0b00d7d0
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2021-02-09 11:18:15 +0100

    Fixes/Cleanup in Vertical Nav (#2355)
    * clean up
    * move dataset generation into helper
    * fixes console warning
    * fixes Flow types
    * clean up service json
    * 🥒 adds some tests
    * removes invalid test

--- app/javascript/packs/vertical_nav.js
+++ app/javascript/packs/vertical_nav.js
@@ -1,15 +1,22 @@
+// @flow
+
 import { VerticalNavWrapper as VerticalNav } from 'Navigation/components/VerticalNav'
 import { safeFromJsonString } from 'utilities/json-utils'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const dataset = document.getElementById('vertical-nav-wrapper').dataset
-  const sections = safeFromJsonString(dataset.sections)
-  const activeSection = dataset.active_section
-  const activeItem = dataset.active_item
-  const currentApi = dataset.current_api
+  const container = document.getElementById('vertical-nav-wrapper')
 
-  // TODO: where does this go?
-  // - if can?(:manage, :plans)
-  //   { title: 'Integration Errors', path: admin_service_errors_path(@service) },
-  VerticalNav({ sections, activeSection, activeItem, currentApi: safeFromJsonString(currentApi) }, 'vertical-nav-wrapper')
+  if (!container) {
+    return
+  }
+
+  const { activeItem, activeSection, currentApi, sections } = container.dataset
+
+  VerticalNav({
+    sections: safeFromJsonString(sections) || [],
+    activeSection,
+    activeItem,
+    currentApi: safeFromJsonString(currentApi) },
+  'vertical-nav-wrapper'
+  )
 })
