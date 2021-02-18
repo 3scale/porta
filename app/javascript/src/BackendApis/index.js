722994c82208dc091d8573ea6633f7deae2db536
josemigallas <josemigallas@gmail.com>
2021-02-18 19:40:59 +0100

    re implements Add Backend page

--- app/javascript/src/BackendApis/index.js
+++ app/javascript/src/BackendApis/index.js
@@ -1,3 +1,6 @@
 // @flow
 
 export * from './components/AddBackendUsage'
+export * from './components/BackendSelect'
+export * from './components/BackendSelectModal'
+export * from './components/PathInput'
