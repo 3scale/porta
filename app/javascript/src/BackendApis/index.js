be6a460b09e1c2fac54d9d8fb0d2c60b5f6f0e73
josemigallas <josemigallas@gmail.com>
2021-02-19 13:27:50 +0100

    renames AddBackendUsage

--- app/javascript/src/BackendApis/index.js
+++ app/javascript/src/BackendApis/index.js
@@ -1,6 +1,7 @@
 // @flow
 
-export * from './components/AddBackendUsage'
+// $FlowFixMe: name mapper confused because it has 'Form' in file name
+export * from './components/AddBackendForm'
 export * from './components/BackendSelect'
 export * from './components/BackendSelectModal'
 export * from './components/PathInput'
