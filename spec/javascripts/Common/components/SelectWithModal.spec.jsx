788b3c43ae5d09434c8026a847c64816fb9c818a
josemigallas <josemigallas@gmail.com>
2021-02-23 11:44:18 +0100

    tests

--- spec/javascripts/Common/components/SelectWithModal.spec.jsx
+++ spec/javascripts/Common/components/SelectWithModal.spec.jsx
@@ -2,30 +2,41 @@
 
 import React from 'react'
 import { mount } from 'enzyme'
+import type { ReactWrapper } from 'enzyme'
 
 import { SelectWithModal } from 'Common'
 
 const onClose = jest.fn()
 const onSelect = jest.fn()
 
-const cells = [{
-  propName: 'name', title: 'Name'
-}]
+const cells = [
+  { propName: 'name', title: 'Name' },
+  { propName: 'role', title: 'Role' }
+]
 
-const item = { id: 0, name: 'My Backend', description: 'example.org' }
+const items = [
+  { id: 0, name: 'J. Holden', role: 'Captain' },
+  { id: 1, name: 'N. Nagata', role: 'Engineer' },
+  { id: 2, name: 'A. Kamal', role: 'Pilot' }
+]
+
 const defaultProps = {
   item: null,
-  items: [item],
+  items,
   onClose,
   onSelect,
   isOpen: true,
   helperText: '',
   id: '',
   label: '',
-  modalTitle: 'Modal',
-  name: 'Name',
+  modalTitle: 'Pick a crew member',
+  name: 'input_name',
   cells
-  // TODO
+}
+
+function openModal <T> (wrapper: ReactWrapper<T>) {
+  wrapper.find('.pf-c-select__toggle-button').simulate('click')
+  wrapper.find('.pf-c-select__menu li button').last().simulate('click')
 }
 
 // $FlowFixMe $FlowIssue It should not complain since Record.id has union "number | string"
@@ -35,7 +46,7 @@ afterEach(() => {
   jest.resetAllMocks()
 })
 
-it.skip('should render itself', () => {
+it('should render itself', () => {
   const wrapper = mountWrapper()
   expect(wrapper.exists()).toBe(true)
 })
@@ -43,46 +54,64 @@ it.skip('should render itself', () => {
 it('should display all items, a title and a sticky button', () => {
   const wrapper = mountWrapper()
   wrapper.find('.pf-c-select__toggle-button').simulate('click')
-  expect(wrapper.find('.pf-c-select__menu li').length).toEqual(backends.length + 2)
+  expect(wrapper.find('.pf-c-select__menu li').length).toEqual(items.length + 2)
 })
 
 it('should be able to show a modal', () => {
   const wrapper = mountWrapper()
+  expect(wrapper.find('TableModal').props().isOpen).toBe(false)
 
-  wrapper.find('.pf-c-select__toggle-button').simulate('click')
-  wrapper.find('.pf-c-select__menu li button').last().simulate('click')
+  openModal(wrapper)
 
-  expect(onShowAllSpy).toBeCalledTimes(1)
-  expect(onSelectSpy).toBeCalledTimes(0)
+  expect(wrapper.find('TableModal').props().isOpen).toBe(true)
+  expect(onSelect).toBeCalledTimes(0)
 })
 
 it('should be able to select and submit an item', () => {
-  const targetItem = backends[0]
+  const targetItem = items[0]
   const wrapper = mountWrapper()
 
   wrapper.find('.pf-c-select__toggle-button').simulate('click')
   wrapper.find('.pf-c-select__menu li button').filterWhere(n => n.text().includes(targetItem.name)).simulate('click')
 
-  expect(onSelectSpy).toBeCalledWith(targetItem)
+  expect(onSelect).toBeCalledWith(targetItem)
 })
 
 it('should be able to submit the selected backend', () => {
-  const targetItem = backends[0]
-  const wrapper = mountWrapper({ backend: targetItem })
+  const targetItem = items[0]
+  const wrapper = mountWrapper({ item: targetItem })
 
-  const input = wrapper.find('[name="backend_api_config[backend_api_id]"]')
+  const input = wrapper.find('input[name="input_name"]')
   expect(input.exists()).toBe(true)
   expect(input.prop('value')).toBe(targetItem.id)
 })
 
-it('should have a button to create a new backend', () => {
-  const wrapper = mountWrapper()
-  expect(wrapper.find(`a[href="${newBackendPath}"]`))
+it('should have a helper text', () => {
+  const wrapper = mountWrapper({ helperText: <p>I'm helpful</p> })
+  expect(wrapper.find('.pf-c-form__helper-text').children()).toMatchInlineSnapshot(`
+    <p>
+      I'm helpful
+    </p>
+  `)
 })
 
-it.skip('should be able to select an option', () => {
+it('should be able to select an option from the modal', () => {
   const wrapper = mountWrapper()
-  wrapper.find('input[type="radio"]').simulate('change', { value: true })
+
+  openModal(wrapper)
+  wrapper.find('input[type="radio"]').first().simulate('change', { value: true })
   wrapper.find('button[data-testid="select"]').simulate('click')
-  expect(onSelect).toHaveBeenCalledWith(item)
+
+  expect(onSelect).toHaveBeenCalledWith(items[0])
+})
+
+it('should display all columns in the modal', () => {
+  const wrapper = mountWrapper()
+
+  openModal(wrapper)
+  const ths = wrapper.find('table th')
+
+  cells.forEach(c => (
+    expect(ths.find(`[data-label="${c.title}"]`).exists()).toBe(true)
+  ))
 })
