a57a020170cdc79e033e45eb79069b2ffd33ed3c
josemigallas <josemigallas@gmail.com>
2021-03-15 12:51:19 +0100

    remove unnused setup mock for npm jquery

--- spec/javascripts/setupTests.js
+++ spec/javascripts/setupTests.js
@@ -4,11 +4,3 @@ import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({adapter: new Adapter()})
-
-// DELETE: this is false
-import $ from 'jquery'
-// $FlowIgnore[prop-missing] flash is our custom function
-$.flash = {
-  notice: jest.fn(),
-  error: jest.fn()
-}
