45df532aa134ac72edb64e654acd740aa67b1dc0
josemigallas <josemigallas@gmail.com>
2021-03-18 12:53:51 +0100

    creates a "card" wrapper component to handle action requests

---
+++ app/javascript/src/Plans/components/ApplicationPlansTableCardWrapper.jsx
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react'
+
+// $FlowIgnore[missing-export] export is there, name_mapper is the problem
+import { ApplicationPlansTableCard } from 'Plans'
+import { createReactWrapper } from 'utilities/createReactWrapper'
+import type { Props } from 'Plans/components/ApplicationPlansTable'
+
+const ApplicationPlansTableCardWrapper = (props: Props, containerId: string): void => createReactWrapper(<ApplicationPlansTableCard {...props} />, containerId)
+
+export { ApplicationPlansTableCardWrapper }
