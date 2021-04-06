9b4189fdf0177f51581460943465b68aa15017d5
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2021-04-06 14:53:56 +0200

    split declaration into multiple lines
    Co-authored-by: Damián Peralta Mariñelarena <damianpm@users.noreply.github.com>

--- app/javascript/src/Plans/components/ApplicationPlansTableCardWrapper.jsx
+++ app/javascript/src/Plans/components/ApplicationPlansTableCardWrapper.jsx
@@ -7,6 +7,8 @@ import { ApplicationPlansTableCard } from 'Plans'
 import { createReactWrapper } from 'utilities/createReactWrapper'
 import type { Props } from 'Plans/components/ApplicationPlansTableCard'
 
-const ApplicationPlansTableCardWrapper = (props: Props, containerId: string): void => createReactWrapper(<ApplicationPlansTableCard {...props} />, containerId)
+const ApplicationPlansTableCardWrapper = (props: Props, containerId: string): void => (
+  createReactWrapper(<ApplicationPlansTableCard {...props} />, containerId)
+)
 
 export { ApplicationPlansTableCardWrapper }
