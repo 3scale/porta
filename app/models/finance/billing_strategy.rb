3scale/porta

commit bae5cab756e33c050f5cf5c472c8e0bd6a9bc388

remove all trace and uses of airbrake

app/models/finance/billing_strategy.rb
@@ -104,7 +104,7 @@ class Finance::BillingStrategy < ApplicationRecord
         message = "BillingStrategy #{id}(#{name}) failed utterly"
 
         Rails.logger.error(message)
-        airbrake(:error_message => message,
+        report_error(:error_message => message,
                  :error_class => 'BillingError',
                  :exception => e)
 
@@ -350,15 +350,15 @@ class Finance::BillingStrategy < ApplicationRecord
   private
 
   # Yields a block for each buyer, passing it as a parameter. If an
-  # exception occurs meanwhile, catches it and reports by airbrake.
+  # exception occurs meanwhile, catches and reports it.
   #
   def bill_and_charge_each(options = {})
     buyer_ids = options[:buyer_ids]
     @failed_buyers = []
 
     if provider.nil?
-      airbrake(:error_message => "WARNING: tried to use billing strategy #{self.id} which has no account",
-               :error_class => 'InvalidData')
+      report_error(:error_message => "WARNING: tried to use billing strategy #{self.id} which has no account",
+                   :error_class => 'InvalidData')
       return
     end
 
@@ -374,10 +374,10 @@ class Finance::BillingStrategy < ApplicationRecord
 
         msg = "Failed to bill or charge #{name}(#{buyer_id}) of provider(#{provider_id}): #{exception.message}\n"
         error(msg, buyer)
-        airbrake(:error_message => msg,
-                 :error_class => 'BillingError',
-                 :parameters => { :buyer_id => buyer_id, :provider_id => provider_id },
-                 :exception => exception)
+        report_error(:error_message => msg,
+                     :error_class => 'BillingError',
+                     :parameters => { :buyer_id => buyer_id, :provider_id => provider_id },
+                     :exception => exception)
 
         @failed_buyers << buyer_id
         raise if Rails.env.test?
@@ -411,14 +411,14 @@ class Finance::BillingStrategy < ApplicationRecord
   end
 
   module ErrorHandling
-    def airbrake(*args)
+    def report_error(*args)
       if Rails.env.production? || Rails.env.preview?
         System::ErrorReporting.report_error(*args)
       end
     end
   end
 
-  # so that we have #airbrake as instance AND class method
+  # so that we have #report_error as instance AND class method
   extend ErrorHandling
   include ErrorHandling
 

.rubocop_todo.yml
@@ -765,7 +765,6 @@ Style/SpecialGlobalVars:
     - 'app/workers/web_hook_worker.rb'
     - 'lib/developer_portal/lib/liquid/tags/form.rb'
     - 'lib/developer_portal/test/dummy/config/boot.rb'
-    - 'lib/tasks/airbrake_tasks.rake'
     - 'lib/tasks/cms/cms.rake'
     - 'script/stress-test.rb'
     - 'test/minitest_helper.rb'

Capfile
@@ -2,7 +2,6 @@ require 'capistrano/ext/multistage'
 
 require 'bundler/capistrano'
 
-require 'airbrake/capistrano'
 require 'new_relic/recipes'
 
 load 'deploy' if respond_to?(:namespace) # cap2 differentiator

app/controllers/frontend_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class FrontendController < ApplicationController
   include SiteAccountSupport
   include AccessCodeProtection
@@ -145,7 +147,7 @@ class FrontendController < ApplicationController
 
   def ensure_buyer_domain
     if Account.is_admin_domain?(request.host) || site_account.master?
-      notify_about_wrong_domain(request.url, :buyer, error_request_data)
+      notify_about_wrong_domain(request.url, :buyer)
       render_wrong_domain_error
       false
     else
@@ -155,16 +157,12 @@ class FrontendController < ApplicationController
 
   def ensure_master_domain
     unless Account.is_master_domain?(request.host)
-      notify_about_wrong_domain(request.url, :master, error_request_data)
+      notify_about_wrong_domain(request.url, :master)
       render_wrong_domain_error
       false
     else
       true
     end
-  end
-
-  def error_request_data
-    defined?(Airbrake) ? airbrake_request_data : {}
   end
 
   def render_wrong_domain_error

app/lib/api_support/params.rb
@@ -22,7 +22,6 @@ module ApiSupport::Params
     #     /api/foo.xml?foo[bar]=1&foo=2      => 422
     #     /api/foo.xml?foo=5                 => 422
     #
-    # https://3scale.airbrake.io/projects/14982/groups/71345830/notices/1099701031757124010
     def wrap_parameters_check
       option_name = _wrapper_options[:name]
       param_name = params[option_name]

app/lib/backend_client/request.rb
@@ -15,7 +15,7 @@ module BackendClient
     rescue => exception
       # this exception is going to be catched by:
       # https://github.com/rails/rails/blob/83e42d52e37a33682fcac856330fd5d06e5a529c/activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb#L371-L375
-      # thats why we need to fire an airbrake
+      # thats why we need to report an error
       logger.error { exception }
       System::ErrorReporting.report_error(exception, parameters: { method: http_method, url: url, params: params})
       raise(exception)

app/lib/system/error_reporting.rb
@@ -11,12 +11,9 @@ module System
       options
     end
 
-    def report_error(exception, rack_env: nil, logger: Rails.logger, **parameters)
-      options = OPTIONS.call(parameters, rack_env)
-
+    def report_error(exception, logger: Rails.logger, **parameters)
       logger.error('Exception') { {exception: {class: exception.class, message: (exception.try(:message) || exception.to_s), backtrace: (exception.try(:backtrace) || [])[0..3]}, parameters: parameters} }
 
-      ::Airbrake.notify_or_ignore(exception, options) if defined?(Airbrake)
       ::Bugsnag.notify(exception) do |report|
         report.add_tab 'parameter', {parameters: parameters}
       end

app/lib/three_scale/message_bus_config.rb
@@ -24,8 +24,8 @@ module ThreeScale
       MessageBus.timer.on_error do |error|
         System::ErrorReporting.report_error(error)
       end
-      MessageBus.on_middleware_error do |env, error|
-        System::ErrorReporting.report_error(error, rack_env: env)
+      MessageBus.on_middleware_error do |_, error|
+        System::ErrorReporting.report_error(error)
       end
     end
 

commit 876e4493629c061db51cc28180b98470efe4b899

[cleanup] Fix report_error not working
Bug was introduced in https://github.com/3scale/porta/commit/bae5cab756e33c050f5cf5c472c8e0bd6a9bc388#diff-106f134b8214b68c3c60c51eebc241612d3e63fb646162b15deea3fd88341e06R107

app/models/finance/billing_strategy.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Finance::BillingStrategy < ApplicationRecord
+  class BillingError < StandardError
+  end
+
   module NonAuditedColumns
     def non_audited_columns
       super - [inheritance_column]
@@ -93,9 +96,8 @@ class Finance::BillingStrategy < ApplicationRecord
         message = "BillingStrategy #{id}(#{name}) failed utterly"
 
         Rails.logger.error(message)
-        report_error(:error_message => message,
-                     :error_class => 'BillingError',
-                     :exception => e)
+
+        System::ErrorReporting.report_error(e, :error_message => message, :error_class => 'BillingError')
 
         raise e
       end
@@ -174,8 +176,8 @@ class Finance::BillingStrategy < ApplicationRecord
     id_prefix = billing_monthly? ? month : month.begin.year
 
     last_of_period = Invoice.by_provider(account)
-                         .with_normalized_friendly_id(numbering_period, month)
-                         .first
+                            .with_normalized_friendly_id(numbering_period, month)
+                            .first
     order = if last_of_period
               last_of_period.friendly_id.split('-').last
             else
@@ -202,15 +204,15 @@ class Finance::BillingStrategy < ApplicationRecord
   end
 
   def warning(txt, buyer = nil)
-    LogEntry.log( :warning, txt, self.account_id, buyer)
+    LogEntry.log(:warning, txt, self.account_id, buyer)
   end
 
   def error(txt, buyer = nil)
-    LogEntry.log( :error, txt, self.account_id, buyer)
+    LogEntry.log(:error, txt, self.account_id, buyer)
   end
 
   def info(txt, buyer = nil)
-    LogEntry.log( :info, txt, self.account_id, buyer)
+    LogEntry.log(:info, txt, self.account_id, buyer)
   end
 
   protected
@@ -221,7 +223,6 @@ class Finance::BillingStrategy < ApplicationRecord
     now = days.shift
     yield if days.include?(now.day)
   end
-
 
   def bill_expired_trials(buyer, now)
     buyer.billable_contracts_with_trial_period_expired(now - 1.day).find_each(batch_size: 50) do |contract|
@@ -346,8 +347,10 @@ class Finance::BillingStrategy < ApplicationRecord
     @failed_buyers = []
 
     if provider.nil?
-      report_error(:error_message => "WARNING: tried to use billing strategy #{self.id} which has no account",
-                   :error_class => 'InvalidData')
+
+      message = "WARNING: tried to use billing strategy #{self.id} which has no account"
+      exception = BillingError.new message
+      System::ErrorReporting.report_error(exception, :error_message => message, :error_class => 'InvalidData')
       return
     end
 
@@ -363,10 +366,10 @@ class Finance::BillingStrategy < ApplicationRecord
 
         msg = "Failed to bill or charge #{name}(#{buyer_id}) of provider(#{provider_id}): #{exception.message}\n"
         error(msg, buyer)
-        report_error(:error_message => msg,
-                     :error_class => 'BillingError',
-                     :parameters => { :buyer_id => buyer_id, :provider_id => provider_id },
-                     :exception => exception)
+        System::ErrorReporting.report_error(exception, :error_message => msg,
+                                            :error_class => 'BillingError',
+                                            :parameters => { :buyer_id => buyer_id, :provider_id => provider_id }
+        )
 
         @failed_buyers << buyer_id
         raise if Rails.env.test?
@@ -408,7 +411,7 @@ class Finance::BillingStrategy < ApplicationRecord
     end
   end
 
-  extend  FindEachFix
+  extend FindEachFix
   include FindEachFix
 end
 