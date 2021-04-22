d9a9dc3eed266a304ea61c86c4cef3f01f48b59e
Damian Peralta <damianperaltam@gmail.com>
2021-04-22 15:57:18 +0200

    Fixing broken cuke: needs any nonce value

--- app/javascript/src/PaymentGateways/braintree/BraintreeSubmitFields.jsx
+++ app/javascript/src/PaymentGateways/braintree/BraintreeSubmitFields.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const BraintreeSubmitFields = () => {
+const BraintreeSubmitFields = ({onSubmitForm}) => {
   return (
     <div className="form-group">
       <div className="col-md-10 operations">
-        <button className="btn btn-primary pull-right">Save details</button>
+        <button className="btn btn-primary pull-right" onClick={onSubmitForm}>Save details</button>
       </div>
     </div>
   )
