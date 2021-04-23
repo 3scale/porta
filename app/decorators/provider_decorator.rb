a61eee69fd9affbf195e02a9594f15c94fc2081b
josemigallas <josemigallas@gmail.com>
2021-04-23 17:06:13 +0200

    moves logic out of application helper into provider decorator

--- app/decorators/provider_decorator.rb
+++ app/decorators/provider_decorator.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 class ProviderDecorator < ApplicationDecorator
+  include System::UrlHelpers.system_url_helpers
+
   self.include_root_in_json = false
+
+  def new_application_form_data(buyer: nil, service: nil, cinstance: nil)
+    {
+      'create-application-path': buyer ? admin_buyers_account_applications_path(buyer) : admin_buyers_applications_path,
+      'create-application-plan-path': new_admin_service_application_plan_path(':id'),
+      'create-service-plan-path': new_admin_service_service_plan_path(':id'),
+      'service-subscriptions-path': admin_buyers_account_service_contracts_path(':id'),
+      'service-plans-allowed': settings.service_plans.allowed?.to_json,
+      product: service && ServiceDecorator.new(service).new_application_data.to_json,
+      products: !service && application_products_data.to_json,
+      buyer: buyer && BuyerDecorator.new(buyer).new_application_data.to_json,
+      buyers: !buyer && application_buyers_data.to_json,
+      'defined-fields': application_defined_fields_data.to_json,
+      errors: cinstance&.errors.to_json
+    }.compact
+  end
 
   def application_products_data
     accessible_services.order(updated_at: :desc)
