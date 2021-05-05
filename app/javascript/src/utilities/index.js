9aa250db864c94a18dd5eb445ee5c41b5938606a
josemigallas <josemigallas@gmail.com>
2021-05-05 12:33:22 +0200

    adds an index file for nicer imports

---
+++ app/javascript/src/utilities/index.js
@@ -0,0 +1,13 @@
+// @flow
+
+export * from './ajax'
+export * from './alert'
+export * from './confirm-dialog'
+export * from './createReactWrapper'
+export * from './CSRFToken'
+export * from './fetchData'
+export * from './ie11Utils'
+export * from './json-utils'
+export * from './patternfly-utils'
+export * from './toggle'
+export * from './useClickOutside'
