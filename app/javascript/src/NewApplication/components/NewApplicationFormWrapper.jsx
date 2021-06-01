90d7e49f62d0c94166c3e4cd01b2b982c7e824ea
josemigallas <josemigallas@gmail.com>
2021-06-01 09:54:36 +0200

    [new-application] updates Flow and moves wrapper into NewApplicationForm

--- app/javascript/src/NewApplication/components/NewApplicationFormWrapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import { NewApplicationForm } from 'NewApplication'
-import { createReactWrapper } from 'utilities'
-
-const NewApplicationFormWrapper = (props, containerId) => createReactWrapper(<NewApplicationForm {...props} />, containerId)
-
-export { NewApplicationFormWrapper }
