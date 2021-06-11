7127751ea4086a78f5334636106fe67b74f71522
Damian Peralta <damianperaltam@gmail.com>
2021-06-11 17:43:41 +0200

    Adding UI test

---
+++ test/functional/sites/emails_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class Sites::EmailsControllerTest < ActionController::TestCase
+
+  def setup
+    @provider  = FactoryBot.create(:provider_account)
+    @service = @provider.default_service
+    request.host = @provider.admin_domain
+    login_as(@provider.admins.first)
+  end
+
+  test 'show Service name sanitized' do
+    service_name_xss = "API<script>alert('XSS')</script>"
+    service_name_xss_sanitized = "APIalert('XSS')"
+    @service.name = service_name_xss
+    @service.save!
+
+    get :edit
+    assert_response :success
+    assert_select '#account_service_support_email_input label', "Service #{ service_name_xss_sanitized }"
+  end
+
+end
