17e4fb6ca82525f3e422cf98f0882c5e55d27fbc
josemigallas <josemigallas@gmail.com>
2021-06-24 11:09:44 +0200

    rearranges functional and integration tests according to new applications controllers

--- test/functional/buyers/applications_controller_test.rb
+++ test/functional/buyers/applications_controller_test.rb
@@ -36,14 +36,6 @@ class Buyers::ApplicationsControllerTest < ActionController::TestCase
     assert_response :redirect
   end
 
-  test 'update application' do
-    @service.update_attribute :default_application_plan, @plan
-    app = FactoryBot.create(:application_contract, :plan => @plan)
-
-    put :update, :id => app.id, :cinstance => { :name => 'whatever' }
-    assert_response :redirect
-  end
-
   test "creates app with a specific service_plan" do
     service_plan = FactoryBot.create(:service_plan, service: @service)
     service_plan2 = FactoryBot.create(:service_plan, service: @service)
@@ -75,57 +67,6 @@ class Buyers::ApplicationsControllerTest < ActionController::TestCase
 
     buyer.reload
     assert_equal buyer.bought_service_contracts.count, 1
-  end
-
-  # Regression test of https://github.com/3scale/system/issues/1354
-  #
-  # TODO: this should be integration test
-  #
-  test 'change plan should correctly mark paid_until' do
-    app = FactoryBot.create(:application_contract, :plan => @plan, :paid_until => Date.new(2001,1,10))
-    app.update_attribute :trial_period_expires_at, nil
-    new_plan = FactoryBot.create(:published_plan, :issuer => @service, :cost_per_month => 300)
-
-    @provider.timezone = 'Mountain Time (US & Canada)'
-    @provider.save!
-    @provider.settings.allow_finance!
-    @provider.reload.billing_strategy.update_attribute(:prepaid, true)
-
-    Timecop.freeze(Date.new(2001,1,25)) do
-      put :change_plan, :id => app.id, :cinstance => { :plan_id => new_plan.id }
-    end
-
-    assert_response :redirect
-    assert_equal app.reload.plan, new_plan
-    assert_equal Date.new(2001,1,31), app.reload.paid_until.to_date
-  end
-
-  test 'change_plan should email provider with link to app page' do
-    Logic::RollingUpdates.expects(skipped?: true).at_least_once
-
-    app = FactoryBot.create :application_contract, :name => "desto mehr", :plan => @plan
-    new_plan = FactoryBot.create :published_plan, :issuer => @service
-
-    ActionMailer::Base.deliveries = []
-    put :change_plan, :id => app.id, :cinstance => { :plan_id => new_plan.id }
-
-    assert_equal app.reload.plan, new_plan
-    assert mail = ActionMailer::Base.deliveries.first, 'missing email'
-    assert_match admin_service_application_url(app.service, app, host: @provider.self_domain), mail.body.to_s
-  end
-
-  #regression test for https://github.com/3scale/system/issues/1889
-  test 'change plan should work even when cinstance misses description' do
-    app = FactoryBot.create(:application_contract, :plan => @plan, :name => "app name", :description => nil)
-    new_plan = FactoryBot.create :published_plan, :issuer => @service
-
-    @provider.settings.allow_multiple_applications!
-    @provider.settings.show_multiple_applications!
-
-    put :change_plan, :id => app.id, :cinstance => { :plan_id => new_plan.id }
-
-    assert_response :redirect
-    assert_equal app.reload.plan, new_plan
   end
 
   class NoTransaction < ActionController::TestCase
