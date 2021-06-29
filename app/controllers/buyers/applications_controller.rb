abcb973fbaa5d9a62860be1871300cb6d40d9da1
josemigallas <josemigallas@gmail.com>
2021-06-29 17:26:44 +0200

    removes index, create and new from the module

--- app/controllers/buyers/applications_controller.rb
+++ app/controllers/buyers/applications_controller.rb
@@ -7,6 +7,8 @@ class Buyers::ApplicationsController < FrontendController
   before_action :authorize_partners
   before_action :find_plans
   before_action :find_buyer
+  before_action :find_states, only: :index
+  before_action :find_applications, only: :index
   before_action :authorize_multiple_applications, only: :create
   before_action :find_application_plan, only: :create
   before_action :find_service, only: :create
@@ -17,15 +19,14 @@ class Buyers::ApplicationsController < FrontendController
 
   activate_menu :buyers, :accounts, :listing
 
+  def index; end
+
   def new
     @cinstance = @account.bought_cinstances.build
     extend_cinstance_for_new_plan
   end
 
-  # Create is handled by each individual controller so that it don't render a different 'new' template in case of an error
   def create
-    super
-
     if @cinstance.save
       flash[:notice] = 'Application was successfully created.'
       redirect_to provider_admin_application_path(@cinstance)
