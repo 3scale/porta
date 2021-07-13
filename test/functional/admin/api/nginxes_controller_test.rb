c2044ba2c825ddff68b7bb4caef1700d56a4b1a2
jsmadis <jakub.smadis@gmail.com>
2021-07-13 12:00:09 +0200

    Remove test for the show method of the nginxes controller

--- test/functional/admin/api/nginxes_controller_test.rb
+++ test/functional/admin/api/nginxes_controller_test.rb
@@ -5,23 +5,6 @@ class Admin::Api::NginxesControllerTest < ActionController::TestCase
     Logic::RollingUpdates.stubs(skipped?: true)
   end
 
-  test 'show' do
-    provider = FactoryBot.create(:provider_account, domain: 'provider.example.com')
-    host! provider.admin_domain
-
-    get :show, format: :zip, provider_key: provider.api_key
-
-    assert_response :success
-    assert_equal 'application/zip', response.content_type
-    assert_includes response.headers, 'Content-Transfer-Encoding', 'Content-Disposition'
-    assert_equal 'attachment; filename="proxy_configs.zip"', response['Content-Disposition']
-    assert_equal 'binary', response['Content-Transfer-Encoding']
-
-    Zip::InputStream.open(StringIO.new(response.body)) do |zip|
-      assert zip.get_next_entry
-    end
-  end
-
   test 'spec returns a json' do
     provider = FactoryBot.create(:provider_account, domain: 'provider.example.com')
     host! provider.admin_domain
