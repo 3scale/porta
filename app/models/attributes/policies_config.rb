c3d0e6d676af343be40925a587cc03a960b72144
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-07-28 17:38:53 +0300

    ugly workaround for Oracle enhanced adapter CLOB field

--- app/models/attributes/policies_config.rb
+++ app/models/attributes/policies_config.rb
@@ -2,11 +2,18 @@
 
 class Attributes::PoliciesConfig < ActiveRecord::Type::Text
   def cast(value)
+    if :oracle == System::Database.adapter.to_sym && value.is_a?(ActiveRecord::OracleEnhanced::Type::Text::Data)
+      value = value.to_s
+    end
     Proxy::PoliciesConfig.new(value)
   end
 
   def serialize(value)
-    value.to_json
+    if :oracle == System::Database.adapter.to_sym
+      ActiveRecord::OracleEnhanced::Type::Text::Data.new(value.presence&.to_json)
+    else
+      value.to_json
+    end
   end
 
   def changed_in_place?(raw_old_value, new_value)
