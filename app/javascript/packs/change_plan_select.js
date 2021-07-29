7c4de99739b8bb45911a56a5e068985603cfedf2
josemigallas <josemigallas@gmail.com>
2021-07-29 13:48:30 +0200

    implements new ChangePlan component for Application show page

--- app/javascript/packs/change_plan_select.js
+++ app/javascript/packs/change_plan_select.js
@@ -3,7 +3,7 @@
 import { ChangePlanSelectCardWrapper } from 'Plans'
 import { safeFromJsonString } from 'utilities'
 
-import type { Product, Plan } from 'Types'
+import type { Plan } from 'Types'
 
 document.addEventListener('DOMContentLoaded', () => {
   const containerId = 'change_plan_select'
@@ -14,15 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const { dataset } = container
-  // $FlowIgnore[incompatible-cast] we can safely assume service is not undefined
-  const service = (safeFromJsonString<Product>(dataset.service): Product)
-  const appPlans = safeFromJsonString<Plan[]>(dataset.applicationPlans) || []
-  const initialSelectedPlan = safeFromJsonString<Plan>(dataset.currentPlan) || null
+  const applicationPlans = safeFromJsonString<Plan[]>(dataset.applicationPlans) || []
   const path: string = dataset.path
 
   ChangePlanSelectCardWrapper({
-    initialSelectedPlan,
-    product: { ...service, appPlans },
+    applicationPlans,
     path
   }, containerId)
 })
