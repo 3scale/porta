a8adc87ac48b25422ef09c6e5b8635ac4955effe
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-08-16 16:53:30 +0300

    changes of rails app:update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -15,6 +14,13 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
+  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
+  # `config/secrets.yml.key`.
+  # config.read_encrypted_secrets = true
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
   # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
@@ -22,6 +28,7 @@ Rails.application.configure do
   }
 
   # Compress JavaScripts and CSS.
+  # TODO: switch to ruby-terser: https://github.com/lautis/uglifier/commit/902cc160d3f8f405a35a8f16da227489366a3ad6
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
@@ -81,7 +88,7 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
