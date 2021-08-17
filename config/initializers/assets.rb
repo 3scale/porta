a5eca1374399494ba0fd0a8442f80a100fbdc7f9
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-08-17 12:47:11 +0300

    font_assets gem is redundant with rack-cors
    For a reference, gem was introduced with (bb04f2f8b82 and 8708422380c).

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
