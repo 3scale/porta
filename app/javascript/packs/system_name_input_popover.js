694dec31be625cf4600da3eb6f2940fbbc951d3b
josemigallas <josemigallas@gmail.com>
2021-08-18 16:22:48 +0200

    adds info popover to new metric/method forms

---
+++ app/javascript/packs/system_name_input_popover.js
@@ -0,0 +1,14 @@
+// @flow
+
+import { SystemNamePopoverWrapper } from 'Common'
+
+document.addEventListener('DOMContentLoaded', function () {
+  const containerId = 'system_name_popover'
+  const container = document.getElementById(containerId)
+
+  if (!container) {
+    return
+  }
+
+  SystemNamePopoverWrapper(container)
+})
