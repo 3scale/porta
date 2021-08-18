3d6f3aa33ff96c9c81d3d7ff0f7d4b439689a833
josemigallas <josemigallas@gmail.com>
2021-08-18 16:22:48 +0200

    adds info popover to new metric/method forms

---
+++ app/javascript/src/Common/components/SystemNamePopover.jsx
@@ -0,0 +1,24 @@
+// @flow
+
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import { Popover } from '@patternfly/react-core'
+
+import './SystemNamePopover.scss'
+
+const SystemNamePopover = (): React.Node => (
+  <Popover
+    maxWidth="420px"
+    aria-label="system name info popover"
+    bodyContent={
+      <div style={{ textAlign: 'start' }}>The system name of methods and metrics includes a numeric string that identifies the backend they are mapped to. You cannot modify this backend identifier.</div>
+    }
+  >
+    <i className="fa fa-question-circle-o"/>
+  </Popover>
+)
+
+const SystemNamePopoverWrapper = (container: Element): React.Node => render(<SystemNamePopover />, container)
+
+export { SystemNamePopoverWrapper }
