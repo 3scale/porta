9f28655c7cb450df17ed0230fbb7f44b5aee5934
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-08-24 11:27:13 +0300

    remove Mail eager loading workaround

--- config/initializers/mail_eager_autoload.rb
+++ config/initializers/mail_eager_autoload.rb
@@ -1,3 +1,3 @@
 # https://github.com/mikel/mail/issues/912
 # uninitialized constant Mail::Parsers::ContentTypeParser
-Mail.eager_autoload! if System::Application.config.eager_load
+# Mail.eager_autoload! if System::Application.config.eager_load
