4f7877e5be940dec1ed9b43002162f794e929f79
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-08-24 12:11:19 +0300

    webpacker does not want us add to assets_path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,11 +3,12 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# We do not add to asset paths because or a webpacker compatibility
+# https://github.com/rails/webpacker/issues/1285#issuecomment-539553734
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-# TODO: is next line needed for us?
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
