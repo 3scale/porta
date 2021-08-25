917a0e8c7a803a67e18291cc488fa4d3608eae76
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-08-25 15:30:31 +0300

    properly filter parameters of BuyersApplicationPlansController

--- app/controllers/admin/api/buyers_application_plans_controller.rb
+++ app/controllers/admin/api/buyers_application_plans_controller.rb
@@ -65,6 +65,6 @@ class Admin::Api::BuyersApplicationPlansController < Admin::Api::BuyersBaseContr
 
   def application_plan_params
     attributes = current_account.fields.for(Cinstance)
-    params.slice(*attributes)
+    params.permit(*attributes)
   end
 end
