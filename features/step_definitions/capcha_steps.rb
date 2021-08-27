aa201b9bae201affe5b17b9d559d729b9553fda5
Damian Peralta <damianperaltam@gmail.com>
2021-08-27 15:35:37 +0200

    Deleting unused captcha methods

--- features/step_definitions/capcha_steps.rb
+++ features/step_definitions/capcha_steps.rb
@@ -13,20 +13,3 @@ end
 Then /^I should see the captcha$/ do
   page.should have_selector(RECAPTCHA_SCRIPT, visible: false)
 end
-
-When /^I leave the captcha empty$/ do
-  captcha_evaluates_to false
-end
-
-When /^I fill in the captcha incorrectly$/ do
-  captcha_evaluates_to false
-end
-
-When /^I fill in the captcha correctly$/ do
-  captcha_evaluates_to true
-end
-
-Then /^I should see captcha check fail$/ do
-  page.should have_content('Word verification response is incorrect, please try again.')
-end
-
