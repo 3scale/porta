a0ad6455af65fcc747a659fd7369e3ec4f7e770d
Hallelujah <hery@rails-royce.org>
2021-08-30 11:16:06 +0200

    [fix] cucumber step legal terms missing ':'
    So it passes

--- features/step_definitions/legal_terms_steps.rb
+++ features/step_definitions/legal_terms_steps.rb
@@ -4,7 +4,7 @@ Given "{provider} has no legal terms" do |provider|
   provider.builtin_legal_terms.delete_all
 end
 
-Given "{provider} has service subscription legal terms" do |provider, text|
+Given "{provider} has service subscription legal terms:" do |provider, text|
   provider.builtin_legal_terms.create!(system_name: CMS::Builtin::LegalTerm::SUBSCRIPTION_SYSTEM_NAME,
                                       published: text)
 end
