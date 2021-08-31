755dc3326341fe89da99a3b796fc4f3f96cf1f34
Damian Peralta <damianperaltam@gmail.com>
2021-08-31 15:05:02 +0200

    Refactor: validating form with hidden input

--- features/step_definitions/capcha_steps.rb
+++ features/step_definitions/capcha_steps.rb
@@ -6,10 +6,14 @@ Then /^I should not see the captcha$/ do
   page.should_not have_selector(RECAPTCHA_SCRIPT)
 end
 
-Then /^I should see submit button disabled$/ do
-  page.should have_css('input[type="submit"][disabled]')
-end
-
 Then /^I should see the captcha$/ do
   page.should have_selector(RECAPTCHA_SCRIPT, visible: false)
+end
+
+Then /^I should see submit button (disabled|enabled)$/ do |value|
+  if value == 'disabled'
+    page.should have_css("input[type='submit'][disabled]")
+  else
+    page.should_not have_css("input[type='submit'][disabled]")
+  end
 end
