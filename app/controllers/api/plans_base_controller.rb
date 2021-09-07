2fdb946eee0854f495226f2b1ccf7b72bd5a4e18
josemigallas <josemigallas@gmail.com>
2021-09-07 13:49:32 +0200

    order all plans alphabetically by default

--- app/controllers/api/plans_base_controller.rb
+++ app/controllers/api/plans_base_controller.rb
@@ -44,7 +44,7 @@ class Api::PlansBaseController < Api::BaseController
   def find_plans
     search = ThreeScale::Search.new(params[:search] || params)
     @plans = collection.not_custom
-                       .order_by(params[:sort], params[:direction])
+                       .order_by(params[:sort] || :name, params[:direction] || :asc)
                        .scope_search(search)
     @page_plans = @plans.paginate(pagination_params)
   end
