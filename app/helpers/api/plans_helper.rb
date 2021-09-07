83b4167c1e032a3b6d26bfd9c41a4da39b5d68ff
josemigallas <josemigallas@gmail.com>
2021-09-07 16:11:53 +0200

    fixes application plans order (application overview vs. application plans index)

--- app/helpers/api/plans_helper.rb
+++ app/helpers/api/plans_helper.rb
@@ -4,7 +4,7 @@ module Api::PlansHelper
 
   def change_application_plan_data(application)
     {
-      'application-plans': application_plans_data(application.available_application_plans),
+      'application-plans': application.available_application_plans.order_by(:name, :asc).to_json(root: false, only: %i[id name]),
       'path': change_plan_provider_admin_application_path
     }
   end
@@ -12,15 +12,10 @@ module Api::PlansHelper
   def default_application_plan_data(service, plans)
     {
       'service': service.to_json(root: false, only: %i[id name]),
-      'application-plans': application_plans_data(plans),
+      'application-plans': plans.to_json(root: false, only: %i[id name]),
       'current-plan': current_application_plan_data(service),
       'path': masterize_admin_service_application_plans_path(':id')
     }
-  end
-
-  def application_plans_data(plans)
-    plans.order(name: :asc)
-         .to_json(root: false, only: %i[id name])
   end
 
   def current_application_plan_data(service)
