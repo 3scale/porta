2864e2403edf425431361b571fd8127ee477d4fb
Hallelujah <hery@rails-royce.org>
2021-09-10 12:44:16 +0300

    Fix integration tests for ObjectsController
    - testing with provider key
    - testing against a non admin

--- test/integration/admin/api/objects_controller_test.rb
+++ test/integration/admin/api/objects_controller_test.rb
@@ -5,7 +5,8 @@ class Admin::Api::ObjectsControllerTest < ActionDispatch::IntegrationTest
   def setup
     @provider = FactoryBot.create(:provider_account)
     @service = @provider.default_service
-    @token = FactoryBot.create(:access_token, owner: @provider.admin_users.first!, scopes: %w[account_management]).value
+    @access_token = FactoryBot.create(:access_token, owner: @provider.admin_users.first!, scopes: %w[account_management])
+    @token = @access_token.value
 
     host! @provider.admin_domain
   end
@@ -32,14 +33,15 @@ class Admin::Api::ObjectsControllerTest < ActionDispatch::IntegrationTest
   end
 
   def test_status_not_admin
-    User.any_instance.expects(:admin?).returns(false)
+    simple_user = FactoryBot.create(:simple_user, account: @provider)
+    @access_token.update(owner: simple_user)
     get admin_api_objects_status_path(object_type: 'service', object_id: @service.id, access_token: @token)
     assert_response 403
   end
 
+  # This is to use a test with the provider_key which has no a user logged in
   def test_status_no_current_user
-    Admin::Api::ObjectsController.any_instance.expects(:current_user).returns(nil)
-    get admin_api_objects_status_path(object_type: 'service', object_id: @service.id, access_token: @token)
+    get admin_api_objects_status_path(object_type: 'service', object_id: @service.id, provider_key: @provider.provider_key)
     assert_response 403
   end
 
