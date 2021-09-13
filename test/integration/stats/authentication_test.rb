ba2bfc42af992201363e70fadb0a3711195d5875
jsmadis <jakub.smadis@gmail.com>
2021-09-13 17:04:06 +0200

    Revert "Fixed the stats integration tests and moved some of them to the /admin/api test folder"
    This reverts commit f1230ad52d02466b07195f8bef38a197430427b5.

--- test/integration/stats/authentication_test.rb
+++ test/integration/stats/authentication_test.rb
@@ -10,22 +10,22 @@ class Stats::AuthenticationTest < ActionDispatch::IntegrationTest
 
   test 'access allowed with authentication' do
     params = { period: 'day', metric_name: 'hits' }
-    get "/admin/api/stats/services/#{@service.id}/usage.json", params.merge(provider_key: @provider_account.api_key)
+    get "/stats/services/#{@service.id}/usage.json", params.merge(provider_key: @provider_account.api_key)
     assert_response :success
     assert_content_type 'application/json'
 
     token = FactoryBot.create(:access_token, owner: @provider_account.first_admin, scopes: ['stats'])
-    get "/admin/api/stats/services/#{@service.id}/usage.json", params.merge(access_token: token.value)
+    get "/stats/services/#{@service.id}/usage.json", params.merge(access_token: token.value)
     assert_response :success
     assert_content_type 'application/json'
   end
 
-  test 'access denied without authentication' do
-    get "/admin/api/stats/services/#{@service.id}/usage.json", period: 'day', metric_name: "hits"
+  test 'access forbidden without authentication' do
+    get "/stats/services/#{@service.id}/usage.json", period: 'day', metric_name: "hits"
 
     assert_response :forbidden
     assert_content_type 'application/json'
-    assert_json 'error' => 'Access denied'
+    assert_json 'status' => 'Forbidden'
   end
 
   # Regression test - DoubleRender error
