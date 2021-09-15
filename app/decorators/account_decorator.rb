168c9d9ace5c69f701ae6a6a209dfcadaf5cce91
josemigallas <josemigallas@gmail.com>
2021-09-15 11:55:44 +0200

    merges buyer and account decorators, and adds missing data for new application page

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -3,9 +3,30 @@
 class AccountDecorator < ApplicationDecorator
   delegate :display_name, :email, to: :admin_user, prefix: true
 
+  self.include_root_in_json = false
+
+  def new_application_data
+    {
+      id: id.to_s,
+      name: name,
+      description: "Admin: #{admin_user_display_name}",
+      createdAt: created_at.to_s(:long),
+      contractedProducts: contracts,
+      createApplicationPath: h.admin_buyers_account_applications_path(object),
+      multipleAppsAllowed: multiple_applications_allowed?
+    }
+  end
+
   private
 
   def admin_user
     @admin_user ||= (super || User.new).decorate
+  end
+
+  def contracts
+    bought_service_contracts.map do |contract|
+      hash = contract.service.as_json(only: %i[id name], root: false)
+      hash['withPlan'] = contract.plan.as_json(only: %i[id name], root: false)
+    end
   end
 end
