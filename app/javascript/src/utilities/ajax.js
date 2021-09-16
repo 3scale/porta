327844a83d1805a44f0c9a7841572a016ad44604
josemigallas <josemigallas@gmail.com>
2021-09-16 10:30:06 +0200

    provides a way to abort ongoing ajax requests

--- app/javascript/src/utilities/ajax.js
+++ app/javascript/src/utilities/ajax.js
@@ -4,12 +4,15 @@ export type Method = 'GET' | 'POST' | 'DELETE'
 
 type FetchFunction = (url: string, method: Method, body?: URLSearchParams) => Promise<Response>
 
+const { signal, abort }: AbortController = new AbortController()
+
 const _ajax = (headers: { [key: string]: string }) => {
   const meta = document.querySelector('meta[name="csrf-token"]')
   const token = (meta && meta.getAttribute('content')) || ''
 
   return function (url, method, body?) {
     return fetch(url, {
+      signal,
       method: method,
       headers: { ...headers, 'X-CSRF-Token': token },
       body
@@ -20,4 +23,4 @@ const _ajax = (headers: { [key: string]: string }) => {
 const ajax: FetchFunction = _ajax({ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' })
 const ajaxJSON: FetchFunction = _ajax({ 'Content-Type': 'application/json; charset=UTF-8' })
 
-export { ajax, ajaxJSON }
+export { ajax, ajaxJSON, abort as ajaxAbort }
