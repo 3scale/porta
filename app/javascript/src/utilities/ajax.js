37417ba15e7e962cefee46d84e126a5a5877f00c
josemigallas <josemigallas@gmail.com>
2021-09-16 16:45:18 +0200

    makes ajax signature more flexible

--- app/javascript/src/utilities/ajax.js
+++ app/javascript/src/utilities/ajax.js
@@ -5,15 +5,12 @@ export type Method = 'GET' | 'POST' | 'DELETE'
 type FetchOptions = { method: Method, body?: URLSearchParams, signal?: AbortSignal }
 type FetchFunction = (url: string, opts: FetchOptions) => Promise<Response>
 
-const { signal, abort }: AbortController = new AbortController()
-
 const _ajax = (headers: { [key: string]: string }) => {
   const meta = document.querySelector('meta[name="csrf-token"]')
   const token = (meta && meta.getAttribute('content')) || ''
 
   return function (url, { method, body, signal }) {
     return fetch(url, {
-      signal,
       method: method,
       headers: { ...headers, 'X-CSRF-Token': token },
       body,
@@ -25,4 +22,4 @@ const _ajax = (headers: { [key: string]: string }) => {
 const ajax: FetchFunction = _ajax({ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' })
 const ajaxJSON: FetchFunction = _ajax({ 'Content-Type': 'application/json; charset=UTF-8' })
 
-export { ajax, ajaxJSON, abort as ajaxAbort }
+export { ajax, ajaxJSON }
