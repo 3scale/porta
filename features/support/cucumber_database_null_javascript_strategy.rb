461ab6ab03de10bfbee8ea40035863174e73b6e8
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-09-20 17:06:02 +0300

    DatabaseCleaner strategy simple switching
    Presently cucumber-rails is exercising special treatment of `@javascript`
    tagged scenarios [1] wrt connection sharing between threads by
    pre-defined hooks [2].
    
    To prevent that and allow switching between desired mode of operation
    depending on scenario tags, we implement a custom clean-up strategy class and change DatabaseCleaner strategy in our own hooks.
    
    [1] https://github.com/cucumber/cucumber-rails/blob/561ee249c418f1c8cd7b629e7cf414442ce62709/features/choose_javascript_database_strategy.feature
    [2]
    https://github.com/cucumber/cucumber-rails/blob/561ee249c418f1c8cd7b629e7cf414442ce62709/lib/cucumber/rails/hooks/active_record.rb

---
+++ features/support/cucumber_database_null_javascript_strategy.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module ThreeScale
+  class CucumberDatabaseNullJavascriptStrategy
+    def before_js; end
+
+    def before_non_js; end
+
+    def after; end
+  end
+end
