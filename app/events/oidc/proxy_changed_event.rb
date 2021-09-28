8bbee80ed48653f1ce3f9e250a6441bea464a92c
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-09-28 13:39:03 +0300

    wip attribute_changed? deprecations

--- app/events/oidc/proxy_changed_event.rb
+++ app/events/oidc/proxy_changed_event.rb
@@ -21,6 +21,6 @@ class OIDC::ProxyChangedEvent < BaseEventStoreEvent
   def self.valid?(proxy)
     service = proxy.try(:service)
     return unless service
-    service.backend_version.oauth? || service.backend_version_change&.include?('oauth')
+    service.backend_version.oauth? || service.saved_changes&.include?('oauth')
   end
 end
