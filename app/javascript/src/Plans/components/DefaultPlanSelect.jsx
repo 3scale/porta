2ae066c9b84c5d31c375aaf596c8acfca5e7eaf1
josemigallas <josemigallas@gmail.com>
2021-09-29 11:51:14 +0200

    updates DefaultPlanSelect with new Select props

--- app/javascript/src/Plans/components/DefaultPlanSelect.jsx
+++ app/javascript/src/Plans/components/DefaultPlanSelect.jsx
@@ -1,13 +1,9 @@
 // @flow
 
 import * as React from 'react'
-import { Select, SelectVariant } from '@patternfly/react-core'
-import {
-  toSelectOptionObject,
-  toSelectOption,
-  SelectOptionObject,
-  handleOnFilter
-} from 'utilities'
+
+import { Select } from 'Common'
+import { toSelectOption } from 'utilities'
 
 import './DefaultPlanSelect.scss'
 
@@ -17,43 +13,35 @@ type Props = {
   plan: Plan,
   plans: Plan[],
   onSelectPlan: Plan => void,
-  isDisabled?: boolean
+  isLoading?: boolean
 }
 
-const DefaultPlanSelect = ({ plan, plans, onSelectPlan, isDisabled = false }: Props): React.Node => {
-  // $FlowIssue[prop-missing] description is optional
-  // $FlowIssue[incompatible-call] should not complain about plan having id as number, since Record has union "number | string"
-  const [selection, setSelection] = React.useState<SelectOptionObject | null>(toSelectOptionObject(plan))
-  const [isExpanded, setIsExpanded] = React.useState(false)
-
-  const onSelect = (_e, option: SelectOptionObject) => {
-    setSelection(option)
-    setIsExpanded(false)
-
-    const newPlan = plans.find(p => p.id.toString() === option.id)
+const DefaultPlanSelect = ({ plan, plans, onSelectPlan, isLoading = false }: Props): React.Node => {
+  const onSelect = (newPlan: Plan | null) => {
     if (newPlan) {
       onSelectPlan(newPlan)
     }
   }
 
   return (
+    // $FlowIssue[prop-missing] missing props are optional
+    // $FlowIssue[incompatible-type-arg] should not complain about plan having id as number, since Record has union "number | string"
     <Select
-      id="default-plan-select"
-      variant={SelectVariant.typeahead}
-      aria-label="Select application plan"
-      placeholderText="Select application plan"
-      onToggle={() => setIsExpanded(!isExpanded)}
+      // $FlowFixMe[incompatible-type] this is nonsense
+      item={plan}
+      items={plans}
       onSelect={onSelect}
-      onClear={() => setSelection(null)}
-      isExpanded={isExpanded}
-      isDisabled={isDisabled}
-      selections={selection}
-      isCreatable={false}
-      // $FlowIssue[prop-missing] description is optional
-      // $FlowIssue[incompatible-call] should not complain about plan having id as number, since Record has union "number | string"
-      onFilter={handleOnFilter(plans)}
+      label="Default plan"
+      fieldId="application_plan_id"
+      name=""
+      placeholderText="Select application plan"
+      isDisabled={isLoading}
+      aria-label="Select application plan"
+      helperText="Default application plan (if any) is selected automatically upon service subscription."
+      isClearable={false}
+      isLoading={isLoading}
     >
-      {/* $FlowIssue[prop-missing] description is optional */}
+      {/* $FlowIssue[prop-missing] missing props are optional */}
       {/* $FlowIssue[incompatible-call] should not complain about plan having id as number, since Record has union "number | string" */}
       {plans.map(toSelectOption)}
     </Select>
