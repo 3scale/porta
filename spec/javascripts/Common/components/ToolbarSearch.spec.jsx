b4a6b8f4116de598e836dc6df05bcc8e59120175
josemigallas <josemigallas@gmail.com>
2021-10-01 18:43:33 +0200

    updates tests

--- spec/javascripts/Common/components/ToolbarSearch.spec.jsx
+++ spec/javascripts/Common/components/ToolbarSearch.spec.jsx
@@ -5,9 +5,23 @@ import { mount } from 'enzyme'
 
 import { ToolbarSearch } from 'Common'
 
-const mountWrapper = (props) => mount(<ToolbarSearch />)
+const defaultProps = {
+  placeholder: ''
+}
+
+const mountWrapper = (props) => mount(<ToolbarSearch {...{ ...defaultProps, ...props }} />)
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
 
 it('should render itself', () => {
   const wrapper = mountWrapper()
   expect(wrapper.exists()).toBe(true)
+})
+
+it('should have a placeholder', () => {
+  const placeholder = 'Find something'
+  const wrapper = mountWrapper({ placeholder })
+  expect(wrapper.find(`input[placeholder="${placeholder}"]`).exists()).toBe(true)
 })
