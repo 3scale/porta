82ce3d18201bcc5db89ecb6caf4d57809dc53163
Luis Villén <l.villenjaldon@gmail.com>
2021-10-06 14:52:56 +0200

    THREESCALE-7576 disables plan select when no plans

--- spec/javascripts/NewApplication/components/ApplicationPlanSelect.spec.jsx
+++ spec/javascripts/NewApplication/components/ApplicationPlanSelect.spec.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { ApplicationPlanSelect } from 'NewApplication'
 import { mount } from 'enzyme'
 
-const product = { id: '0', name: 'API Product', description: 'api-product', updatedAt: '1 Jan 2021', appPlans: [{ id: 0, name: 'The Plan' }], servicePlans: [] }
+const product = { id: '0', name: 'API Product', description: 'api-product', updatedAt: '1 Jan 2021', appPlans: [{ id: 0, name: 'The Plan' }], servicePlans: [], buyerCanSelectPlan: true }
 const props = {
   product,
   appPlan: { id: 0, name: 'The Plan' },
@@ -17,4 +17,23 @@ const props = {
 it('should render', () => {
   const wrapper = mount(<ApplicationPlanSelect {...props} />)
   expect(wrapper.exists()).toBe(true)
+})
+
+it('should be disabled when application plans is empty', () => {
+  const newProps = {
+    ...props,
+    product: {
+      ...product,
+      appPlans: []
+    }
+  }
+  const wrapper = mount(<ApplicationPlanSelect {...newProps} />)
+  expect(wrapper.find('.pf-c-select__toggle-typeahead').props().disabled).toBe(true)
+  expect(wrapper.find('.pf-c-select__toggle-button').props().disabled).toBe(true)
+})
+
+it('should not be disabled when application plans is not empty', () => {
+  const wrapper = mount(<ApplicationPlanSelect {...props} />)
+  expect(wrapper.find('.pf-c-select__toggle-typeahead').props().disabled).toBe(false)
+  expect(wrapper.find('.pf-c-select__toggle-button').props().disabled).toBe(false)
 })
