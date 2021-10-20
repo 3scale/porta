2b2b036f6e0525ee0d0360df11d875a574d6a020
josemigallas <josemigallas@gmail.com>
2021-10-20 17:13:56 +0200

    [test/integration] ✅ fixes deprecation params as positional arg, and request_via_redirect 🚨 fixes lint 🎨 improves code format ♻️ use named routes vs. hardcoded

--- test/integration/by_sso_token_test.rb
+++ test/integration/by_sso_token_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ApiAuthentication::BySsoTokenTest < ActionDispatch::IntegrationTest
@@ -10,24 +12,24 @@ class ApiAuthentication::BySsoTokenTest < ActionDispatch::IntegrationTest
     # apicast mapping service use case
     FactoryBot.create(:active_admin, account: @account, username: ThreeScale.config.impersonation_admin['username'])
     host! Account.master.admin_domain
-    post '/admin/api/sso_tokens/provider_create.json', provider_key: @master.api_key, provider_id: @account.id
+    post provider_create_admin_api_sso_tokens_path(format: :json), params: { provider_key: @master.api_key, provider_id: @account.id }
     sso_token = JSON.parse(response.body)['sso_token']
 
     host! @account.admin_domain
 
     # all good, all fine, life's awesome, i'm making a soup
     params = { host: 'http://example.com', token: sso_token['token'] }
-    get "/admin/api/services/proxy/configs/production.json?#{params.to_query}"
+    get admin_api_proxy_configs_path(:production, format: :json, params: params)
     assert_response :success
 
     # token parameter is missing
     params = { host: 'http://example.com', token: '' }
-    get "/admin/api/services/proxy/configs/production.json?#{params.to_query}"
+    get admin_api_proxy_configs_path(:production, format: :json, params: params)
     assert_response :forbidden
 
     # user not found - token does not exist
     params = { host: 'http://example.com', token: 'alaska' }
-    get "/admin/api/services/proxy/configs/production.json?#{params.to_query}"
+    get admin_api_proxy_configs_path(:production, format: :json, params: params)
     assert_response :forbidden
   end
 end
