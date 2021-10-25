64cf78d3c8923797f3db4f57cc4433de0250c030
Nidhi Soni <nidhisoni4411@gmail.com>
2021-10-25 19:42:01 +0530

    3scale-managed gateway allows localhost domain

--- app/models/proxy.rb
+++ app/models/proxy.rb
@@ -42,6 +42,8 @@ class Proxy < ApplicationRecord
 
   HTTP_HEADER =  /\A[{}\[\]\d,.;@#~%&()?\w_"= \/\\:-]+\Z/
 
+  LOCALHOST =  /\A^((?!localhost)[\s\S])*\z/
+
   OIDC_ISSUER_TYPES = {
     keycloak: I18n.t(:keycloak, scope: 'proxy.oidc_issuer_type').freeze,
     rest: I18n.t(:rest, scope: 'proxy.oidc_issuer_type').freeze,
@@ -53,6 +55,9 @@ class Proxy < ApplicationRecord
 
   validates :endpoint,         uri: true, allow_nil: true, allow_blank: true
   validates :sandbox_endpoint, uri: true, allow_nil: true, allow_blank: true
+
+  validates :sandbox_endpoint, format: { with: LOCALHOST , on: :update, if: :hosted? }
+  validates :endpoint, format: { with: LOCALHOST , on: :update, if: :hosted? }
 
   validates :hostname_rewrite, format: { with: HOST, allow_nil: true, allow_blank: true }
 
