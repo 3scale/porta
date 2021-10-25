4ced7adea1da58cb1b7ce296d3b435983863036c
josemigallas <josemigallas@gmail.com>
2021-10-25 17:49:25 +0200

    a couple more test files

--- test/integration/buyers/accounts/bulk/change_plans_controller_test.rb
+++ test/integration/buyers/accounts/bulk/change_plans_controller_test.rb
@@ -6,7 +6,7 @@ class Buyers::Accounts::Bulk::ChangePlansControllerTest < ActionDispatch::Integr
   def setup
     tenant = FactoryBot.create(:provider_account)
     tenant.settings.account_plans.allow
-    tenant.settings.update_column(:account_plans_ui_visible, true)
+    tenant.settings.account_plans_ui_visible = true
     login! tenant
     @buyer = FactoryBot.create(:buyer_account, provider_account: tenant)
   end
@@ -14,7 +14,7 @@ class Buyers::Accounts::Bulk::ChangePlansControllerTest < ActionDispatch::Integr
   attr_reader :buyer
 
   test '#new displays the buyer\'s admin_user_display_name' do
-    get new_admin_buyers_accounts_bulk_change_plan_path, selected: [buyer.id]
+    get new_admin_buyers_accounts_bulk_change_plan_path, params: { selected: [buyer.id] }
 
     assert_xpath('//main//span', buyer.decorate.admin_user_display_name)
   end
