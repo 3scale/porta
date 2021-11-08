a115295a5e8c6205bc5c69df5565bc1f318e1a78
Nidhi Soni <nidhisoni4411@gmail.com>
2021-11-08 20:52:47 +0530

    THREESCALE-7194 added test case for backend_api_nav_sections

---
+++ test/helpers/vertical_nav_helper_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class VerticalNavHelperTest < ActionView::TestCase
+
+  test '#backend_api_nav_sections' do
+    @backend_api = FactoryBot.create(:backend_api)
+
+    # if permitted
+    stubs(can?: true)
+    assert_equal(backend_api_nav_sections.pluck(:title), ["Overview", "Analytics", "Methods & Metrics", "Mapping Rules"])
+
+    # if not permitted
+    stubs(can?: false)
+    assert_equal(backend_api_nav_sections.pluck(:title), ["Overview", "Methods & Metrics", "Mapping Rules"])
+  end
+end
