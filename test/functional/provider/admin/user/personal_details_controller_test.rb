35a775138a120ccf58add5392721419d09267314
Nidhi Soni <nidhisoni4411@gmail.com>
2021-11-12 23:15:50 +0530

    THREESCALE-7852 Fixed functional provider test

--- test/functional/provider/admin/user/personal_details_controller_test.rb
+++ test/functional/provider/admin/user/personal_details_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class Provider::Admin::User::PersonalDetailsControllerTest < ActionController::TestCase
@@ -10,17 +12,17 @@ class Provider::Admin::User::PersonalDetailsControllerTest < ActionController::T
 
 
   test "put update should redirect to users" do
-    put :update, user: {current_password: 'supersecret' , username: 'test', email: 'test@example.com'}, origin: 'users'
+    put :update, params: { user: {current_password: 'supersecret', username: 'test', email: 'test@example.com'}, origin: 'users' }
     assert_redirected_to provider_admin_account_users_path
   end
 
   test "put update should redirect to edit personal details" do
-    put :update, user: {current_password: 'supersecret' , username: 'test', email: 'test@example.com'}
+    put :update, params: { user: {current_password: 'supersecret', username: 'test', email: 'test@example.com'} }
     assert_redirected_to edit_provider_admin_user_personal_details_path
   end
 
   test  "put update should fail and render edit" do
-    put :update, user: {username: ''}
+    put :update, params: { user: {username: ''} }
     assert_response :success
     assert_template 'edit'
   end
