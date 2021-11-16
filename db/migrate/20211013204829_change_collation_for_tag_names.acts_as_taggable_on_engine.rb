6573a33402b08aeb4c9ce89c510b1ceb26ba8037
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-11-16 18:57:59 +0200

    THREESCALE-7898 delay some unclear migrations

--- db/migrate/20211013204829_change_collation_for_tag_names.acts_as_taggable_on_engine.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from acts_as_taggable_on_engine (originally 5)
-# This migration is added to circumvent issue #623 and have special characters
-# work properly
-class ChangeCollationForTagNames < ActiveRecord::Migration[4.2]
-  # ignore this migration, our DB is already correct
-end
--- db/migrate/20211117094501_add_missing_unique_indices.acts_as_taggable_on_engine.rb
+++ db/migrate/20211117094501_add_missing_unique_indices.acts_as_taggable_on_engine.rb
@@ -10,19 +10,20 @@ AddMissingUniqueIndices.class_eval do
   def self.up
     add_index ActsAsTaggableOn.tags_table, [:name, :tenant_id], unique: true, name: 'index_tags_on_name', **index_options
 
-    remove_index ActsAsTaggableOn.taggings_table, :tag_id if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
-    remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_taggable_context_idx' if index_exists?(ActsAsTaggableOn.taggings_table, :taggings_taggable_context_idx)
-    add_index ActsAsTaggableOn.taggings_table,
-              [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type],
-              unique: true, name: 'taggings_idx', **index_options
+    # see https://issues.redhat.com/browse/THREESCALE-7898
+    # remove_index ActsAsTaggableOn.taggings_table, :tag_id if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
+    # remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_taggable_context_idx' if index_exists?(ActsAsTaggableOn.taggings_table, :taggings_taggable_context_idx)
+    # add_index ActsAsTaggableOn.taggings_table,
+    #           [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type],
+    #           unique: true, name: 'taggings_idx', **index_options
   end
 
   def self.down
     remove_index ActsAsTaggableOn.tags_table, name: 'index_tags_on_name'
 
-    remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_idx'
+    # remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_idx'
 
-    add_index ActsAsTaggableOn.taggings_table, :tag_id, **index_options unless index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
+    # add_index ActsAsTaggableOn.taggings_table, :tag_id, **index_options unless index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
     # add_index ActsAsTaggableOn.taggings_table, [:taggable_id, :taggable_type, :context], name: 'taggings_taggable_context_idx', **index_options
   end
 
