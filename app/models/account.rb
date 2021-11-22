ce1387bc10dcb66bb4f107f73f7d48ddc4ded2f6
Sergio Franco <sfrancog@redhat.com>
2021-11-22 16:08:10 +0000

    fix: Allow tenant_id to be propagated from account

--- app/models/account.rb
+++ app/models/account.rb
@@ -99,7 +99,7 @@ class Account < ApplicationRecord
 
   before_validation(on: :create, if: :provider?) { generate_s3_prefix }
   before_validation(on: :create, if: :provider?) { generate_domains }
-  before_create :generate_site_access_code
+  before_create :generate_site_access_code, :set_default_master
 
   attr_protected :master, :provider, :buyer, :from_email, :vat_rate, :sample_data, :default_service_id, :s3_prefix,
                  :provider_account_id, :paid_at, :paid, :signs_legal_terms, :tenant_id, :default_account_plan_id,
@@ -578,6 +578,12 @@ class Account < ApplicationRecord
 
   def generate_site_access_code
     self.site_access_code ||= SecureRandom.hex(5) if provider?
+  end
+
+  def set_default_master
+    # prevent null values that confuse some SQL hooks
+    self.master = provider? && master?
+    true
   end
 
   def destroy_all_users
