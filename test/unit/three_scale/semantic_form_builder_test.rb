de04d7a697ebeca6b84ce650ccd8b219043da777
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-11-24 18:55:52 +0200

    better guard formtastic from no-column fields
    `has_attribute?` is not enough because one can have an
    attribute not associated with a column. Like in
    acts-as-taggable-on version 6+, see
    https://github.com/mbleigh/acts-as-taggable-on/issues/1064

--- test/unit/three_scale/semantic_form_builder_test.rb
+++ test/unit/three_scale/semantic_form_builder_test.rb
@@ -29,6 +29,25 @@ class ThreeScale::SemanticFormBuilderTest < ActionView::TestCase
     end
   end
 
+  def test_column_for_override_needed
+    file = FactoryBot.create(:cms_file)
+    message = "Probably Formtastic was updated and ThreeScale::SemanticFormBuilder#column_for can be removed."
+
+    assert_not_empty(semantic_form_for(file, url: '') { |f| f.input :tag_list; f.input :date })
+
+    assert_raises message do
+      semantic_form_for(file, url: '', builder: ::Formtastic::SemanticFormBuilder) do |f|
+        f.input :tag_list
+      end
+    end
+
+    assert_raises message do
+      semantic_form_for(file, url: '', builder: ::Formtastic::SemanticFormBuilder) do |f|
+        f.input :date
+      end
+    end
+  end
+
   test 'inline errors' do
     dummy = Dummy.new
 
