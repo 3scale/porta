91b6d5c3adc359acf52a60bedce087d683c20820
josemigallas <josemigallas@gmail.com>
2021-11-25 13:59:13 +0100

    fixes rubocop and refactor

--- test/integration/mandatory_keys_test.rb
+++ test/integration/mandatory_keys_test.rb
@@ -4,11 +4,10 @@ require 'test_helper'
 
 class MandatoryKeysTest < ActionDispatch::IntegrationTest
   setup do
-    @provider = FactoryBot.create :provider_account, :domain => 'provider.example.com'
-    @plan = FactoryBot.create :application_plan, :issuer => @provider.default_service
-    @service = @provider.first_service!
-    @service.update_attribute(:backend_version, '2')
-    @buyer = FactoryBot.create :buyer_account, :provider_account => @provider
+    provider = FactoryBot.create(:provider_account, domain: 'provider.example.com')
+    @service = FactoryBot.create(:simple_service, account: provider, backend_version: 2)
+    @plan = FactoryBot.create(:application_plan, issuer: @service)
+    @buyer = FactoryBot.create(:buyer_account, provider_account: provider)
 
     ApplicationKey.disable_backend!
   end
@@ -16,7 +15,7 @@ class MandatoryKeysTest < ActionDispatch::IntegrationTest
   class ServiceWithMandatoryAppKeyTest < self
     def setup
       super
-      @service.update_attribute(:mandatory_app_key, true)
+      @service.update(mandatory_app_key: true)
     end
 
     test 'create a key on app creation' do
@@ -31,7 +30,7 @@ class MandatoryKeysTest < ActionDispatch::IntegrationTest
   class ServiceWithoutMandatoryAppKeyTest < self
     def setup
       super
-      @service.update_attribute(:mandatory_app_key, false)
+      @service.update(mandatory_app_key: false)
     end
 
     test 'not create a key on app creation' do
