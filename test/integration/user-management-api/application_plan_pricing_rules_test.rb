4b071f913960dbe66e2953e7b033452b208236e9
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2021-11-29 19:43:33 +0100

    Remove `context` and `should`  from application_plan_*_test (#2794)
    THREESCALE-7939: removes usage of context and should from application_plan_*_test

--- test/integration/user-management-api/application_plan_pricing_rules_test.rb
+++ test/integration/user-management-api/application_plan_pricing_rules_test.rb
@@ -5,9 +5,9 @@ require 'test_helper'
 class Admin::Api::ApplicationPlanPricingRulesTest < ActionDispatch::IntegrationTest
   def setup
     @provider = FactoryBot.create(:provider_account, domain: 'provider.example.com')
-    @service  = FactoryBot.create(:service, account: @provider)
+    @service = FactoryBot.create(:service, account: @provider)
     @app_plan = FactoryBot.create(:application_plan, issuer: @service)
-    @metric   = FactoryBot.create(:metric, service: @service)
+    @metric = FactoryBot.create(:metric, service: @service)
     @pricing_rule = FactoryBot.create(:pricing_rule, plan: @app_plan, metric: @metric)
 
     host! @provider.admin_domain
@@ -18,41 +18,32 @@ class Admin::Api::ApplicationPlanPricingRulesTest < ActionDispatch::IntegrationT
       super
       user = FactoryBot.create(:member, account: @provider, admin_sections: %w[partners plans])
       @token = FactoryBot.create(:access_token, owner: user, scopes: 'account_management')
+
+      User.any_instance.stubs(:has_access_to_all_services?).returns(false)
     end
 
-    context 'without access to all services' do
-      setup do
-        User.any_instance.stubs(:has_access_to_all_services?).returns(false)
-      end
-
-      should 'index with no token' do
-        get admin_api_application_plan_pricing_rules_path(@app_plan)
-        assert_response :forbidden
-      end
-
-      should 'index with access to no services' do
-        User.any_instance.expects(:member_permission_service_ids).returns([]).at_least_once
-        get admin_api_application_plan_pricing_rules_path(@app_plan), params: params
-        assert_response :not_found
-      end
-
-      should 'index with access to some service' do
-        User.any_instance.expects(:member_permission_service_ids).returns([@service.id]).at_least_once
-        get admin_api_application_plan_pricing_rules_path(@app_plan), params: params
-        assert_response :success
-      end
+    test 'index with no token' do
+      get admin_api_application_plan_pricing_rules_path(@app_plan)
+      assert_response :forbidden
     end
 
-    context 'with access to all services' do
-      setup do
-        User.any_instance.stubs(:has_access_to_all_services?).returns(true)
-      end
+    test 'index with access to no services' do
+      User.any_instance.expects(:member_permission_service_ids).returns([]).at_least_once
+      get admin_api_application_plan_pricing_rules_path(@app_plan), params: params
+      assert_response :not_found
+    end
 
-      should 'index' do
-        User.any_instance.expects(:member_permission_service_ids).never
-        get admin_api_application_plan_pricing_rules_path(@app_plan), params: params
-        assert_response :success
-      end
+    test 'index with access to some service' do
+      User.any_instance.expects(:member_permission_service_ids).returns([@service.id]).at_least_once
+      get admin_api_application_plan_pricing_rules_path(@app_plan), params: params
+      assert_response :success
+    end
+
+    test 'index with access to all services' do
+      User.any_instance.stubs(:has_access_to_all_services?).returns(true)
+      User.any_instance.expects(:member_permission_service_ids).never
+      get admin_api_application_plan_pricing_rules_path(@app_plan), params: params
+      assert_response :success
     end
 
     private
