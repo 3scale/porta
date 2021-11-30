4a83a4c0e491675156a4c9b05f3a3db6fb206f1e
josemigallas <josemigallas@gmail.com>
2021-11-30 12:13:34 +0100

    remove usage of context and should from unit/account tests

--- test/unit/account/fields_test.rb
+++ test/unit/account/fields_test.rb
@@ -1,28 +1,29 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class Account::FieldsTest < ActiveSupport::TestCase
+  class BuyerTest < Account::FieldsTest
+    def setup
+      @user_attrs = { username: 'buyer', email: 'email@buyer.com', password: 'password', password_confirmation: 'password' }
 
-  def setup
-    @user_attrs = { :username => 'buyer', :email => 'email@buyer.com', :password => 'password', :password_confirmation => 'password' }
-  end
-
-  context 'source of field definitions for buyer' do
-
-    should 'built by normal association' do
       @provider = FactoryBot.create(:simple_provider)
-      buyer = @provider.buyers.build :org_name => 'buyer'
+      @buyer = FactoryBot.create(:simple_buyer, provider_account: @provider, org_name: 'buyer')
+    end
+
+    attr_reader :buyer
+
+    test 'built by normal association' do
       assert_equal @provider, buyer.fields_definitions_source_root
 
       user = buyer.users.build_with_fields(@user_attrs)
       assert_equal @provider, user.fields_definitions_source_root
 
       buyer.save!
-      assert_equal @provider, Account.find_by_org_name!('buyer').fields_definitions_source_root
+      assert_equal @provider, Account.find_by!(org_name: 'buyer').fields_definitions_source_root
     end
 
-    should 'buit with fields' do
-      @provider = FactoryBot.create(:simple_provider)
-      buyer = @provider.buyers.build_with_fields :org_name => 'buyer'
+    test 'built with fields' do
       assert_equal @provider, buyer.fields_definitions_source_root
 
       user = buyer.users.build_with_fields(@user_attrs)
@@ -30,12 +31,11 @@ class Account::FieldsTest < ActiveSupport::TestCase
 
       buyer.save!
 
-      assert_equal @provider, Account.find_by_org_name!('buyer').fields_definitions_source_root
-      assert_equal @provider, User.find_by_username!('buyer').fields_definitions_source_root
+      assert_equal @provider, Account.find_by!(org_name: 'buyer').fields_definitions_source_root
+      assert_equal @provider, User.find_by!(username: 'buyer').fields_definitions_source_root
     end
 
-    should 'which exists' do
-      buyer = FactoryBot.create(:simple_buyer)
+    test 'which exists' do
       assert_equal buyer.provider_account, buyer.fields_definitions_source_root
 
       user = buyer.users.build(@user_attrs)
@@ -43,33 +43,33 @@ class Account::FieldsTest < ActiveSupport::TestCase
     end
   end
 
-  context 'source of field definitions for provider' do
-    setup do
+  class ProviderTest < Account::FieldsTest
+    def setup
+      super
       @master = master_account
+      @provider = FactoryBot.create(:simple_provider, provider: master_account, org_name: 'provider')
     end
 
-    should 'buit by normal association' do
-      provider = @master.buyers.build :org_name => 'provider'
+    attr_reader :provider
+
+    test 'built by normal association' do
       assert_equal @master, provider.fields_definitions_source_root
       provider.save!
-      assert_equal @master, Account.find_by_org_name!('provider').fields_definitions_source_root
+      assert_equal @master, Account.find_by!(org_name: 'provider').fields_definitions_source_root
     end
 
-    should 'built with fields' do
-      provider = @master.buyers.build_with_fields :org_name => 'provider'
+    test 'built with fields' do
       assert_equal @master, provider.fields_definitions_source_root
       provider.save!
-      assert_equal @master, Account.find_by_org_name!('provider').fields_definitions_source_root
+      assert_equal @master, Account.find_by!(org_name: 'provider').fields_definitions_source_root
     end
 
-    should 'which exists' do
-      provider = FactoryBot.create(:simple_provider)
+    test 'which exists' do
       assert_equal provider.provider_account, provider.fields_definitions_source_root
     end
-  end
 
-  test 'source of field definitions for master' do
-    master = master_account
-    assert_equal master, master.fields_definitions_source_root
+    test 'source of field definitions for master' do
+      assert_equal @master, @master.fields_definitions_source_root
+    end
   end
 end
--- test/unit/account/provider_test.rb
+++ test/unit/account/provider_test.rb
@@ -1,56 +1,55 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class Account::ProviderTest < ActiveSupport::TestCase
+  test 'destroy dependent' do
+    account = FactoryBot.create(:simple_provider)
+    FactoryBot.create(:authentication_provider, account: account)
+    FactoryBot.create(:self_authentication_provider, account: account)
+    assert_difference('AuthenticationProvider.count', -2) { account.destroy! }
+  end
 
-  context 'authentication_providers' do
-    setup do
-      @account = FactoryBot.create(:simple_provider)
+  test 'authentication_providers build_kind' do
+    account = FactoryBot.create(:simple_provider)
+    (AuthenticationProvider.available(AuthenticationProvider.account_types[:developer]) + [AuthenticationProvider::Custom]).each do |authentication_provider_class|
+      kind_name = authentication_provider_class.to_s.demodulize
+      authentication_provider = account.authentication_providers.build_kind(kind: kind_name, client_id: 'id', client_secret: 'secret', site: 'http://example.com')
+      assert_equal authentication_provider_class, authentication_provider.class
+      assert_equal kind_name.downcase, authentication_provider.kind
+      assert_equal account, authentication_provider.account
+      authentication_provider.valid? && puts(authentication_provider.errors.full_messages)
+      assert authentication_provider.valid?
     end
+  end
 
-    should 'destroy dependent' do
-      FactoryBot.create(:authentication_provider, account: @account)
-      FactoryBot.create(:self_authentication_provider, account: @account)
-      assert_difference('AuthenticationProvider.count', -2) { @account.destroy! }
+  test 'self_authentication_providers build_kind valid kinds' do
+    account = FactoryBot.create(:simple_provider)
+    AuthenticationProvider.available(AuthenticationProvider.account_types[:provider]).each do |authentication_provider_class|
+      kind_name = authentication_provider_class.to_s.demodulize
+      authentication_provider = account.self_authentication_providers.build_kind(kind: kind_name, client_id: 'id', client_secret: 'secret', site: 'http://example.com')
+      assert_equal authentication_provider_class, authentication_provider.class
+      assert_equal kind_name.downcase, authentication_provider.kind
+      assert_equal account, authentication_provider.account
+      authentication_provider.valid? && puts(authentication_provider.errors.full_messages)
+      assert authentication_provider.valid?
     end
+  end
 
-    should 'authentication_providers build_kind' do
-      (AuthenticationProvider.available(AuthenticationProvider.account_types[:developer]) + [AuthenticationProvider::Custom]).each do |authentication_provider_class|
-        kind_name = authentication_provider_class.to_s.demodulize
-        authentication_provider = @account.authentication_providers.build_kind(kind: kind_name, client_id: 'id', client_secret: 'secret', site: 'http://example.com')
-        assert_equal authentication_provider_class, authentication_provider.class
-        assert_equal kind_name.downcase, authentication_provider.kind
-        assert_equal @account, authentication_provider.account
-        authentication_provider.valid? && puts(authentication_provider.errors.full_messages)
-        assert authentication_provider.valid?
-      end
-    end
-
-    should 'self_authentication_providers build_kind valid kinds' do
-      AuthenticationProvider.available(AuthenticationProvider.account_types[:provider]).each do |authentication_provider_class|
-        kind_name = authentication_provider_class.to_s.demodulize
-        authentication_provider = @account.self_authentication_providers.build_kind(kind: kind_name, client_id: 'id', client_secret: 'secret', site: 'http://example.com')
-        assert_equal authentication_provider_class, authentication_provider.class
-        assert_equal kind_name.downcase, authentication_provider.kind
-        assert_equal @account, authentication_provider.account
-        authentication_provider.valid? && puts(authentication_provider.errors.full_messages)
-        assert authentication_provider.valid?
-      end
-    end
-
-    should 'self_authentication_providers build_kind invalid kinds' do
-      available_only_for_developers = (AuthenticationProvider.available(AuthenticationProvider.account_types[:developer]) - AuthenticationProvider.available(AuthenticationProvider.account_types[:provider])).map { |ap_class| ap_class.to_s.demodulize }
-      available_only_for_developers += ['CustomKind-123456']
-      available_only_for_developers.each do |authentication_provider_class|
-        authentication_provider = @account.self_authentication_providers.build_kind(kind: authentication_provider_class, client_id: 'id', client_secret: 'secret', site: 'http://example.com')
-        assert_equal @account, authentication_provider.account
-        refute authentication_provider.valid?
-      end
+  test 'self_authentication_providers build_kind invalid kinds' do
+    account = FactoryBot.create(:simple_provider)
+    available_only_for_developers = (AuthenticationProvider.available(AuthenticationProvider.account_types[:developer]) - AuthenticationProvider.available(AuthenticationProvider.account_types[:provider])).map { |ap_class| ap_class.to_s.demodulize }
+    available_only_for_developers += ['CustomKind-123456']
+    available_only_for_developers.each do |authentication_provider_class|
+      authentication_provider = account.self_authentication_providers.build_kind(kind: authentication_provider_class, client_id: 'id', client_secret: 'secret', site: 'http://example.com')
+      assert_equal account, authentication_provider.account
+      assert_not authentication_provider.valid?
     end
   end
 
   test '#api_key?' do
     provider = FactoryBot.create(:simple_provider)
-    refute provider.api_key?
+    assert_not provider.api_key?
 
     FactoryBot.create(:cinstance, user_account: provider)
     assert provider.api_key?
@@ -61,12 +60,12 @@ class Account::ProviderTest < ActiveSupport::TestCase
     assert provider.missing_api_key?
 
     FactoryBot.create(:cinstance, user_account: provider)
-    refute provider.missing_api_key?
+    assert_not provider.missing_api_key?
   end
 
-  test 'provider?'do
+  test 'provider?' do
     account = Account.new
-    refute account.partner?
+    assert_not account.partner?
     account.expects(:partner_id).returns(42)
     assert account.partner?
   end
@@ -75,19 +74,19 @@ class Account::ProviderTest < ActiveSupport::TestCase
     provider = FactoryBot.create :provider_account
     assert provider.should_apply_email_engagement_footer?, 'Expected to have a viral footer'
 
-    plan = FactoryBot.create :published_plan, :system_name => 'enterprise', :issuer => master_account.services.first
+    plan = FactoryBot.create :published_plan, :system_name => 'enterprise', issuer: master_account.services.first
 
     provider.force_upgrade_to_provider_plan! plan
-    refute provider.should_apply_email_engagement_footer?, 'Expected to skip the viral footer'
+    assert_not provider.should_apply_email_engagement_footer?, 'Expected to skip the viral footer'
   end
 
   test '#require_billing_information! and #require_billing_information? and validations' do
     account = Account.new
-    refute account.require_billing_information?
+    assert_not account.require_billing_information?
     account.require_billing_information!
     assert account.require_billing_information?
 
-    refute account.valid?
+    assert_not account.valid?
 
     assert account.errors.messages[:org_legaladdress].present?
     assert account.errors.messages[:country].present?
@@ -136,21 +135,18 @@ class Account::ProviderTest < ActiveSupport::TestCase
   should have_one(:billing_strategy)
   should have_many(:buyer_invoices)
   should have_many(:buyer_line_items)
-
   should have_many(:redirects)
-
   should have_many(:templates)
-
   should have_many(:groups)
   should have_many(:provided_sections)
 
-  should '#show_xss_protection_options?' do
+  test '#show_xss_protection_options? should' do
     account = FactoryBot.build_stubbed(:provider_account)
     settings = account.settings
 
     settings.cms_escape_published_html = true
-    settings.cms_escape_draft_html =  true
-    refute account.show_xss_protection_options?
+    settings.cms_escape_draft_html = true
+    assert_not account.show_xss_protection_options?
 
     settings.cms_escape_published_html = true
     settings.cms_escape_draft_html = false
@@ -165,17 +161,20 @@ class Account::ProviderTest < ActiveSupport::TestCase
     assert account.show_xss_protection_options?
   end
 
-  context 'after created' do
+  class AfterCreatedTest < ActiveSupport::TestCase
+    setup do
+      @subject = Account.new(org_name: "prov", provider_account: master_account, subdomain: 'prov', self_subdomain: 'prov-admin')
+    end
 
-    subject { Account.new :org_name => "prov", :provider_account => master_account, :subdomain => 'prov', :self_subdomain => 'prov-admin' }
+    attr_reader :subject
 
-    should 'have an sso_key' do
-      subject.provider= true
+    test 'should have an sso_key' do
+      subject.provider = true
       subject.save!
       assert_not_nil subject.settings.sso_key
     end
 
-    should 'not have a default service anymore' do
+    test 'should not have a default service anymore' do
       prov = subject
       prov.provider = true
       prov.save!
@@ -184,7 +183,7 @@ class Account::ProviderTest < ActiveSupport::TestCase
       assert prov.default_service_id.blank?
     end
 
-    should 'have a s3_prefix' do
+    test 'should have a s3_prefix' do
       prov = subject
       prov.provider = true
       prov.save!
@@ -193,7 +192,7 @@ class Account::ProviderTest < ActiveSupport::TestCase
       assert_equal 'prov', prov.s3_prefix
     end
 
-    should 'have a go_live_state' do
+    test 'should have a go_live_state' do
       prov = subject
       prov.provider = true
       prov.save
@@ -201,95 +200,59 @@ class Account::ProviderTest < ActiveSupport::TestCase
     end
   end
 
-  context "for the provider" do
-
+  class ForTheProviderTest < ActiveSupport::TestCase
     setup do
       @provider =  FactoryBot.create(:provider_account)
-      @acc_plan = FactoryBot.create(:account_plan, :issuer => @provider)
-      @buyers = []
-      @buyers << FactoryBot.create(:simple_buyer, :provider_account => @provider)
-      @buyers << FactoryBot.create(:simple_buyer, :provider_account => @provider)
+      @buyers = FactoryBot.create_list(:simple_buyer, 2, provider_account: @provider)
     end
 
-    context 'Account#from_email' do
-      should 'have default' do
+    class WithoutPlansTest < ForTheProviderTest
+      test 'Account#from_email should have default' do
         assert_equal Rails.configuration.three_scale.noreply_email, @provider.from_email
       end
 
-      should 'return correct if customized' do
+      test 'Account#from_email should return correct if customized' do
         mail = 'foo@example.net'
         @provider.from_email = mail
         assert_equal mail, @provider.from_email
       end
     end
 
-    context 'with provided plans' do
-      setup do
+    class WithPlansTest < ForTheProviderTest
+      def setup
+        super
+        services = FactoryBot.create_list(:simple_service, 2, account: @provider)
+        @service_one, @service_two = services
 
-        @service_one = FactoryBot.create(:simple_service, :account => @provider)
-        @service_two = FactoryBot.create(:simple_service, :account => @provider)
-        @service_three = FactoryBot.create(:simple_service, :account => @provider)
+        service_one_application_plans = FactoryBot.create_list(:simple_application_plan, 2, issuer: @service_one)
+        @service_two_application_plans = FactoryBot.create_list(:simple_application_plan, 2, issuer: @service_two)
 
-        @plans = {
-            :service => [
-              FactoryBot.create(:service_plan, :issuer => @service_one),
-              FactoryBot.create(:service_plan, :issuer => @service_two)
-            ],
-            :application => [
-              FactoryBot.create(:simple_application_plan, :issuer => @service_one),
-              FactoryBot.create(:simple_application_plan, :issuer => @service_one),
+        service_plans = services.map { |service| FactoryBot.create(:service_plan, issuer: service) }
+        service_plans.map { |plan| @buyers.first.buy!(plan) }
 
-              FactoryBot.create(:simple_application_plan, :issuer => @service_two),
-              FactoryBot.create(:simple_application_plan, :issuer => @service_two)
-            ]
-        }
-
-        assert_equal 2, ApplicationPlan.issued_by(@service_one).count
-
-        @contracts = {:service => [], :cinstance => []}
-
-        @contracts[:service] << @buyers.first.buy!(@plans[:service].first)
-        @contracts[:cinstance] << @buyers.first.buy!(@plans[:application].first)
-        @contracts[:cinstance] << @buyers.first.buy!(@plans[:application].second)
-
-
-        @contracts[:service] << @buyers.last.buy!(@plans[:service].last)
-        @contracts[:cinstance] << @buyers.last.buy!(@plans[:application].third)
-        @contracts[:cinstance] << @buyers.last.buy!(@plans[:application].last)
-
-        @provider.reload
-
-        @plans[:account] = @acc_plan
+        @service_one_cinstances = service_one_application_plans.map { |plan| @buyers.first.buy!(plan) }
+        @service_two_application_plans.map { |plan| @buyers.second.buy!(plan) }
       end
 
-
-      context 'Account#account_plans' do
-        should 'have  #default' do
-          assert_not_nil @provider.account_plans.default
-        end
+      test 'Account#account_plans should have #default' do
+        assert_not_nil @provider.account_plans.default
       end
 
-      context 'Account#application_plans' do
-        should 'return all application plans provided by one of the service of the account' do
-          assert_same_elements ApplicationPlan.provided_by(@provider), @provider.application_plans
-        end
-
-        should 'return only issued by issuer if called with issued_by scope' do
-          assert_same_elements @plans[:application][2..3], @provider.application_plans.issued_by(@service_two)
-        end
+      test 'Account#application_plans should return all application plans provided by one of the service of the account' do
+        assert_same_elements ApplicationPlan.provided_by(@provider), @provider.application_plans
       end
 
-      context 'Account#provided_cinstances' do
-        should 'return all provided cinstances' do
-          assert_same_elements Cinstance.provided_by(@provider), @provider.provided_cinstances
-        end
-
-        should 'return only cinstances for issuer if called with issued_by scope' do
-          assert_same_elements @contracts[:cinstance][0..1], @provider.provided_cinstances.by_service(@service_one)
-        end
+      test 'Account#application_plans should return only issued by issuer if called with issued_by scope' do
+        assert_same_elements @service_two_application_plans, @provider.application_plans.issued_by(@service_two)
       end
 
+      test 'Account#provided_cinstances should return all provided cinstances' do
+        assert_same_elements Cinstance.provided_by(@provider), @provider.provided_cinstances
+      end
+
+      test 'Account#provided_cinstances should return only cinstances for issuer if called with issued_by scope' do
+        assert_same_elements @service_one_cinstances, @provider.provided_cinstances.by_service(@service_one)
+      end
     end
-
   end
 end
