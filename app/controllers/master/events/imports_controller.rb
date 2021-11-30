60cf84eb8b67a2453aaa70e6d7de75608c8662ac
Nidhi Soni <nidhisoni4411@gmail.com>
2021-11-30 15:23:23 +0530

    [params] Fixes events/imports_controller.rb

--- app/controllers/master/events/imports_controller.rb
+++ app/controllers/master/events/imports_controller.rb
@@ -13,6 +13,7 @@ class Master::Events::ImportsController < Master::BaseController
   private
 
   def check_shared_secret
-    head(403) unless params[:secret] == Events.shared_secret
+    permitted_params = params.permit(%i[secret]).to_h
+    head(403) unless permitted_params[:secret] == Events.shared_secret
   end
 end
