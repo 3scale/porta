7bcbd50b1cd8dc65b5ff8974142b437267238fb0
josemigallas <josemigallas@gmail.com>
2021-12-01 14:41:53 +0100

    removes usage of context and should from yet another bunch of tests

--- test/unit/liquid/drops/application_drop_test.rb
+++ test/unit/liquid/drops/application_drop_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class Liquid::Drops::ApplicationDropTest < ActiveSupport::TestCase
@@ -6,6 +8,17 @@ class Liquid::Drops::ApplicationDropTest < ActiveSupport::TestCase
   def setup
     @app = FactoryBot.create(:cinstance, :name => 'some instance')
     @buyer = @app.user_account
+
+    [{ :target => "Cinstance", :name => "visible_extra", :label => "visible_extra" },
+     { :target => "Cinstance", :name => "hidden_extra",  :label => "hidden_extra", :hidden => true }]
+      .each do |field|
+      FactoryBot.create :fields_definition, field.merge({:account_id => @buyer.provider_account.id})
+    end
+
+    @app.reload
+    @app.extra_fields = {"visible_extra" => "visible extra value", "hidden_extra" => "hidden extra value" }
+    @app.save!
+
     @drop = Drops::Application.new(@app)
   end
 
@@ -28,51 +41,34 @@ class Liquid::Drops::ApplicationDropTest < ActiveSupport::TestCase
     assert_same_elements Drops::Collection.for_drop(Drops::Alert).new(expected_alerts), @drop.alerts
   end
 
-  context "field definitions" do
-    setup do
-      [{ :target => "Cinstance", :name => "visible_extra", :label => "visible_extra" },
-       { :target => "Cinstance", :name => "hidden_extra",  :label => "hidden_extra", :hidden => true }]
-       .each do |field|
-         FactoryBot.create :fields_definition, field.merge({:account_id => @buyer.provider_account.id})
-      end
+  test 'builtin_fields should be Fields' do
+    builtins = @drop.builtin_fields
+    assert_instance_of Drops::Fields, builtins
+    assert_instance_of Drops::Field, builtins.first
+  end
 
-      @app.reload
-      @app.extra_fields = {"visible_extra" => "visible extra value", "hidden_extra" => "hidden extra value" }
-      @app.save!
+  test 'bultin fields have visible fields' do
+    assert_equal @app.name, @drop.builtin_fields["name"]
+  end
 
-      @drop = Drops::Application.new(@app)
-    end
+  test 'builtin_fields does not include extra fields' do
+    assert_nil @drop.builtin_fields["visible_extra"]
+  end
 
+  test '#extra_fields are Fields' do
+    extras = @drop.extra_fields
+    assert_instance_of Drops::Fields, extras
+    assert_instance_of Drops::Field, extras.first
+  end
 
-    should 'builtin_fields should be Fields' do
-      builtins = @drop.builtin_fields
-      assert_instance_of Drops::Fields, builtins
-      assert_instance_of Drops::Field, builtins.first
-    end
+  test '#extra_fields have both visible and hidden ones' do
+    extras = @drop.extra_fields
+    assert_equal "visible extra value", extras["visible_extra"]
+    assert_not_nil extras["hidden_extra"]
+  end
 
-    should 'bultin fields have visible fields' do
-      assert_equal @app.name, @drop.builtin_fields["name"]
-    end
-
-    should 'builtin_fields does not include extra fields' do
-      assert_nil @drop.builtin_fields["visible_extra"]
-    end
-
-    should '#extra_fields are Fields' do
-      extras = @drop.extra_fields
-      assert_instance_of Drops::Fields, extras
-      assert_instance_of Drops::Field, extras.first
-    end
-
-    should '#extra_fields have both visible and hidden ones' do
-      extras = @drop.extra_fields
-      assert_equal "visible extra value", extras["visible_extra"]
-      assert_not_nil extras["hidden_extra"]
-    end
-
-    should '#extra_fields does not return builtin fields' do
-      extras = @drop.extra_fields
-      assert_nil extras["name"]
-    end
-  end # field definitions
+  test '#extra_fields does not return builtin fields' do
+    extras = @drop.extra_fields
+    assert_nil extras["name"]
+  end
 end
