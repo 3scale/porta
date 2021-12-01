39e3012cb5b41e45c0965e40ee07d24285c07c2a
Nidhi Soni <nidhisoni4411@gmail.com>
2021-12-01 19:04:05 +0530

    [params] Fixes partners user controler

--- app/controllers/partners/users_controller.rb
+++ app/controllers/partners/users_controller.rb
@@ -3,30 +3,30 @@ class Partners::UsersController < Partners::BaseController
   before_action :find_account
 
   def index
-    @users = @account.users.page(params[:page])
-    @users = @users.where(open_id: params[:open_id]) if params[:open_id]
+    @users = @account.users.page(permitted_params[:page])
+    @users = @users.where(open_id: permitted_params[:open_id]) if permitted_params[:open_id]
     render json: @users
   end
 
   def show
-    @user = @account.users.find(params[:id])
+    @user = @account.users.find(permitted_params[:id])
     render json: @user
   end
 
   def destroy
-    @user = @account.users.find(params[:id])
+    @user = @account.users.find(permitted_params[:id])
     @user.destroy
     render json: {success: true}
   end
 
   def create
     @user = @account.users.build
-    @user.email = params[:email]
+    @user.email = permitted_params[:email]
     @user.password = SecureRandom.hex
-    @user.first_name = params[:first_name].presence
-    @user.last_name = params[:last_name].presence
-    @user.open_id = params[:open_id].presence
-    @user.username = params[:username]
+    @user.first_name = permitted_params[:first_name].presence
+    @user.last_name = permitted_params[:last_name].presence
+    @user.open_id = permitted_params[:open_id].presence
+    @user.username = permitted_params[:username]
     @user.signup_type = :partner
     @user.role = :admin
     @user.activate!
@@ -40,6 +40,10 @@ class Partners::UsersController < Partners::BaseController
   private
 
   def find_account
-    @account = @partner.providers.find(params[:provider_id])
+    @account = @partner.providers.find(permitted_params[:provider_id])
+  end
+
+  def permitted_params
+    params.permit!.to_h
   end
 end
