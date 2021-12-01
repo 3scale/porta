7e13223b43803feca5189c0b763559f9d80cdf45
josemigallas <josemigallas@gmail.com>
2021-12-01 15:42:36 +0100

    removes usage of context from the last batch of tests

--- test/unit/liquid/drops/user_drop_test.rb
+++ test/unit/liquid/drops/user_drop_test.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class Liquid::Drops::UserDropTest < ActiveSupport::TestCase
   include Liquid
-
 
   def setup
     @buyer = FactoryBot.create(:buyer_account)
@@ -10,120 +11,111 @@ class Liquid::Drops::UserDropTest < ActiveSupport::TestCase
     @drop = Drops::User.new(@user)
   end
 
-  test '#display_name' do
-    assert_equal @user.decorate.display_name, @drop.display_name
+  class NoCurrentUserTest < Liquid::Drops::UserDropTest
+    test '#display_name' do
+      assert_equal @user.decorate.display_name, @drop.display_name
+    end
+
+    test '#informal_name' do
+      assert_equal @user.decorate.informal_name, @drop.informal_name
+    end
+
+    test '#name' do
+      assert_equal @user.decorate.full_name, @drop.name
+    end
+
+    def test_oauth2
+      @user.signup.expects(:oauth2?).returns(true)
+      assert @drop.oauth2?
+
+      @user.signup.expects(:oauth2?).returns(false)
+      assert_not @drop.oauth2?
+    end
+
+    test 'returns roles_collection' do
+      User.current = @user
+      assert_kind_of Array, @drop.roles_collection
+      assert_kind_of Liquid::Drops::User::Role, @drop.roles_collection[0]
+    end
+
+    test 'returns url' do
+      assert_equal "/admin/account/users/#{@user.id}", @drop.url
+    end
+
+    test 'returns edit_url' do
+      assert_equal "/admin/account/users/#{@user.id}/edit", @drop.edit_url
+    end
+
+    test 'returns the role as string' do
+      assert_equal(@user.role.to_s, @drop.role)
+    end
   end
 
-  test '#informal_name' do
-    assert_equal @user.decorate.informal_name, @drop.informal_name
-  end
-
-  test '#name' do
-    assert_equal @user.decorate.full_name, @drop.name
-  end
-
-  def test_oauth2
-    @user.signup.expects(:oauth2?).returns(true)
-    assert @drop.oauth2?
-
-    @user.signup.expects(:oauth2?).returns(false)
-    refute @drop.oauth2?
-  end
-
-  should 'returns roles_collection' do
-    User.current = @user
-    assert_kind_of Array, @drop.roles_collection
-    assert_kind_of Liquid::Drops::User::Role, @drop.roles_collection[0]
-  end
-
-  should 'returns url' do
-    assert_equal "/admin/account/users/#{@user.id}", @drop.url
-  end
-
-  should 'returns edit_url' do
-    assert_equal "/admin/account/users/#{@user.id}/edit", @drop.edit_url
-  end
-
-  should 'return the role as string' do
-    assert_equal(@user.role.to_s, @drop.role)
-  end
-
-
-  context 'by a buyer' do
-    setup do
+  class BuyerUserTest < Liquid::Drops::UserDropTest
+    def setup
+      super
       ::User.current = @user
 
       @drop2 = Drops::User.new(FactoryBot.create(:user, account: @buyer))
-
     end
 
-    should "can be destroyed" do
+    test "can be destroyed" do
       assert @drop2.can.be_destroyed?
     end
 
-    should "can be managed" do
+    test "can be managed" do
       assert @drop2.can.be_managed?
     end
 
-    should "can be update role" do
+    test "can be update role" do
       assert @drop2.can.be_update_role?
     end
-
   end
 
-  context 'by a normal user' do
-    setup do
+  class NormalUserTest < Liquid::Drops::UserDropTest
+    def setup
+      super
       ::User.current = FactoryBot.create(:user, account: @buyer)
     end
 
-    should "can't be destroyed" do
-      assert !@drop.can.be_destroyed?
+    test "can't be destroyed" do
+      assert_not @drop.can.be_destroyed?
     end
 
-    should "can't be managed" do
-      assert !@drop.can.be_managed?
+    test "can't be managed" do
+      assert_not @drop.can.be_managed?
     end
 
-    should "can't be update role" do
-      assert !@drop.can.be_update_role?
+    test "can't be update role" do
+      assert_not @drop.can.be_update_role?
     end
-
   end
 
-  context '#sections' do
-    context 'users with no sections' do
+  class SectionsTest < Liquid::Drops::UserDropTest
+    test 'users with no sections should be empty' do
+      user_drop = Drops::User.new(@user)
+      assert user_drop.sections.empty?
+    end
 
-      should 'be empty' do
-        user_drop = Drops::User.new(@user)
-        assert user_drop.sections.empty?
-      end
+    test 'users with sections should return sections paths' do
+      @section = FactoryBot.create(:cms_section, public: false,
+                                                 provider: @buyer.provider_account,
+                                                 title: "protected-section",
+                                                 parent: @buyer.provider_account.sections.root)
+      grant_buyer_access_to_section @buyer, @section
+      user_drop = Drops::User.new(@user)
+      assert_equal ["/protected-section"], user_drop.sections
+    end
+  end
 
-    end # users with no sections
-
-    context 'users with sections' do
-      setup do
-        @section = FactoryBot.create(:cms_section, :public => false,
-                           :provider => @buyer.provider_account,
-                           :title => "protected-section",
-                           :parent => @buyer.provider_account.sections.root)
-        grant_buyer_access_to_section @buyer, @section
-      end
-
-      should 'return sections paths' do
-        user_drop = Drops::User.new(@user)
-        assert user_drop.sections == ["/protected-section"]
-      end
-
-    end # users with sections
-  end # sections
-
-  context "field definitions" do
-    setup do
+  class FieldDefinitionsTest < Liquid::Drops::UserDropTest
+    def setup
+      super
       [{ target: "User", name: "first_name",    label: "first_name", hidden: true },
        { target: "User", name: "visible_extra", label: "visible_extra" },
        { target: "User", name: "hidden_extra",  label: "hidden_extra", hidden: true }]
        .each do |field|
-         FactoryBot.create :fields_definition, field.merge({account_id: @buyer.provider_account.id})
+        FactoryBot.create :fields_definition, field.merge({account_id: @buyer.provider_account.id})
       end
 
       @buyer.reload
@@ -134,11 +126,11 @@ class Liquid::Drops::UserDropTest < ActiveSupport::TestCase
       @drop = Drops::User.new(@user)
     end
 
-    should '#fields' do
+    test '#fields' do
       assert @drop.fields.first.is_a?(Drops::Field)
     end
 
-    should '#fields contain both visible and invisible' do
+    test '#fields should contain both visible and invisible' do
       @user.username = 'someone'
       @user.first_name = 'John'
 
@@ -146,25 +138,24 @@ class Liquid::Drops::UserDropTest < ActiveSupport::TestCase
       assert_equal 'John', @drop.fields["first_name"].to_str
     end
 
-    should '#fields include extra fields and builtin fields' do
+    test '#fields should include extra fields and builtin fields' do
       #regression test
       assert_not_nil @drop.fields["visible_extra"]
       assert_not_nil @drop.fields["first_name"]
       assert_not_nil @drop.fields["username"]
     end
 
-    should 'be fields' do
+    test 'should be fields' do
       assert @drop.extra_fields.first.is_a?(Drops::Field)
     end
 
-    should '#extra visible and invisible' do
+    test '#extra visible and invisible' do
       assert_equal "visible extra value", @drop.extra_fields["visible_extra"]
       assert_equal "hidden extra value", @drop.extra_fields["hidden_extra"]
     end
 
-    should 'not return builtin fields' do
+    test 'should not return builtin fields' do
       assert_nil @drop.extra_fields["username"]
     end
-
-  end # field definitions
+  end
 end
