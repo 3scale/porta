6359cd944ba1693b2fde61eb511d2f769575128f
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-12-06 19:36:52 +0200

    remove context test syntax

--- test/unit/metric_test.rb
+++ test/unit/metric_test.rb
@@ -106,14 +106,11 @@ class MetricTest < ActiveSupport::TestCase
     assert_equal backend_api, method.owner
   end
 
-  context 'on :destroy' do
-    should 'destroy pricing_rules'
-    should 'destroy usage_limits'
+  pending_test 'should destroy pricing_rules on :destroy'
+  pending_test 'should destroy usage_limits on :destroy'
+  pending_test 'should not destroy metric if there are some reports on :destroy'
 
-    should 'not destroy metric if there are some reports'
-  end
-
-  should 'create_default! with :hits' do
+  test 'create_default! with :hits' do
     service = FactoryBot.create(:service)
     service.metrics.find_by_system_name("hits").delete
     metric = Metric.create_default!(:hits, :service_id => service.id)
@@ -124,7 +121,7 @@ class MetricTest < ActiveSupport::TestCase
     assert  metric.default?(:hits)
   end
 
-  should 'return false on default?(:hits) if not hits' do
+  test 'return false on default?(:hits) if not hits' do
     service = FactoryBot.create(:service)
     metric = Metric.new(:system_name => 'foos', :friendly_name => 'Foos', :unit => 'foo')
     metric.service = service
@@ -143,7 +140,7 @@ class MetricTest < ActiveSupport::TestCase
     refute non_hits_metric.hits?
   end
 
-  context 'child metric' do
+  class ChildMetric < ActiveSupport::TestCase
     setup do
       @service = FactoryBot.create(:service)
       @parent_metric = @service.metrics.first
@@ -151,58 +148,58 @@ class MetricTest < ActiveSupport::TestCase
                                                       :friendly_name => 'Create an article')
     end
 
-    should 'respond true to child?' do
+    test 'respond true to child?' do
       assert @child_metric.child?
     end
 
-    should 'have the same unit as the parent metric' do
+    test 'have the same unit as the parent metric' do
       assert_equal @parent_metric.unit, @child_metric.unit
     end
 
-    should 'have readonly unit' do
+    test 'have readonly unit' do
       @child_metric.unit = 'call'
       assert_equal 'hit', @child_metric.unit
     end
 
-    should 'be associated to the same service as parent metric' do
+    test'be associated to the same service as parent metric' do
       assert_equal @service, @child_metric.service
     end
 
-    should 'only be child of hits' do
+    test 'only be child of hits' do
       invalid = @child_metric.children.new(:system_name => 'grandchild', :friendly_name => 'invalid')
       assert invalid.invalid?, "Expected Metric to be invalid"
       assert invalid.errors[:parent_id].present?, "Expected Metric to have error on parent_id"
     end
   end
 
-  context 'metric that is not child' do
+  class NonChildMetric < ActiveSupport::TestCase
     setup { @metric = FactoryBot.create(:metric) }
 
-    should 'respond false to child?' do
+    test 'respond false to child?' do
       refute @metric.child?
     end
   end
 
-  context 'metric that has children' do
+  class MetricWithChildren < ActiveSupport::TestCase
     setup do
       @metric = service = FactoryBot.create(:service).metrics.first
       @metric.children.create!(:friendly_name => 'Foos')
     end
 
-    should 'return true on parent?' do
+    test 'return true on parent?' do
       assert @metric.parent?
     end
   end
 
-  context 'metric that does not have children' do
+  class MetricWithoutChildren < ActiveSupport::TestCase
     setup { @metric = FactoryBot.create(:metric) }
 
-    should 'return false on parent?' do
+    test 'return false on parent?' do
       refute @metric.parent?
     end
   end
 
-  should 'return only top-level metric on top_level' do
+  test 'return only top-level metric on top_level' do
     service = FactoryBot.create(:service)
     metric_one = FactoryBot.create(:metric, :service => service)
     metric_two = FactoryBot.create(:metric, :service => service)
@@ -211,7 +208,7 @@ class MetricTest < ActiveSupport::TestCase
     assert_same_elements [service.metrics.hits, metric_one, metric_two], service.metrics.top_level
   end
 
-  should '.ids_indexed_by_names returns metric ids indexed by names' do
+  test '.ids_indexed_by_names returns metric ids indexed by names' do
     service = FactoryBot.create(:service)
     metric_1 = FactoryBot.create(:metric, :system_name => 'foo', :service => service)
     metric_2 = FactoryBot.create(:metric, :system_name => 'bar', :service => service)
@@ -221,7 +218,7 @@ class MetricTest < ActiveSupport::TestCase
                  service.metrics.ids_indexed_by_names)
   end
 
-  should '.ancestors_ids returns hash of ancestors ids indexed by descendant id' do
+  test '.ancestors_ids returns hash of ancestors ids indexed by descendant id' do
     service = FactoryBot.create(:service)
     hits = service.metrics.hits
     child = FactoryBot.create(:metric, :parent => hits, :service => service)
@@ -246,17 +243,17 @@ class MetricTest < ActiveSupport::TestCase
     assert_equal metric_one, service.metrics.hits
   end
 
-  context 'visibility options for plan' do
+  class PlanVisibilityOptions < ActiveSupport::TestCase
     setup do
       @plan =  FactoryBot.create(:application_plan)
       @metric = FactoryBot.create(:metric, :service => @plan.service)
     end
 
-    should 'be visible by default' do
+    test 'be visible by default' do
       assert @metric.visible_in_plan?(@plan)
     end
 
-    should 'toggle visibility' do
+    test 'toggle visibility' do
       assert @metric.visible_in_plan?(@plan)
 
       @metric.toggle_visible_for_plan(@plan)
@@ -266,11 +263,11 @@ class MetricTest < ActiveSupport::TestCase
       assert @metric.visible_in_plan?(@plan)
     end
 
-    should 'be limits_only_text by default' do
+    test 'be limits_only_text by default' do
       assert @metric.limits_only_text_in_plan?(@plan)
     end
 
-    should 'toggle limits_only_text' do
+    test 'toggle limits_only_text' do
       assert @metric.limits_only_text_in_plan?(@plan)
 
       @metric.toggle_limits_only_text_for_plan(@plan)
@@ -282,7 +279,7 @@ class MetricTest < ActiveSupport::TestCase
 
   end # visibility options for plan
 
-  context 'availability for plan' do
+  class PlanAvailability < ActiveSupport::TestCase
     setup do
       @plan =  FactoryBot.create(:application_plan)
       @metric1 = FactoryBot.create(:metric,  :service => @plan.service)
@@ -296,21 +293,21 @@ class MetricTest < ActiveSupport::TestCase
       FactoryBot.create :usage_limit, :plan => @disabled_plan, :metric => @metric2, :period => :day, :value => 0
     end
 
-    should 'be enabled for plan if no limit 0 is present' do
+    test 'be enabled for plan if no limit 0 is present' do
       assert @plan.usage_limits.where(metric_id: @metric1.id, value: 0).empty?
 
       assert @metric1.enabled_for_plan?(@plan)
       refute @metric1.disabled_for_plan?(@plan)
     end
 
-    should 'be disabled for plan if a limit 0 is present' do
+    test 'be disabled for plan if a limit 0 is present' do
       refute @disabled_plan.usage_limits.where(metric_id: @metric1.id, value: 0).empty?
 
       assert @metric1.disabled_for_plan?(@disabled_plan)
       refute @metric1.enabled_for_plan?(@disabled_plan)
     end
 
-    should 'disable only the given metric' do
+    test 'disable only the given metric' do
       assert @metric1.enabled_for_plan?(@plan)
       assert @metric2.enabled_for_plan?(@plan)
 
@@ -320,7 +317,7 @@ class MetricTest < ActiveSupport::TestCase
       assert @metric2.enabled_for_plan?(@plan)
     end
 
-    should 'disable the given metric even if usage limit already exists' do
+    test 'disable the given metric even if usage limit already exists' do
       FactoryBot.create :usage_limit, plan: @plan, metric: @metric1, period: :minute, value: 1
 
       assert @metric1.enabled_for_plan?(@plan)
@@ -332,7 +329,7 @@ class MetricTest < ActiveSupport::TestCase
       refute @metric1.enabled_for_plan?(@plan)
     end
 
-    should 'not disable the given metric - all periods are being used' do
+    test 'not disable the given metric - all periods are being used' do
       used_periods = @plan.usage_limits.of_metric(@metric1).pluck(:period).uniq
 
       (UsageLimit::PERIODS - used_periods.map(&:to_sym)).each do |period|
@@ -350,7 +347,7 @@ class MetricTest < ActiveSupport::TestCase
       refute @metric1.errors.blank?
     end
 
-    should 'enable only the given metric' do
+    test 'enable only the given metric' do
       assert @metric1.disabled_for_plan?(@disabled_plan)
       assert @metric2.disabled_for_plan?(@disabled_plan)
 
@@ -360,7 +357,7 @@ class MetricTest < ActiveSupport::TestCase
       assert @metric2.disabled_for_plan?(@disabled_plan)
     end
 
-    should 'toggle availability' do
+    test 'toggle availability' do
       assert @metric1.enabled_for_plan?(@plan)
 
       @metric1.disable_for_plan(@plan)
@@ -369,7 +366,7 @@ class MetricTest < ActiveSupport::TestCase
       @metric1.enable_for_plan(@plan)
       assert @metric1.enabled_for_plan?(@plan)
     end
-  end # enabled or disabled for plan
+  end
 
   test '.by_provider' do
     provider, other_provider = FactoryBot.create_list(:simple_provider, 2)
@@ -381,8 +378,8 @@ class MetricTest < ActiveSupport::TestCase
 
     other_service = FactoryBot.create(:simple_service, account: other_provider)
     other_backend_api = FactoryBot.create(:backend_api, account: other_provider)
-    other_service_metric = FactoryBot.create(:metric, owner: other_service)
-    other_backend_metric = FactoryBot.create(:metric, owner: other_backend_api)
+    FactoryBot.create(:metric, owner: other_service)
+    FactoryBot.create(:metric, owner: other_backend_api)
 
     assert_same_elements [service.metrics.hits, service_metric, backend_api.metrics.hits, backend_metric], Metric.by_provider(provider)
   end
