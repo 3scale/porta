1e5d9045fb227fd0aec6af8423802c5aeaaad92f
Nidhi Soni <nidhisoni4411@gmail.com>
2021-12-06 22:52:33 +0530

    [params] Fixes dashboard service base controller

--- app/controllers/provider/admin/dashboard/service/base_controller.rb
+++ app/controllers/provider/admin/dashboard/service/base_controller.rb
@@ -5,7 +5,7 @@ class Provider::Admin::Dashboard::Service::BaseController < Provider::Admin::Das
   helper_method :service
 
   def service
-    @_service ||= current_user.accessible_services.find(params[:service_id])
+    @_service ||= current_user.accessible_services.find(params.require(:service_id))
 
     authorize! :show, @_service
 
