19fce502496f1e72f0c37bd7ff2f57cea12dcc5e
josemigallas <josemigallas@gmail.com>
2021-12-13 10:03:05 +0100

    dry setup steps

--- test/unit/finance/bill_variable_for_plan_changed_test.rb
+++ test/unit/finance/bill_variable_for_plan_changed_test.rb
@@ -3,12 +3,15 @@
 require 'test_helper'
 
 class Finance::BillVariableForPlanChangedTest < ActiveSupport::TestCase
-  attr_reader :contract, :account, :app_plan
+  attr_reader :contract, :app_plan
 
   setup do
-    @contract = FactoryBot.build_stubbed(:application_contract)
-    @account  = FactoryBot.build_stubbed(:simple_account)
+    @contract = FactoryBot.build_stubbed(:contract)
+    account = FactoryBot.build_stubbed(:simple_account)
     @app_plan = FactoryBot.build_stubbed(:application_plan)
+
+    contract.stubs(:provider_account).returns(account)
+    account.stubs(:provider_can_use?).with(:instant_bill_plan_change).returns(true)
 
     @org_tz = ENV["TZ"]
   end
@@ -19,9 +22,6 @@ class Finance::BillVariableForPlanChangedTest < ActiveSupport::TestCase
 
   test "bill for variable" do
     Timecop.travel(15.days.ago) if Time.now.mday == 1 # rubocop:disable Rails/TimeZone we don't use timezones in billing
-
-    contract.stubs(:provider_account).returns(account)
-    account.stubs(:provider_can_use?).returns(true)
 
     contract.expects(:save).returns(true)
     contract.notify_observers(:bill_variable_for_plan_changed, app_plan)
@@ -34,19 +34,13 @@ class Finance::BillVariableForPlanChangedTest < ActiveSupport::TestCase
     # this is 4 hours before beginning of month in UTC and 4 after local
     Timecop.travel(Time.now.utc.beginning_of_month - 4.hours)
 
-    contract.stubs(:provider_account).returns(account)
-    account.stubs(:provider_can_use?).returns(true)
-    contract.stubs(:variable_cost_paid_until).returns(Time.now.to_date - 7.days)
-
     contract.expects(:save).never
     contract.notify_observers(:bill_variable_for_plan_changed, app_plan)
   end
 
   test "no variable billing if last billed until is today" do
-    contract.stubs(:provider_account).returns(account)
     Timecop.freeze
     contract.stubs(:variable_cost_paid_until).returns(Time.now.to_date) # rubocop:disable Rails/TimeZone we don't use timezones in billing
-    account.stubs(:provider_can_use?).returns(true)
 
     contract.expects(:save).never
     contract.notify_observers(:bill_variable_for_plan_changed, app_plan)
