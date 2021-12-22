3ddf0c93e69b0abca74e46a91026131f1a5345a2
Aleksandar N. Kostadinov <akostadi@redhat.com>
2021-12-23 00:49:00 +0200

    migration, model validations, search and tests

--- app/lib/three_scale/email_configuration_interceptor.rb
+++ app/lib/three_scale/email_configuration_interceptor.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 class ThreeScale::EmailConfigurationInterceptor
   def self.delivering_email(message)
-    found_mapping = find_mapping
+    found_mapping = find_mapping(message)
     return unless found_mapping
 
-    message.delivery_method.settings = found_mapping.mailer_settings
+    message.delivery_method.settings = found_mapping.smtp_settings
   end
 
-  def self.find_mapping
-    from = message.from.first
+  def self.find_mapping(message)
+    from = message.from&.first
     return unless from
 
     # If we allow each provider to set their own mapping, then
     # account.email_configurations.find_by(email: from)
-    EmailConfiguration.find_by(email: from)
+    EmailConfiguration.for(from).first
   end
 end
-
--- app/models/email_configuration.rb
+++ app/models/email_configuration.rb
@@ -1,30 +1,67 @@
 # frozen_string_literal: true
 
 class EmailConfiguration < ApplicationRecord
-  validates :email, :password, presence: true
-  belongs_to :account, required: true
+
+  validates :email, :password, :user_name, :authentication, :domain, :tls, :address, length: { maximum: 255 }
+
+  validates :email, :password, :user_name, presence: true
+  validates :email, format: { with: Authentication::RE_EMAIL_OK, message: Authentication::MSG_EMAIL_BAD }
+  validates :email, uniqueness: { case_sensitive: false, message: "This email address is already used." }
+  validates :address, :domain, format: { with: Authentication::RE_DOMAIN_OK }, allow_nil: true
+  validates :address, presence: true, unless: :only_auth_changes?
+  validates :authentication, inclusion: { in: %w[plain login cram_md5] }, allow_nil: true
+  validates :openssl_verify_mode, inclusion: { in: [OpenSSL::SSL::VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE] }, allow_nil: true
+  validates :tls, inclusion: { in: %w[starttls starttls_auto tls no] }, allow_nil: true
+  validates :port, inclusion: { in: 1..65535 }, allow_nil: true
+
+  belongs_to :account, optional: false
+  validate :account_is_provider
 
   before_save :set_tenant_id, on: :create
 
-  def smtp_settings
+  scope :for, ->(search_email) {
+    where arel_table[:email].matches(search_email.gsub(/([_%\\])/, '\\\\\\1'), "\\", false)
+  }
+
+  def local_settings
     {
-      user_name: username,
+      user_name: user_name,
       password: password,
-      # # This below we do not have to do for SaaS OCP
-      # # Can be done in 2nd and 3rd steps
-      # address: nil,
-      # port: nil,
-      # domain: nil,
-      # openssl_verify_mode: nil,
-      # enable_starttls_auto: nil,
-      # domain: nil,
-      # authentication: nil
-    }.reverse_merge(ActionMailer::Base.smtp_settings)
+      address: address,
+      port: port,
+      domain: domain,
+      authentication: authentication,
+    }.compact.merge(tls_settings)
+  end
+
+  def tls_settings
+    return {} unless tls
+
+    {
+      enable_starttls_auto: tls == "starttls_auto",
+      enable_starttls: tls == "starttls",
+      tls: tls == "tls",
+    }
+  end
+
+  def smtp_settings
+    base_settings = only_auth_changes? ? ActionMailer::Base.smtp_settings : {}
+    base_settings.merge local_settings
+  end
+
+  def only_auth_changes?
+    %i[address port tls authentication openssl_verify_mode].none? { |attr| send(attr) }
   end
 
   protected
 
   def set_tenant_id
     self.tenant_id ||= account&.id if account&.provider?
+  end
+
+  private
+
+  def account_is_provider
+    errors.add(:account_id, "is not a provider account") unless account.try(:provider?)
   end
 end
