6addf12802797f93ed0e683946bbdaded2a6c990
josemigallas <josemigallas@gmail.com>
2022-01-04 10:06:52 +0100

    fixes undefined method with_indifferent_access in settings controller

--- app/controllers/finance/provider/settings_controller.rb
+++ app/controllers/finance/provider/settings_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Finance::Provider::SettingsController < Finance::Provider::BaseController
 
   before_action :set_strategy
@@ -13,15 +15,15 @@ class Finance::Provider::SettingsController < Finance::Provider::BaseController
   end
 
   def update
-    if params['finance_billing_strategy']['currency'].blank?
-      params['finance_billing_strategy']['currency'] = nil
+    if finance_billing_strategy_params['currency'].blank?
+      finance_billing_strategy_params['currency'] = nil
     end
 
-    if @billing_strategy.numbering_period != params['finance_billing_strategy']['numbering_period']
+    if @billing_strategy.numbering_period != finance_billing_strategy_params['numbering_period']
       ok_message = "Already existent invoices won't change their id."
     end
 
-    if @billing_strategy.update_attributes(params['finance_billing_strategy'])
+    if @billing_strategy.update_attributes(finance_billing_strategy_params.to_h)
       flash[:message] = 'Finance settings updated.' << ok_message.to_s
       redirect_to :action => 'show'
     else
@@ -36,4 +38,7 @@ class Finance::Provider::SettingsController < Finance::Provider::BaseController
     @billing_strategy = current_user.account.billing_strategy
   end
 
+  def finance_billing_strategy_params
+    params.require(:finance_billing_strategy).permit(:charging_enabled, :currency, :numbering_period, account_attributes: %i[invoice_footnote vat_zero_text id])
+  end
 end
