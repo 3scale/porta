c596bc00ceb7c420237cbc1b3e7984283dffab59
jsmadis <jakub.smadis@gmail.com>
2022-01-18 12:56:53 +0100

    thinking sphinx preload indices unneeded in 5.3

--- config/application.rb
+++ config/application.rb
@@ -273,11 +273,6 @@ module System
       require 'system/redis_pool'
       redis_config = ThreeScale::RedisConfig.new(config.redis)
       System.redis = System::RedisPool.new(redis_config.config)
-
-      # Prevents concurrent threads (e.g. sidekiq, puma) to deadlock while racing to obtain access to the mutex block at https://github.com/pat/thinking-sphinx/blob/v3.4.2/lib/thinking_sphinx/configuration.rb#L78
-      # This is a ThinkingSphinx's known bug, fixed in v4.3.0+ - see: https://github.com/pat/thinking-sphinx/commit/814beb0aa3d9dd1227c0f41d630888a738f7c0d6
-      # See also https://github.com/pat/thinking-sphinx/issues/1051 and https://github.com/pat/thinking-sphinx/issues/1132
-      ThinkingSphinx::Configuration.instance.preload_indices if ActiveRecord::Base.connected?
     end
 
     config.assets.quiet = true
