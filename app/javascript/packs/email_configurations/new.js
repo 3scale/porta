24b47b09b9401efa12444030db4f85c546ab840c
josemigallas <josemigallas@gmail.com>
2022-01-28 15:22:16 +0100

    updates email_configuration/new pack

--- app/javascript/packs/email_configurations/new.js
+++ app/javascript/packs/email_configurations/new.js
@@ -15,10 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const { dataset } = container
   const { url } = dataset
+  // $FlowIgnore[incompatible-type] Safe to assume a model is always passed. See app/presenters/provider/admin/account/email_configurations_presenter.rb#new_data
   const emailConfiguration: FormEmailConfiguration = safeFromJsonString<FormEmailConfiguration>(dataset.emailConfiguration)
   const errors = safeFromJsonString<FormErrors>(dataset.errors)
-
-  console.log({ emailConfiguration, errors })
 
   NewPageWrapper({
     url,
