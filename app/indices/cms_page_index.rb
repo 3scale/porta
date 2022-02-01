32c8bec50a38e59c0a9216947f8d23bbe216ced0
josemigallas <josemigallas@gmail.com>
2022-02-01 12:19:41 +0100

    [thinking-sphinx] updates indexes definition

--- app/indices/cms_page_index.rb
+++ app/indices/cms_page_index.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-ThinkingSphinx::Index.define 'cms/page', with: :real_time do
-  indexes :title
-  has :tenant_id, type: :bigint
+ThinkingSphinx::Index.define('cms/page'.to_sym, with: :real_time) do
+  # Fields
+  indexes title
+
+  # Attributes
+  has tenant_id, type: :bigint
 
   indexes :published
   scope { CMS::Page.where(searchable: true) }
