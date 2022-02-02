1b48126710a0df5e439f33cb02a2ec6978eab063
josemigallas <josemigallas@gmail.com>
2022-02-02 10:44:14 +0100

    adds field email to email_configuration_index

--- app/indices/email_configuration_index.rb
+++ app/indices/email_configuration_index.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 ThinkingSphinx::Index.define(:email_configuration, with: :real_time) do
-  indexes :user_name
-
-  # has :account_id, type: :bigint # TODO: useful?
+  indexes email
+  indexes user_name
 end
