eaceeb6816bcaab1be47ab2d6d63b232550878a2
josemigallas <josemigallas@gmail.com>
2022-02-03 17:20:09 +0100

    💩 revert hardcoded feature

--- app/lib/features/email_configuration_config.rb
+++ app/lib/features/email_configuration_config.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
 module Features
-  class EmailConfigurationConfig < Config
-    # TODO: REMOVE THIS!!!!!!
-    def enabled?
-      true
-    end
-  end
+  class EmailConfigurationConfig < Config; end
 end
