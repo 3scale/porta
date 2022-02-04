025556abdf24cef8cb1aabeaa714bb35fd392ef8
Sergio Franco <sfrancog@redhat.com>
2022-02-04 17:17:36 +0000

    Implement suggestions

--- app/workers/set_tenant_id_worker.rb
+++ app/workers/set_tenant_id_worker.rb
@@ -5,13 +5,9 @@ class SetTenantIdWorker < ApplicationJob
   def perform(provider)
     provider.update_column(:master, false)
 
-    {
-      BackendApi => :account_id,
-      LogEntry => :provider_id,
-      Alert => :account_id,
-    }.each do |model, relation|
-      model.where(relation => provider.id).find_each do |instance|
-        ModelTenantIdWorker.perform_later(instance, :tenant_id, provider)
+    [:backend_apis, :log_entries, :alerts].each do |relation|
+      provider.public_send(relation).where(tenant_id: nil).find_each do |instance|
+        ModelTenantIdWorker.perform_later(instance, provider.tenant_id)
       end
     end
   end
@@ -20,16 +16,16 @@ class SetTenantIdWorker < ApplicationJob
     unique :until_executed
 
     def perform(*)
-      Account.providers.not_master.find_each do |provider_batch|
-        SetTenantIdWorker.perform_later(provider_batch)
+      Account.providers.where(master: nil).find_each do |provider|
+        SetTenantIdWorker.perform_later(provider)
       end
     end
   end
 
   class ModelTenantIdWorker < ApplicationJob
 
-    def perform(object, attribute, provider)
-      object.update_column(attribute, provider.tenant_id)
+    def perform(object, tenant_id)
+      object.update_column(:tenant_id, tenant_id)
     end
 
   end
