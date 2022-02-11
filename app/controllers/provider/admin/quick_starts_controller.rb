0a78e1ef58eeaa1cb62782bdbc7f4430225e80b9
josemigallas <josemigallas@gmail.com>
2022-02-11 12:51:58 +0100

    🍰  removes tarts from code

--- app/controllers/provider/admin/quick_starts_controller.rb
+++ app/controllers/provider/admin/quick_starts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Provider::Admin::QuickStartsController < FrontendController
-  before_action :hide_quicks_tarts
+  before_action :hide_quick_starts
 
   activate_menu :quick_starts
 
@@ -11,7 +11,7 @@ class Provider::Admin::QuickStartsController < FrontendController
 
   private
 
-  def hide_quicks_tarts
+  def hide_quick_starts
     raise ActionController::RoutingError, '' unless provider_can_use?(:quick_starts)
   end
 end
