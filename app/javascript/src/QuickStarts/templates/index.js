f7734ea1a84549bbb665132bbf31f4b0cc61af04
Darren Fennessy <44813955+dfennessy@users.noreply.github.com>
2022-03-10 12:35:26 +0000

    Added content for creating an application.

--- app/javascript/src/QuickStarts/templates/index.js
+++ app/javascript/src/QuickStarts/templates/index.js
@@ -4,12 +4,14 @@ import basicAPIintegration from './basicAPIintegration.yaml'
 import createBackend from './createBackend.yaml'
 import createApplicationPlan from './createApplicationPlan.yaml'
 import createProduct from './createProduct.yaml'
+import createProduct from './createApplication.yaml'
 
 const resources = [
     basicAPIintegration,
     createBackend,
     createApplicationPlan,
-    createProduct
+    createProduct,
+    createApplication
 ]
 
 export default resources
