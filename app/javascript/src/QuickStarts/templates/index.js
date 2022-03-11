07f0fe63fe45545da2b9d320405200ea844dcd41
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2022-03-11 15:10:22 +0100

    Update index.js

--- app/javascript/src/QuickStarts/templates/index.js
+++ app/javascript/src/QuickStarts/templates/index.js
@@ -4,20 +4,20 @@ import basicAPIintegration from './basicAPIintegration.yaml'
 import createBackend from './createBackend.yaml'
 import createProduct from './createProduct.yaml'
 import createApplicationPlan from './createApplicationPlan.yaml'
-import createProduct from './createApplication.yaml'
+import createApplication from './createApplication.yaml'
 import createMethod from './createMethod.yaml'
 import createMetric from './createMetric.yaml'
 import createMappingRule from './createMappingRule.yaml'
 
 const resources = [
-    basicAPIintegration,
-    createBackend,
-    createProduct,
-    createApplicationPlan,
-    createApplication,
-    createMethod,
-    createMetric,
-    createMappingRule
+  basicAPIintegration,
+  createBackend,
+  createProduct,
+  createApplicationPlan,
+  createApplication,
+  createMethod,
+  createMetric,
+  createMappingRule
 ]
 
 export default resources
