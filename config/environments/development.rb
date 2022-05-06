026f0f5fe11f38e42457224f591dba60a40d5c86
Aleksandar N. Kostadinov <akostadi@redhat.com>
2022-05-06 19:22:09 +0300

    applicable changes from rake app:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,12 +29,13 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -42,10 +43,21 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # config.active_storage.service = :local
+
   # Don't care if the mailer can't send.
-  # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method =
+    if defined?(LetterOpener)
+      :letter_opener
+    elsif config.action_mailer.smtp_settings[:address]
+      :smtp
+    else
+      :test
+    end
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -56,16 +68,8 @@ Rails.application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.delivery_method =
-    if defined?(LetterOpener)
-      :letter_opener
-    elsif config.action_mailer.smtp_settings[:address]
-      :smtp
-    else
-      :test
-    end
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
