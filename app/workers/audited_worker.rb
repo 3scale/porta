6d83cdf12ad16d7d2438944daad43f3c13e31e9d
Aleksandar N. Kostadinov <akostadi@redhat.com>
2022-05-18 13:31:30 +0300

    async auditing broke rails internals

--- app/workers/audited_worker.rb
+++ app/workers/audited_worker.rb
@@ -3,7 +3,6 @@ class AuditedWorker
 
   def perform(attributes)
     audit = Audited.audit_class.new
-    audit.synchronous = true
     audit.assign_attributes(attributes, without_protection: true) # Rails 4 can remove the option
     audit.save!
   end
