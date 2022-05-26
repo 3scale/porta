021c7e2e535cb729a3c76298a4e2217048616816
Aleksandar N. Kostadinov <akostadi@redhat.com>
2022-05-26 16:38:08 +0300

    note about upstreaming a sphinx monkey patch

--- config/initializers/sphinx.rb
+++ config/initializers/sphinx.rb
@@ -2,6 +2,7 @@ SPHINX_DELTA_INTERVAL = 90.minutes
 
 # Patches ThinkingSphinx so `sphinx_internal_class_name` field is avoided
 # for no inheritance indices despite other indices with inheritance defined
+# trying to upstream with https://github.com/pat/thinking-sphinx/pull/1222
 ThinkingSphinx::Configuration::MinimumFields.prepend(Module.new do
   private
 
