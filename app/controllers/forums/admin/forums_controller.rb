24367559d7550000a7c6b69a8a864b68215e7121
josemigallas <josemigallas@gmail.com>
2022-07-19 15:35:55 +0200

    🔥 removes Forum admin stuff

--- app/controllers/forums/admin/forums_controller.rb
+++
@@ -1,6 +0,0 @@
-class Forums::Admin::ForumsController < Forums::Admin::HidenForumController
-  include ForumSupport::Admin
-  include ForumSupport::Forums
-
-  activate_menu :buyers, :forum, :threads
-end
