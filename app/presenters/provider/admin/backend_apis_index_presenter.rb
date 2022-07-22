b562972cc277fa0be8f8eb74d04dfa27a6ff79ab
lvillen <l.villenjaldon@gmail.com>
2022-07-22 17:45:36 +0200

    #backend_apis excludes deleted backend_apis

--- app/presenters/provider/admin/backend_apis_index_presenter.rb
+++ app/presenters/provider/admin/backend_apis_index_presenter.rb
@@ -16,6 +16,7 @@ class Provider::Admin::BackendApisIndexPresenter
 
   def backend_apis
     @backend_apis ||= current_account.backend_apis
+                                     .where.not(state: 'deleted')
                                      .order(sorting_params)
                                      .scope_search(search)
                                      .paginate(pagination_params)
