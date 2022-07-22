6b5b0fc5a923991c1028d322b1644870650a362c
josemigallas <josemigallas@gmail.com>
2022-07-22 10:59:45 +0200

    🥒 fixes "Delete Account" scenario by asserting flash message container even when invisible

--- features/step_definitions/flash_steps.rb
+++ features/step_definitions/flash_steps.rb
@@ -1,3 +1,22 @@
+# frozen_string_literal: true
+
 Then /^I should see the flash message "([^"]*)"$/ do |flash_text|
-  find(:xpath, "//div[@id='flash-messages'] | //div[@id='flashWrapper']").text.should have_content(flash_text)
+  assert_flash flash_text
+end
+
+def assert_flash(message)
+  # Sometimes the flash hides before the a test asserts it and fails. Therefore, we target that the flash element
+  # is rendered with the proper message, regardless of visibility.
+
+  # For admin portal see app/views/shared/provider/_flash.html.erb
+  if has_css?('#flashWrapper', visible: :all)
+    wrapper = find('#flashWrapper span', visible: :all)
+  # For dev portal see app/assets/javascripts/flash-buyer.js
+  elsif has_css?('#flash-messages')
+    wrapper = find('#flash-messages .alert .container')
+  else
+    raise "No flash messages container has been found"
+  end
+
+  wrapper.assert_text(:all, message)
 end
