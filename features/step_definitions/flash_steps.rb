eec656ef2c959645e06891e6e216a0d309dcafb7
josemigallas <josemigallas@gmail.com>
2022-07-28 17:15:53 +0200

    test

--- features/step_definitions/flash_steps.rb
+++ features/step_definitions/flash_steps.rb
@@ -9,8 +9,11 @@ def assert_flash(message)
   # is rendered with the proper message, regardless of visibility.
 
   # For admin portal see app/views/shared/provider/_flash.html.erb
-  if has_css?('#flashWrapper', visible: :all)
+  if has_css?('#flashWrapper span', visible: :all)
     wrapper = find('#flashWrapper span', visible: :all)
+  # lib/developer_portal/app/views/shared/_flash_message.html.erb
+  elsif has_css?('#flashWrapper p', visible: :all)
+    wrapper = find('#flashWrapper p', visible: :all)
   # For dev portal see app/assets/javascripts/flash-buyer.js
   elsif has_css?('#flash-messages')
     wrapper = find('#flash-messages .alert .container')
