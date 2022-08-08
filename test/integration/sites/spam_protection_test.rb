589d60fee792e8acd56b0f26ca6003a5a20ecacb
josemigallas <josemigallas@gmail.com>
2022-08-08 12:31:18 +0200

    ✅ updates failing tests in circleci

--- test/integration/sites/spam_protection_test.rb
+++ test/integration/sites/spam_protection_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Sites::SpamProtectionTest < ActionDispatch::IntegrationTest
 
-  def setup
+  setup do
     provider = FactoryBot.create(:provider_account)
 
     login_provider provider
@@ -10,12 +10,15 @@ class Sites::SpamProtectionTest < ActionDispatch::IntegrationTest
     host! provider.external_admin_domain
   end
 
-  def test_edit
+  test 'edit without captcha' do
+    Recaptcha.expects(:captcha_configured?).returns(false).twice
     get edit_admin_site_spam_protection_path
     assert_response :success
     assert_match 'reCAPTCHA has not been configured correctly', response.body
+  end
 
-    Recaptcha.expects(:captcha_configured?).returns(true).at_least_once
+  test 'edit with captcha' do
+    Recaptcha.expects(:captcha_configured?).returns(true).twice
     get edit_admin_site_spam_protection_path
     assert_response :success
     assert_not_match 'reCAPTCHA has not been configured correctly', response.body
