9cdd4813d504bfb68e0a4a85a95e0c02febe114a
Daria Mayorova <mayorova@users.noreply.github.com>
2022-09-01 10:31:14 +0200

    Add cucumber test for pagination

--- features/step_definitions/proxy_steps.rb
+++ features/step_definitions/proxy_steps.rb
@@ -85,3 +85,29 @@ Then(/^the mapping rules should be in the following order:$/) do |table|
     end
   end
 end
+
+Given('the service {string} has {int} mapping rules starting with pattern {string}') do |service_name, rules_size, pattern|
+  service = @provider.services.find_by_name(service_name)
+  hits_metric = service.metrics.first
+  proxy_rules = service.proxy.proxy_rules
+  rules_size.times do |num|
+    proxy_rules.create(http_method: 'GET', pattern: "#{pattern}/num", delta: 1, metric: hits_metric)
+  end
+end
+
+Then(/^the mapping rules of service "([^"]*)" should be in the following order:$/) do |service_name, table|
+  service = @provider.services.find_by_name(service_name)
+  data = service.proxy.proxy_rules.includes(:metric).ordered
+  data.each_with_index do |mapping_rule, index|
+    MAPPING_RULE_ATTR.each do |attr|
+      actual_value = mapping_rule.public_send(attr)
+      actual_value = actual_value.name if attr == 'metric'
+      assert_equal table.hashes[index][attr].to_s, actual_value.to_s
+    end
+  end
+end
+
+When('I search mapping rules for pattern {string}') do |query|
+  fill_in('search_query', :with => query)
+  click_button('Search')
+end
