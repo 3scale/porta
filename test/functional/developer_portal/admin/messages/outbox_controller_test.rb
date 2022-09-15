3c25bfbad4e5eaa9f895ec672593b051d8db8186
Nidhi Soni <84672731+nidhi-soni1104@users.noreply.github.com>
2022-09-15 11:57:50 +0530

    update assertion
    Co-authored-by: Daria Mayorova <mayorova@users.noreply.github.com>

--- test/functional/developer_portal/admin/messages/outbox_controller_test.rb
+++ test/functional/developer_portal/admin/messages/outbox_controller_test.rb
@@ -55,6 +55,6 @@ class DeveloperPortal::Admin::Messages::OutboxControllerTest < DeveloperPortal::
     msg = Message.new
     msg.sender_id = buyer.id
     msg.body = "I am body"
-    assert !msg.valid?
+    assert_not msg.valid?
   end
 end
