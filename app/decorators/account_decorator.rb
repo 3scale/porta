ab9fa4f4e366e7fb9fad791acacb08b9d0c811bf
Joan Lledó <jlledo@redhat.com>
2022-09-21 16:22:47 +0200

    THREESCALE-8530: Simplify the code

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -4,22 +4,11 @@ class AccountDecorator < ApplicationDecorator
   delegate :display_name, :email, to: :admin_user, prefix: true
 
   def first_admin
-    @first_admin ||= (first_admin_cached || super)
+    # when users are eager loaded, filter manually to save an extra query
+    @first_admin ||= users.loaded? ? users.find { |user| user.admin? && !user.impersonation_admin? } : super
   end
 
   private
-
-  def first_admin_cached
-    @first_admin_cached ||= admins&.first
-  end
-
-  def admins
-    return nil unless users.loaded?
-
-    @admins ||= users.select do |user|
-      user.role == :admin && user.username != ThreeScale.config.impersonation_admin['username']
-    end
-  end
 
   def admin_user
     @admin_user ||= (super || User.new).decorate
