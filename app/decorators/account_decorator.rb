4b97751b73f9a650b14e266f097aaaf30bdb1a37
Joan Lledó <jlledo@redhat.com>
2022-09-22 11:51:03 +0200

    THREESCALE-8530: remove the redundant logic
    We don't need it as first_user is equivalent to admin_user, which is already being eager loaded

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -3,11 +3,6 @@
 class AccountDecorator < ApplicationDecorator
   delegate :display_name, :email, to: :admin_user, prefix: true
 
-  def first_admin
-    # when admin_users is eager loaded, filter manually to save an extra query
-    @first_admin ||= admin_users.loaded? ? admin_users.find { |user| !user.impersonation_admin? } : super
-  end
-
   private
 
   def admin_user
