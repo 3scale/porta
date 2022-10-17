b89bf8e2097614f53a680c878a388fb04adf2255
Thales Miguel <thales.miguel@gmail.com>
2022-10-17 12:19:34 +0200

    Break PotentialUpgradesWidget into smaller components

--- app/javascript/src/Dashboard/components/PotentialUpgradesWidget.jsx
+++ app/javascript/src/Dashboard/components/PotentialUpgradesWidget.jsx
@@ -7,14 +7,16 @@ import pluralize from 'pluralize'
 
 import 'Dashboard/styles/dashboard.scss'
 
+type Violation = {
+  id: number,
+  account_id: number,
+  account_name: string,
+  alerts_count: number,
+  url: string
+}
+
 type Props = {
-  violations: Array<{
-    id: number,
-    account_id: number,
-    account_name: string,
-    alerts_count: number,
-    url: string
-  }>,
+  violations: [Violation],
   incorrectSetUp: boolean,
   links: {
     adminServiceApplicationPlans: string,
@@ -22,42 +24,55 @@ type Props = {
   }
 }
 
-const PotentialUpgradesWidget = ({ violations, incorrectSetUp, links }: Props): React.Node => {
-  return (
-    <article className='DashboardWidget' id='dashboard-widget-potential_upgrades'>
-      <h1 className='DashboardWidget-title'>
-        Potential Upgrades
-      </h1>
-      <span className='DashboardWidget-intro'>
-        Accounts that hit their Usage Limits in the last 30 days
-      </span>
+const IncorrectSetUpMessages = ({ links }: {links: Props.links}) => (
+  <div>
+    <p>
+      In order to show Potential Upgrades, add 1 or more usage limits to your <a href={links.adminServiceApplicationPlans}>Application Plans</a>.
+    </p>
+    <p>
+      Furthermore, <a href={links.settingsAdminService}>Web Alerts for Admins of this Account of 100% (and up)</a> should be enabled for service(s) with usage limits.
+    </p>
+  </div>
+)
 
-      {
-        violations.some((violation) => violation)
-          ? <ol className='DashboardWidgetList'>
-            {
-              violations.map(violation =>
-                <li className='DashboardWidgetList-item' key={violation.account_id}>
-                  <a href={violation.url} className='DashboardWidgetList-link'>
-                    {violation.account_name} has {violation.alerts_count} { pluralize('Usage Limit alert', violation.alerts_count) }
-                  </a>
-                </li>
-            )}
-          </ol>
-          : (incorrectSetUp)
-            ? <>
-                <p>
-                  In order to show Potential Upgrades, add 1 or more usage limits to your <a href={links.adminServiceApplicationPlans}>Application Plans</a>.
-                </p>
-                <p>
-                  Furthermore, <a href={links.settingsAdminService}>Web Alerts for Admins of this Account of 100% (and up)</a> should be enabled for service(s) with usage limits.
-                </p>
-              </>
-          : <p>No Potential Upgrades. Yet…</p>
-      }
-    </article>
+const ViolationsList = ({ violations }: {violations: Props.violations}): React.Node => {
+  const violationsList = violations.map(
+    violation => <PotentialUpgradeWidgetItem violation={violation} key={violation.account_id}/>
+  )
+
+  return (
+    <ol className='DashboardWidgetList'>
+      { violationsList }
+    </ol>
   )
 }
+
+const PotentialUpgradeWidgetItem = ({ violation }: {violation: Violation}) => (
+  <li className='DashboardWidgetList-item'>
+    <a href={violation.url} className='DashboardWidgetList-link'>
+      {violation.account_name} has {violation.alerts_count} { pluralize('Usage Limit alert', violation.alerts_count) }
+    </a>
+  </li>
+)
+
+const PotentialUpgradesWidget = ({ violations, incorrectSetUp, links }: Props): React.Node => (
+  <article className='DashboardWidget' id='dashboard-widget-potential_upgrades'>
+    <h1 className='DashboardWidget-title'>
+      Potential Upgrades
+    </h1>
+    <span className='DashboardWidget-intro'>
+      Accounts that hit their Usage Limits in the last 30 days
+    </span>
+
+    {
+      (incorrectSetUp)
+        ? <IncorrectSetUpMessages links={links}/>
+        : violations.length > 0
+          ? <ViolationsList violations={violations}/>
+        : <p>No Potential Upgrades. Yet…</p>
+    }
+  </article>
+)
 
 const PotentialUpgradesWidgetWrapper = (props: Props, containerId: string): void => createReactWrapper(<PotentialUpgradesWidget {...props} />, containerId)
 
