776f5021ddad2095255998cd723c314e0dd9cfe4
josemigallas <josemigallas@gmail.com>
2022-10-20 15:23:21 +0200

    fixes typo in new_application

--- app/javascript/packs/new_application.ts
+++ app/javascript/packs/new_application.ts
@@ -30,8 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const buyers = safeFromJsonString<Buyer[]>(dataset.mostRecentlyCreatedBuyers)
   const buyersCount = safeFromJsonString<number>(dataset.buyersCount)
   const definedFields = safeFromJsonString<FieldDefinition[]>(dataset.definedFields)
-  const validationErrors = safeFromJsonString<{ base: string[] }>(dataset.errors) || { base: [] }
-  const error: string | undefined = validationErrors.base[0]
+  const validationErrors = safeFromJsonString<{ base?: string[] }>(dataset.errors) || { base: [] }
+  const error: string | undefined = validationErrors.base?.[0]
 
   NewApplicationFormWrapper({
     createApplicationPath,
