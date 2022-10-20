1794212ad4b96a0b0c34556723313f04eff13f10
Thales Miguel <thales.miguel@gmail.com>
2022-10-20 14:48:40 +0200

    Break NewAccountsWidget into smaller components

--- app/javascript/src/Dashboard/components/NewAccountsWidget.jsx
+++ app/javascript/src/Dashboard/components/NewAccountsWidget.jsx
@@ -37,6 +37,34 @@ type Props = {
   percentualChange: number
 }
 
+const NewAccountsWidgetTitle = ({ currentRangeAdminBuyersAccount, newAccountsTotal }: { currentRangeAdminBuyersAccount: Props.links.currentRangeAdminBuyersAccount, newAccountsTotal: number }) => (
+  <h1 className="DashboardWidget-title">
+    <a className="DashboardWidget-link" href={currentRangeAdminBuyersAccount.url}>
+      <strong data-title-count="true">{newAccountsTotal}</strong> Signups
+    </a>
+  </h1>
+)
+
+const NewAccountsWidgetPercentageInfo = ({ hasHistory, percentualChange, previousRangeAdminBuyersAccount, lastDayInRangeAdminBuyersAccount }: { hasHistory: boolean, percentualChange: number, previousRangeAdminBuyersAccount: Props.links.previousRangeAdminBuyersAccount, lastDayInRangeAdminBuyersAccount: Props.links.lastDayInRangeAdminBuyersAccount }) => (
+  <div className="DashboardWidget-percentageInfo" data-toggle-visibility="true">
+    {
+      hasHistory
+        ? <a href={previousRangeAdminBuyersAccount.url} className={`DashboardWidget-link ${percentualChange > 0 ? 'u-plus' : 'u-minus' }`}>
+            {previousRangeAdminBuyersAccount.value}
+          </a>
+        : <a href={lastDayInRangeAdminBuyersAccount.url} className="DashboardWidget-link DashboardWidget-link--today">
+            {lastDayInRangeAdminBuyersAccount.value}
+          </a>
+    }
+    <span className="DashboardWidget-intro DashboardWidget-intro--primary" data-title-intro="true">
+      last 30 days
+    </span>
+    <span className="DashboardWidget-intro DashboardWidget-intro--secondary" data-toggle-visibility="true">
+      {hasHistory ? 'vs. previous 30 days' : 'today'}
+    </span>
+  </div>
+)
+
 const NewAccountsWidget = ({ chartData, newAccountsTotal, hasHistory, links, percentualChange }: Props): React.Node => {
   const [isLoading, setIsLoading] = useState(true)
 
@@ -50,28 +78,16 @@ const NewAccountsWidget = ({ chartData, newAccountsTotal, hasHistory, links, per
       {isLoading && <Spinner size="md" isSVG className="pf-u-ml-md DashboardWidget-spinner" />}
       <div className="Dashboard-chart c3" data-chart/>
       <header className="DashboardWidget-badge">
-        <h1 className="DashboardWidget-title">
-          <a className="DashboardWidget-link" href={links.currentRangeAdminBuyersAccount.url}>
-            <strong data-title-count="true">{newAccountsTotal}</strong> Signups
-          </a>
-        </h1>
-        <div className="DashboardWidget-percentageInfo" data-toggle-visibility="true">
-          {
-            hasHistory
-              ? <a href={links.previousRangeAdminBuyersAccount.url} className={`DashboardWidget-link ${percentualChange > 0 ? 'u-plus' : 'u-minus' }`}>
-                  {links.previousRangeAdminBuyersAccount.value}
-                </a>
-              : <a href={links.lastDayInRangeAdminBuyersAccount.url} className="DashboardWidget-link DashboardWidget-link--today">
-                  {links.lastDayInRangeAdminBuyersAccount.value}
-                </a>
-          }
-          <span className="DashboardWidget-intro DashboardWidget-intro--primary" data-title-intro="true">
-            last 30 days
-          </span>
-          <span className="DashboardWidget-intro DashboardWidget-intro--secondary" data-toggle-visibility="true">
-            {hasHistory ? 'vs. previous 30 days' : 'today'}
-          </span>
-        </div>
+        <NewAccountsWidgetTitle
+          currentRangeAdminBuyersAccount={links.currentRangeAdminBuyersAccount}
+          newAccountsTotal={newAccountsTotal}
+        />
+        <NewAccountsWidgetPercentageInfo
+          hasHistory={hasHistory}
+          percentualChange={percentualChange}
+          previousRangeAdminBuyersAccount={links.previousRangeAdminBuyersAccount}
+          lastDayInRangeAdminBuyersAccount={links.lastDayInRangeAdminBuyersAccount}
+        />
       </header>
     </article>
   )
