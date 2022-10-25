acdb5e02de2ad4a80fef9202c9310e6fe70b9902
Joan Lledó <jlledo@redhat.com>
2022-10-25 10:37:40 +0200

    THREESCALE-6851: Stripe webhook: Ack. reception
    Returns 204 to acknowledge reception even for payments we don't care about

--- app/controllers/finance/api/payment_callbacks/stripe_callbacks_controller.rb
+++ app/controllers/finance/api/payment_callbacks/stripe_callbacks_controller.rb
@@ -21,7 +21,14 @@ class Finance::Api::PaymentCallbacks::StripeCallbacksController < Finance::Api::
 
   # Undocumented endpoint used for update callbacks of async-authorized payment transactions (mostly due to SCA regulations)
   def create
-    service = Finance::StripePaymentIntentUpdateService.new(current_account, stripe_event)
+    begin
+      service = Finance::StripePaymentIntentUpdateService.new(current_account, stripe_event)
+    rescue ActiveRecord::RecordNotFound
+      # Returning 204 to acknowledge reception even for payments we don't care about
+      # https://issues.redhat.com/browse/THREESCALE-6851
+      Rails.logger.info("Acknowledging reception: '#{stripe_event.id}'")
+      return head(:no_content)
+    end
 
     return head(:no_content) if service.call
 
