96d5f9f69858766bc8fb671cd24db2b3eb0cf30b
Joan Lledó <jlledo@redhat.com>
2022-11-08 11:57:28 +0100

    THREESCALE-5442: Fix codeclimate complains

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,8 +3,8 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += %i[activation_code cms_token credit_card credit_card_auth_code
-                                   credit_card_authorize_net_payment_profile_token credit_card_expires_on
-                                   credit_card_partial_number crypted_password janrain_api_key lost_password_token
-                                   password password_digest payment_gateway_options payment_service_reference salt
-                                   site_access_code sso_key user_key access_token invitation_token encoded_token
-                                   encoded_key variation_key]
+                                                 credit_card_authorize_net_payment_profile_token credit_card_expires_on
+                                                 credit_card_partial_number crypted_password janrain_api_key lost_password_token
+                                                 password password_digest payment_gateway_options payment_service_reference salt
+                                                 site_access_code sso_key user_key access_token invitation_token encoded_token
+                                                 encoded_key variation_key]
