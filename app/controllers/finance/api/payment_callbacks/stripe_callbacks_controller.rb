e175b8144d060c360ddb0132445b86f76c5188d3
Joan Lledó <jlledo@redhat.com>
2022-11-11 10:21:54 +0100

    THREESCALE-6851: Use easier-to-read syntax

--- app/controllers/finance/api/payment_callbacks/stripe_callbacks_controller.rb
+++ app/controllers/finance/api/payment_callbacks/stripe_callbacks_controller.rb
@@ -21,8 +21,8 @@ class Finance::Api::PaymentCallbacks::StripeCallbacksController < Finance::Api::
 
   # Undocumented endpoint used for update callbacks of async-authorized payment transactions (mostly due to SCA regulations)
   def create
-    service = begin
-      Finance::StripePaymentIntentUpdateService.new(current_account, stripe_event)
+    begin
+      service =  Finance::StripePaymentIntentUpdateService.new(current_account, stripe_event)
     rescue ActiveRecord::RecordNotFound
       # Returning 204 to acknowledge reception even for payments we don't care about.
       #
@@ -34,10 +34,10 @@ class Finance::Api::PaymentCallbacks::StripeCallbacksController < Finance::Api::
       # too often.
       #
       # https://issues.redhat.com/browse/THREESCALE-6851
-      nil
+      return head(:no_content)
     end
 
-    return head(:no_content) if service.blank? || service.call
+    return head(:no_content) if service.call
 
     exception = StripeCallbackError.new('Cannot update Stripe payment intent')
     report_error(exception, event: stripe_event, payment_intent: service.payment_intent)
