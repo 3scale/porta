b116020d06d53b2b0f8da0f46e727532f197da88
Joan Lledó <jlledo@redhat.com>
2022-11-22 17:01:51 +0100

    Fix developer portal html escaping

--- lib/developer_portal/lib/liquid/xss_protection.rb
+++ lib/developer_portal/lib/liquid/xss_protection.rb
@@ -10,27 +10,31 @@ module Liquid::XssProtection
     ERB::Util.html_escape(super.dup)
   end
 
+  module Renderer
+    def render(context)
+      output = super(context)
+      should_escape = context.registers[:escape_html] && !output.html_safe?
+
+      if should_escape && output.respond_to?(:to_str)
+        output = output.dup.extend(Liquid::XssProtection)
+      end
+
+      case output
+      when String
+        output.to_str
+      else
+        output
+      end
+    end
+
+    alias render_with_html_escape render
+  end
+
   def self.enable!
     Liquid::Variable.class_eval do
       next if method_defined?(:render_with_html_escape)
 
-      prepend(Module.new do
-        def render(context)
-          output = super(context)
-          should_escape = context.registers[:escape_html] && !output.html_safe?
-
-          if should_escape && output.respond_to?(:to_str)
-            output = output.dup.extend(Liquid::XssProtection)
-          end
-
-          case output
-          when String
-            output.to_str
-          else
-            output
-          end
-        end
-      end)
+      prepend Renderer
 
     end
   end
