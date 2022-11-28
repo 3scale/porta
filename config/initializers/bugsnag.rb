0ce5375b8fa496bfff5d08a1b2a16dd2c3129b09
Daria Mayorova <mayorova@users.noreply.github.com>
2022-11-28 15:55:10 +0100

    Add BUGSNAG_RELEASE_STAGE env var and use it in Bugsnag initializer

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -3,9 +3,9 @@
 Bugsnag.configure do |config|
   config.api_key = Rails.configuration.three_scale.bugsnag_api_key
   config.app_version = System::Deploy.info.revision
-  config.notify_release_stages = %w[production]
   stages = Rails.configuration.three_scale.error_reporting_stages
-  config.notify_release_stages = stages if stages.present?
+  config.notify_release_stages = stages.present? ? stages : %w[production]
+  config.release_stage = Rails.configuration.three_scale.bugsnag_release_stage || Rails.env
 
   ignore_error_names = ActionDispatch::ExceptionWrapper.rescue_responses.keys + ['WebHookWorker::ClientError']
 
