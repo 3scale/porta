1b1da3d4948fbc3c29f2eb6b5f27af68b1d99a6e
Joan Lledó <jlledo@redhat.com>
2022-12-20 17:10:32 +0100

    CMS: Sections: Hide the system name error message
    The form doesn't contain a system_name field, so showing an error message for it may lead ton confusion.

--- app/models/cms/section.rb
+++ app/models/cms/section.rb
@@ -19,7 +19,8 @@ class CMS::Section < ApplicationRecord
   alias sections children
   alias section= parent=
 
-  validates :system_name, :provider, presence: true
+  validates :title, :provider, presence: true
+  validate :system_name_presence
   validates :parent_id, presence: { :unless => :root? }
 
   validates :title, uniqueness: { :scope => [:provider_id, :parent_id] }
@@ -199,6 +200,10 @@ class CMS::Section < ApplicationRecord
     if child_of?(self.id)
       errors.add(:base, "cannot be it's own ancestor")
     end
+  end
+
+  def system_name_presence
+    errors.add(:system_name) if system_name.blank? && title.present?
   end
 
   def strip_trailing_slashes
