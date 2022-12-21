3scale/porta

commit a53c45996613048447ca1c5144199e7db523bab3

📄 Update `license_finder` and licenses file (#3093)
📄 👷 ⬆️ upgrades license_finder gem, licenses report and related tasks

lib/tasks/licenses.rake
@@ -1,0 +1,50 @@
+# frozen_string_literal: true
+
+namespace :licenses do  # rubocop:disable Metrics/BlockLength
+  require 'license_finder'
+
+  report_path = Rails.root.join('doc/licenses/licenses.xml')
+
+  desc 'Generates a report with the dependencies and their licenses'
+  task :report do
+    warn 'Generating report...'
+    license_finder_report(report_path)
+
+    warn 'License report generated.'
+    exit 0
+  end
+
+  desc 'Check license compliance of dependencies'
+  task :compliance do
+    warn 'Checking license action items...'
+    unless cli.action_items
+      warn '*** License compliance test failed  ***'
+      exit 1
+    end
+
+    warn 'Checking license report is up to date...'
+    Tempfile.new(report_path.to_s) do |temp_file|
+      temp_report_path = temp_file.path
+      license_finder_report(temp_report_path)
+
+      unless identical?(report_path, temp_report_path)
+        warn '*** License report outdated. Please run "rails licenses:report" ***'
+        exit 1
+      end
+    end
+
+    warn 'License report up to date.'
+    exit 0
+  end
+
+  private
+
+  def cli
+    @cli ||= LicenseFinder::CLI::Main.new
+  end
+
+  def license_finder_report(path)
+    cli.options = { "format" => "xml", "save" => path }
+    cli.report
+  end
+end

lib/tasks/ci/licenses.rake
@@ -1,27 +1,0 @@
-# frozen_string_literal: true
-
-namespace :ci do
-  namespace :license_finder do
-
-    desc 'Run compliance task and generates the license report if complies'
-    task :run do
-      if Rake::Task['ci:license_finder:compliance'].invoke
-        Rake::Task['ci:license_finder:report'].invoke
-      end
-    end
-    desc 'Check license compliance of dependencies'
-    task :compliance do
-      STDOUT.puts 'Checking license compliance'
-      unless system("license_finder")
-        STDERR.puts "*** License compliance test failed  ***"
-        exit 1
-      end
-    end
-    desc 'Generates a report with the dependencies and their licenses'
-    task :report do
-      STDOUT.puts 'Generating report...'
-      licenses_xml_path = Rails.root.join('doc', 'licenses', 'licenses.xml')
-      exec("license_finder report --format=xml --save #{licenses_xml_path}")
-    end
-  end
-end

.circleci/config.yml
@@ -477,9 +477,9 @@ jobs:
       - checkout
       - *attach-to-workspace
       - run:
-          name: Licences check
+          name: Licenses check
           command: |
-            bundle exec rake ci:license_finder:run
+            bundle exec rake licenses:compliance
       - run:
           name: Swagger validation
           command: |

Gemfile
@@ -236,7 +236,7 @@ group :development, :test do
   gem 'bootsnap', '~> 1.4'
   gem 'colorize'
   gem 'factory_bot_rails', '~> 6.2'
-  gem 'license_finder', '~> 6.12.0'
+  gem 'license_finder', '~> 7.0.1'
 
   gem 'pry-byebug', '>= 3.7.0'
   gem 'pry-doc', '>= 0.8', require: false

config/license_finder.yml
@@ -1,0 +1,5 @@
+---
+dependencies_file_dir: './doc/licenses'
+enabled_package_managers:
+  - bundler
+  - yarn

doc/licenses/licenses.xml
@@ -4403,7 +4403,7 @@
       </dependency>
           <dependency>
         <packageName>debug</packageName>
-        <version>4.3.2</version>
+        <version>4.3.4</version>
         <licenses>
                       <license>
               <name>MIT</name>
@@ -7733,7 +7733,7 @@
       </dependency>
           <dependency>
         <packageName>loader-utils</packageName>
-        <version>1.4.0</version>
+        <version>1.4.2</version>
         <licenses>
                       <license>
               <name>MIT</name>
@@ -8316,6 +8316,16 @@
                   </licenses>
       </dependency>
           <dependency>
+        <packageName>minimist</packageName>
+        <version>1.2.7</version>
+        <licenses>
+                      <license>
+              <name>MIT</name>
+              <url>http://opensource.org/licenses/mit-license</url>
+            </license>
+                  </licenses>
+      </dependency>
+          <dependency>
         <packageName>minipass</packageName>
         <version>3.1.3</version>
         <licenses>
@@ -8528,6 +8538,16 @@
           <dependency>
         <packageName>nan</packageName>
         <version>2.14.2</version>
+        <licenses>
+                      <license>
+              <name>MIT</name>
+              <url>http://opensource.org/licenses/mit-license</url>
+            </license>
+                  </licenses>
+      </dependency>
+          <dependency>
+        <packageName>nan</packageName>
+        <version>2.16.0</version>
         <licenses>
                       <license>
               <name>MIT</name>
@@ -12209,7 +12229,7 @@
       </dependency>
           <dependency>
         <packageName>signal-exit</packageName>
-        <version>3.0.3</version>
+        <version>3.0.7</version>
         <licenses>
                       <license>
               <name>ISC</name>
@@ -13069,7 +13089,7 @@
       </dependency>
           <dependency>
         <packageName>thor</packageName>
-        <version>1.0.1</version>
+        <version>1.2.1</version>
         <licenses>
                       <license>
               <name>MIT</name>

commit 33edfb13cba9e852d83164b72d9130dc482c126e

fix rake in production environment
issue introduced with a53c459

Because `license_finder` is a development dependency and these are
not installed in images, the `require` statement should only be
evaluated if one tries to call the dependent tasks.

Otherwise any rake call raises an error to load `license_finder`.

This is consistent with how it worked when we used cli.

lib/tasks/licenses.rake
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 namespace :licenses do  # rubocop:disable Metrics/BlockLength
-  require 'license_finder'
-
   report_path = Rails.root.join('doc/licenses/licenses.xml')
 
   desc 'Generates a report with the dependencies and their licenses'
@@ -40,7 +38,10 @@ namespace :licenses do  # rubocop:disable Metrics/BlockLength
   private
 
   def cli
-    @cli ||= LicenseFinder::CLI::Main.new
+    @cli ||= begin
+               require 'license_finder'
+               LicenseFinder::CLI::Main.new
+             end
   end
 
   def license_finder_report(path)