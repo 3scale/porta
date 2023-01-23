a49ab2244cf2929888b6c3c8d1b0f3b16c11a0ed
josemigallas <josemigallas@gmail.com>
2023-01-23 10:24:18 +0100

    ♻️ dry: moves some logic to Account::Gateway module

--- lib/developer_portal/lib/developer_portal/controller_methods/payment_paths_methods.rb
+++ lib/developer_portal/lib/developer_portal/controller_methods/payment_paths_methods.rb
@@ -1,25 +1,26 @@
+# frozen_string_literal: true
+
 module DeveloperPortal
   module ControllerMethods
     module PaymentPathsMethods
       protected
 
       def hosted_success_payment_url(merchant_account = site_account)
-        return '' if unacceptable_payment_gateway?(merchant_account)
+        return '' if merchant_account.unacceptable_payment_gateway?
+
         polymorphic_url([:hosted, :success, :admin, :account, merchant_account.payment_gateway_type])
       end
 
       def payment_details_path(merchant_account = site_account)
-        return '' if unacceptable_payment_gateway?(merchant_account)
+        return '' if merchant_account.unacceptable_payment_gateway?
+
         polymorphic_path([:admin, :account, merchant_account.payment_gateway_type])
       end
 
       def edit_payment_details_path(merchant_account = site_account)
-        return '' if unacceptable_payment_gateway?(merchant_account)
-        polymorphic_path([:edit, :admin, :account, merchant_account.payment_gateway_type])
-      end
+        return '' if merchant_account.unacceptable_payment_gateway?
 
-      def unacceptable_payment_gateway?(merchant_account)
-        ['bogus', ''].include?(merchant_account.payment_gateway_type.to_s)
+        polymorphic_path([:edit, :admin, :account, merchant_account.payment_gateway_type])
       end
     end
   end
