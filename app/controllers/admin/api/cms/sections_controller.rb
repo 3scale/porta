a0bc69db5df79e4a78bd66db5a0286c4c0f84714
Joan Lledó <jlledo@redhat.com>
2023-02-02 09:39:02 +0100

    CMS API Docs: system_name allowed for sections

--- app/controllers/admin/api/cms/sections_controller.rb
+++ app/controllers/admin/api/cms/sections_controller.rb
@@ -34,6 +34,7 @@ class Admin::Api::CMS::SectionsController < Admin::Api::CMS::BaseController
   #
   ##~ op.parameters.add @parameter_access_token
   ##~ op.parameters.add :name => "title", :description => "Title of the section", :paramType => "query", :required => true
+  ##~ op.parameters.add :name => "system_name", :description => "Human readable and unique identifier", :paramType => "query"
   ##~ op.parameters.add :name => "public", :description => "Public or not", :default => "true", :type => "boolean", :paramType => "query"
   ##~ op.parameters.add :name => "parent_id", :description => "ID of a parent section", :paramType => "query", :default => "root section id", :type => "int"
   ##~ op.parameters.add :name => "partial_path", :description => "Path of the section", :paramType => "query"
@@ -75,6 +76,7 @@ class Admin::Api::CMS::SectionsController < Admin::Api::CMS::BaseController
   ##~ op.parameters.add @parameter_access_token
   ##~ op.parameters.add @parameter_section_id
   ##~ op.parameters.add :name => "title", :description => "Title of the section", :paramType => "query"
+  ##~ op.parameters.add :name => "system_name", :description => "Human readable and unique identifier", :paramType => "query"
   ##~ op.parameters.add :name => "public", :description => "Public or not", :default => "true", :type => "boolean", :paramType => "query"
   ##~ op.parameters.add :name => "parent_id", :description => "ID of a parent section", :paramType => "query", :default => "root section id", :type => "int"
   ##~ op.parameters.add :name => "partial_path", :description => "Path of the section", :paramType => "query"
