231040fd3df60185705f952786d00fd74e5e93cd
Daria Mayorova <mayorova@users.noreply.github.com>
2023-02-10 18:07:36 +0100

    Report 2.x version for SaaS, despite what's set in .deploy_info (#3207)
    * Report 2.x version for SaaS, despite what's set in .deploy_info
    
    * Make the deploy VERSION constant private
    
    * Add clarification comment

--- test/unit/lib/system/deploy_test.rb
+++ test/unit/lib/system/deploy_test.rb
@@ -3,6 +3,7 @@
 require 'test_helper'
 
 class DeployTest < ActiveSupport::TestCase
+
   test 'default release is 2.x' do
     System::Deploy.load_info!
     assert_equal '2.x', System::Deploy.info.release
@@ -14,11 +15,32 @@ class DeployTest < ActiveSupport::TestCase
     assert_equal 'x', System::Deploy.info.minor_version
   end
 
-  test 'custom release' do
-    path = Rails.root.join('test', 'fixtures', 'deploy_info').expand_path
-    System::Deploy.load_info! ActiveSupport::JSON.decode(path.read)
+  class OnpremisesDeployTest < DeployTest
+    setup do
+      ThreeScale.config.stubs(onpremises: true)
+    end
 
-    assert_equal '4', System::Deploy.info.major_version
-    assert_equal '5', System::Deploy.info.minor_version
+    test 'custom release' do
+      path = Rails.root.join('test', 'fixtures', 'deploy_info').expand_path
+      System::Deploy.load_info! ActiveSupport::JSON.decode(path.read)
+
+      assert_equal '4', System::Deploy.info.major_version
+      assert_equal '5', System::Deploy.info.minor_version
+    end
   end
+
+  class SaasDeployTest < DeployTest
+    setup do
+      ThreeScale.config.stubs(onpremises: false)
+    end
+
+    test 'custom release from .deploy_info is ignored' do
+      path = Rails.root.join('test', 'fixtures', 'deploy_info').expand_path
+      System::Deploy.load_info! ActiveSupport::JSON.decode(path.read)
+
+      assert_equal '2', System::Deploy.info.major_version
+      assert_equal 'x', System::Deploy.info.minor_version
+    end
+  end
+
 end
