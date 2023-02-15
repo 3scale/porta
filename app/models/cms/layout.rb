df440d829c224c4e2fecc0297b01a416f5204170
Joan Lledó <jlledo@redhat.com>
2023-02-15 14:49:26 +0100

    CMS API: Implement API Contract (#3193)

--- app/models/cms/layout.rb
+++ app/models/cms/layout.rb
@@ -8,6 +8,8 @@ class CMS::Layout < CMS::Template
 
   before_destroy :avoid_destruction
 
+  has_data_tag :layout
+
   def human_name
     title or I18n.t(system_name, :scope => [:cms, :layout], :default => system_name)
   end
@@ -19,22 +21,18 @@ class CMS::Layout < CMS::Template
   def to_xml(options = {})
     xml = options[:builder] || Nokogiri::XML::Builder.new
 
-    xml.layout do |x|
+    xml.__send__(self.class.data_tag) do |x|
       unless new_record?
         xml.id id
         xml.created_at created_at.xmlschema
         xml.updated_at updated_at.xmlschema
       end
-
-      x.system_name system_name
-      x.content_type content_type
-      x.handler handler
-      x.liquid_enabled liquid_enabled
       x.title title
-
+      x.system_name system_name
+      x.liquid_enabled liquid_enabled
       unless options[:short]
-        x.draft draft
-        x.published published
+        x.draft { |node| node.cdata draft }
+        x.published { |node| node.cdata published }
       end
     end
 
