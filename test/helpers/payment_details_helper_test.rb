20ef60f3fb0715cb90db3d44f34f2ab280cdf18a
josemigallas <josemigallas@gmail.com>
2023-02-17 10:05:22 +0100

    ✅ adds more tests for PaymentDetailsHelper

--- test/helpers/payment_details_helper_test.rb
+++ test/helpers/payment_details_helper_test.rb
@@ -3,9 +3,66 @@
 require 'test_helper'
 
 class PaymentDetailsHelperTest < DeveloperPortal::ActionView::TestCase
+  test '#credit_card_terms_url' do
+    stubs(:site_account).returns(mock(settings: mock(cc_terms_path: '/terms')))
+    assert_match '/terms', credit_card_terms_url
+  end
+
+  test '#credit_card_privacy_url' do
+    stubs(:site_account).returns(mock(settings: mock(cc_privacy_path: '/privacy')))
+    assert_match '/privacy', credit_card_privacy_url
+  end
+
+  test '#credit_card_refunds_url' do
+    stubs(:site_account).returns(mock(settings: mock(cc_refunds_path: '/refunds')))
+    assert_match '/refunds', credit_card_refunds_url
+  end
+
+  test '#link_to_payment_details' do
+    stubs(:payment_details_path).returns('/payment-details')
+    assert_match '/payment-details', link_to_payment_details('Payment Details')
+  end
+
   test '#payment_details_path' do
-    account = Account.new(payment_gateway_type: :stripe)
+    unacceptable_account = mock(unacceptable_payment_gateway?: true)
+    assert_empty payment_details_path(unacceptable_account)
+
+    account = mock(unacceptable_payment_gateway?: false, payment_gateway_type: :stripe)
     assert_equal '/admin/account/stripe?foo=bar&hello=world', payment_details_path(account, {foo: 'bar', hello: 'world'})
+  end
+
+  test '#merchant_countries' do
+    assert_equal %w[Afghanistan AF], merchant_countries[0]
+  end
+
+  test '#payment_details_definition_list_item' do
+    account = FactoryBot.build(:account, billing_address_first_name: '',
+                                         billing_address_last_name: '',
+                                         billing_address_address1: '',
+                                         billing_address_city: '',
+                                         billing_address_country: '',
+                                         billing_address_name: '',
+                                         billing_address_phone: '',
+                                         billing_address_state: '',
+                                         billing_address_zip: '')
+
+    %w[first_name last_name phone zip city state country].each do |field|
+      assert_nil payment_details_definition_list_item(field, account)
+    end
+
+    account = FactoryBot.build(:account, billing_address_first_name: 'first_name',
+                                         billing_address_last_name: 'last_name',
+                                         billing_address_address1: 'address1',
+                                         billing_address_city: 'city',
+                                         billing_address_country: 'Spain',
+                                         billing_address_name: 'name',
+                                         billing_address_phone: 'phone',
+                                         billing_address_state: 'state',
+                                         billing_address_zip: 'zip')
+
+    %w[first_name last_name phone zip city state country].each do |field|
+      assert_not_nil payment_details_definition_list_item(field, account)
+    end
   end
 
   test '#stripe_billing_address_json' do
@@ -62,6 +119,7 @@ class PaymentDetailsHelperTest < DeveloperPortal::ActionView::TestCase
   end
 
   test '#billing_address_data' do
+    stubs(:country_code_for).with('country').returns('ES').once
     account = FactoryBot.build(:account, billing_address_first_name: 'first_name',
                                          billing_address_last_name: 'last_name',
                                          billing_address_address1: 'address1',
@@ -72,25 +130,35 @@ class PaymentDetailsHelperTest < DeveloperPortal::ActionView::TestCase
                                          billing_address_state: 'state',
                                          billing_address_zip: 'zip')
     stubs(:current_account).returns(account)
+
     expected = {
       firstName: 'first_name',
       lastName: 'last_name',
       address: 'address1',
       city: 'city',
       country: 'country',
+      countryCode: 'ES',
       company: 'name',
       phone: 'phone',
       state: 'state',
       zip: 'zip'
     }
-
     assert_equal expected, billing_address_data
+  end
+
+  test '#country_code_for' do
+    stubs(:merchant_countries).returns([%w[Spain ES]])
+
+    assert_nil country_code_for('')
+    assert_nil country_code_for(nil)
+
+    assert_equal 'ES', country_code_for('Spain')
   end
 
   private
 
   def empty_billing_address_data
-    %i[firstName lastName address city country company phone state zip].collect { |field| [field, ''] }.to_h
+    %i[firstName lastName address city country countryCode company phone state zip].collect { |field| [field, ''] }.to_h
   end
 
   def current_account
