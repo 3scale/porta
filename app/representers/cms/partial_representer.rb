20a2d5b7e535e8928f66b2b09d9496b786b1e8ca
Thales Miguel <thales.miguel@gmail.com>
2023-03-03 14:40:58 +0100

    Adapt CMS::PartialRepresenter JSON response

--- app/representers/cms/partial_representer.rb
+++ app/representers/cms/partial_representer.rb
@@ -1,19 +1,15 @@
 module CMS::PartialRepresenter
   include ThreeScale::JSONRepresenter
 
-  wraps_resource
+  wraps_resource -> (*) { self.class.data_tag }
 
   property :id
-  property :system_name
-
-  with_options(if: ->(options) { options[:short] == false }) do |p|
-    p.property :published, render_nil: true
-    p.property :draft, render_nil: true
-  end
-
-  property :handler
-  property :liquid_enabled
-
   property :created_at
   property :updated_at
+  property :system_name
+
+  with_options(if: ->(options) { !options[:short] }) do |p|
+    p.property :draft, render_nil: true
+    p.property :published, render_nil: true
+  end
 end
