3345d0a5460eb41a4cb2d34b2ebfe6919535c6b5
Thales Miguel <thales.miguel@gmail.com>
2023-03-03 14:40:24 +0100

    Adapt CMS::LayoutRepresenter JSON response

--- app/representers/cms/layout_representer.rb
+++ app/representers/cms/layout_representer.rb
@@ -4,17 +4,14 @@ module CMS::LayoutRepresenter
   wraps_resource
 
   property :id
-
-  property :title
-  property :system_name
-  property :content_type
-  property :handler, render_nil: true
-
-  with_options(if: ->(options) { options[:short] == false }) do |p|
-    p.property :published, render_nil: true
-    p.property :draft, render_nil: true
-  end
-
   property :created_at
   property :updated_at
+  property :title
+  property :system_name
+  property :liquid_enabled
+
+  with_options(if: ->(options) { !options[:short] }) do |p|
+    p.property :draft, render_nil: true
+    p.property :published, render_nil: true
+  end
 end
