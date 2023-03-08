f320ff84c0e44014caa00a24e2d79cc29b90035d
Thales Miguel <thales.miguel@gmail.com>
2023-03-08 10:24:02 +0100

    CMS API: Implement API Contract for JSON responses (#3235)
    * Adapt CMS::FileRepresenter JSON response
    * Adapt CMS::SectionRepresenter JSON response
    * Adapt CMS::LayoutRepresenter JSON response
    * Adapt CMS::PartialRepresenter JSON response
    * Adapt CMS::PageRepresenter JSON response
    * Add data_tag wrap for CMS::LayoutRepresenter
    * Add data_tag wrap for CMS::FileRepresenter

--- app/models/cms/layout.rb
+++ app/models/cms/layout.rb
@@ -31,8 +31,8 @@ class CMS::Layout < CMS::Template
       x.system_name system_name
       x.liquid_enabled liquid_enabled
       unless options[:short]
-        x.draft { |node| node.cdata draft }
-        x.published { |node| node.cdata published }
+        x.draft { |node| node.cdata(draft) if draft }
+        x.published { |node| node.cdata(published) if published }
       end
     end
 
--- app/representers/cms/layout_representer.rb
+++ app/representers/cms/layout_representer.rb
@@ -1,20 +1,19 @@
+# frozen_string_literal: true
+
 module CMS::LayoutRepresenter
   include ThreeScale::JSONRepresenter
 
-  wraps_resource
+  wraps_resource ->(*) { self.class.data_tag }
 
   property :id
-
-  property :title
-  property :system_name
-  property :content_type
-  property :handler, render_nil: true
-
-  with_options(if: ->(options) { options[:short] == false }) do |p|
-    p.property :published, render_nil: true
-    p.property :draft, render_nil: true
-  end
-
   property :created_at
   property :updated_at
+  property :title
+  property :system_name
+  property :liquid_enabled
+
+  with_options(if: ->(options) { !options[:short] }) do
+    property :draft, render_nil: true
+    property :published, render_nil: true
+  end
 end
