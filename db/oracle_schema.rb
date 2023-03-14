8ae765815c656a562a501620e75a0aae6daf54a2
Joan Lledó <jlledo@redhat.com>
2023-03-14 11:22:47 +0100

    Update Oracle and Postgres schemas

--- db/oracle_schema.rb
+++ db/oracle_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_215614) do
+ActiveRecord::Schema.define(version: 2023_03_08_155529) do
 
   create_table "access_tokens", force: :cascade do |t|
     t.integer "owner_id", precision: 38, null: false
