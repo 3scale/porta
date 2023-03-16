52da136976c59cc1e8bae7c273b6581683b1b64c
Joan Lledó <jlledo@redhat.com>
2023-03-16 10:33:20 +0100

    Implement action on extra params at controller level
    Add the logic to ApiSupport::Params

--- app/lib/api_support/params.rb
+++ app/lib/api_support/params.rb
@@ -6,32 +6,85 @@ module ApiSupport::Params
   included do
     wrap_parameters format: %i[url_encoded_form json multipart_form]
     before_action :wrap_parameters_check
+    before_action :unpermitted_parameters_check
+    class_attribute :_class_action_on_unpermitted_parameters
+    class_attribute :_class_always_permitted_parameters, default: []
+  end
 
-    private
+  module ClassMethods
 
-    def flat_params
-      params.except(:format, :controller, :action, :provider_key, :api_key)
+    # What to do when the client sends additional parameters not permitted by Strong Parameters
+    #
+    # === Allowed values:
+    #   * <tt>:log</tt> -   Writes a warning on the logs, containing the list of unpermitted parameters.
+    #   * <tt>:raise</tt> - Raises an +ActionController::UnpermittedParameters+ error.
+    #   * <tt>nil</tt>, not set, any other value - Defaults to the action globally set through +ActionController::Parameters.action_on_unpermitted_parameters+
+    #
+    def class_action_on_unpermitted_parameters(action)
+      self._class_action_on_unpermitted_parameters = action
     end
 
-    # This basically checks if the wrapped parameter is passed as a Hash, if not it will go 422
+    # White list of parameters that won't trigger an UnpermittedParameters error.
     #
-    # when ```wrap_parameter Foo``` it will produce this results:
+    # The parameters in this list are still unpermitted for mass-assignment, they must be
+    # manually permitted for that purpose.
     #
-    #     /api/foo.xml?foo[bar]=1&foo[baz]=2 => OK
-    #     /api/foo.xml?bar=1&baz=2           => OK
-    #     /api/foo.xml?foo[bar]=1&foo=2      => 422
-    #     /api/foo.xml?foo=5                 => 422
+    # Accepts symbols and strings
     #
-    def wrap_parameters_check
-      option_name = _wrapper_options[:name]
-      param_name = params[option_name]
-      return if !param_name || param_name.is_a?(ActionController::Parameters)
+    def class_always_permitted_parameters(permitted_params)
+      self._class_always_permitted_parameters = self._class_always_permitted_parameters.dup
+      self._class_always_permitted_parameters.push(*permitted_params).uniq!
+    end
+  end
 
-      respond_to do |format|
-        format.any(:xml, :json) { render request.format.to_sym => "Wrong type for parameter: #{option_name}", status: :unprocessable_entity }
-      end
+  protected
 
-      false
+  def flat_params
+    params.except(:format, :controller, :action, :provider_key, :api_key, :access_token)
+  end
+
+  def wrapped_keys
+    [_wrapper_key, *_wrapper_options[:include]].map(&:to_s)
+  end
+
+  def class_always_permitted_keys
+    _class_always_permitted_parameters.map(&:to_s)
+  end
+
+  def unpermitted_keys
+    flat_params.keys - wrapped_keys - class_always_permitted_keys
+  end
+
+  # This basically checks if the wrapped parameter is passed as a Hash, if not it will go 422
+  #
+  # when ```wrap_parameter Foo``` it will produce this results:
+  #
+  #     /api/foo.xml?foo[bar]=1&foo[baz]=2 => OK
+  #     /api/foo.xml?bar=1&baz=2           => OK
+  #     /api/foo.xml?foo[bar]=1&foo=2      => 422
+  #     /api/foo.xml?foo=5                 => 422
+  #
+  def wrap_parameters_check
+    option_name = _wrapper_options[:name]
+    param_name = params[option_name]
+    return if !param_name || param_name.is_a?(ActionController::Parameters)
+
+    respond_to do |format|
+      format.any(:xml, :json) { render request.format.to_sym => "Wrong type for parameter: #{option_name}", status: :unprocessable_entity }
+    end
+
+    false
+  end
+
+  def unpermitted_parameters_check
+    return unless _class_action_on_unpermitted_parameters
+    return if unpermitted_keys.blank?
+
+    case self.class._class_action_on_unpermitted_parameters
+    when :log
+      Rails.logger.warn("Unpermitted parameters: #{unpermitted_keys}")
+    when :raise
+      raise ActionController::UnpermittedParameters, unpermitted_keys
     end
   end
 end
---
+++ test/integration/api_support/params_test.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ApiSupport::ParamsTest < ActionDispatch::IntegrationTest
+
+  class TestController < ApplicationController
+    include ApiSupport::Params
+    def create
+      params.permit(:permitted)
+    end
+  end
+
+  def with_test_routes
+    Rails.application.routes.draw do
+      post '/test/default' => 'api_support/params_test/class_action_on_unpermitted/default/default#create'
+      post '/test/log' => 'api_support/params_test/class_action_on_unpermitted/log/log#create'
+      post '/test/raise' => 'api_support/params_test/class_action_on_unpermitted/raise/raise#create'
+      post '/test/permitted_param' => 'api_support/params_test/class_action_on_unpermitted/permitted_param/permitted_param#create'
+    end
+    yield
+  ensure
+    Rails.application.routes_reloader.reload!
+  end
+
+  def capture_log
+    orig_logger = Rails.logger.dup
+    logger_output = StringIO.new
+
+    begin
+      Rails.logger = ActiveSupport::Logger.new(logger_output)
+      yield
+    ensure
+      Rails.logger = orig_logger
+    end
+
+    logger_output.string
+  end
+
+  class ClassActionOnUnpermitted < ApiSupport::ParamsTest
+    class Default <ClassActionOnUnpermitted
+      class DefaultController < TestController; end
+
+      def setup
+        ActionController::Parameters.action_on_unpermitted_parameters = :raise
+      end
+
+      def teardown
+        ActionController::Parameters.action_on_unpermitted_parameters = false
+      end
+
+      test 'defaults to global action_on_unpermitted_parameters' do
+        with_test_routes do
+          assert_raises(ActionController::UnpermittedParameters) { post '/test/default', params: { unpermitted: true } }
+        end
+      end
+    end
+
+    class Log <ClassActionOnUnpermitted
+      class LogController < TestController
+        class_action_on_unpermitted_parameters :log
+      end
+
+      test 'writes on the log when class_action_on_unpermitted_parameters = log' do
+        with_test_routes do
+          log_output = capture_log do
+            post '/test/log', params: { unpermitted: true }
+          end
+
+          assert log_output.include?('Unpermitted parameters: ["unpermitted"]')
+        end
+      end
+    end
+
+    class Raise <ClassActionOnUnpermitted
+      class RaiseController < TestController
+        class_action_on_unpermitted_parameters :raise
+      end
+
+      test 'raises an ActionController::UnpermittedParameters when class_action_on_unpermitted_parameters = :raise' do
+        with_test_routes do
+          assert_raises(ActionController::UnpermittedParameters) { post '/test/raise', params: { unpermitted: true } }
+        end
+      end
+    end
+  end
+
+  class PermittedParam < ApiSupport::ParamsTest
+    class PermittedParamController < TestController
+      class_action_on_unpermitted_parameters :raise
+      class_always_permitted_parameters :extra_permitted
+    end
+
+    test "doesn't raise an error for an always permitted parameter" do
+      with_test_routes do
+        post '/test/permitted_param', params: { extra_permitted: true }
+
+        assert true # No error was raised
+      end
+    end
+  end
+end
