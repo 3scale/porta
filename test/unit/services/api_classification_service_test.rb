3f46ae59301eec6fa87f54c905923ce6dc93c546
Daria Mayorova <mayorova@users.noreply.github.com>
2023-03-27 13:49:13 +0200

    More fixes

--- test/unit/services/api_classification_service_test.rb
+++ test/unit/services/api_classification_service_test.rb
@@ -7,11 +7,9 @@ class ApiClassificationServiceTest < ActiveSupport::TestCase
   end
 
   def test_real_api?
-    refute @service.test('https://hello-world-api.3scale.net').real_api?
+    assert_not @service.test('https://echo-api.3scale.net').real_api?
 
-    refute @service.test('http://hello-world-api.3scale.net').real_api?
-
-    refute @service.test('https://echo-api.3scale.net').real_api?
+    assert_not @service.test('http://echo-api.3scale.net').real_api?
 
     assert @service.test('https://api.github.com').real_api?
   end
