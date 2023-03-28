e17a5bcb0124f6c4dfdbcaacbc7288d72ac6c55e
Thales Miguel <thales.miguel@gmail.com>
2023-03-28 11:19:33 +0200

    Run rails app:update

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,9 +1,9 @@
-# frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[activation_code cms_token credit_card credit_card_auth_code
-                                                 credit_card_authorize_net_payment_profile_token credit_card_expires_on
-                                                 credit_card_partial_number crypted_password janrain_api_key lost_password_token
-                                                 password password_digest payment_gateway_options payment_service_reference salt
-                                                 site_access_code sso_key user_key access_token service_token provider_key app_key]
+Rails.application.config.filter_parameters += %i[
+  _key access_token activation_code app_key certificate cms_token credit_card credit_card_auth_code
+  credit_card_authorize_net_payment_profile_token credit_card_expires_on credit_card_partial_number crypt
+  crypted_password janrain_api_key lost_password_token otp passw password password_digest payment_gateway_options
+  payment_service_reference provider_key salt secret service_token site_access_code ssn sso_key token user_key
+]
