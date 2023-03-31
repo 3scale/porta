61a5917000d27afc7cce7970ce84a6852b0c2ab7
Thales Miguel <thales.miguel@gmail.com>
2023-03-31 12:05:59 +0200

    Set i18n.raise_on_missing_translations to remove warning

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
-  config.action_view.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   config.active_support.test_order = :sorted # who has the balls can set it to :random
 
