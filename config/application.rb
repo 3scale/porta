623eeaf5af2092c2080c3a08fead720a845101ae
Thales Miguel <thales.miguel@gmail.com>
2023-04-04 08:59:00 +0200

    Remove patch files

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,6 @@ module System
     config.load_defaults 6.1
     config.active_record.belongs_to_required_by_default = false
     config.active_record.include_root_in_json = true
-    # Make `form_with` generate non-remote forms. Defaults true in Rails 5.1 to 6.0
 
     # Applying the patch for CVE-2022-32224 broke YAML deserialization because some classes are disallowed in the serialized YAML
     # NOTE: Symbol was later added to enabled classes by default, see https://github.com/rails/rails/pull/45584,
@@ -44,6 +43,7 @@ module System
                                                           ActiveSupport::TimeZone,
                                                           ActiveSupport::HashWithIndifferentAccess]
 
+    # Make `form_with` generate non-remote forms.
     config.action_view.form_with_generates_remote_forms = false
 
     # Make Ruby preserve the timezone of the receiver when calling `to_time`.
