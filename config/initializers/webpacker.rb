5a00e58665c4ae961998044f9dbda7d4116c3915
Aleksandar N. Kostadinov <akostadi@redhat.com>
2023-04-10 20:12:15 +0300

    😢 assets compilation now requires 3GB

--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -2,5 +2,5 @@
 # This initializer affects rails server in dev mode as well webpack-dev-server
 # Interesting enough rake assets:precompile doesn't appear to need it
 unless "#{ENV['NODE_OPTIONS']}".include? "--max-old-space-size"
-  Webpacker::Compiler.env['NODE_OPTIONS'] = "#{ENV['NODE_OPTIONS']} --max-old-space-size=2560"
+  Webpacker::Compiler.env['NODE_OPTIONS'] = "#{ENV['NODE_OPTIONS']} --max-old-space-size=3072"
 end
