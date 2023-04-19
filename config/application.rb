11bd990118372e48e63fc697b070587e378b6897
Joan Lledó <jlledo@redhat.com>
2023-04-19 13:43:24 +0200

    Disable Rails unused compoents
    The disabled components were added in previous rails versions and we never used them.
    
    Disabling them simplifies things, I think. It saves memory also.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,31 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require 'rails/all'
+# We don't want to load any Rails component we don't use
+# See https://github.com/rails/rails/blob/v6.1.7.3/railties/lib/rails/all.rb for the list
+# of what is being included here
+require "rails"
+
+# Omitted Rails components
+#   active_storage/engine
+#   action_cable/engine
+#   action_mailbox/engine
+#   action_text/engine
+
+%w[
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  rails/test_unit/railtie
+  sprockets/railtie
+].each do |railtie|
+  require railtie
+rescue LoadError
+end
+
 
 ActiveSupport.on_load(:active_record) do
   # Some rails tasks like db:create may load database before environment
