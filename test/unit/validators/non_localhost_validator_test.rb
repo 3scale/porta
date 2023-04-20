3aed7d193fecc3f9ca3b47f887e3cfae5689bbe9
Thales Miguel <thales.miguel@gmail.com>
2023-04-20 10:10:31 +0200

    Adapt NonLocalhostValidatorTest to the new ActiveModel::Errors structure

--- test/unit/validators/non_localhost_validator_test.rb
+++ test/unit/validators/non_localhost_validator_test.rb
@@ -15,7 +15,7 @@ class NonLocalhostValidatorTest < ActiveSupport::TestCase
     assert record.errors.empty?
 
     errors = validator.validate_each(record, :api_backend, 'http://localhost/')
-    assert_equal ["can't be localhost"], errors
+    assert_equal "can't be localhost", errors.message
     assert record.errors.present?
 
     record = FactoryBot.build_stubbed(:proxy)
@@ -24,7 +24,7 @@ class NonLocalhostValidatorTest < ActiveSupport::TestCase
     assert record.errors.empty?
 
     errors = validator.validate_each(record, :api_backend, ' http://34.210.51.155:8181')
-    assert_equal ["Invalid URL format"], errors
+    assert_equal "Invalid URL format", errors.message
     assert record.errors.present?
 
     errors = validator.validate_each(record, :api_backend, 'hrdt://smth')
@@ -40,7 +40,7 @@ class NonLocalhostValidatorTest < ActiveSupport::TestCase
     assert record.errors.present?
 
     errors = validator.validate_each(record, :api_backend, 'https://<yours>')
-    assert_includes errors, "Invalid URL format"
+    assert_equal 'Invalid URL format', errors.message
     assert record.errors.present?
   end
 end
