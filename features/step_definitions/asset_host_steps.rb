1b2fc700632466d416399233f0bbee2d080f3620
Joan Lledó <jlledo@redhat.com>
2023-04-24 12:36:59 +0200

    Use %r{} for regular expressions

--- features/step_definitions/asset_host_steps.rb
+++ features/step_definitions/asset_host_steps.rb
@@ -11,8 +11,8 @@ end
 
 Then /^((javascript|font)\s)?assets should be loaded from the asset host$/ do |asset_type|
   cdn_url = Rails.configuration.three_scale.asset_host
-  js_regexp = Regexp.new("https?://#{cdn_url}/packs.*?\\.js")
-  font_regexp = Regexp.new("https?://#{cdn_url}/packs.*?\\.eot")
+  js_regexp = %r{https?://#{cdn_url}/packs.*?\.js}
+  font_regexp =  %r{https?://#{cdn_url}/packs.*?\.eot}
 
   assert cdn_url.present?
   assert_not_nil Capybara.page.source.match js_regexp if ['javascript', nil].include? asset_type
@@ -21,8 +21,8 @@ end
 
 Then /^((javascript|font)\s)?assets shouldn't be loaded from the asset host$/ do |asset_type|
   # When no CDN is set, we expect to find relative paths for fonts and js
-  js_regexp = Regexp.new('src="/packs.*?\.js"')
-  font_regexp = Regexp.new('url\(/packs.*?\.eot\)')
+  js_regexp =  %r{src="/packs.*?\.js"}
+  font_regexp =  %r{url\(/packs.*?\.eot\)}
 
   assert_not_nil Capybara.page.source.match js_regexp if ['javascript', nil].include? asset_type
   assert_not_nil Capybara.page.source.match font_regexp if ['font', nil].include? asset_type
@@ -30,7 +30,7 @@ end
 
 Then /^provided assets should be loaded from the asset host$/ do
   cdn_url = Rails.configuration.three_scale.asset_host
-  js_regexp = Regexp.new("https?://#{cdn_url}/dev-portal-assets/.*?\\.js")
+  js_regexp =  %r{https?://#{cdn_url}/dev-portal-assets/.*?\.js}
 
   assert cdn_url.present?
   assert_not_nil Capybara.page.source.match js_regexp
@@ -38,7 +38,7 @@ end
 
 Then /^provided assets shouldn't be loaded from the asset host$/ do
   # When no CDN is set, we expect to find relative paths to reusable assets
-  js_regexp = Regexp.new('src="/dev-portal-assets/.*?\.js"')
+  js_regexp =  %r{src="/dev-portal-assets/.*?\.js"}
 
   assert_not_nil Capybara.page.source.match js_regexp
 end
