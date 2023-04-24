10c6a8b7d22ba9da82158335ecb218f48437bcc1
lvillen <l.villenjaldon@gmail.com>
2023-04-24 13:04:09 +0200

    fix css imports and inconsistant codemirror

--- app/javascript/packs/codemirror.ts
+++ app/javascript/packs/codemirror.ts
@@ -1,8 +1,4 @@
 import CodeMirror from 'codemirror'
 import 'codemirror/mode/htmlmixed/htmlmixed'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/neat.css'
-
-import './codemirror-cms.scss'
 
 window.CodeMirror = CodeMirror
--- app/javascript/src/ActiveDocs/useCodeMirror.ts
+++ app/javascript/src/ActiveDocs/useCodeMirror.ts
@@ -1,18 +1,13 @@
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/neat.css'
-import 'codemirror/mode/javascript/javascript'
-import CodeMirror from 'codemirror'
 import { useEffect } from 'react'
+import 'codemirror/mode/javascript/javascript'
 
 import type { Editor } from 'codemirror'
-
-import '../../packs/codemirror-cms.scss'
 
 const useCodeMirror = (textAreaId: string, initialValue: string, onChange: (value: string) => void): void => {
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- We're sure this is safe
     const textarea = document.getElementById(textAreaId)! as HTMLTextAreaElement
-    const editor = CodeMirror.fromTextArea(textarea, {
+    const editor = window.CodeMirror.fromTextArea(textarea, {
       // @ts-expect-error TS is complaining, TODO: check @types/codemirror version so it matches with codemirror version
       matchBrackets: true,
       autoCloseBrackets: true,
