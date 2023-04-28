511974f543fde56206647d7c05f503634487d54b
josemigallas <josemigallas@gmail.com>
2023-04-28 13:04:17 +0200

    sets NODE_ENV to test during tests

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'test'
 
 const TerserPlugin = require('terser-webpack-plugin')
 const environment = require('./environment')
