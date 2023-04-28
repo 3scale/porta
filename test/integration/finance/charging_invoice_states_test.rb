7dfae07b7e6cc0f9d5a015941d263f644c500ec9
Daria Mayorova <mayorova@users.noreply.github.com>
2023-04-28 14:41:27 +0200

    More fixes

--- test/integration/finance/charging_invoice_states_test.rb
+++ test/integration/finance/charging_invoice_states_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class Finance::ChargingInvoiceStatesTest < ActionDispatch::IntegrationTest
   class UnpaidTest < Finance::ChargingInvoiceStatesTest
     setup do
-      @invoice = FactoryBot.create(:invoice, period: Month.new(Time.zone.local(1984, 1, 1)))
+      @invoice = FactoryBot.create(:invoice)
 
       @invoice.stubs(:cost).returns(100.to_has_money('EUR'))
       @invoice.issue_and_pay_if_free!
@@ -29,8 +29,7 @@ class Finance::ChargingInvoiceStatesTest < ActionDispatch::IntegrationTest
       @master = master_account
       @provider = FactoryBot.create(:provider_account)
       @invoice = FactoryBot.create(:invoice, provider_account: @master,
-                                             buyer_account: @provider,
-                                             period: Month.new(Time.zone.local(1984, 1, 1)))
+                                             buyer_account: @provider)
 
       @invoice.stubs(:cost).returns(100.to_has_money('EUR'))
       @invoice.issue_and_pay_if_free!
@@ -43,7 +42,7 @@ class Finance::ChargingInvoiceStatesTest < ActionDispatch::IntegrationTest
       @invoice.buyer_account.bought_plan.update!(name: 'Paid', cost_per_month: 100)
 
       ThreeScale::Analytics.expects(:track).with(@provider.first_admin!, 'Charged Invoice',
-                                                 {plan: 'Paid', period: 'January 01, 1984 - January 31, 1984', revenue: 100.0})
+                                                 {plan: 'Paid', period: @invoice.period.to_s, revenue: 100.0})
 
       @invoice.charge!
       assert @invoice.paid?, 'Should be paid after successful charge'
