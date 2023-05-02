7ad51dfd643308ecac8e636416a841b1dc117c76
Thales Miguel <thales.miguel@gmail.com>
2023-05-02 10:52:42 +0200

    Run rails app:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
     request.headers['host']
   }
 
+  # Match custom domains on development
+  config.hosts << /[a-z0-9-]+\.3scale\.localhost/
+
   config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
 
   config.public_file_server.enabled = true
