7150e286e91764a21c0d37a1ef122bb3f4a6aa36
josemigallas <josemigallas@gmail.com>
2023-05-19 08:54:41 +0200

    🦋 apply proper PF4 styles and structure to nav

--- app/helpers/vertical_nav_helper.rb
+++ app/helpers/vertical_nav_helper.rb
@@ -145,7 +145,7 @@ module VerticalNavHelper
       items << {id: :ActiveDocs,         title: 'ActiveDocs',         path: admin_api_docs_services_path}          if can?(:manage, :plans)
     end
 
-    items << {title: ' '} # Blank space
+    items << {title: ''} # Separator
     items << {title: 'Visit Portal', path: access_code_url(host: current_account.external_domain, cms_token: current_account.settings.cms_token!, access_code: current_account.site_access_code).html_safe, target: '_blank'}
 
     if can?(:manage, :portal)
