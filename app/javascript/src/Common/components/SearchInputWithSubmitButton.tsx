3eb36965f0f158bbfc0411a691223f3eb47ff9ce
lvillen <l.villenjaldon@gmail.com>
2023-06-07 13:03:49 +0200

    🦋 Back to SearchInput PF4

--- app/javascript/src/Common/components/SearchInputWithSubmitButton.tsx
+++ app/javascript/src/Common/components/SearchInputWithSubmitButton.tsx
@@ -1,13 +1,8 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
-  Button,
-  InputGroup,
-  TextInputGroup,
-  TextInputGroupMain
+  Popover,
+  SearchInput
 } from '@patternfly/react-core'
-import ArrowRightIcon from '@patternfly/react-icons/dist/js/icons/arrow-right-icon'
-import SearchIcon from '@patternfly/react-icons/dist/js/icons/search-icon'
-import TimesIcon from '@patternfly/react-icons/dist/js/icons/times-icon'
 
 import { createReactWrapper } from 'utilities/createReactWrapper'
 
@@ -17,6 +12,7 @@ interface Props {
 }
 
 const INPUT_NAME_QUERY = 'search[query]'
+const INPUT_NAME_UTF8 = 'utf8'
 
 const SearchInputWithSubmitButton: React.FunctionComponent<Props> = ({
   placeholder,
@@ -25,70 +21,56 @@ const SearchInputWithSubmitButton: React.FunctionComponent<Props> = ({
   const query = new URL(window.location.href).searchParams.get(name)
   const url = new URL(window.location.href)
   const [searchText, setSearchText] = useState<string>(query ?? '')
-  const [isDisabled, setIsDisabled] = useState<boolean>(true)
-  const [isSearchSubmitted, setIsSearchSubmitted] = useState<boolean>(false)
-
-  const inputRef = useRef<HTMLInputElement>()
-
-  const handleChange = (_event: React.FormEvent<HTMLInputElement>, value: string) => {
-    setSearchText(value)
-    setIsDisabled(value.length < 3)
-  }
-
-  const showClearButton = !!searchText
-
-  const showUtilities = showClearButton
-
-  const onClearSearch = ()  => {
-    setSearchText('')
-    if (isSearchSubmitted) {
-      url.searchParams.delete(name)
-      window.location.replace(url.toString())
-      window.sessionStorage.setItem('search-submitted', 'false')
-    }
-    setIsSearchSubmitted(false)
-  }
-
-  const onSubmitSearch = (value: string) => {
-    url.searchParams.set(name, value)
-    window.location.replace(url.toString())
-    window.sessionStorage.setItem('search-submitted', 'true')
-  }
+  const [showPopover, setShowPopover] = useState<boolean>(false)
 
   useEffect(() => {
-    if (window.sessionStorage.getItem('search-submitted') === 'true') {
-      setIsSearchSubmitted(true)
+    if (showPopover) {
+      setShowPopover(false)
     }
-  }, [])
+  }, [searchText])
+
+  const onSubmitSearch = (value: string) => {
+    url.searchParams.set(INPUT_NAME_UTF8, '✓')
+    url.searchParams.set(name, value)
+    window.location.replace(url.toString())
+  }
+
+  const onSearch = () => {
+    const searchTextTooShortForSphinx = searchText.length < 3
+
+    if (searchTextTooShortForSphinx) {
+      setShowPopover(true)
+      return
+    }
+
+    onSubmitSearch(searchText)
+  }
+
+  const onClear = () => {
+    const inputClearedBeforeAnySearch = !query
+
+    if (inputClearedBeforeAnySearch) {
+      setSearchText('')
+    } else {
+      onSubmitSearch('')
+    }
+  }
 
   return (
-    <InputGroup>
-      <TextInputGroup>
-        <TextInputGroupMain
-          icon={<SearchIcon />}
-          placeholder={placeholder}
-          ref={inputRef as unknown as React.Ref<HTMLInputElement> | undefined}
-          value={searchText}
-          onChange={handleChange}
-        />
-        {showUtilities && (
-          <Button
-            aria-label="Clear button and input"
-            variant="plain"
-            onClick={onClearSearch}
-          >
-            <TimesIcon />
-          </Button>
-        )}
-      </TextInputGroup>
-      <Button
-        isDisabled={isDisabled}
-        variant="control"
-        onClick={() => { onSubmitSearch(searchText) }}
-      >
-        <ArrowRightIcon />
-      </Button>
-    </InputGroup>
+    <Popover
+      aria-label="search minimum length"
+      bodyContent={<div>To search, type at least 3 characters</div>}
+      isVisible={showPopover}
+      shouldClose={() => { setShowPopover(false) }}
+    >
+      <SearchInput
+        placeholder={placeholder}
+        value={searchText}
+        onChange={(_event, value) => { setSearchText(value) }}
+        onClear={onClear}
+        onSearch={onSearch}
+      />
+    </Popover>
   )
 }
 
