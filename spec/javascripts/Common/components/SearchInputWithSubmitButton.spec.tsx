61016f9d3b55188dc0d34428d5c37d59d51c3bfd
lvillen <l.villenjaldon@gmail.com>
2023-06-13 09:54:02 +0200

    🧪 Increment spec

--- spec/javascripts/Common/components/SearchInputWithSubmitButton.spec.tsx
+++ spec/javascripts/Common/components/SearchInputWithSubmitButton.spec.tsx
@@ -1,6 +1,9 @@
 import { mount } from 'enzyme'
+import { act } from 'react-dom/test-utils'
+import { Popover, SearchInput } from '@patternfly/react-core'
 
 import { SearchInputWithSubmitButton } from 'Common/components/SearchInputWithSubmitButton'
+import { mockLocation } from 'utilities/test-utils'
 
 import type { Props } from 'Common/components/SearchInputWithSubmitButton'
 
@@ -15,8 +18,79 @@ it('should render itself', () => {
   expect(wrapper.exists()).toEqual(true)
 })
 
-it('should have a placeholder', () => {
-  const placeholder = 'Find something'
-  const wrapper = mountWrapper({ placeholder })
-  expect(wrapper.exists(`input[placeholder="${placeholder}"]`)).toEqual(true)
+describe('Popover is or is not visible', () => {
+  const event = {} as React.FormEvent<HTMLInputElement>
+  const wrapper = mountWrapper()
+
+  it('should display the Popover if input has less than 3 characters', () => {
+    const value = 'ab'
+
+    act(() => { wrapper.find(SearchInput).props().onChange!(event, value) })
+    wrapper.update()
+
+    const button = wrapper.find('button[type="submit"]')
+    button.simulate('click')
+
+    expect(wrapper.find(Popover).props().isVisible).toBe(true)
+  })
+
+  it('should not display the Popover if input has more than 3 characters', () => {
+    const value = 'abc'
+
+    act(() => { wrapper.find(SearchInput).props().onChange!(event, value) })
+    wrapper.update()
+
+    const button = wrapper.find('button[type="submit"]')
+    button.simulate('click')
+
+    expect(wrapper.find(Popover).props().isVisible).toBe(false)
+  })
+})
+
+describe('Replace when search has or has not been submitted', () => {
+  const event = {} as React.FormEvent<HTMLInputElement>
+  const wrapper = mountWrapper()
+  mockLocation('http://example.com')
+
+  it('should not replace when clearing the input if search is not submitted', () => {
+    const value = 'abc'
+
+    act(() => { wrapper.find(SearchInput).props().onChange!(event, value) })
+    wrapper.update()
+
+    const clearButton = wrapper.find('button[aria-label="Reset"]')
+    clearButton.simulate('click')
+
+    // expect(clearButton.exists()).toBe(false)
+    expect(window.location.replace).not.toHaveBeenCalled()
+  })
+
+  it('should replace if search is submitted', () => {
+    const value = 'abc'
+
+    act(() => { wrapper.find(SearchInput).props().onChange!(event, value) })
+    wrapper.update()
+
+    const button = wrapper.find('button[type="submit"]')
+    button.simulate('click')
+
+    expect(window.location.replace).toHaveBeenCalledWith(`http://example.com/?utf8=%E2%9C%93&search%5Bquery%5D=${value}`)
+  })
+
+  it('should replace with empty search when clearing the input if search is submitted', () => {
+    const value = 'abc'
+
+    act(() => { wrapper.find(SearchInput).props().onChange!(event, value) })
+    wrapper.update()
+
+    const button = wrapper.find('button[type="submit"]')
+    button.simulate('click')
+
+    expect(window.location.replace).toHaveBeenCalledWith(`http://example.com/?utf8=%E2%9C%93&search%5Bquery%5D=${value}`)
+
+    const clearButton = wrapper.find('button[aria-label="Reset"]')
+    clearButton.simulate('click')
+
+    expect(window.location.replace).toHaveBeenCalledWith('http://example.com/?utf8=%E2%9C%93&search%5Bquery%5D=')
+  })
 })
