61204a867fee169379b2fac5548884a25e5fa183
Joan Lledó <jlledo@redhat.com>
2023-06-14 10:09:50 +0200

    Fix autoloader error on CMS::Toolbar

--- lib/developer_portal/lib/liquid/template/fallback_resolver_no_prefix.rb
+++ lib/developer_portal/lib/liquid/template/fallback_resolver_no_prefix.rb
@@ -2,28 +2,20 @@
 
 module Liquid
   class Template
-    class FallbackResolver < ActionView::FileSystemResolver
+    class FallbackResolverNoPrefix < FallbackResolver
+
       def initialize(path = DeveloperPortal::VIEW_PATH)
         super
       end
 
       def _find_all(name, prefix, partial, details, key = nil, locals = [])
-        path = build_path(name, prefix, partial)
+        path = build_path(name, prefix = nil, partial)
 
         # force just liquid format
         details = details.merge(handlers: [:liquid])
 
         query(path, details, details[:formats], locals, cache: !!key)
       end
-
-      def build_path(name, prefix, partial)
-        prefix = prefix ? [prefix] : []
-        prefix = ::File.join(*prefix)
-        ::Rails.logger.debug { "FallbackResolver: path: #{[name, prefix, partial].inspect}" }
-        Path.build(name, prefix, partial)
-      end
-
-      public :_find_all
     end
   end
 end
