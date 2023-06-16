b1998ccd88a374bcd92b0d31c596b0b22696bbb5
lvillen <l.villenjaldon@gmail.com>
2023-06-16 12:03:03 +0200

    Merge master into THREESCALE-9638

--- app/javascript/src/QuickStarts/components/QuickStartContainer.tsx
+++ app/javascript/src/QuickStarts/components/QuickStartContainer.tsx
@@ -1,13 +1,18 @@
-import {
-  QuickStartContainer as PF4QuickStartContainer,
-  QuickStartCatalogPage,
-  useLocalStorage
-} from '@patternfly/quickstarts/dist/quickstarts-full.es'
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// import {
+//   QuickStartContainer as PF4QuickStartContainer,
+//   QuickStartCatalogPage,
+//   useLocalStorage
+// } from '@patternfly/quickstarts/dist/quickstarts-full.es'
+
+import { QuickStartContainer as PF4QuickStartContainer, QuickStartCatalogPage, useLocalStorage } from '@patternfly/quickstarts'
 import { PageSection } from '@patternfly/react-core'
 
 import quickStarts from 'QuickStarts/templates'
 import replaceLinksExtension from 'QuickStarts/utils/replaceLinksExtension'
 import { createReactWrapper } from 'utilities/createReactWrapper'
+
+import type { QuickStart } from '@patternfly/quickstarts'
 
 import './QuickStartContainer.scss'
 
@@ -29,7 +34,7 @@ const QuickStartContainer: React.FunctionComponent<Props> = ({
     // eslint-disable-next-line react/no-multi-comp, react/jsx-no-useless-fragment -- TODO: remove this when bug is fixed
     renderExtension: () => <></>,
     extensions: [replaceLinksExtension(links)]
-  } as const
+  }
 
   return (
     <PF4QuickStartContainer
@@ -39,7 +44,7 @@ const QuickStartContainer: React.FunctionComponent<Props> = ({
       language="en"
       loading={false}
       markdown={markdown}
-      quickStarts={quickStarts}
+      quickStarts={quickStarts as QuickStart[]}
       setActiveQuickStartID={setActiveQuickStartID}
       setAllQuickStartStates={setAllQuickStartStates}
       showCardFooters={false}
