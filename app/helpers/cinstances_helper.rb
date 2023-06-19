fcc496f134ccd025b2b9e113b0f3f2e0136a00c2
josemigallas <josemigallas@gmail.com>
2023-06-19 18:15:43 +0200

    💄 fixes overriden legacy styles and adds missing pf

--- app/helpers/cinstances_helper.rb
+++ app/helpers/cinstances_helper.rb
@@ -38,6 +38,6 @@ Are you sure?}
 
   def delete_application_link(application)
     msg = t('api.applications.edit.delete_confirmation', name: j(application.name))
-    pf_delete_link_for(provider_admin_application_path(application), confirm: msg, title: 'Delete Application')
+    delete_link_for(provider_admin_application_path(application), confirm: msg, title: 'Delete Application')
   end
 end
