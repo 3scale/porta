29c94a95459a92e0c2d3f33f35f515c5f5408e0e
lvillen <l.villenjaldon@gmail.com>
2023-06-19 16:44:22 +0200

    🧪 Increment spec

--- spec/javascripts/Common/components/ToolbarSearch.spec.tsx
+++ spec/javascripts/Common/components/ToolbarSearch.spec.tsx
@@ -17,6 +17,12 @@ it('should render itself', () => {
   expect(wrapper.exists()).toEqual(true)
 })
 
+it('should have a placeholder', () => {
+  const placeholder = 'Find something'
+  const wrapper = mountWrapper({ placeholder })
+  expect(wrapper.exists(`input[placeholder="${placeholder}"]`)).toEqual(true)
+})
+
 describe('before a search has been submitted', () => {
   const wrapper = mountWrapper()
   mockLocation('http://example.com')
