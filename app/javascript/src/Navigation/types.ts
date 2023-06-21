9f5e29b0aa2ddfb5e67afa2606a924ead92eafcb
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2023-06-21 16:40:35 +0200

    🦋 Add third level navigation (#3434)
    🦋 updates vertical nav, adding third level items

--- app/javascript/src/Navigation/types.ts
+++ app/javascript/src/Navigation/types.ts
@@ -1,12 +1,16 @@
 export interface Item {
   id: string;
-  title: string;
+  title?: string;
   path?: string;
   target?: string;
   itemOutOfDateConfig?: boolean;
+  subItems?: SubItem[];
 }
 
-export type Section = Item & {
+export type Section = Omit<Item, 'subitems'> & {
+  title: string;
   items?: Item[];
   outOfDateConfig?: boolean;
 }
+
+export type SubItem = Omit<Item, 'subitems'>
