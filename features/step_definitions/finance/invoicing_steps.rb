ac5d8c782af1b9e838e860435cdfc7545692d8b6
josemigallas <josemigallas@gmail.com>
2023-06-28 17:04:03 +0200

    🦋 applies basic PF4 to invoices table

--- features/step_definitions/finance/invoicing_steps.rb
+++ features/step_definitions/finance/invoicing_steps.rb
@@ -69,7 +69,7 @@ Then /^the buyer should have following line items for "([^"]*)"(?: in the( \d(?:
 end
 
 Then(/^I should see the first invoice belonging to "([^"]*)"$/) do |buyer|
-  assert find(:xpath, '//table/tbody/tr[1]').text.include?(buyer)
+  assert has_css?('table tbody tr.invoice td[data-label="Account"]', text: buyer)
 end
 
 Then(/^I should have (\d+) invoices?$/) do |count|
