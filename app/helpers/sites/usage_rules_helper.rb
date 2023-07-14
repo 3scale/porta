a619f92e3c1b0408f3d6a2ef81fc8890b5b1d909
josemigallas <josemigallas@gmail.com>
2023-07-14 12:36:10 +0200

    🦋 implements site usage_rules using patternfly

---
+++ app/helpers/sites/usage_rules_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Sites::UsageRulesHelper
+  def approval_required_editable_hint(account)
+    t("formtastic.hints.settings.account_approval_required") + sso_hint(account)
+  end
+
+  def approval_required_disabled_hint(account)
+    t("formtastic.hints.settings.approval_required_referer_html", link: link_to("Account Plans", admin_buyers_account_plans_path) ) + sso_hint(account)
+  end
+
+  def sso_hint(account)
+    if account.authentication_providers.any?
+      " #{t('sites.usage_rules.edit.sso_integrations_info_html', link: provider_admin_authentication_providers_path)}"
+    else
+      ''
+    end
+  end
+end
