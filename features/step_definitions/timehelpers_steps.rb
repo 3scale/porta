4355ed96130e9aace2e211856e9acec25e75837d
Joan Lledó <jlledo@redhat.com>
2023-07-24 09:30:40 +0200

    Fix bug in `safe_travel_to`
    If being called when in real time, it must return to real time. It was freezing to time at the moment of being called instead.

--- features/step_definitions/timehelpers_steps.rb
+++ features/step_definitions/timehelpers_steps.rb
@@ -6,11 +6,14 @@ end
 
 # Wrapper for travel_to but implementing safe nested traveling
 def safe_travel_to(time, &block)
+  time_was_frozen = time_frozen?
   previous_time = Time.zone.now
   travel_back
   travel_to(time, &block)
 ensure
-  travel_to(previous_time) if block_given?
+  if block_given?
+    time_was_frozen ? travel_to(previous_time) : travel_back
+  end
 end
 
 def access_user_sessions
