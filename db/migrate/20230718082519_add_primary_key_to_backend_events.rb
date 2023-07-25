e922549ea156386e28407ecb995a8ec329b3eb92
Joan Lledó <jlledo@redhat.com>
2023-07-25 12:22:41 +0200

    Migrations: make them safer.
    It's better to remove the index after creating the primary key, so if the primary key creation fails, the index is not removed.
    
    However, this fails for Oracle, so we keep removing the index first for this DB

--- db/migrate/20230718082519_add_primary_key_to_backend_events.rb
+++ db/migrate/20230718082519_add_primary_key_to_backend_events.rb
@@ -1,25 +1,27 @@
 class AddPrimaryKeyToBackendEvents < ActiveRecord::Migration[5.2]
   def up
-    remove_index :backend_events, :id
-
     if System::Database.mysql?
       execute 'ALTER TABLE backend_events ADD PRIMARY KEY (id)'
+      remove_index :backend_events, :id
     elsif System::Database.postgres?
       execute 'ALTER TABLE backend_events ADD CONSTRAINT backend_events_pk PRIMARY KEY (id)'
+      remove_index :backend_events, :id
     else
+      remove_index :backend_events, :id
       execute 'ALTER TABLE BACKEND_EVENTS MODIFY ID PRIMARY KEY'
     end
   end
 
   def down
     if System::Database.mysql?
+      add_index :backend_events, :id, unique: true
       execute 'ALTER TABLE backend_events DROP PRIMARY KEY'
     elsif System::Database.postgres?
+      add_index :backend_events, :id, unique: true
       execute 'ALTER TABLE backend_events DROP CONSTRAINT backend_events_pk'
     else
       execute 'ALTER TABLE BACKEND_EVENTS DROP PRIMARY KEY'
+      add_index :backend_events, :id, unique: true
     end
-
-    add_index :backend_events, :id, unique: true
   end
 end
