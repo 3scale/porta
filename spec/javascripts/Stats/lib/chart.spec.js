72f80367371bd15941cd61e04b1ae09c29e6d1d1
Daria Mayorova <mayorova@users.noreply.github.com>
2023-07-26 15:00:57 +0200

    Add some basic unit tests

--- spec/javascripts/Stats/lib/chart.spec.js
+++ spec/javascripts/Stats/lib/chart.spec.js
@@ -1,3 +1,14 @@
+import { StatsChart } from 'Stats/lib/chart'
+
+describe('StatsChart', () => {
+  const chart = new StatsChart({ container: '#chart' })
+
+  it('Y-axis is formatted correctly', () => {
+    expect(chart.chartOptions().axis.y.tick.format(12700)).toBe('12.7K')
+    expect(chart.chartOptions().axis.y.tick.format(42)).toBe('42')
+  })
+})
+
 it.todo('renders a c3 chart')
 
 it.todo('has the right options')
