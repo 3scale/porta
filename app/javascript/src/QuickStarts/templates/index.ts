74e60b82ae195245ade2ea98797d2bb871da10fc
Darren Fennessy <dfenness@redhat.com>
2023-07-27 16:20:30 +0100

    THREESCALE-8759: Added all the Quick Starts from separate MRs to this one Merge Request.

--- app/javascript/src/QuickStarts/templates/index.ts
+++ app/javascript/src/QuickStarts/templates/index.ts
@@ -6,6 +6,12 @@ import createApplication from './createApplication.yaml'
 import createMethod from './createMethod.yaml'
 import createMetric from './createMetric.yaml'
 import createMappingRule from './createMappingRule.yaml'
+import learnAboutProducts from './learnAboutProducts.yaml'
+import learnAboutApplicationPlans from './learnAboutApplicationPlans.yaml'
+import learnAboutMethods from './learnAboutMethods.yaml'
+import learnAboutMappingRules from './learnAboutMappingRules.yaml'
+import createAPIproduct from './createAPIproduct.yaml'
+import createAccessTiersForAPIconsumers from './createAccessTiersForAPIconsumers.yaml'
 
 const resources = [
   basicAPIintegration,
@@ -15,7 +21,13 @@ const resources = [
   createApplication,
   createMethod,
   createMetric,
-  createMappingRule
+  createMappingRule,
+  learnAboutProducts,
+  learnAboutApplicationPlans,
+  learnAboutMethods,
+  learnAboutMappingRules,
+  createAPIproduct,
+  createAccessTiersForAPIconsumers
 ]
 
 export default resources
