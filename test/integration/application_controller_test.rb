a15fdf74347ce6eb58ee3dd0e902fd404dfd1bed
Joan Lledó <jlledo@redhat.com>
2023-09-27 10:15:36 +0200

    Add test RF skipped w/o auth

--- test/integration/application_controller_test.rb
+++ test/integration/application_controller_test.rb
@@ -62,6 +62,20 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     assert_not_nil user.user_sessions.reload[0][:revoked_at]
   end
 
+  test "forgery protection is skipped for API requests without authentication" do
+    provider = FactoryBot.create(:provider_account)
+    host! provider.external_admin_domain
+
+    ApplicationController.any_instance.expects(:verify_authenticity_token).never
+
+    with_forgery_protection do
+      post admin_api_signup_path(format: :json), params: {
+        org_name: 'Alaska', username: 'hello', email: 'foo@example.com', password: '123456'
+      }
+    end
+    assert_response :forbidden
+  end
+
   test "forgery protection is skipped for API requests with access token" do
     provider = FactoryBot.create(:provider_account)
     user = provider.admins.first
@@ -78,7 +92,6 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     end
     assert_response :created
   end
-
 
   test "forgery protection is skipped for API requests with basic auth and access token" do
     provider = FactoryBot.create(:provider_account)
