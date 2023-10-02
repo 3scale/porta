4ecaecac3eee2c7b207ab0a671e8d3bb1f0990bb
josemigallas <josemigallas@gmail.com>
2023-10-02 17:26:49 +0200

    💎🥒 part of ruby 2.7 upgrade

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,2 +1,3 @@
 require 'webmock/cucumber'
 WebMock.disable_net_connect!(allow_localhost: true, allow: [/\.example\.com/, /\.3scale\.localhost/, /__identify__/])
+WebMock.allow_net_connect!(net_http_connect_on_start: true)
