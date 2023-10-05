020f89b4b75dc7d0633be54b85334b3a0c2aef5b
josemigallas <josemigallas@gmail.com>
2023-10-05 12:51:26 +0200

    Revert "💎🥒 part of ruby 2.7 upgrade"
    This reverts commit 4ecaecac3eee2c7b207ab0a671e8d3bb1f0990bb.

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,3 +1,2 @@
 require 'webmock/cucumber'
 WebMock.disable_net_connect!(allow_localhost: true, allow: [/\.example\.com/, /\.3scale\.localhost/, /__identify__/])
-WebMock.allow_net_connect!(net_http_connect_on_start: true)
