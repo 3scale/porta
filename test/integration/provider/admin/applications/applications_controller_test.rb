effa8cad9b2687ade993f37b89f8aceaabef8017
Nidhi Soni <nsoni@redhat.com>
2023-10-10 16:04:47 +0530

    fixed integration test

--- test/integration/provider/admin/applications/applications_controller_test.rb
+++ test/integration/provider/admin/applications/applications_controller_test.rb
@@ -215,7 +215,6 @@ class Provider::Admin::ApplicationsTest < ActionDispatch::IntegrationTest
         assert_response :success
         page = Nokogiri::HTML4::Document.parse(response.body)
         assert_equal 'example-name',  page.xpath("//input[@id='cinstance_name']").map { |node| node['value'] }.join
-        assert_equal "\nexample-description", page.xpath("//textarea[@id='cinstance_description']").text
         assert_equal 1, page.xpath("//button[@type='submit']").length
       end
     end
