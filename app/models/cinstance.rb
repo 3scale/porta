5ade255dffe3276ba2abd47d7ed9924b61029eca
Nidhi Soni <84672731+nidhi-soni1104@users.noreply.github.com>
2023-10-25 16:43:38 +0530

    THREESCALE-8746: Make the 'description' field optional for applications (#3567)
    * THREESCALE-8746 remove description as mandatory field
    
    * removed description as mandatory field
    
    * removed required filed from API also
    
    * THREESCALE-8746 revert change
    
    * make it required again in decorator
    
    * remove description from cucumber
    
    * Update app/models/cinstance.rb
    
    Co-authored-by: Daria Mayorova <mayorova@users.noreply.github.com>
    
    * fixed rspec
    
    * fixed unit test
    
    * fixed unit test
    
    * fixed integration test
    
    * updated validation test
    
    * fixed unit and integration test
    
    * Fix cucumbers
    
    * Some changes in tests
    
    * Fix tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
    
    * Keep application 'description' field as optional
    
    * Restore some tests that now don't need to be changed
    
    * Another fix
    
    * Unit test for default fields
    
    ---------
    
    Co-authored-by: Daria Mayorova <mayorova@users.noreply.github.com>
    Co-authored-by: Jose Miguel Gallas Olmedo <josemigallas@gmail.com>

--- app/models/cinstance.rb
+++ app/models/cinstance.rb
@@ -45,7 +45,9 @@ class Cinstance < Contract
 
   # FIXME: including Fields after other includes makes Fields break
   include Fields::Fields
-  required_fields_are :name, :description
+  required_fields_are :name
+  optional_fields_are :description
+  default_fields_are :name, :description
   set_fields_account_source :user_account
 
   include Backend::ModelExtensions::Cinstance
@@ -80,7 +82,6 @@ class Cinstance < Contract
 
   validates :plan, presence: true
   validates :name,        presence: { :if => :name_required? }
-  validates :description, presence: { :if => :description_required? }
 
   after_commit :push_webhook_key_updated, :on => :update, :if => :user_key_updated?
   after_commit :push_application_updated_event, on: :update, unless: :only_traffic_updated?
@@ -438,10 +439,6 @@ class Cinstance < Contract
 
   def name_required?
     @validate_human_edition
-  end
-
-  def description_required?
-    @validate_human_edition && (multiple_applications_allowed? || service_intentions_required?)
   end
 
   def service_intentions_required?
