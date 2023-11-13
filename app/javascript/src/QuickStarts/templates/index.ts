bce1ab8e15c2aa44c476089c4d0535edc1909aaf
Darren Fennessy <dfenness@redhat.com>
2023-11-13 15:01:34 +0000

    THREESCALE-8759: Quick Start content improvement
    THREESCALE-8759: Quick Start content improvement

--- app/javascript/src/QuickStarts/templates/index.ts
+++ app/javascript/src/QuickStarts/templates/index.ts
@@ -6,6 +6,17 @@ import createApplication from './createApplication.yaml'
 import createMethod from './createMethod.yaml'
 import createMetric from './createMetric.yaml'
 import createMappingRule from './createMappingRule.yaml'
+import learnAboutProducts from './learnAboutProducts.yaml'
+import learnAboutApplicationPlans from './learnAboutApplicationPlans.yaml'
+import learnAboutMethods from './learnAboutMethods.yaml'
+import learnAboutMappingRules from './learnAboutMappingRules.yaml'
+import createAPIproduct from './createAPIproduct.yaml'
+import createAccessTiersForAPIconsumers from './createAccessTiersForAPIconsumers.yaml'
+import learnAboutBackends from './learnAboutBackends.yaml'
+import learnAboutAccounts from './learnAboutAccounts.yaml'
+import learnAboutApplications from './learnAboutApplications.yaml'
+import learnAboutMetrics from './learnAboutMetrics.yaml'
+import trackUsageForAPIProduct from './trackUsageForAPIProduct.yaml'
 
 const resources = [
   basicAPIintegration,
@@ -15,7 +26,18 @@ const resources = [
   createApplication,
   createMethod,
   createMetric,
-  createMappingRule
+  createMappingRule,
+  learnAboutProducts,
+  learnAboutApplicationPlans,
+  learnAboutMethods,
+  learnAboutMappingRules,
+  createAPIproduct,
+  createAccessTiersForAPIconsumers,
+  learnAboutBackends,
+  learnAboutAccounts,
+  learnAboutApplications,
+  learnAboutMetrics,
+  trackUsageForAPIProduct
 ]
 
 export default resources
