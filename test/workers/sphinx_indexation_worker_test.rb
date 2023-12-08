cb4fc854f4a35adeee738c460718525d77a88c16
Aleksandar N. Kostadinov <akostadi@redhat.com>
2023-12-08 18:56:17 +0200

    redundant includes

--- test/workers/sphinx_indexation_worker_test.rb
+++ test/workers/sphinx_indexation_worker_test.rb
@@ -3,9 +3,6 @@
 require 'test_helper'
 
 class SphinxIndexationWorkerTest < ActiveSupport::TestCase
-
-  include ActiveJob::TestHelper
-
   test 'it does not raises if id does not exist' do
     enable_search_jobs!
     SphinxIndexationWorker.perform_now(ThinkingSphinx::Test.indexed_models.sample, 42)
