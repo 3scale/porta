db453fb1203bc70be0883e0d3eef509b72225e12
Joan Lledó <jlledo@redhat.com>
2023-12-12 17:10:35 +0100

    Add tests for new bot protection

---
+++ test/unit/lib/three_scale/bot_protection/form_test.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ThreeScale::BotProtection::FormTest < ActiveSupport::TestCase
+
+  class Test
+    include ThreeScale::BotProtection::Form
+  end
+
+  setup do
+    @subject = Test.new.tap do |s|
+      template = mock('template')
+      s.stubs(:template).returns(template)
+    end
+  end
+
+  class BotProtectionInputsTest < ThreeScale::BotProtection::FormTest
+    test 'bot_protection_inputs returns nothing if bot protection is disabled' do
+      @subject.stubs(:bot_protection_enabled?).returns(false)
+
+      result = @subject.send :bot_protection_inputs
+
+      assert_empty result
+    end
+
+    test 'bot_protection_inputs returns reCaptcha if bot protection is enabled' do
+      @subject.stubs(:bot_protection_enabled?).returns(true)
+      @subject.template.stubs(:controller).returns(mock('controller', controller_path: '/test'))
+
+      result = @subject.send(:bot_protection_inputs)
+
+      assert_match /g-recaptcha/, result
+    end
+  end
+end
