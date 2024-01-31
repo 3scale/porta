178d604370561b4435fd13f85a965f70017e7271
Nidhi Soni <nsoni@redhat.com>
2024-01-31 22:52:12 +0530

    removed service id and updated create API

--- app/controllers/admin/api/service_contracts_controller.rb
+++ app/controllers/admin/api/service_contracts_controller.rb
@@ -6,25 +6,36 @@ class Admin::Api::ServiceContractsController < Admin::Api::ServiceBaseController
 
   before_action :deny_on_premises_for_master
   before_action :authorize_service_plans!
-  before_action :find_service, only: %i[create]
+  before_action :find_account
 
   # Service Subscription Create
   # POST /admin/api/accounts/:account_id/service_contracts.xml
   def create
-    service_contract = account.bought_service_contracts.create(service_contract_params)
-    respond_with service_contract
+    plan = service_plan
+
+    unless @account
+      render_error('Buyer not found with this account ID', status: :unprocessable_entity)
+      return
+    end
+
+    unless valid_plan_and_account?(plan)
+      render_error('Invalid service plan or account id', status: :unprocessable_entity)
+      return
+    end
+
+    create_service_contract(plan)
   end
 
   # Service Subscription List
   # GET /admin/api/accounts/{account_id}/service_contracts.xml
   def index
-    respond_with account.bought_service_contracts
+    respond_with @account.bought_service_contracts
   end
 
   # Service Subscription Delete
   # DELETE /admin/api/accounts/{account_id}/service_contracts/{id}.xml
   def destroy
-    service_subscription = ServiceSubscriptionService.new(account)
+    service_subscription = ServiceSubscriptionService.new(@account)
 
     respond_with(service_subscription.unsubscribe(service_contract))
   end
@@ -40,16 +51,12 @@ class Admin::Api::ServiceContractsController < Admin::Api::ServiceBaseController
 
   protected
 
-  def account
-    @account ||= current_account.buyers.find params.require(:account_id)
-  end
-
-  def find_service
-    @service = service
+  def find_account
+    @account ||= current_account.buyers.find_by(id: params.require(:account_id))
   end
 
   def service_contract
-    @service_contract ||= account.bought_service_contracts.find params.require(:id)
+    @service_contract ||= @account.bought_service_contracts.find params.require(:id)
   end
 
   def service_contract_params
@@ -57,15 +64,20 @@ class Admin::Api::ServiceContractsController < Admin::Api::ServiceBaseController
           .fetch(:service_contract).merge(plan: service_plan)
   end
 
-  def service
-    service ||= accessible_services.find(params[:service_id])
+  def service_plan
+    ServicePlan.find_by(id: service_contract_plan_id)
   end
 
-  def service_plan(plan_id = service_contract_plan_id)
-    @service_plan ||= service.service_plans.find_by(id: plan_id)
+  def create_service_contract(plan)
+    service_contract = @account.bought_service_contracts.create(service_contract_params.merge(plan: plan))
+    respond_with service_contract
   end
 
   def service_contract_plan_id
     params[:service_contract][:plan_id]
+  end
+
+  def valid_plan_and_account?(plan)
+    plan && @account.provider_account_id == plan.service.account_id
   end
 end
