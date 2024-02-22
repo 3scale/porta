fcb4519d67f7eab9bdc65ac9a71cca9a8a548fa6
Joan Lledó <jlledo@redhat.com>
2024-02-22 16:48:03 +0100

    Upgrade to Sidekiq7 again

--- features/support/sidekiq.rb
+++ features/support/sidekiq.rb
@@ -4,7 +4,10 @@ require 'sidekiq/testing'
 require 'sidekiq/batch'
 
 # Turn off Sidekiq logging which pollutes the CI logs
-Sidekiq.logger = Sidekiq::Logger.new(nil, level: Logger::FATAL)
+Sidekiq.configure_client do |config|
+  config.logger = Sidekiq::Logger.new(nil, level: Logger::FATAL)
+end
+
 Sidekiq::Testing.inline!
 
 module Sidekiq
