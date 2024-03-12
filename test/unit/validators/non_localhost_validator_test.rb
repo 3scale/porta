98d89981fdc8132985bdd389ff11247eb3c02521
Daria Mayorova <mayorova@users.noreply.github.com>
2024-03-12 15:29:00 +0100

    Fix test for errors

--- test/unit/validators/non_localhost_validator_test.rb
+++ test/unit/validators/non_localhost_validator_test.rb
@@ -14,33 +14,33 @@ class NonLocalhostValidatorTest < ActiveSupport::TestCase
     assert_nil errors
     assert record.errors.empty?
 
-    errors = validator.validate_each(record, :api_backend, 'http://localhost/')
-    assert_equal ["can't be localhost"], errors
-    assert record.errors.present?
+    error = validator.validate_each(record, :api_backend, 'http://localhost/')
+    assert_equal ActiveModel::Error, error.class
+    assert "can't be localhost", record.errors.messages_for(:api_backend)
 
     record = FactoryBot.build_stubbed(:proxy)
-    errors = validator.validate_each(record, :api_backend, 'http://mylocalhost/')
-    assert_nil errors
+    error = validator.validate_each(record, :api_backend, 'http://mylocalhost/')
+    assert_nil error
     assert record.errors.empty?
 
-    errors = validator.validate_each(record, :api_backend, ' http://34.210.51.155:8181')
-    assert_equal ["Invalid URL format"], errors
+    error = validator.validate_each(record, :api_backend, ' http://34.210.51.155:8181')
+    assert_equal ActiveModel::Error, error.class
+    assert "Invalid URL format", record.errors.messages_for(:api_backend)
+
+    error = validator.validate_each(record, :api_backend, 'hrdt://smth')
+    assert_nil error
     assert record.errors.present?
 
-    errors = validator.validate_each(record, :api_backend, 'hrdt://smth')
-    assert_nil errors
+    error = validator.validate_each(record, :api_backend, '')
+    assert_nil error
     assert record.errors.present?
 
-    errors = validator.validate_each(record, :api_backend, '')
-    assert_nil errors
+    error = validator.validate_each(record, :api_backend, nil)
+    assert_nil error
     assert record.errors.present?
 
-    errors = validator.validate_each(record, :api_backend, nil)
-    assert_nil errors
-    assert record.errors.present?
-
-    errors = validator.validate_each(record, :api_backend, 'https://<yours>')
-    assert_includes errors, "Invalid URL format"
+    error = validator.validate_each(record, :api_backend, 'https://<yours>')
+    assert "Invalid URL format", error.message
     assert record.errors.present?
   end
 end
