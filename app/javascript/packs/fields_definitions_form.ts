98f006581cd485c7edc5fe3f5c924146a6edf1d9
josemigallas <josemigallas@gmail.com>
2024-04-26 11:50:08 +0200

    updates field_definitions_form to jquery 3

--- app/javascript/packs/fields_definitions_form.ts
+++ app/javascript/packs/fields_definitions_form.ts
@@ -1,6 +1,4 @@
-import jquery3 from 'jquery'
-
-const jquery1 = window.$
+import $ from 'jquery'
 
 document.addEventListener('DOMContentLoaded', () => {
   function enableCheckboxes () {
@@ -12,28 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function checkboxesDisabled (action: boolean) {
-    jquery3<HTMLInputElement>('#fields_definition_hidden').attr('disabled', action ? 'true' : null)
-    jquery3<HTMLInputElement>('#fields_definition_read_only').attr('disabled', action ? 'true' : null)
-    jquery3<HTMLInputElement>('#fields_definition_required').attr('disabled', action ? 'true' : null)
+    $<HTMLInputElement>('#fields_definition_hidden').attr('disabled', action ? 'true' : null)
+    $<HTMLInputElement>('#fields_definition_read_only').attr('disabled', action ? 'true' : null)
+    $<HTMLInputElement>('#fields_definition_required').attr('disabled', action ? 'true' : null)
   }
 
   function disableNameField () {
-    jquery3<HTMLInputElement>('#fields_definition_name')[0].value = jquery3<HTMLInputElement>('#fields_definition_fieldname')[0].value
-    jquery3<HTMLInputElement>('#fields_definition_name').attr('readonly', 'true')
+    $<HTMLInputElement>('#fields_definition_name')[0].value = $<HTMLInputElement>('#fields_definition_fieldname')[0].value
+    $<HTMLInputElement>('#fields_definition_name').attr('readonly', 'true')
   }
 
   function clearCheckboxes () {
-    jquery3<HTMLInputElement>('#fields_definition_hidden')[0].checked = false
-    jquery3<HTMLInputElement>('#fields_definition_read_only')[0].checked = false
-    jquery3<HTMLInputElement>('#fields_definition_required')[0].checked = false
+    $<HTMLInputElement>('#fields_definition_hidden')[0].checked = false
+    $<HTMLInputElement>('#fields_definition_read_only')[0].checked = false
+    $<HTMLInputElement>('#fields_definition_required')[0].checked = false
   }
 
   function clearChoices () {
-    jquery3<HTMLInputElement>('#fields_definition_choices_for_views')[0].value = ''
+    $<HTMLInputElement>('#fields_definition_choices_for_views')[0].value = ''
   }
 
   function readOnlyChoices (action: boolean) {
-    jquery3('#fields_definition_choices_for_views').attr('readonly', action ? 'true' : null)
+    $('#fields_definition_choices_for_views').attr('readonly', action ? 'true' : null)
   }
 
   function enableChoices () {
@@ -41,25 +39,30 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // new view
-  if (jquery3('#fields_definition_fieldname').length !== 0) {
-    jquery3('#fields_definition_fieldname').change(function () {
-      if (jquery3<HTMLInputElement>('#fields_definition_fieldname')[0].value === '[new field]') {
-        jquery3('#fields_definition_name').attr('readonly', null)
-        jquery3<HTMLInputElement>('#fields_definition_name')[0].value = ''
+  const $fieldname = $<HTMLInputElement>('#fields_definition_fieldname')
+  const requiredFields = $<HTMLInputElement>('#required_fields')[0].value.split(',')
+
+  const fieldNameValue = $fieldname[0].value
+
+  if ($fieldname.length !== 0) {
+    $fieldname.on('change', () => {
+      if (fieldNameValue === '[new field]') {
+        const $name = $<HTMLInputElement>('#fields_definition_name')
+        $name.attr('readonly', null)
+        $name[0].value = ''
         clearCheckboxes()
         enableCheckboxes()
         clearChoices()
         enableChoices()
-      } else if (jquery1.inArray(
-        jquery3<HTMLInputElement>('#fields_definition_fieldname')[0].value,
-        jquery3<HTMLInputElement>('#required_fields')[0].value.split(',')
-      ) !== -1) {
+
+      } else if (requiredFields.includes(fieldNameValue)) {
         //non_modifiable fields
         disableNameField()
         clearCheckboxes()
         disableCheckboxes()
         clearChoices()
         enableChoices()
+
       } else {
         disableNameField()
         clearCheckboxes()
@@ -70,28 +73,37 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  if (jquery3('#fields_definition_required').length !== 0 && jquery3<HTMLInputElement>('#fields_definition_required')[0].checked) {
-    jquery3<HTMLInputElement>('#fields_definition_hidden')[0].checked = false
-    jquery3<HTMLInputElement>('#fields_definition_read_only')[0].checked = false
-    jquery3('#fields_definition_hidden').attr('disabled', 'true')
-    jquery3('#fields_definition_read_only').attr('disabled', 'true')
+  const $required = $<HTMLInputElement>('#fields_definition_required')
+
+  if ($required.length > 0 && $required[0].checked) {
+    const $hidden = $<HTMLInputElement>('#fields_definition_hidden')
+    $hidden[0].checked = false
+    $hidden.attr('disabled', 'true')
+
+    const $readonly = $<HTMLInputElement>('#fields_definition_read_only')
+    $readonly[0].checked = false
+    $readonly.attr('disabled', 'true')
   }
 
   //edit view
-  if (jquery3('#fields-definitions-edit-view').length !== 0) {
-    jquery3('#fields_definition_name').attr('readonly', 'true')
-    jquery3('#fields_definition_name').attr('disabled', 'true')
+  if (document.getElementById('fields-definitions-edit-view') !== null) {
+    const $name = $('#fields_definition_name')
+    $name.attr('readonly', 'true')
+    $name.attr('disabled', 'true')
   }
 
-  jquery3('#fields_definition_required').on('change', () => {
-    if (jquery3<HTMLInputElement>('#fields_definition_required')[0].checked) {
-      jquery3<HTMLInputElement>('#fields_definition_hidden')[0].checked = false
-      jquery3<HTMLInputElement>('#fields_definition_read_only')[0].checked = false
-      jquery3('#fields_definition_hidden').attr('disabled', 'true')
-      jquery3('#fields_definition_read_only').attr('disabled', 'true')
+  $required.on('change', () => {
+    const $hidden = $<HTMLInputElement>('#fields_definition_hidden')
+    const $readonly = $<HTMLInputElement>('#fields_definition_read_only')
+
+    if ($required[0].checked) {
+      $hidden[0].checked = false
+      $readonly[0].checked = false
+      $hidden.attr('disabled', 'true')
+      $readonly.attr('disabled', 'true')
     } else {
-      jquery3('#fields_definition_hidden').attr('disabled', null)
-      jquery3('#fields_definition_read_only').attr('disabled', null)
+      $hidden.attr('disabled', null)
+      $readonly.attr('disabled', null)
     }
   })
 })
