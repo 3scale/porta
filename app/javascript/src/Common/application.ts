074f96bf63e7ae86671279f7a0eab92b886dd1bb
josemigallas <josemigallas@gmail.com>
2024-04-26 10:19:34 +0200

    update jquery and types in applications.ts

--- app/javascript/src/Common/application.ts
+++ app/javascript/src/Common/application.ts
@@ -1,30 +1,30 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 import type { JQueryXHR } from 'Types/jquery/v1'
 
-const $ = window.$
+const jQuery1 = window.$
 
-export default function (): void { /* eslint-disable */
+export default function (): void {
   // disable links with 'data-disabled' attribute and display alert instead
   // delegation on body fires before rails.js. FIXME: this is not a valid guard. If "data-disabled"
   // removed manually from the DOM, the request will still go through. The link should be disabled
   // (not clickable) and the server should return a flash error.
-  $('body').delegate('a[data-disabled]', 'click', (event) => {
-    alert((event.currentTarget as HTMLAnchorElement).dataset.disabled)
+  $('body').on('click', 'a[data-disabled]', ({ currentTarget }) => {
+    alert((currentTarget as HTMLAnchorElement).dataset.disabled)
     return false
   })
 
-  // DEPRECATED: since the introduction of PF4 and React, colorbox is being removed.
-  // Response of this form will be presented inside a colorbox.
-  $(document).on('submit', 'form.colorbox[data-remote]', function (e) {
-    $(this).on('ajax:complete', function (event, xhr, status) {
-      const form = $(this).closest('form')
-      const width = form.data('width')
-      $.colorbox({
+  /**
+   * This seems to be used only in app/views/master/providers/plans/_widget.html.slim:23
+   * DEPRECATED: replace jquery/colorbox modals with Patternfly modals.
+   * TODO: add a more specific selector to separate this form from all other "remote" ones.
+   */
+  $(document).on('submit', 'form.colorbox[data-remote]', ({ currentTarget }) => {
+    // MUST USE RAILS-JQUERY since ajax:complete is triggered by rails (do not use $ from node_modules).
+    const $form = jQuery1(currentTarget as HTMLFormElement)
+    $form.on('ajax:complete', (_event, xhr: JQueryXHR) => {
+      jQuery1.colorbox({
         open: true,
         html: xhr.responseText,
-        width: width,
+        width: $form.data('width') as string | undefined,
         maxWidth: '85%',
         maxHeight: '90%'
       })
@@ -34,27 +34,28 @@ export default function (): void { /* eslint-disable */
   // TODO: replace .fancybox with .colorbox
   // This link will load its content into a colorbox modal
   $(document).on('click', 'a.fancybox, a.colorbox', ({ currentTarget }) => {
-    $(currentTarget).colorbox({ open: true })
+    jQuery1(currentTarget as HTMLAnchorElement).colorbox({ open: true })
     return false
   })
 
   // TODO: replace .fancybox with .colorbox
   // This is used in some modals with a "Cancel" button.
   $(document).on('click', '.fancybox-close', () => {
-    $.colorbox.close()
+    jQuery1.colorbox.close()
     return false
   })
 
   /**
    * Handle errors in formtastic forms rendered inside a colorbox. The error template is passed a
-   * responseText inside the XHR response.
+   * responseText inside the XHR response. MUST USE RAILS-JQUERY since ajax:error is triggered by
+   * rails (do not use $ from node_modules).
    *
    * DEPRECATED: replace jquery/colorbox modals with Patternfly modals.
    * TODO: add a colorbox specific selector to separate this with non-legacy implementations.
    */
-  $(document).on('ajax:error', 'form:not(.pf-c-form)', (_event, xhr: JQueryXHR, status) => {
+  jQuery1(document).on('ajax:error', 'form:not(.pf-c-form)', (_event, xhr: JQueryXHR, status) => {
     if (status === 'error') {
-      $.colorbox({ html: xhr.responseText })
+      jQuery1.colorbox({ html: xhr.responseText })
       return false
     }
   })
