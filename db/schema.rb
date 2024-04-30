eea43d58f4e1636cb14561cfa4940de06d07c817
Joan Lledó <jlledo@redhat.com>
2024-04-30 16:44:16 +0200

    THREESCALE-11011: Authorize.net is a living dead 🧟‍♀️ (#3771)
    * Authorize.net is a living dead
    
    Reverts 65a284e0, afa1b52d and a31cd401

--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2024_03_02_090207) do
     t.text "vat_zero_text"
     t.bigint "default_account_plan_id"
     t.bigint "default_service_id"
+    t.string "credit_card_authorize_net_payment_profile_token"
     t.bigint "tenant_id"
     t.string "self_domain"
     t.string "s3_prefix"
--- test/test_helpers/active_merchant_test_helpers.rb
+++ test/test_helpers/active_merchant_test_helpers.rb
@@ -6,6 +6,55 @@ module ActiveMerchantTestHelpers
     ActiveMerchant::Billing::Response.new(success, success ? 'success' : 'failed', json, options)
   end
 
+  module AuthorizeNet
+
+    def failed_get_customer_profile_response
+      json_string = <<-JSON
+      {
+        "messages": {
+          "result_code": "Error",
+          "message": {
+            "test": "Failed to get profile"
+          }
+        }
+      }
+      JSON
+      build_active_merchant_response(false, json_string)
+    end
+
+    def successful_get_customer_profile_response
+      json_string = <<-JSON
+      {
+        "messages": {
+          "result_code": "Ok"
+        },
+        "profile": {
+          "payment_profiles": {
+            "customer_payment_profile_id": 12345,
+            "payment": {
+              "credit_card": {
+                "card_number": "4444",
+                "expiration_date": "XXXX"
+              }
+            },
+            "bill_to": {
+              "company": "3scale",
+              "address": "Carrer Napols",
+              "city": "Barcelona",
+              "country": "Spain",
+              "state": "Barcelona",
+              "zip": "08013",
+              "phone_number": "+34123456789"
+            }
+          }
+        }
+      }
+      JSON
+      build_active_merchant_response(true, json_string)
+    end
+
+  end
+
   module BraintreeBlue
     def successful_result(user = nil)
       json = {
