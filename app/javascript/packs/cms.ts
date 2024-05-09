93f070a9ab524ed0c4f6d184384943eb3edab71e
josemigallas <josemigallas@gmail.com>
2024-05-09 09:13:23 +0200

    updates intro tabs script and move it to Typescript

--- app/javascript/packs/cms.ts
+++ app/javascript/packs/cms.ts
@@ -4,7 +4,6 @@ import 'jquery-ui/ui/widgets/draggable'
 import 'jquery-ui/ui/widgets/tabs'
 
 // Export jQuery 3.7 with jquery-ui widgets to be used in:
-// - app/assets/javascripts/provider/admin/cms/intro.js.coffee
 // - app/assets/javascripts/provider/admin/cms/templates.js
 // - app/assets/javascripts/provider/admin/cms/sidebar.js.coffee
 window.jQueryUI = jQueryUI
