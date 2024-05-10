0650281c7a3befc321b4bfcc65e990b401f760e6
Daria Mayorova <mayorova@users.noreply.github.com>
2024-05-10 17:12:56 +0200

    Fix Oracle issues by disabling 'use_old_oracle_visitor'
    Old Arel visitor was causing issues with ROWNUM, so we removed it.
    The new Arel visitor forms some queries that incude subqueries with
    ORDER BY, which throws an error in Oracle: ORA-00907: missing right parenthesis
    So, order has been removed from some scopes to avoid this issue.

--- lib/arel/visitors/oracle12_hack.rb
+++ lib/arel/visitors/oracle12_hack.rb
@@ -2,8 +2,7 @@
 
 module Arel
   module Visitors
-    # if we stop using use_old_oracle_visitor = true, then change to Oracle12
-    Oracle.class_eval do
+    Oracle12.class_eval do
       # we need to strip ORDER from subqueries because Oracle does not support it
       def strip_order_from_select(o)
         case (node = o.right)
@@ -28,7 +27,7 @@ module Arel
       # Oracle can't compare CLOB columns with standard SQL operators for comparison.
       # We need to replace standard equality for text/binary columns to use DBMS_LOB.COMPARE function.
       # Fixes ORA-00932: inconsistent datatypes: expected - got CLOB
-      # remove if https://github.com/rsim/oracle-enhanced/issues/2239 is fixed
+      # remove if https://github.com/rsim/oracle-enhanced/issues/2239 is fixed (in Rails 7.0.1)
       def visit_Arel_Nodes_Equality(o, collector)
         case (left = o.left)
         when Arel::Attributes::Attribute
