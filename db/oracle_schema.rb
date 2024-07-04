d9c51dc8687bc85fac526d23fdb23c8c4c9d4c5f
Aleksandar N. Kostadinov <akostadi@redhat.com>
2024-07-04 12:17:47 +0300

    fix event_store_events tenant_id trigger

--- db/oracle_schema.rb
+++ db/oracle_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_02_090207) do
+ActiveRecord::Schema.define(version: 2024_07_04_135614) do
 
   create_table "access_tokens", force: :cascade do |t|
     t.integer "owner_id", precision: 38, null: false
--- db/postgres_schema.rb
+++ db/postgres_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_02_090207) do
+ActiveRecord::Schema.define(version: 2024_07_04_135614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
