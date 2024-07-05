bb1b2f0eeaeaa608211aa781b73daa42bf0f883b
Daria Mayorova <mayorova@users.noreply.github.com>
2024-07-05 16:17:17 +0200

    THREESCALE-10074: Do not reveal that email doesn't exist on Forgot password form (#3837)
    * Do not reveal that email doesn't exist on Forgot password form
    
    * Some fixes
    
    * Reorganize cucumbers for password reset

--- features/step_definitions/password_steps.rb
+++ features/step_definitions/password_steps.rb
@@ -14,7 +14,7 @@ Then "the password of {user} should not be {string}" do |user, password|
   assert !user.authenticated?(password)
 end
 
-When /^I follow the link found in the password reset email send to "([^"]*)"$/ do |email|
+When /^(?:|I |they )follow the link found in the password reset email send to "([^"]*)"$/ do |email|
   visit_url_in_email(email, /Lost password recovery/)
 
 end
@@ -34,7 +34,7 @@ def visit_url_in_email(email, subject)
   visit url
 end
 
-Then 'I should see the password confirmation error' do
+Then /^(?:|I |they )should see the password confirmation error$/ do
   %q(I should see error "doesn't match Password" for field "Password confirmation")
 end
 
