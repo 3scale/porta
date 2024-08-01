ce82152516d4034493dff0793384c2c59d4a0842
Joan Lledó <jlledo@redhat.com>
2024-08-01 09:53:37 +0200

    CSP: Allow everything

--- test/integration/secure_headers_test.rb
+++ test/integration/secure_headers_test.rb
@@ -15,7 +15,7 @@ class SecureHeadersTest < ActionDispatch::IntegrationTest
     assert_equal 'DENY', response.headers['X-Frame-Options']
     assert_equal 'nosniff', response.headers['X-Content-Type-Options']
     assert_equal '1; mode=block', response.headers['X-XSS-Protection']
-    assert_equal "default-src 'self'; font-src 'self' ; img-src 'self' data: ; script-src 'self' 'unsafe-inline' 'unsafe-eval' ; style-src 'self' 'unsafe-inline' ; connect-src *", response.headers['Content-Security-Policy']
+    assert_equal "default-src * data: mediastream: blob: filesystem: ws: wss: 'unsafe-eval' 'unsafe-inline'", response.headers['Content-Security-Policy']
   end
 
   test 'do not add non used secure headers in the response' do
