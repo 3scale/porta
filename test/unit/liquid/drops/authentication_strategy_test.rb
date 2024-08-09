8fbd41d94f65b6284555d33f9d2ab56ce3e41eef
Daria Mayorova <mayorova@users.noreply.github.com>
2024-08-09 11:25:49 +0200

    auth strat test fixup

--- test/unit/liquid/drops/authentication_strategy_test.rb
+++ test/unit/liquid/drops/authentication_strategy_test.rb
@@ -4,13 +4,13 @@ class Liquid::Drops::AuthenticationStrategyTest < ActiveSupport::TestCase
   include Liquid
 
   def setup
-    provider = FactoryBot.create :provider_account
+    provider = FactoryBot.create(:provider_account, domain: 'company-domain.com')
     provider.settings.update(cas_server_url: 'https://cas.example.com')
     strategy = Authentication::Strategy::Cas.new provider
     @drop = Drops::AuthenticationStrategy::Cas.new strategy
   end
 
   test '#login_url' do
-    assert_equal "https://cas.example.com/login?service=http%3A%2F%2Fcompany1.com%2Fsession%2Fcreate", @drop.login_url
+    assert_equal "https://cas.example.com/login?service=http%3A%2F%2Fcompany-domain.com%2Fsession%2Fcreate", @drop.login_url
   end
 end
