dc939e68390a358d242824efc7ccc35c719bd82c
Joan Lledó <jlledo@redhat.com>
2024-08-13 17:32:29 +0200

    Create annotation banner for 'managed_by'

--- app/helpers/patternfly_components_helper.rb
+++ app/helpers/patternfly_components_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module PatternflyComponentsHelper
-  def pf_inline_alert(text, variant: nil)
+  def pf_inline_alert(title, body, variant: nil)
     icon_name = case variant
                 when :info then 'info-circle'
                 when :success then 'check-circled'
@@ -9,16 +9,20 @@ module PatternflyComponentsHelper
                 else 'bell'
     end
 
-    icon = tag.div class: 'pf-c-alert__icon' do
+    icon_tag = tag.div class: 'pf-c-alert__icon' do
       tag.i class: "fas fa-fw fa-#{icon_name}", 'aria-hidden': 'true'
     end
 
-    title = tag.p class: 'pf-c-alert__title' do
-      text
+    title_tag = tag.p class: 'pf-c-alert__title' do
+      title
+    end
+
+    body_tag =tag.div class: 'pf-c-alert__description' do
+      tag.p body
     end
 
     tag.div class: "pf-c-alert pf-m-#{variant} pf-m-inline" do
-      icon + title
+      icon_tag + title_tag + body_tag
     end
   end
 end
