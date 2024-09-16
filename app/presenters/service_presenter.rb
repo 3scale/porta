3e149eefd7414a5e89e7a6f4f686ad04d9522fc7
josemigallas <josemigallas@gmail.com>
2024-09-16 11:26:06 +0200

    🛂 updates what a member can see and what not from dashboard

--- app/presenters/service_presenter.rb
+++ app/presenters/service_presenter.rb
@@ -7,19 +7,6 @@ class ServicePresenter < SimpleDelegator
     {service: stringify_nil_values(filtered_values)}.merge(error_messages)
   end
 
-  def index_data
-    {
-      id: id,
-      name: name,
-      systemName: system_name,
-      updatedAt: updated_at.to_s(:long),
-      links: links,
-      appsCount: cinstances.size,
-      backendsCount: backend_api_configs.size,
-      unreadAlertsCount: decorate.unread_alerts_count
-    }
-  end
-
   def new_application_data
     {
       id: id,
@@ -30,16 +17,6 @@ class ServicePresenter < SimpleDelegator
       servicePlans: service_plans.reorder(:name).select(:id, :name).as_json(root: false),
       defaultServicePlan: default_service_plan.as_json(root: false, only: %i[id name]),
       defaultAppPlan: default_application_plan.as_json(root: false, only: %i[id name])
-    }
-  end
-
-  def dashboard_widget_data
-    {
-      id: id,
-      name: name,
-      updated_at: updated_at.to_s(:long),
-      link: decorate.link,
-      links: links
     }
   end
 
@@ -55,16 +32,5 @@ class ServicePresenter < SimpleDelegator
 
   def stringify_nil_values(hash)
     hash.transform_values(&:to_s)
-  end
-
-  def links
-    [
-      { name: 'Edit', path: edit_admin_service_path(self) },
-      { name: 'Overview', path: admin_service_path(self) },
-      { name: 'Analytics', path: admin_service_stats_usage_path(self) },
-      { name: 'Applications', path: admin_service_applications_path(self) },
-      { name: 'ActiveDocs', path: admin_service_api_docs_path(self) },
-      { name: 'Integration', path: admin_service_integration_path(self) },
-    ]
   end
 end
