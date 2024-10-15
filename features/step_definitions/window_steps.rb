5c9bd269be3475132577c76667a984cbc11739ae
Daria Mayorova <mayorova@users.noreply.github.com>
2024-10-15 14:49:43 +0200

    Resize browser width with a hook

--- features/step_definitions/window_steps.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-And "(they )set the screen width to {string} pixels" do |width|
-  height = page.driver.browser.manage.window.size.height
-  page.driver.browser.manage.window.resize_to(width.to_i, height)
-end
