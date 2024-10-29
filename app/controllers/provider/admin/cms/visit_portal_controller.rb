301e48afa5ba2a35f69ba5bc1267abbdc4cc8b2c
Joan Lledó <jlledo@redhat.com>
2024-10-29 12:37:49 +0100

    Remove redundant `to_i`

--- app/controllers/provider/admin/cms/visit_portal_controller.rb
+++ app/controllers/provider/admin/cms/visit_portal_controller.rb
@@ -4,7 +4,7 @@ class Provider::Admin::CMS::VisitPortalController < Provider::Admin::CMS::BaseCo
   # Encrypt the CMS token under a temporary SSO token and redirect to the Developer Portal
   def with_token
     cms_token = current_account.settings.cms_token!
-    expires_at = Time.now.utc + 1.minute.to_i
+    expires_at = Time.now.utc + 1.minute
     encrypted_token = ThreeScale::SSO::Encryptor.new(current_account.settings.sso_key, expires_at.to_i).encrypt_token cms_token
 
     redirect_to access_code_url(
