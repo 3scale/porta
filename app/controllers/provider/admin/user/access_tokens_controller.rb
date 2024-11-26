603673581d7c4dd24bd7852f6a711ce8d6025493
Joan Lledó <jlledo@redhat.com>
2024-11-26 12:14:30 +0100

    Add the new component to Access Token forms

--- app/controllers/provider/admin/user/access_tokens_controller.rb
+++ app/controllers/provider/admin/user/access_tokens_controller.rb
@@ -1,44 +1,57 @@
-class Provider::Admin::User::AccessTokensController < Provider::Admin::User::BaseController
-  inherit_resources
-  defaults route_prefix: 'provider_admin_user', resource_class: AccessToken
-  actions :index, :new, :create, :edit, :update, :destroy
+# frozen_string_literal: true
 
-  authorize_resource
-  activate_menu :account, :personal, :tokens
-  before_action :authorize_access_tokens
-  before_action :disable_client_cache
+module Provider
+  module Admin
+    module User
+      class AccessTokensController < BaseController
+        inherit_resources
+        defaults route_prefix: 'provider_admin_user', resource_class: AccessToken
+        actions :index, :new, :create, :edit, :update, :destroy
 
-  def create
-    create! do |success, _failure|
-      success.html do
-        flash[:token] = @access_token.id
-        flash[:notice] = 'Access Token was successfully created.'
-        redirect_to(collection_url)
+        authorize_resource
+        activate_menu :account, :personal, :tokens
+        before_action :authorize_access_tokens
+        before_action :disable_client_cache
+
+        def new
+          @presenter = AccessTokensNewPresenter.new(current_account)
+        end
+
+        def create
+          @presenter = AccessTokensNewPresenter.new(current_account)
+          create! do |success, _failure|
+            success.html do
+              flash[:token] = @access_token.id
+              flash[:notice] = 'Access Token was successfully created.'
+              redirect_to(collection_url)
+            end
+          end
+        end
+
+        def index
+          index!
+          @last_access_key = flash[:token]
+        end
+
+        def update
+          update! do |success, _failure|
+            success.html do
+              flash[:notice] = 'Access Token was successfully updated.'
+              redirect_to(collection_url)
+            end
+          end
+        end
+
+        private
+
+        def authorize_access_tokens
+          authorize! :manage, :access_tokens, current_user
+        end
+
+        def begin_of_association_chain
+          current_user
+        end
       end
     end
-  end
-
-  def index
-    index!
-    @last_access_key = flash[:token]
-  end
-
-  def update
-    update! do |success, _failure|
-      success.html do
-        flash[:notice] = 'Access Token was successfully updated.'
-        redirect_to(collection_url)
-      end
-    end
-  end
-
-  private
-
-  def authorize_access_tokens
-    authorize! :manage, :access_tokens, current_user
-  end
-
-  def begin_of_association_chain
-    current_user
   end
 end
