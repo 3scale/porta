485c2f87fee6d777dee0742abe80f7450c8381b9
Daria Mayorova <mayorova@users.noreply.github.com>
2024-12-19 13:02:53 +0100

    Bring back on_load

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -3,5 +3,7 @@ require 'three_scale/money_conversions'
 Rails.application.config.after_initialize do
   ActionController::Base.helper(ThreeScale::MoneyHelper)
 
-  ActiveRecord::Base.include ThreeScale::HasMoney
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.include ThreeScale::HasMoney
+  end
 end
