91a1c70b84ca5e19a59820353845ea2c6a60356b
Aleksandar N. Kostadinov <akostadi@redhat.com>
2025-01-09 22:31:19 +0200

    further deletion progress

--- app/models/concerns/background_deletion.rb
+++ app/models/concerns/background_deletion.rb
@@ -5,7 +5,7 @@ module BackgroundDeletion
 
   included do
     class_attribute :background_deletion, default: [], instance_writer: false
-    class_attribute :background_deletion_method, default: :destroy, instance_writer: false
+    class_attribute :background_deletion_method, default: :destroy!, instance_writer: false
     class_attribute :background_deletion_scope_name, default: :all, instance_writer: false
   end
 
@@ -17,36 +17,5 @@ module BackgroundDeletion
 
   def background_deletion_method_call
     send background_deletion_method
-  end
-
-  class Reflection
-
-    DEFAULT_DESTROY_METHOD = 'destroy'
-    DEFAULT_HAS_MANY_OPTION = true
-
-    attr_reader :name, :options
-
-    def initialize(association_config)
-      config = Array(association_config)
-
-      @name = config[0]
-      @options = config[1].presence || {}
-    end
-
-    def many?
-      options.fetch(:has_many) { DEFAULT_HAS_MANY_OPTION }
-    end
-
-    def class_name
-      @class_name ||= options[:class_name].presence || name.to_s.singularize.classify
-    end
-
-    def klass
-      @klass ||= class_name.constantize
-    end
-
-    def background_destroy_method
-      options[:action].to_s.presence || DEFAULT_DESTROY_METHOD
-    end
   end
 end
