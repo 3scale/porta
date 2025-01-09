96c709717cee4f6479b6aaf04d71c3f1385dd284
Joan Lledó <jlledo@redhat.com>
2025-01-09 14:23:50 +0100

    Move date picker props to presenter

--- app/presenters/provider/admin/user/access_tokens_new_presenter.rb
+++ app/presenters/provider/admin/user/access_tokens_new_presenter.rb
@@ -9,4 +9,12 @@ class Provider::Admin::User::AccessTokensNewPresenter
   def provider_timezone_offset
     @timezone.utc_offset
   end
+
+  def date_picker_props
+    {
+      id: 'access_token[expires_at]',
+      label: I18n.t('access_token_options.expires_in'),
+      tzOffset: provider_timezone_offset
+    }
+  end
 end
