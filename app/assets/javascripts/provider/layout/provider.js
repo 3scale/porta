c5f49d680700c53f2c275d6404cebe21e3b2a9ef
josemigallas <josemigallas@gmail.com>
2025-01-29 10:16:39 +0100

    ⬆️ upgrades jquery 1.11.3 to 1.12.4

--- app/assets/javascripts/provider/layout/provider.js
+++ app/assets/javascripts/provider/layout/provider.js
@@ -1,4 +1,5 @@
-//= require vendor/jquery
+//= require vendor/jquery-1.12.4.min
+//= require jquery_ujs
 //= require remote
 //= require vendor/underscore.min.js
 //= require vendor/colorbox/jquery.colorbox.js
