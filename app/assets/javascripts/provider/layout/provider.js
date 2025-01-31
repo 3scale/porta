eedebf44d3c72d6bbbc779cbeb66d41c877371cf
josemigallas <josemigallas@gmail.com>
2025-01-31 10:28:48 +0100

    update remote.js to jQuery 3.x

--- app/assets/javascripts/provider/layout/provider.js
+++ app/assets/javascripts/provider/layout/provider.js
@@ -1,6 +1,5 @@
 //= require vendor/jquery-1.12.4.min
 //= require jquery_ujs
-//= require remote
 //= require vendor/underscore.min.js
 //= require vendor/colorbox/jquery.colorbox.js
 //= require master_application
