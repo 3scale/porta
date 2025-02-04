cc4b46d50c8bbc8fa124d3e4a8dcac6880e133e5
Daria Mayorova <mayorova@users.noreply.github.com>
2025-02-04 16:24:32 +0100

    Make policy_registry permission per-service

--- app/controllers/admin/api/services/proxy/policies_controller.rb
+++ app/controllers/admin/api/services/proxy/policies_controller.rb
@@ -4,6 +4,8 @@ class Admin::Api::Services::Proxy::PoliciesController < Admin::Api::Services::Ba
   wrap_parameters ::Proxy
   representer Proxy::PoliciesConfig
 
+  self.access_token_scopes = :policy_registry
+
   # Proxy Policies Chain Show
   # GET /admin/api/services/{service_id}/proxy/policies.json
   def show
