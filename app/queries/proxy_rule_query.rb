a80ce94568d738c257f2cda83fad04277a1ea1ef
Joan Lledó <jlledo@redhat.com>
2025-02-20 13:42:13 +0100

    ProxyRule index: expand on query

--- app/queries/proxy_rule_query.rb
+++ app/queries/proxy_rule_query.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ProxyRuleQuery
+  DEFAULT_SEARCH_OPTIONS = {
+    ids_only: true, per_page: ThreeScale::Search::Helpers::SPHINX_PAGE_SIZE_INFINITE, ignore_scopes: true
+  }.freeze
+
   def initialize(owner_type:, owner_id:, sort: nil, direction: nil)
     @direction  = direction
     @owner_type = owner_type
@@ -12,10 +16,7 @@ class ProxyRuleQuery
     scope = scope.order_by(@sort, @direction).includes(:metric)
     return scope if query.blank?
 
-    options = {
-      ids_only: true, star: true, per_page: ThreeScale::Search::Helpers::SPHINX_PAGE_SIZE_INFINITE, ignore_scopes: true,
-      with: { owner_type: @owner_type, owner_id: @owner_id }
-    }
+    options = DEFAULT_SEARCH_OPTIONS.merge(with: { owner_type: @owner_type, owner_id: @owner_id })
     ids = ProxyRule.search(ThinkingSphinx::Query.escape(query), options)
     scope.where(id: ids, owner_type: @owner_type, owner_id: @owner_id)
   end
