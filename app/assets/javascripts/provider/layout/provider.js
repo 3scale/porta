1d3dd88b6a93e054c4930bb5d4366e5f3b97c301
josemigallas <josemigallas@gmail.com>
2025-02-21 11:25:53 +0100

    ⚰️ removes unused asset pluralized

--- app/assets/javascripts/provider/layout/provider.js
+++ app/assets/javascripts/provider/layout/provider.js
@@ -4,6 +4,5 @@
 //= require switch
 //= require vendor/underscore.min.js
 //= require vendor/colorbox/jquery.colorbox.js
-//= require vendor/pluralize
 //= require master_application
 //= require flash
