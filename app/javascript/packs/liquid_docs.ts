94f855945d59fba1055ecbe0a5c2099d79474bab
josemigallas <josemigallas@gmail.com>
2025-02-24 12:36:30 +0100

    ⬆️ update highlight.js to 8.9.1

--- app/javascript/packs/liquid_docs.ts
+++ app/javascript/packs/liquid_docs.ts
@@ -1,5 +1,7 @@
 import hljs from 'highlight.js'
 
+hljs.configure({ classPrefix: '' })
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll<HTMLElement>('#liquid-docs pre code')
     .forEach((el) => {
--- app/javascript/src/Types/highlight.d.ts
+++ app/javascript/src/Types/highlight.d.ts
@@ -4,4 +4,5 @@
  */
 declare module 'highlight.js' {
   function highlightBlock (block: HTMLElement): void
+  function configure (options: unknown): void
 }
