c36b87f421ff657a5d2408b013faa1b04749690f
josemigallas <josemigallas@gmail.com>
2025-03-24 12:45:19 +0100

    🧹 remove unused assets from app, use jquery gem

--- app/assets/javascripts/provider/layout/provider.js
+++ app/assets/javascripts/provider/layout/provider.js
@@ -1,4 +1,5 @@
-//= require vendor/jquery
+//= require jquery
+//= require jquery_ujs
 //= require vendor/underscore.min.js
 //= require vendor/colorbox/jquery.colorbox.js
 //= require master_application
