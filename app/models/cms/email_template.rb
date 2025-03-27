5ef76bf702c7dea4ad80ede654d80a03c050a4b1
Joan Lledó <jlledo@redhat.com>
2025-03-27 13:25:11 +0100

    Remove old new_app notification

--- app/models/cms/email_template.rb
+++ app/models/cms/email_template.rb
@@ -33,7 +33,6 @@ class CMS::EmailTemplate < CMS::Template
     account_messenger_new_signup
     account_messenger_plan_change_request
     cinstance_messenger_contract_cancellation
-    cinstance_messenger_new_application
     cinstance_messenger_plan_change
     service_contract_messenger_contract_cancellation
     service_contract_messenger_new_contract
