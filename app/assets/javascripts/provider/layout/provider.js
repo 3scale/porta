7a0ee4c7c82c74d1d4c978885433b94f9790caad
josemigallas <josemigallas@gmail.com>
2025-04-08 12:55:31 +0200

    removes duplicate colorbox

--- app/assets/javascripts/provider/layout/provider.js
+++ app/assets/javascripts/provider/layout/provider.js
@@ -1,6 +1,6 @@
 //= require jquery
 //= require rails-ujs
 //= require vendor/underscore.min.js
-//= require vendor/colorbox/jquery.colorbox.js
+//= require vendor/jquery.colorbox-min.js
 //= require master_application
 //= require flash
