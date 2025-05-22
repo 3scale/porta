786b5a8d8225d0d132026cae85316b8e1bc0b71a
josemigallas <josemigallas@gmail.com>
2025-05-22 18:33:43 +0200

    🦋 updates/fixes inbox page

--- app/controllers/provider/admin/messages/base_controller.rb
+++ app/controllers/provider/admin/messages/base_controller.rb
@@ -7,8 +7,8 @@ class Provider::Admin::Messages::BaseController < FrontendController
     { page: params.permit(:page)[:page] || 1, per_page: params.permit(:per_page)[:per_page] || 20 }
   end
 
-  def toolbar_props
-    {
+  def toolbar_props # rubocop:disable Metrics/MethodLength
+    props = {
       totalEntries: @messages.total_entries,
       pageEntries: @messages.length,
       actions: [{
@@ -21,7 +21,17 @@ class Provider::Admin::Messages::BaseController < FrontendController
         url: new_provider_admin_messages_bulk_trash_path(scope: scope),
         title: 'Delete selected messages',
         variant: 'danger'
-      }]
+      }],
+      overflow: []
     }
+
+    if can?(:export, :data)
+      props[:overflow].append({ href: new_provider_admin_account_data_exports_path,
+                                label: 'Export to CSV',
+                                isShared: false,
+                                variant: :secondary })
+    end
+
+    props
   end
 end
