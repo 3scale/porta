cce4b08d6fdd36a19f91349e91403bf513d51d83
josemigallas <josemigallas@gmail.com>
2025-05-22 17:08:59 +0200

    🦋 updates messages Trash with Patternfly 4

--- app/controllers/provider/admin/messages/trash_controller.rb
+++ app/controllers/provider/admin/messages/trash_controller.rb
@@ -1,12 +1,13 @@
 class Provider::Admin::Messages::TrashController < FrontendController
   before_action :find_message, only: [:show, :destroy]
   activate_menu :buyers, :messages, :trash
+  helper_method :toolbar_props
 
   def index
     @messages = current_account.trashed_messages
                                .not_system
                                .latest_first
-                               .paginate(page: params[:page])
+                               .paginate(pagination_params)
                                .decorate
   end
 
@@ -33,5 +34,16 @@ class Provider::Admin::Messages::TrashController < FrontendController
 
   def find_message
     @message = current_account.trashed_messages.not_system.find(params.require(:id)).decorate
+  end
+
+  def pagination_params
+    { page: params[:page], per_page: params[:per_page] || 20 }
+  end
+
+  def toolbar_props
+    {
+      totalEntries: @messages.total_entries,
+      pageEntries: @messages.length,
+    }
   end
 end
