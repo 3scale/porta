281ffb327072e2543af723202651d34979d3e04d
josemigallas <josemigallas@gmail.com>
2025-05-28 17:15:46 +0200

    🧹tiny cleanup

--- app/assets/javascripts/provider/layout/iframe.js
+++ app/assets/javascripts/provider/layout/iframe.js
@@ -1,17 +1,2 @@
 //= require jquery
 //= require rails-ujs
-//= require_self
-
-
-
-(function(){
-    "use strict";
-
-    window.gaReady = function gaReady (fn) {
-        if (typeof(ga) === 'undefined' && typeof(analytics) === 'object') {
-            analytics.ready(fn);
-        } else {
-            fn();
-        }
-    };
-}());
