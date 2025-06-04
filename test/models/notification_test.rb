cdf4b4f5ce798b31d058a4939731adce568cdd65
Daria Mayorova <mayorova@users.noreply.github.com>
2025-06-04 23:12:37 +0200

    Fix some unit tests

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -12,7 +12,7 @@ class NotificationTest < ActiveSupport::TestCase
   end
 
   test 'readonly' do
-    assert_equal Set['event_id', 'user_id', 'system_name'],
+    assert_same_elements %w[event_id user_id system_name],
                  Notification.readonly_attributes
   end
 
