3266057c291ecf6358d61ba17364e917af66fcbd
josemigallas <josemigallas@gmail.com>
2025-06-05 12:03:51 +0200

    typo

--- app/controllers/buyers/accounts_controller.rb
+++ app/controllers/buyers/accounts_controller.rb
@@ -99,7 +99,7 @@ class Buyers::AccountsController < Buyers::BaseController
     account_type = account.provider? ? 'tenant' : 'developer'
     flash_type = account.fire_events(action_name) ? :success : :danger
 
-    flash[flash_type] = t(".#{flash_type}", account_type:).capitalization
+    flash[flash_type] = t(".#{flash_type}", account_type:).capitalize
 
     redirect_to admin_buyers_account_path(account)
   end
