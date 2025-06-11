72707fb0b6fff8853c403b0fb1332177fef3478b
Daria Mayorova <mayorova@users.noreply.github.com>
2025-06-11 14:03:19 +0200

    Disable 'config.action_controller.raise_on_missing_callback_actions'

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Raise error when a before_action's only/except options reference missing actions
-  config.action_controller.raise_on_missing_callback_actions = true
+  config.action_controller.raise_on_missing_callback_actions = false # TODO: fix controllers and enable
 
   config.active_support.test_order = :random
 
