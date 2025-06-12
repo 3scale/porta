4f4cf6d9961835cef1775c479e40876c056159bb
Daria Mayorova <mayorova@users.noreply.github.com>
2025-06-12 11:22:33 +0200

    Enable 'active_record.commit_transaction_on_non_local_return = true'

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -219,7 +219,7 @@ Rails.application.config.active_record.marshalling_format_version = 7.1
 ###
 # Whether a `transaction` block is committed or rolled back when exited via `return`, `break` or `throw`.
 #++
-# Rails.application.config.active_record.commit_transaction_on_non_local_return = true # TODO
+Rails.application.config.active_record.commit_transaction_on_non_local_return = true
 
 ###
 # Controls when to generate a value for <tt>has_secure_token</tt> declarations.
