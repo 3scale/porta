a654b9fcb52618b00e9eadfb5d80cedf0b193521
Daria Mayorova <mayorova@users.noreply.github.com>
2025-06-16 15:42:42 +0200

    Apply 7.1 defaults in application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,19 +58,20 @@ module System
     # we do here instead of using initializers because of a Rails 5.1 vs
     # MySQL bug where `rake db:reset` causes ActiveRecord to be loaded
     # before initializers and causes configuration not to be respected.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # TODO: consider removing this to enable the default value 'true', and setting `allow_other_host: true` for `redirect_to` only where needed
     # Protect from open redirect attacks in `redirect_back_or_to` and `redirect_to`.
     config.action_controller.raise_on_open_redirects = false
 
-    # ** Please read carefully, this must be configured in config/application.rb **
     # Change the format of the cache entry.
+    #
     # Changing this default means that all new cache entries added to the cache
-    # will have a different format that is not supported by Rails 6.1 applications.
-    # Only change this value after your application is fully deployed to Rails 7.0
+    # will have a different format that is not supported by Rails 7.0
+    # applications.
+    # Only change this value after your application is fully deployed to Rails 7.1
     # and you have no plans to rollback.
-    # When you're ready to change format, change the value to 7.0
+    # TODO: update to 7.1
     config.active_support.cache_format_version = 7.0
 
     # To migrate an existing application to the `:json` serializer, use the `:hybrid` option.
@@ -110,6 +111,17 @@ module System
                                                           ActiveSupport::TimeZone,
                                                           ActiveSupport::HashWithIndifferentAccess]
 
+    # Keeping the historic behavior by setting to `YAML`
+    # It is recommended to explicitly define the serialization method for each column
+    # rather than to rely on a global default.
+    # TODO: see if we can use the Rails 7.1 default `nil` value by setting serialization for each column explicitly
+    config.active_record.default_column_serializer = YAML
+
+    # The default behavior in 7.1 is to raise on assignment to attr_readonly attributes.
+    # This setting restores the previous behavior which allows assignment but silently not persist changes to the
+    # database.
+    config.active_record.raise_on_assign_to_attr_readonly = false
+
     config.active_job.queue_adapter = :sidekiq
 
     def try_config_for(*args)
@@ -137,6 +149,8 @@ module System
     # Include developer_portal into the autoload and eager load path
     config.autoload_paths += [Rails.root.join('lib', 'developer_portal', 'app'), Rails.root.join('lib', 'developer_portal', 'lib')]
     config.eager_load_paths += [Rails.root.join('lib', 'developer_portal', 'app'), Rails.root.join('lib', 'developer_portal', 'lib')]
+
+    config.add_autoload_paths_to_load_path = true # TODO: default value in 7.1 is false, but it requires significant refactoring
 
     config.eager_load = true
 
