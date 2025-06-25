21966b1ef55fd4e9fc717bb573ff80ad1c179023
Daria Mayorova <mayorova@users.noreply.github.com>
2025-06-25 12:14:48 +0200

    Try to reduce cucumber failures on API docs

--- features/step_definitions/toast_steps.rb
+++ features/step_definitions/toast_steps.rb
@@ -5,12 +5,14 @@ Given /^a(?: )?(\w+)? toast alert is displayed with text "(.*)"$/ do |type, mess
 end
 
 Then /^(?:they )?should see a(?: )?(\w+)? toast alert with text "(.*)"$/ do |type, message|
+  find '.pf-c-alert-group.pf-m-toast'
   within '.pf-c-alert-group.pf-m-toast' do
     find(".pf-c-alert#{type ? %(.pf-m-#{type}) : nil} .pf-c-alert__title", text: message)
   end
 end
 
 Then "they should not see any toast alerts" do
+  find '.pf-c-alert-group.pf-m-toast', visible: :all
   within('.pf-c-alert-group.pf-m-toast', visible: :all) do
     assert_no_css '.pf-c-alert', wait: 1
   end
