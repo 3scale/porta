7d09fc39271ff8703a748fb84f22c6b33d144d60
Joan Lledó <jlledo@redhat.com>
2025-08-06 13:50:20 +0200

    Add support for recaptcha enterprise

--- config/initializers/captchas.rb
+++ config/initializers/captchas.rb
@@ -2,9 +2,12 @@
 
 Recaptcha.configure do |config|
   # Do not verify recaptcha keys if it is not correctly configured
-  (config.skip_verify_env ||= []) << Rails.env if Rails.configuration.three_scale.recaptcha_private_key.blank?
+  (config.skip_verify_env ||= []) << Rails.env if Rails.configuration.three_scale.recaptcha_public_key.blank?
   config.site_key = Rails.configuration.three_scale.recaptcha_public_key
   config.secret_key = Rails.configuration.three_scale.recaptcha_private_key
+  config.enterprise = Rails.configuration.three_scale.recaptcha_enterprise_enabled
+  config.enterprise_api_key = Rails.configuration.three_scale.recaptcha_enterprise_api_key
+  config.enterprise_project_id = Rails.configuration.three_scale.recaptcha_enterprise_project_id
 end
 
 module Recaptcha
