94f57fb5decb31833e8003e82d1a22a81c84d630
Joan Lledó <jlledo@redhat.com>
2025-08-07 09:29:41 +0200

    Simplify env variables

--- config/initializers/captchas.rb
+++ config/initializers/captchas.rb
@@ -3,11 +3,16 @@
 Recaptcha.configure do |config|
   # Do not verify recaptcha keys if it is not correctly configured
   (config.skip_verify_env ||= []) << Rails.env if Rails.configuration.three_scale.recaptcha_public_key.blank?
+
+  config.enterprise = Rails.configuration.three_scale.recaptcha_project_id.present?
   config.site_key = Rails.configuration.three_scale.recaptcha_public_key
-  config.secret_key = Rails.configuration.three_scale.recaptcha_private_key
-  config.enterprise = Rails.configuration.three_scale.recaptcha_enterprise_enabled
-  config.enterprise_api_key = Rails.configuration.three_scale.recaptcha_enterprise_api_key
-  config.enterprise_project_id = Rails.configuration.three_scale.recaptcha_enterprise_project_id
+
+  if config.enterprise
+    config.enterprise_api_key = Rails.configuration.three_scale.recaptcha_private_key
+    config.enterprise_project_id = Rails.configuration.three_scale.recaptcha_project_id
+  else
+    config.secret_key = Rails.configuration.three_scale.recaptcha_private_key
+  end
 end
 
 module Recaptcha
