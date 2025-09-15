c821d83a2f3383234d58c01ce726f622aa733cc4
Daria Mayorova <mayorova@users.noreply.github.com>
2025-09-15 18:32:15 +0200

    Get rid of sass-rails in favor of dartsass-rails

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,13 @@
+//= link_tree ../images
+//= link_tree ../builds
+
+//= link_directory ../javascripts .js
+
+// Explicitly include these files
+//= link font-awesome.css
+//= link provider/signup_v2.js
+//= link provider/signup_form.js
+//= link provider/layout/provider.js
+//= link vendor/jquery/extensions/jquery.cookie.js
+//= link provider/admin/cms/sidebar.js
+//= link codemirror/modes/liquid.js
