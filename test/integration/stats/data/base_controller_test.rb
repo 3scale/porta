71a5a879e1fc079553831352b419043a4578e654
Joan Lledó <jlledo@redhat.com>
2025-11-04 17:07:27 +0100

    Add some tests

--- test/integration/stats/data/base_controller_test.rb
+++ test/integration/stats/data/base_controller_test.rb
@@ -42,4 +42,29 @@ class Stats::Data::BaseControllerTest < ActionDispatch::IntegrationTest
 
     # TODO: add a test for the undocumented use case: stats_params.except(:period).merge(granularity: 'hour', range: ?)
   end
+
+  test 'returns 403 when no access token or provider key is passed' do
+    url_params = { service_id: service.id, format: :json }
+    stats_params = { metric_name: metric.system_name, period: 'day', timezone: ActiveSupport::TimeZone['UTC'].name, skip_change: false }
+
+    # No authentication parameters at all
+    get usage_stats_data_services_path(url_params), params: stats_params
+    assert_response :forbidden
+  end
+
+  # There's no way a buyer can get an access token in real world, however, the endpoint seems to consider
+  # the possibility to be called by a buyer. That's probably old/dead code, but we add a test to ensure
+  # such a request would be denied.
+  test 'returns 403 when the user is a buyer' do
+    buyer = FactoryBot.create(:buyer_account, provider_account: provider)
+    buyer_user = FactoryBot.create(:user, account: buyer)
+
+    buyer_access_token = FactoryBot.create(:access_token, owner: buyer_user, scopes: ['stats'])
+
+    url_params = { service_id: service.id, format: :json, access_token: buyer_access_token.value }
+    stats_params = { metric_name: metric.system_name, period: 'day', timezone: ActiveSupport::TimeZone['UTC'].name, skip_change: false }
+
+    get usage_stats_data_services_path(url_params), params: stats_params
+    assert_response :forbidden
+  end
 end
