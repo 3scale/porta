92b3ac3ece27ee603560299afe3d5d191e71c006
Daria Mayorova <mayorova@users.noreply.github.com>
2025-11-18 14:55:36 +0100

    Add images used from CSS to NonDigestAssets

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -4,4 +4,16 @@ NonDigestAssets.asset_selectors += %w(
   tree-line.png
   3scale-logo.png
   error.css
+  xmag_16.png
+  dashboard_arrow.gif
+  cross.png
+  tick.png
+  child_metric_background.png
+  spinner.svg
+  up_green.png
+  down_green.png
+  clear-left.png
+  key.png
+  provider/key-background.png
+  drag-icon.png
 ) if defined?(NonDigestAssets) # container image excludes the assets gem group
