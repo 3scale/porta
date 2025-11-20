03b39aab4c9aad6ed6a4596923261cda16d7534d
Daria Mayorova <mayorova@users.noreply.github.com>
2025-11-20 11:46:39 +0100

    Fix a missing stylesheet

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,3 +6,6 @@
 //= link codemirror/modes/liquid.js
 
 //= link_tree ../../../lib/developer_portal/app/assets/javascripts .js
+
+// Missing from swagger-ui_rails2 gem's engine.rb precompile list
+//= link swagger-ui2/typography.css
