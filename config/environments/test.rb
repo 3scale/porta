638140998a0c0381750529e548efb75af6910929
Daria Mayorova <mayorova@users.noreply.github.com>
2025-11-26 15:16:57 +0100

    Fix config.action_dispatch.show_exceptions value

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+  config.action_dispatch.show_exceptions = :none
 
   # Render exception templates for rescuable exceptions and raise for other exceptions.
   # config.action_dispatch.show_exceptions = :rescuable
