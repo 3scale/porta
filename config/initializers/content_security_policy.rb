04a1f9619b861fc0044846dd12c1c6df4e7385b7
Joan Lledó <jlledo@redhat.com>
2025-12-11 13:53:35 +0100

    Simplify initializer

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,29 +3,22 @@
 # Configure Content Security Policy headers
 # See: https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-require_dependency 'three_scale/content_security_policy'
+require 'three_scale/content_security_policy'
 
-if ThreeScale::ContentSecurityPolicy::AdminPortal.enabled?
-  # Apply configurable CSP from YAML
-  Rails.application.configure do
-    # Set report-only mode if configured
-    config.content_security_policy_report_only = true if ThreeScale::ContentSecurityPolicy::AdminPortal.report_only?
-  end
-
-  # Apply global CSP policy from configuration
-  Rails.application.config.to_prepare do
+Rails.application.configure do
+  if ThreeScale::ContentSecurityPolicy::AdminPortal.enabled?
     policy_config = ThreeScale::ContentSecurityPolicy::AdminPortal.policy_config
 
     if policy_config.present?
-      Rails.application.config.content_security_policy do |policy|
+      config.content_security_policy do |policy|
         ThreeScale::ContentSecurityPolicy::AdminPortal.add_policy_config(policy, policy_config)
       end
     end
-  end
-else
-  # Fallback to permissive policy when config is disabled
-  Rails.application.config.to_prepare do
-    Rails.application.config.content_security_policy do |policy|
+
+    # Set report-only mode if configured
+    config.content_security_policy_report_only = true if ThreeScale::ContentSecurityPolicy::AdminPortal.report_only?
+  else
+    config.content_security_policy do |policy|
       policy.default_src '*', :data, :mediastream, :blob, :filesystem, :ws, :wss, :unsafe_eval, :unsafe_inline
     end
   end
