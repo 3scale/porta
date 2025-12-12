de3babc0340bd9017c69816ccdeaf4ffce024af5
josemigallas <josemigallas@gmail.com>
2025-12-12 13:28:41 +0100

    🦋🧹 updates Account > Export with PF4 and removes old "export all" button

--- test/integration/provider/admin/account/data_exports_controller_test.rb
+++ test/integration/provider/admin/account/data_exports_controller_test.rb
@@ -14,37 +14,43 @@ class Provider::Admin::Account::DataExportsControllerTest < ActionDispatch::Inte
   test 'invoices is present only if finance is allowed' do
     assert_not provider.settings.finance.allowed?
     get new_provider_admin_account_data_exports_path
-    assert_select 'select#data option[value="invoices"]', false
+    assert_select 'select#export_data option[value="invoices"]', false
 
     provider.settings.finance.allow
     assert provider.settings.finance.allowed?
     get new_provider_admin_account_data_exports_path
-    assert_select 'select#data option[value="invoices"]'
+    assert_select 'select#export_data option[value="invoices"]'
   end
 
   test 'for: today/this_week/period not given' do
-    post provider_admin_account_data_exports_path, params: { period: 'today', data: 'messages' }
-    assert_redirected_to new_provider_admin_account_data_exports_path
+    post provider_admin_account_data_exports_path, params: { export: { period: 'today', data: 'messages' } },
+                                                   xhr: true
+    assert_response :success
 
-    post provider_admin_account_data_exports_path, params: { period: 'this_week', data: 'messages' }
-    assert_redirected_to new_provider_admin_account_data_exports_path
+    post provider_admin_account_data_exports_path, params: { export: { period: 'this_week', data: 'messages' } },
+                                                   xhr: true
+    assert_response :success
 
-    post provider_admin_account_data_exports_path, params: { data: 'messages' }
-    assert_redirected_to new_provider_admin_account_data_exports_path
+    post provider_admin_account_data_exports_path, params: { export: { data: 'messages' } },
+                                                   xhr: true
+    assert_response :success
   end
 
   test 'invalid params to export submitted redirect to new action on empty/invalid params' do
-    post provider_admin_account_data_exports_path, params: { data: '' }
-    assert_redirected_to new_provider_admin_account_data_exports_path
+    post provider_admin_account_data_exports_path, params: { export: { data: '' } },
+                                                   xhr: true
+    assert_response :success
 
-    post provider_admin_account_data_exports_path, params: { data: 'invalid' }
-    assert_redirected_to new_provider_admin_account_data_exports_path
+    post provider_admin_account_data_exports_path, params: { export: { data: 'invalid' } },
+                                                   xhr: true
+    assert_response :success
   end
 
   test 'with correct params enqueue sidekiq'  do
     DataExportsWorker.jobs.clear
     assert_difference 'DataExportsWorker.jobs.size' do
-      post provider_admin_account_data_exports_path, params: { period: 'this_week', data: 'applications' }
+      post provider_admin_account_data_exports_path, params: { export: { period: 'this_week', data: 'applications' } },
+                                                     xhr: true
     end
     DataExportsWorker.jobs.clear
   end
--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -4,16 +4,6 @@ class ApplicationHelperTest < ActionView::TestCase
   attr_accessor :current_user
   delegate :can?, to: :ability
 
-  def test_link_to_export_widget_for
-    self.current_user = FactoryBot.build_stubbed(:member, account: account)
-    link = link_to_export_widget_for('Accounts')
-    assert_nil link
-
-    self.current_user = FactoryBot.build_stubbed(:admin, account: account)
-    link = link_to_export_widget_for('Accounts')
-    assert_match /a/, link
-  end
-
   def test_css_class
     assert_equal 'some class', css_class('some', 'class')
 
