c7ffd16efbc136507515991a3039bce2c1577d6c
Daria Mayorova <mayorova@users.noreply.github.com>
2026-01-23 17:30:05 +0100

    Swagger Spec accepts server URL with variables

--- app/lib/three_scale/open_api/url_resolver.rb
+++ app/lib/three_scale/open_api/url_resolver.rb
@@ -17,11 +17,29 @@ module ThreeScale
         end.flatten
       end
 
+      # By default, the variable values are URL-encoded. The purpose of this processor is 
+      # to skip URL-encoding, so that URL can be used in the variable, e.g.
+      #   "servers": [
+      #     {
+      #       "url": "{apiRoot}/echo/v1",
+      #       "variables": {
+      #         "apiRoot": {
+      #           "default": "https://echo-api.3scale.net"
+      #         }
+      #       }
+      #     }
+      #   ],
+      class SkipURLEncodingProcessor
+        def self.transform(name, value)
+          return value
+        end
+      end
+
       def self.resolve_servers_from_variables(server_url, variables = {})
         variable_names = variables.keys
         variable_values = variables.values.map { |variable| variable['enum'] || [variable['default']] }
         value_combinations = variable_values.reduce(&:product).map(&Array.method(:wrap)).map { |values| variable_names.zip(values).to_h }
-        value_combinations.map { |values| Addressable::Template.new(server_url).expand(values).to_s }
+        value_combinations.map { |values| Addressable::Template.new(server_url).expand(values, SkipURLEncodingProcessor).to_s }
       end
 
       delegate :resolve_servers_from_variables, to: 'self.class'
