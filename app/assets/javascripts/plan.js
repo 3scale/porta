b4aaff6d6c11e4d473d9e7dcc90cbdeec6e8482b
josemigallas <josemigallas@gmail.com>
2026-01-30 11:10:54 +0100

    ⚰️ removes plans.js

--- app/assets/javascripts/plan.js
+++
@@ -1,5 +0,0 @@
-jQuery(function(){
-  jQuery(document).on('mousedown', 'table#plans input[type=radio]', function () {
-    jQuery('table#plans input[type=radio]:checked').removeAttr('checked');
-  });
-});
