11dee4721929c2b29976d1a18d627907b0b4125f
josemigallas <josemigallas@gmail.com>
2026-02-03 13:19:15 +0100

    🧹 cleans services helper up

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,33 +1,17 @@
+# frozen_string_literal: true
+
 module ServicesHelper
-
-  def service_selector( element = :li, services = current_account.accessible_services)
-    content_tag element, :id => 'service_selector_widget' do
-      form_tag url_for(params), :id => 'service_selector_form', :method => :get do
-        concat 'Service: '
-
-        options = options_from_collection_for_select(services, :id, :name, @service.try!(:id))
-        concat content_tag(:select, options, :id => 'service_selector', :name => 'service_id')
-
-        concat content_tag(:button, 'Change', :type => :submit)
-      end
-    end
-  end
-
   def human_backend(backend_version)
-    case backend_version
-    when "1" then "API key"
-    when "2" then "App Id"
-    when "oauth" then "OAuth"
-    when "oidc" then "OpenID Connect"
-    end
+    {
+      "1" => "API key",
+      "2" => "App Id",
+      "oauth" => "OAuth",
+      "oidc" => "OpenID Connect"
+    }[backend_version]
   end
 
-  def show_mappings?
-    !@service.deployment_option.to_s.start_with?('plugin_') && @service.proxiable?
-  end
-
-  def plugin_language_name
-    @service.deployment_option.remove('plugin_')
+  def plugin_language_name(service)
+    service.deployment_option.remove('plugin_')
   end
 
   def deployment_options
