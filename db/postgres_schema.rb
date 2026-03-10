8df29cfc655cb9f71e1292c6859252d6eb60c331
Joan Lledó <jlledo@redhat.com>
2026-03-10 18:04:37 +0100

    New migration to hash tokens

--- db/postgres_schema.rb
+++ db/postgres_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_22_195407) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_10_134934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
