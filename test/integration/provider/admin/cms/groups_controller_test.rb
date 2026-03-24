de59eef7fd2b4658c3344f3a4bd16dd3f793e7e2
Daria Mayorova <mayorova@users.noreply.github.com>
2026-03-24 12:51:24 +0100

    Fix a groups controller section ID issue

--- test/integration/provider/admin/cms/groups_controller_test.rb
+++ test/integration/provider/admin/cms/groups_controller_test.rb
@@ -31,5 +31,34 @@ class Provider::Admin::CMS::GroupsControllerTest < ActionDispatch::IntegrationTe
     assert_equal ['New Name'], @provider.provided_groups.pluck(:name)
     assert_equal [another_section.id], group.sections.pluck(:id)
   end
-end
 
+  test '#update allowed sections with invalid IDs' do
+    group = @provider.provided_groups.create(name: 'Group')
+    group.sections << @section
+    assert_equal ['Group'], @provider.provided_groups.pluck(:name)
+    assert_equal [@section.id], group.sections.pluck(:id)
+
+    invalid_section_id = CMS::Section.maximum(:id) + 1
+
+    assert_no_change of: -> { group.reload.sections.pluck(:id) } do
+      put provider_admin_cms_group_path(group), params: { cms_group: { name: 'New Name', section_ids: ['', invalid_section_id] } }
+    end
+
+    assert_response :not_found
+  end
+
+  test "#update group with another provider's section" do
+    group = @provider.provided_groups.create(name: 'Group')
+    assert_empty group.sections
+
+    another_provider = FactoryBot.create(:provider_account)
+    another_provider.settings.allow_groups!
+    another_section = FactoryBot.create(:cms_section, parent: another_provider.sections.first, provider: another_provider, public: false)
+
+    assert_no_difference group.sections.method(:count) do
+      put provider_admin_cms_group_path(group), params: { cms_group: { section_ids: ['', @section.id, another_section.id] } }
+    end
+
+    assert_response :not_found
+  end
+end
