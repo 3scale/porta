41d5157f40449c612ed4b6b8181e6ca77e19b0f4
Daria Mayorova <mayorova@users.noreply.github.com>
2026-03-24 13:20:43 +0100

    Fix sites settings controller

--- test/integration/sites/settings_controller_test.rb
+++ test/integration/sites/settings_controller_test.rb
@@ -4,29 +4,6 @@ require 'test_helper'
 
 class Sites::SettingsControllerTest < ActionDispatch::IntegrationTest
 
-  test 'show emails tab if not master account' do
-    provider = FactoryBot.create(:provider_account)
-
-    login_provider provider
-
-    get edit_admin_site_emails_path
-
-    assert_response :success
-  end
-
-  test 'do not show emails tab if master account' do
-    ThreeScale.config.stubs(onpremises: true, tenant_mode: 'master')
-
-    member = FactoryBot.create(:simple_admin, account: master_account)
-    member.activate!
-
-    login! master_account, user: member
-
-    get edit_admin_site_emails_path
-
-    assert_response :forbidden
-  end
-
   test 'update credit card policies paths successfully' do
     provider = FactoryBot.create(:provider_account)
     login_provider provider
@@ -65,5 +42,30 @@ class Sites::SettingsControllerTest < ActionDispatch::IntegrationTest
     provider.settings.reload
     assert_equal '', provider.settings.cc_terms_path
     assert_equal '', provider.settings.cc_privacy_path
+  end
+
+  test 'update with invalid values shows errors' do
+    provider = FactoryBot.create(:provider_account)
+    login_provider provider
+
+    long_value = "x"*256
+
+    put admin_site_settings_path, params: {
+      settings: {
+        cc_terms_path: long_value,
+        cc_privacy_path: long_value,
+        cc_refunds_path: long_value
+      }
+    }
+
+    assert_response :success
+    assert_template :edit
+    assert_equal "Settings could not be updated", flash[:danger]
+
+    page = Nokogiri::HTML4::Document.parse(response.body)
+    errors = page.xpath("//p[@class='pf-c-form__helper-text pf-m-error']")
+
+    assert_equal 3, errors.length
+    assert_equal ["is too long (maximum is 255 characters)"], errors.map(&:text).uniq
   end
 end
