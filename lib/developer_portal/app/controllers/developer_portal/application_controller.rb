f64b88b962603c6fa7e9b4632dcf3af14da89842
Aleksandar N. Kostadinov <akostadinov@gmail.com>
2026-04-06 21:39:25 +0300

    move permissions-policy header down where it belongs

--- lib/developer_portal/app/controllers/developer_portal/application_controller.rb
+++ lib/developer_portal/app/controllers/developer_portal/application_controller.rb
@@ -5,6 +5,18 @@ module DeveloperPortal
 
     before_action :disable_for_suspended_provider_account
 
+    private
+
+    def set_permissions_policy_header
+      header_value = AccountSettings::CachedRetrievalService.call(
+        account: site_account,
+        setting_name: 'permissions_policy_header_developer'
+      ).result
+
+      # Set header only if value is present (even if only whitespaces)
+      response.headers['Permissions-Policy'] = header_value if header_value&.size&.nonzero?
+    end
+
     protected
 
     def disable_for_suspended_provider_account
--- lib/developer_portal/app/controllers/developer_portal/base_controller.rb
+++ lib/developer_portal/app/controllers/developer_portal/base_controller.rb
@@ -33,15 +33,4 @@ class DeveloperPortal::BaseController < DeveloperPortal::ApplicationController
     params.except(*read_only_fields)
   end
 
-  private
-
-  def set_permissions_policy_header
-    header_value = AccountSettings::CachedRetrievalService.call(
-      account: site_account,
-      setting_name: 'permissions_policy_header_developer'
-    ).result
-
-    # Set header only if value is present (even if only whitespaces)
-    response.headers['Permissions-Policy'] = header_value if header_value&.size&.nonzero?
-  end
 end
