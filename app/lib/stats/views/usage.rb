23167032d9880c6222597c849486863342f5f064
Madni Ali <madnialihussain@outlook.com>
2026-04-07 14:40:57 +0100

    Fix TimeWithZone requirement and update XSS test for eternity granularity

--- app/lib/stats/views/usage.rb
+++ app/lib/stats/views/usage.rb
@@ -150,8 +150,9 @@ module Stats
           if period.to_sym == :eternity
             raise InvalidParameterError, "Only 'eternity' granularity is supported for period 'eternity'" if options[:granularity].present? && options[:granularity].to_sym != :eternity
 
-            range_since = Time.utc(1970)
-            range_until = Time.current.utc.end_of_day
+            utc = ActiveSupport::TimeZone['UTC']
+            range_since = utc.parse('1970-01-01')
+            range_until = utc.now.end_of_day
             return [range_since..range_until, :eternity]
           end
 
