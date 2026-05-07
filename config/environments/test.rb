8faf9c9f05310e139949f03ae7cc002a38f940a4
Madni Ali <madnialihussain@outlook.com>
2026-05-07 09:31:50 +0100

    Make invoice includes format-conditional for XML only

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -134,7 +134,6 @@ Rails.application.configure do
       Bullet.add_safelist class_name: "Invoice", type: :counter_cache, association: :payment_transactions
       Bullet.add_safelist class_name: "Invoice", type: :n_plus_one_query, association: :buyer_account
       Bullet.add_safelist class_name: "Invoice", type: :n_plus_one_query, association: :provider_account
-      Bullet.add_safelist class_name: "Invoice", type: :unused_eager_loading, association: :line_items
       Bullet.add_safelist class_name: "Invoice", type: :unused_eager_loading, association: :provider_account
       Bullet.add_safelist class_name: "LineItem::PlanCost", type: :n_plus_one_query, association: :contract
       Bullet.add_safelist class_name: "Message", type: :n_plus_one_query, association: :sender
