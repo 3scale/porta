bb887f6542aed7d5f9de5b413acc70bbacc3d264
Joan Lledó <jlledo@redhat.com>
2026-08-27 12:40:07 +0200

    escaper: Simplify block syntax

--- app/lib/apicast/pcre_escaper.rb
+++ app/lib/apicast/pcre_escaper.rb
@@ -11,10 +11,10 @@ module Apicast
       # ? is not valid in path segments, so splitting on the first ? is safe
       path_part, query_part = pattern.split('?', 2)
 
-      escaped_path = path_part.gsub(PATH_METACHARACTERS) { |c| "\\#{c}" }
+      escaped_path = path_part.gsub(PATH_METACHARACTERS) { "\\#{_1}" }
 
       if query_part
-        "#{escaped_path}?#{query_part.gsub(QUERY_METACHARACTERS) { |c| "\\#{c}" }}"
+        "#{escaped_path}?#{query_part.gsub(QUERY_METACHARACTERS) { "\\#{_1}" }}"
       else
         escaped_path
       end
